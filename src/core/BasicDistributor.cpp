8ece7fa264250af9170dec9a136e828d09e65990
Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
2017-03-25 15:12:00 +0100

    BasicDistributor: new basic Distributor implementation
    BasicDistributor implements simple messages transporting.
    Any data message received from any DeviceManager is sequentially
    resent to all registered Exporters without buffering.
    
    Exporting is secured by lock so only one thread at a time can be
    exporting data.
    
    Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/core/BasicDistributor.cpp
@@ -0,0 +1,41 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include <exception>
+#include <string>
+
+#include "di/Injectable.h"
+#include "core/BasicDistributor.h"
+#include "core/Exporter.h"
+#include "model/SensorData.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, BasicDistributor)
+BEEEON_OBJECT_CASTABLE(Distributor)
+BEEEON_OBJECT_REF("exporter", &BasicDistributor::registerExporter)
+BEEEON_OBJECT_END(BeeeOn, BasicDistributor)
+
+using namespace BeeeOn;
+
+void BasicDistributor::exportData(const SensorData &sensorData)
+{
+	Poco::FastMutex::ScopedLock lock(m_exportMutex);
+
+	for (Poco::SharedPtr<Exporter> exporter : m_exporters) {
+		try {
+			exporter->ship(sensorData);
+			poco_debug(logger(), "Data shipped successfully");
+
+		} catch (Poco::Exception &ex) {
+			poco_error(logger(), "Data failed to ship: " + ex.displayText());
+
+		} catch (std::exception &ex) {
+			poco_critical(logger(), "Data failed to ship: " + std::string(ex.what()));
+
+		} catch (...) {
+			poco_critical(logger(), "Unknown error occured when shipping data");
+
+		}
+	}
+}
