BeeeOn/gateway

commit a4fbabc5a38b975a7308f72322c668adb13afc0c

IncompleteTimestamp: typedef around Poco::Timestamp
The gateway application can encounter a situation when after boot
there is no actual date-time available. The reasons are:

* no battery for RTC module
* no NTP sync available (e.g. network is down)

This does not stop the sensor data collection to work, however,
an incomplete time is sampled. The incomplete time can be fixed
when the time is set up e.g. via the NTP later. Until the system
time is fixed, collect incomplete timestamps (time since 1. 1. 1970).

We can fix a timestamp by treating the incomplete timestamp as an
offset starting at 0. If we know the uptime and current time then
we can fix the timestamp by simple arithmetics.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/IncompleteTimestamp.h
@@ -1,0 +1,45 @@
+#ifndef BEEEON_INCOMPLETE_TIMESTAMP_H
+#define BEEEON_INCOMPLETE_TIMESTAMP_H
+
+#include <ostream>
+
+#include <Poco/Timespan.h>
+#include <Poco/Timestamp.h>
+
+#include "util/Incomplete.h"
+
+namespace BeeeOn {
+
+struct TimestampCompleteTest {
+	// Thu Jan  1 01:00:00 CET 2015
+	static const unsigned long TOO_OLD = 1420070400UL;
+
+	bool operator ()(const Poco::Timestamp &t) const
+	{
+		static const Poco::Timestamp tooOld(
+			Poco::Timestamp::fromEpochTime(TOO_OLD));
+
+		return t > tooOld;
+	}
+};
+
+struct TimestampComplete {
+	Poco::Timestamp operator ()(const Poco::Timestamp &t) const
+	{
+		return m_now - m_uptime + t.epochMicroseconds();
+	}
+
+	Poco::Timespan m_uptime;
+	Poco::Timestamp m_now;
+};
+
+/**
+ * Shortcut to represent an incomplete timestamp.
+ */
+typedef Incomplete<
+	Poco::Timestamp, TimestampCompleteTest, TimestampComplete
+> IncompleteTimestamp;
+
+};
+
+#endif

test/CMakeLists.txt
@@ -12,6 +12,7 @@ find_library (POCO_XML PocoXML)
 add_definitions(-std=c++11)
 
 file(GLOB TEST_SOURCES
+	${PROJECT_SOURCE_DIR}/util/IncompleteTimestampTest.cpp
 )
 
 include_directories(

test/util/IncompleteTimestampTest.cpp
@@ -1,0 +1,139 @@
+#include <Poco/Timespan.h>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/IncompleteTimestamp.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class IncompleteTimestampTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(IncompleteTimestampTest);
+	CPPUNIT_TEST(testIsComplete);
+	CPPUNIT_TEST(testEqual);
+	CPPUNIT_TEST(testCompare);
+	CPPUNIT_TEST(deriveComplete);
+	CPPUNIT_TEST(deriveCompleteFromComplete);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testIsComplete();
+	void testEqual();
+	void testCompare();
+	void deriveComplete();
+	void deriveCompleteFromComplete();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(IncompleteTimestampTest);
+
+void IncompleteTimestampTest::testIsComplete()
+{
+	IncompleteTimestamp ts; // now
+
+	// complete
+	CPPUNIT_ASSERT(ts.isComplete());
+
+	ts = Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD + 1);
+
+	CPPUNIT_ASSERT(ts.isComplete());
+
+	// incomplete
+	ts = Timestamp(TimestampCompleteTest::TOO_OLD);
+
+	CPPUNIT_ASSERT(!ts.isComplete());
+
+	ts = Timestamp(0);
+	CPPUNIT_ASSERT(!ts.isComplete());
+}
+
+void IncompleteTimestampTest::testEqual()
+{
+	Timestamp now;
+
+	IncompleteTimestamp ts0(now);
+	IncompleteTimestamp ts1(now);
+	IncompleteTimestamp ts2(now + 1);
+
+	CPPUNIT_ASSERT(ts0 == ts0);
+	CPPUNIT_ASSERT(ts0 == ts1);
+	CPPUNIT_ASSERT(ts0 != ts2);
+
+	IncompleteTimestamp incomplete0;
+	IncompleteTimestamp incomplete1;
+	IncompleteTimestamp incomplete2;
+	incomplete0 = Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD);
+	incomplete1 = Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD);
+	incomplete2 = Timestamp::fromEpochTime(
+				       TimestampCompleteTest::TOO_OLD - 1);
+
+	CPPUNIT_ASSERT(incomplete0 == incomplete0);
+	CPPUNIT_ASSERT(incomplete0 == incomplete1);
+	CPPUNIT_ASSERT(incomplete0 != incomplete2);
+}
+
+void IncompleteTimestampTest::testCompare()
+{
+	Timestamp now;
+
+	IncompleteTimestamp ts0(now);
+	IncompleteTimestamp ts1(now);
+	IncompleteTimestamp ts2(now - 1);
+	IncompleteTimestamp ts3(
+		Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD));
+	IncompleteTimestamp ts4(
+		Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD));
+	IncompleteTimestamp ts5(
+		Timestamp::fromEpochTime(TimestampCompleteTest::TOO_OLD - 1));
+
+	CPPUNIT_ASSERT(ts2 < ts0);
+	CPPUNIT_ASSERT(!(ts2 > ts0));
+
+	CPPUNIT_ASSERT(!(ts0 < ts1)); // they equal
+	CPPUNIT_ASSERT(!(ts1 > ts0)); // they equal
+
+	CPPUNIT_ASSERT(ts3 < ts0);
+	CPPUNIT_ASSERT(!(ts3 > ts0));
+
+	CPPUNIT_ASSERT(ts3 < ts2);
+	CPPUNIT_ASSERT(!(ts3 > ts2));
+
+	CPPUNIT_ASSERT(!(ts3 < ts4)); // they equal
+	CPPUNIT_ASSERT(!(ts4 > ts3)); // they equal
+
+	CPPUNIT_ASSERT(ts5 < ts3);
+	CPPUNIT_ASSERT(!(ts3 < ts5));
+
+	CPPUNIT_ASSERT(ts5 < ts0);
+	CPPUNIT_ASSERT(!(ts5 > ts0));
+}
+
+void IncompleteTimestampTest::deriveComplete()
+{
+	// 1 hour after 1.1.1970
+	IncompleteTimestamp ts(Timestamp(60 * 60 * 1000000UL));
+	// uptime 2 hours
+	Timespan uptime(2 * 60 * 60, 0);
+
+	Timestamp now;
+	Timestamp derived = ts.deriveComplete({uptime, now});
+
+	// derived must be now - 1 hour
+	CPPUNIT_ASSERT(now - Timespan(60 * 60, 0) == derived);
+}
+
+void IncompleteTimestampTest::deriveCompleteFromComplete()
+{
+	// now - complete
+	const IncompleteTimestamp ts;
+	// uptime 1 hour
+	Timespan uptime(60 * 60, 0);
+
+	Timestamp now;
+	Timestamp derived = ts.deriveComplete({uptime, now});
+
+	// derived must not be different
+	CPPUNIT_ASSERT(ts.value() == derived);
+}
+
+}

commit 09b09c43da447d373b9f29e6fb0529492e769670

IncompleteTimestamp: fix extra ‘;’ after namespace
Fixes: a4fbabc5 ("IncompleteTimestamp: typedef around Poco::Timestamp")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/IncompleteTimestamp.h
@@ -40,6 +40,6 @@ typedef Incomplete<
 	Poco::Timestamp, TimestampCompleteTest, TimestampComplete
 > IncompleteTimestamp;
 
-};
+}
 
 #endif