BeeeOn/gateway

commit 7d24501309df8e0e1e34de8ad4cc0c51e4bcf127

ModelID: new class
Represents the identification of a sensor module, type of measured value
(temperature, pressure, motion, ...).

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/model/ModuleID.h
@@ -1,0 +1,77 @@
+#ifndef BEEEON_MODULE_ID_H
+#define BEEEON_MODULE_ID_H
+
+#include <cstdint>
+#include <string>
+
+namespace BeeeOn {
+
+/*
+ * Represents the identification of a sensor module, type of a measured value
+ * (temperature, pressure, motion, ...).
+ */
+class ModuleID {
+public:
+	/*
+	 * Construct ModuleID with default ModuleID.
+	 */
+	ModuleID();
+
+	ModuleID(const uint16_t &moduleID);
+
+	const uint16_t value() const
+	{
+		return m_moduleID;
+	}
+
+	static ModuleID parse(const std::string &s);
+
+	std::string toString() const;
+
+	bool operator !=(const ModuleID &id) const
+	{
+		return m_moduleID != id.m_moduleID;
+	}
+
+	bool operator ==(const ModuleID &id) const
+	{
+		return m_moduleID == id.m_moduleID;
+	}
+
+	bool operator <(const ModuleID &id) const
+	{
+		return m_moduleID < id.m_moduleID;
+	}
+
+	bool operator >(const ModuleID &id) const
+	{
+		return m_moduleID > id.m_moduleID;
+	}
+
+	bool operator <=(const ModuleID &id) const
+	{
+		return m_moduleID <= id.m_moduleID;
+	}
+
+	bool operator >=(const ModuleID &id) const
+	{
+		return m_moduleID >= id.m_moduleID;
+	}
+
+	operator unsigned short() const
+	{
+		return m_moduleID;
+	}
+
+	operator uint16_t()
+	{
+		return m_moduleID;
+	}
+
+private:
+	uint16_t m_moduleID;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -11,10 +11,16 @@ find_library (POCO_XML PocoXML)
 
 add_definitions(-std=c++11)
 
+file(GLOB SOURCES
+	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
+)
+
 include_directories(
 	${PROJECT_SOURCE_DIR}
 	${CMAKE_SOURCE_DIR}/base/src
 )
+
+add_library(BeeeOnGateway ${SOURCES})
 
 add_executable(beeeon-gateway main.cpp)
 
@@ -30,12 +36,13 @@ set(LIBS
 
 target_link_libraries(beeeon-gateway
 		-Wl,--whole-archive
+		BeeeOnGateway
 		BeeeOnBase
 		-Wl,--no-whole-archive
 		${LIBS}
 )
 
-install(TARGETS beeeon-gateway
+install(TARGETS beeeon-gateway BeeeOnGateway
 	RUNTIME DESTINATION bin
 	LIBRARY DESTINATION lib
 	ARCHIVE DESTINATION lib

src/model/ModuleID.cpp
@@ -1,0 +1,40 @@
+#include <climits>
+
+#include <Poco/NumberParser.h>
+#include <Poco/NumberFormatter.h>
+
+#include "model/ModuleID.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+const uint16_t DEFAULT_MODULE_ID = 0;
+
+ModuleID::ModuleID():
+	m_moduleID(DEFAULT_MODULE_ID)
+{
+}
+
+ModuleID::ModuleID(const uint16_t &moduleID):
+	m_moduleID(moduleID)
+{
+}
+
+ModuleID ModuleID::parse(const string &s)
+{
+	unsigned int moduleID;
+
+	moduleID = Poco::NumberParser::parseUnsigned(s);
+	if (moduleID > USHRT_MAX)
+		throw RangeException(
+			"module ID too high: " + to_string(moduleID));
+
+	return ModuleID(moduleID);
+}
+
+string ModuleID::toString() const
+{
+	return Poco::NumberFormatter::formatHex(m_moduleID);
+}
+

commit 3774c5199d71dfc99286163ddd5ca5c804988aa6

ModuleID: fix type qualifiers ignored on function return type
Fixes: 7d245013 ("ModelID: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleID.h
@@ -19,7 +19,7 @@ public:
 
 	ModuleID(const uint16_t &moduleID);
 
-	const uint16_t value() const
+	uint16_t value() const
 	{
 		return m_moduleID;
 	}