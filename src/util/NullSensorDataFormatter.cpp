BeeeOn/gateway

commit f41ec1b5f8e2a15381216d816babe650ee504c2e

SensorDataFormatter: interface for formatter
Interface for convert SensorData to some text format.
NullSensorDataFormatter - instead of Null pointer.

Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/NullSensorDataFormatter.cpp
@@ -1,0 +1,28 @@
+#include <Poco/Exception.h>
+#include <Poco/SingletonHolder.h>
+
+#include "NullSensorDataFormatter.h"
+#include "model/SensorData.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+NullSensorDataFormatter::NullSensorDataFormatter()
+{
+}
+
+NullSensorDataFormatter::~NullSensorDataFormatter()
+{
+}
+
+SensorDataFormatter &NullSensorDataFormatter::instance()
+{
+	static SingletonHolder<NullSensorDataFormatter> singleton;
+	return *singleton.get();
+}
+
+std::string NullSensorDataFormatter::format(const SensorData &data)
+{
+	throw NotImplementedException(__func__);
+	return "";
+}

src/CMakeLists.txt
@@ -15,6 +15,8 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
+	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
+	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 )
 
 include_directories(

src/util/NullSensorDataFormatter.h
@@ -1,0 +1,28 @@
+#ifndef BEEEON_NULL_SENSOR_DATA_FORMATTER_H
+#define BEEEON_NULL_SENSOR_DATA_FORMATTER_H
+
+#include <string>
+
+#include "SensorDataFormatter.h"
+
+namespace BeeeOn {
+
+class SensorData;
+
+/**
+ * NullSensorDataFormatter can be and is used as a default
+ * instance of a SensorDataFormatter (instead of NULL pointer).
+ */
+class NullSensorDataFormatter : public SensorDataFormatter {
+public:
+	NullSensorDataFormatter();
+	~NullSensorDataFormatter();
+
+	static SensorDataFormatter &instance();
+
+	std::string format(const SensorData &data) override;
+};
+
+}
+
+#endif // BEEEON_NULL_SENSOR_DATA_FORMATTER_H

src/util/SensorDataFormatter.cpp
@@ -1,0 +1,12 @@
+#include "NullSensorDataFormatter.h"
+#include "SensorDataFormatter.h"
+
+using namespace BeeeOn;
+
+SensorDataFormatter::SensorDataFormatter()
+{
+}
+
+SensorDataFormatter::~SensorDataFormatter()
+{
+}

src/util/SensorDataFormatter.h
@@ -1,0 +1,23 @@
+#ifndef BEEEON_SENSOR_DATA_FORMATTER_H
+#define BEEEON_SENSOR_DATA_FORMATTER_H
+
+#include <string>
+
+namespace BeeeOn {
+
+class SensorData;
+
+class SensorDataFormatter {
+public:
+	SensorDataFormatter();
+	virtual ~SensorDataFormatter();
+
+	/**
+	 * Convert data from struct SensorData to some formatted text
+	 */
+	virtual std::string format(const SensorData &data) = 0;
+};
+
+}
+
+#endif // BEEEON_SENSOR_DATA_FORMATTER_H

commit 96a2e821c2e4dab1124d921fa2c215029da22723

SensorDataFormatter: fix unused parameter ‘data’
Fixes: f41ec1b5 ("SensorDataFormatter: interface for formatter")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/NullSensorDataFormatter.cpp
@@ -21,8 +21,7 @@ SensorDataFormatter &NullSensorDataFormatter::instance()
 	return *singleton.get();
 }
 
-std::string NullSensorDataFormatter::format(const SensorData &data)
+std::string NullSensorDataFormatter::format(const SensorData &)
 {
 	throw NotImplementedException(__func__);
-	return "";
 }