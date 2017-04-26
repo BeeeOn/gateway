BeeeOn/gateway

commit 1feec018e20cd07f575f0bbfda164db596d5c569

SensorValue: new class
Representation of a single value coming from a sensor. SensorValue
contains module identification, value and valid flag.
The class allows to represent:
 - invalid value coming from a module (valid flag is false)
 - valid value coming from a module.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/model/SensorValue.h
@@ -1,0 +1,68 @@
+#ifndef BEEEON_SENSOR_VALUE_H
+#define BEEEON_SENSOR_VALUE_H
+
+#include "model/ModuleID.h"
+
+namespace BeeeOn {
+
+/*
+ * Representation of a single value coming from a sensor. SensorValue
+ * contains module identification, value and valid flag.
+ * The class allows to represent:
+ *  - invalid value coming from a module (valid flag is false)
+ *  - valid value coming from a module.
+ */
+class SensorValue {
+public:
+	/*
+	 * Construct a default invalid sensor value. The isValid() method
+	 * would return false.
+	 */
+	SensorValue();
+	SensorValue(const ModuleID &moduleID);
+
+	/*
+	 * Construct a valid sensor value. The isValid() method
+	 * would return true.
+	 */
+	SensorValue(const ModuleID &moduleID, const double &value);
+
+	void setModuleID(const ModuleID &moduleID)
+	{
+		m_moduleID = moduleID;
+	}
+
+	const ModuleID moduleID() const
+	{
+		return m_moduleID;
+	}
+
+	void setValue(const double &value)
+	{
+		m_value = value;
+	}
+
+	const double value() const
+	{
+		return m_value;
+	}
+
+	void setValid(const bool valid)
+	{
+		m_valid = valid;
+	}
+
+	bool isValid()
+	{
+		return m_valid;
+	}
+
+private:
+	ModuleID m_moduleID;
+	double m_value;
+	bool m_valid;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -13,6 +13,7 @@ add_definitions(-std=c++11)
 
 file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
+	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 )
 
 include_directories(

src/model/SensorValue.cpp
@@ -1,0 +1,25 @@
+#include <cmath>
+
+#include "model/SensorValue.h"
+
+using namespace BeeeOn;
+
+SensorValue::SensorValue():
+	m_value(NAN),
+	m_valid(false)
+{
+}
+
+SensorValue::SensorValue(const ModuleID &moduleID):
+	m_moduleID(moduleID),
+	m_value(NAN),
+	m_valid(false)
+{
+}
+
+SensorValue::SensorValue(const ModuleID &moduleID, const double &value):
+	m_moduleID(moduleID),
+	m_value(value),
+	m_valid(true)
+{
+}

commit 92c75e74edeb7708717f1cddee8bd88f2d84f6e8

SensorValue: fix type qualifiers ignored on function return type
Fixes: 1feec018 ("SensorValue: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/SensorValue.h
@@ -42,7 +42,7 @@ public:
 		m_value = value;
 	}
 
-	const double value() const
+	double value() const
 	{
 		return m_value;
 	}