de9a2c16ff3cc27aae3f5991054edc3a8d49981f
David Bednařík <xbedna62@stud.fit.vutbr.cz>
2017-06-09 08:25:01 +0200

    UPnP: new class
    Provide searching devices on network by UPnP protocol
    using ssdp(Simple Service Discovery Protocol).
    
    Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
    [moved into net/]
    Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/net/UPnP.h
@@ -0,0 +1,31 @@
+#ifndef BEEEON_UPNP_H
+#define BEEEON_UPNP_H
+
+#include <list>
+#include <string>
+
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Timespan.h>
+
+#include "util/Loggable.h"
+
+// Documentation for UPnP protocol
+// http://upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.1.pdf
+// Section 1 Discovery
+#define UPNP_MULTICAST_IP "239.255.255.250"
+#define UPNP_PORT 1900
+
+namespace BeeeOn {
+
+class UPnP : protected Loggable {
+public:
+	UPnP(const Poco::Net::SocketAddress& address = Poco::Net::SocketAddress(UPNP_MULTICAST_IP, UPNP_PORT));
+	std::list<Poco::Net::SocketAddress> discover(const Poco::Timespan& timeout, const std::string& deviceType);
+
+private:
+	Poco::Net::SocketAddress m_multicastAddress;
+};
+
+}
+
+#endif
