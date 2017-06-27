BeeeOn/gateway

commit 57e2bf62c1d83494eb41233f3e4fa3ddf1ab2066

CredentialStorage: new class
Collects credentials for devices and allows to query them.
Includes methods save and load credentials working with
AbstractConfiguration.

Signed-off-by: Martin Hosala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/credentials/CredentialsStorage.cpp
@@ -1,0 +1,88 @@
+#include "credentials/CredentialsStorage.h"
+
+using namespace BeeeOn;
+using namespace std;
+using namespace Poco;
+using namespace Poco::Util;
+
+CredentialsStorage::CredentialsStorage()
+{
+}
+
+CredentialsStorage::CredentialsStorage(
+		const map<string, CredentialsFactory> &factory):
+	m_factory(factory)
+{
+}
+
+CredentialsStorage::~CredentialsStorage()
+{
+}
+
+SharedPtr<Credentials> CredentialsStorage::find(const DeviceID &ID)
+{
+	auto search = m_credentialsMap.find(ID);
+
+	if (search != m_credentialsMap.end())
+		return search->second;
+	else
+		return NULL;
+}
+
+void CredentialsStorage::insertOrUpdate(
+		const DeviceID &device,
+		const SharedPtr<Credentials> credentials)
+{
+	m_credentialsMap[device] = credentials;
+}
+
+void CredentialsStorage::remove(const DeviceID &device)
+{
+	m_credentialsMap.erase(device);
+}
+
+void CredentialsStorage::save(
+		AutoPtr<AbstractConfiguration> conf,
+		const std::string &root) const
+{
+	for (auto const& itr : m_credentialsMap)
+		itr.second->save(conf, itr.first, root);
+}
+
+SharedPtr<Credentials> CredentialsStorage::createCredential(
+		AutoPtr<AbstractConfiguration> conf)
+{
+	string type = conf->getString("type");
+
+	if (m_factory.find(type) == m_factory.end())
+		throw InvalidArgumentException("Unrecognized credential type: " + type);
+
+	return m_factory[type](conf);
+}
+
+void CredentialsStorage::load(
+		AutoPtr <AbstractConfiguration> rootConf,
+		const std::string &root)
+{
+	AutoPtr <AbstractConfiguration> conf = rootConf->createView(root);
+
+	AbstractConfiguration::Keys keys;
+	conf->keys(keys);
+
+	for (auto itr : keys) {
+		DeviceID id;
+
+		try {
+			id = DeviceID::parse(itr);
+		} catch (const Exception &e) {
+			logger().warning("Expected DeviceID, got: " + itr);
+			continue;
+		}
+
+		try {
+			insertOrUpdate(id, createCredential(conf->createView(itr)));
+		} catch (const Exception &e) {
+			logger().log(e, __FILE__, __LINE__);
+		}
+	}
+}

src/CMakeLists.txt
@@ -31,6 +31,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/Result.cpp
 	${PROJECT_SOURCE_DIR}/core/VirtualSensor.cpp
 	${PROJECT_SOURCE_DIR}/credentials/Credentials.cpp
+	${PROJECT_SOURCE_DIR}/credentials/CredentialsStorage.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp

src/credentials/CredentialsStorage.h
@@ -1,0 +1,73 @@
+#pragma once
+
+#include <functional>
+#include <map>
+
+#include <Poco/Util/AbstractConfiguration.h>
+#include <Poco/StringTokenizer.h>
+
+#include "credentials/Credentials.h"
+#include "model/DeviceID.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class CredentialsStorage : protected Loggable {
+public:
+	/**
+	* Factory method to construct a Credentials instance from the
+	* AbstractConfiguration. The given configuration is a view into
+	* the last level of the credentials key.
+	*
+	* Example:
+	*
+	* Configuration:
+	*
+	* credentials.0xff0123456789abcd.type = sometype
+	* credentials.0xff0123456789abcd.value = X
+	*
+	* is send to function CredentialsStorage::load and
+	* transformed into:
+	*
+	* type = sometype
+	* value = X
+	*
+	* The corresponding CredentialsFactory function is selected from
+	* CredentialsStorage::m_factory based on the "type". It creates
+	* the appropriate Credentials instance from the transformed
+	* configuration.
+	*/
+	typedef std::function<
+		Poco::SharedPtr<Credentials> (Poco::AutoPtr<Poco::Util::AbstractConfiguration>)>
+		CredentialsFactory;
+
+	CredentialsStorage();
+	CredentialsStorage(const std::map<std::string, CredentialsFactory> &factory);
+
+	~CredentialsStorage();
+
+	Poco::SharedPtr<Credentials> find(const DeviceID &ID);
+
+	void insertOrUpdate(
+		const DeviceID &device,
+		const Poco::SharedPtr<Credentials> credentials);
+
+	void remove(const DeviceID &device);
+
+	void save(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf,
+		const std::string &root = "credentials") const;
+
+	void load(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> rootConf,
+		const std::string &root = "credentials");
+
+private:
+	Poco::SharedPtr<Credentials> createCredential(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf);
+
+	std::map<DeviceID, Poco::SharedPtr<Credentials>> m_credentialsMap;
+	std::map<std::string, CredentialsFactory> m_factory;
+};
+
+}

commit 0c52f695cf983fd253480e45e81d879cb4cad411

CredentialsStorage: coding style fix
Removed empty line and log messages now begin with lower case.

Fixes: 57e2bf62c1d8 ("CredentialStorage: new class")

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/credentials/CredentialsStorage.cpp
@@ -11,7 +11,6 @@ CredentialsStorage::CredentialsStorage()
 {
 	m_factory[PasswordCredentials::TYPE] = &PasswordCredentials::create;
 	m_factory[PinCredentials::TYPE] = &PinCredentials::create;
-
 }
 
 CredentialsStorage::CredentialsStorage(
@@ -60,16 +59,16 @@ SharedPtr<Credentials> CredentialsStorage::createCredential(
 	string type = conf->getString("type");
 
 	if (m_factory.find(type) == m_factory.end())
-		throw InvalidArgumentException("Unrecognized credential type: " + type);
+		throw InvalidArgumentException("unrecognized credential type: " + type);
 
 	return m_factory[type](conf);
 }
 
 void CredentialsStorage::load(
-		AutoPtr <AbstractConfiguration> rootConf,
+		AutoPtr<AbstractConfiguration> rootConf,
 		const std::string &root)
 {
-	AutoPtr <AbstractConfiguration> conf = rootConf->createView(root);
+	AutoPtr<AbstractConfiguration> conf = rootConf->createView(root);
 
 	AbstractConfiguration::Keys keys;
 	conf->keys(keys);
@@ -80,7 +79,7 @@ void CredentialsStorage::load(
 		try {
 			id = DeviceID::parse(itr);
 		} catch (const Exception &e) {
-			logger().warning("Expected DeviceID, got: " + itr);
+			logger().warning("expected DeviceID, got: " + itr);
 			continue;
 		}
 