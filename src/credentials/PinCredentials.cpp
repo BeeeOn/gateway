BeeeOn/gateway

commit 4d5f772d4a10bdc79e315eb8a87e04e7ef2d197c

PinCredentials: new class
Stores pin in crypted form.
Pin is sequence of printable characters without any
association (for example with username...).

Signed-off-by: Martin Hosala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/credentials/PinCredentials.cpp
@@ -1,0 +1,53 @@
+#include <Poco/Crypto/Cipher.h>
+
+#include "credentials/PinCredentials.h"
+
+using namespace BeeeOn;
+using namespace std;
+using namespace Poco;
+using namespace Poco::Crypto;
+using namespace Poco::Util;
+
+PinCredentials::PinCredentials()
+{
+}
+
+PinCredentials::~PinCredentials()
+{
+}
+
+const string PinCredentials::TYPE = "pin";
+
+void PinCredentials::PinCredentials::setPin(const string &pin, Cipher *cipher)
+{
+	m_pin = cipher->encryptString(pin, Cipher::ENC_BASE64);
+}
+
+void PinCredentials::setRawPin(const string &pin)
+{
+	m_pin = pin;
+}
+
+string PinCredentials::pin(Cipher *cipher) const
+{
+	return cipher->decryptString(m_pin, Cipher::ENC_BASE64);
+}
+
+void PinCredentials::save(
+	Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf,
+	const DeviceID &device,
+	const std::string &root) const
+{
+	conf->setString(makeConfString(device, "type", root), TYPE);
+	conf->setString(makeConfString(device, "params", root), m_params.toString());
+	conf->setString(makeConfString(device, "pin", root), m_pin);
+}
+
+SharedPtr<Credentials> PinCredentials::create(
+	AutoPtr<AbstractConfiguration> conf)
+{
+	SharedPtr<PinCredentials> ptr(new PinCredentials);
+	ptr->setParams(CryptoParams::parse(conf->getString("params")));
+	ptr->m_pin = conf->getString("pin");
+	return ptr;
+}

src/CMakeLists.txt
@@ -33,6 +33,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/Credentials.cpp
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsStorage.cpp
 	${PROJECT_SOURCE_DIR}/credentials/PasswordCredentials.cpp
+	${PROJECT_SOURCE_DIR}/credentials/PinCredentials.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp

src/credentials/CredentialsStorage.cpp
@@ -1,5 +1,6 @@
 #include "credentials/CredentialsStorage.h"
 #include "credentials/PasswordCredentials.h"
+#include "credentials/PinCredentials.h"
 
 using namespace BeeeOn;
 using namespace std;
@@ -9,6 +10,8 @@ using namespace Poco::Util;
 CredentialsStorage::CredentialsStorage()
 {
 	m_factory[PasswordCredentials::TYPE] = &PasswordCredentials::create;
+	m_factory[PinCredentials::TYPE] = &PinCredentials::create;
+
 }
 
 CredentialsStorage::CredentialsStorage(

src/credentials/PinCredentials.h
@@ -1,0 +1,39 @@
+#pragma once
+
+#include "credentials/Credentials.h"
+
+namespace Poco {
+namespace Crypto {
+
+class Cipher;
+
+}
+}
+
+namespace BeeeOn {
+
+class PinCredentials: public Credentials {
+public:
+	PinCredentials();
+	~PinCredentials();
+
+	static const std::string TYPE;
+
+	void setPin(const std::string &pin, Poco::Crypto::Cipher *cipher);
+	void setRawPin(const std::string &pin);
+
+	std::string pin(Poco::Crypto::Cipher *cipher) const;
+
+	void save(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf,
+		const DeviceID &device,
+		const std::string &root = "credentials") const override;
+
+	static Poco::SharedPtr<Credentials> create(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf);
+
+private:
+	std::string m_pin;
+};
+
+}

commit 088792640c70c1d40c14bfb2a587e890810f5267

Credentials: namespace fix
Removed unnecessary namespces in PinCredentials.cpp and CredentialsStorageTest.cpp.

Fixes: 4d5f772d4a10 ("PinCredentials: new class")
Fixes: 13fcd5c5fa41 ("CredentialsStorageTest: new test class")

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/credentials/PinCredentials.cpp
@@ -34,9 +34,9 @@ string PinCredentials::pin(Cipher *cipher) const
 }
 
 void PinCredentials::save(
-	Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf,
+	AutoPtr<AbstractConfiguration> conf,
 	const DeviceID &device,
-	const std::string &root) const
+	const string &root) const
 {
 	conf->setString(makeConfString(device, "type", root), TYPE);
 	conf->setString(makeConfString(device, "params", root), m_params.toString());

test/credentials/CredentialsStorageTest.cpp
@@ -17,20 +17,20 @@ namespace BeeeOn {
 
 class TestingCredentials : public Credentials {
 public:
-	void setName(const std::string &name)
+	void setName(const string &name)
 	{
 		m_name = name;
 	}
 
-	std::string name()
+	string name()
 	{
 		return m_name;
 	}
 
 	void save(
-		Poco::AutoPtr<Poco::Util::AbstractConfiguration> conf,
+		AutoPtr<AbstractConfiguration> conf,
 		const DeviceID &device,
-		const std::string &root) const
+		const string &root) const
 	{
 		conf->setString(root + "." + device.toString() + "." + "type", "test");
 		conf->setString(root + "." + device.toString() + "." + "name", m_name);
@@ -44,7 +44,7 @@ public:
 	}
 
 private:
-        std::string m_name;
+        string m_name;
 };
 
 