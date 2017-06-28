81c64b4a879ee6106cf757845d80d7ae4073a20d
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2017-06-28 17:06:50 +0200

    ServerLastValueResult: implement ModuleID and DeviceID
    Adding of DeviceID and ModuleID to the result so that these values don't
    have to be stored when the ServerLastValueCommand is sent.
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

--- src/commands/ServerLastValueResult.cpp
+++ src/commands/ServerLastValueResult.cpp
@@ -35,3 +35,51 @@ double ServerLastValueResult::valueUnlocked() const
 	assureLocked();
 	return m_value;
 }
+
+void ServerLastValueResult::setDeviceID(const DeviceID &deviceID)
+{
+	Poco::FastMutex::ScopedLock guard(lock());
+	setDeviceIDUnlocked(deviceID);
+}
+
+void ServerLastValueResult::setDeviceIDUnlocked(const DeviceID &deviceID)
+{
+	assureLocked();
+	m_deviceID = deviceID;
+}
+
+DeviceID ServerLastValueResult::deviceID() const
+{
+	Poco::FastMutex::ScopedLock guard(lock());
+	return deviceIDUnlocked();
+}
+
+DeviceID ServerLastValueResult::deviceIDUnlocked() const
+{
+	assureLocked();
+	return m_deviceID;
+}
+
+void ServerLastValueResult::setModuleID(const ModuleID &moduleID)
+{
+	Poco::FastMutex::ScopedLock guard(lock());
+	setModuleIDUnlocked(moduleID);
+}
+
+void ServerLastValueResult::setModuleIDUnlocked(const ModuleID &moduleID)
+{
+	assureLocked();
+	m_moduleID = moduleID;
+}
+
+ModuleID ServerLastValueResult::moduleID() const
+{
+	Poco::FastMutex::ScopedLock guard(lock());
+	return moduleIDUnlocked();
+}
+
+ModuleID ServerLastValueResult::moduleIDUnlocked() const
+{
+	assureLocked();
+	return  m_moduleID;
+}
