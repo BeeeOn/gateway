dcf222fed451e5f510fbb06f142f2183ce0eeb7b
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2017-06-29 13:01:52 +0200

    JablotronDevice: new class
    JablotronDevice is a common ancestor of classes that represent
    particular Jablotron devices. The devices uses different messages
    for communication and provides different types of data. For this
    purpose, the methods extractSensorData(), moduleTypes() are to be
    implemented.
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

---
+++ src/jablotron/JablotronDevice.cpp
@@ -0,0 +1,72 @@
+#include <Poco/NumberParser.h>
+#include <Poco/StringTokenizer.h>
+
+#include "jablotron/JablotronDevice.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+static const int LOW_BATTERY = 5;
+static const int FULL_BATTERY = 100;
+
+const Timespan JablotronDevice::REFRESH_TIME_SUPPORTED_BEACON = -1;
+const Timespan JablotronDevice::REFRESH_TIME_UNSUPPORTED_BEACON = 9 * Timespan::MINUTES;
+
+JablotronDevice::~JablotronDevice()
+{
+}
+
+JablotronDevice::Ptr JablotronDevice::create(uint32_t serialNumber)
+{
+	throw InvalidArgumentException(
+		"unsupported device: " + to_string(serialNumber));
+}
+
+JablotronDevice::JablotronDevice(
+		const DeviceID &deviceID, const string &name):
+	m_deviceID(deviceID),
+	m_name(name)
+{
+}
+
+int JablotronDevice::parseValue(const string &msg) const
+{
+	StringTokenizer token(msg, ":");
+	return NumberParser::parse(token[1]);
+}
+
+void JablotronDevice::setPaired(bool paired)
+{
+	m_paired = paired;
+}
+
+bool JablotronDevice::paired() const
+{
+	return m_paired;
+}
+
+DeviceID JablotronDevice::buildID(uint32_t serialNumber)
+{
+	return DeviceID(DevicePrefix::PREFIX_JABLOTRON, serialNumber);
+}
+
+DeviceID JablotronDevice::deviceID() const
+{
+	return m_deviceID;
+}
+
+int JablotronDevice::extractBatteryLevel(const string &battery)
+{
+	return parseValue(battery) ? LOW_BATTERY : FULL_BATTERY;
+}
+
+string JablotronDevice::name() const
+{
+	return m_name;
+}
+
+Timespan JablotronDevice::refreshTime()
+{
+	return REFRESH_TIME_UNSUPPORTED_BEACON;
+}
