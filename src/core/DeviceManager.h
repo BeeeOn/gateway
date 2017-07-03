BeeeOn/gateway

commit 31adc85c1fce86e8a9afa9c6edc83a1fd75433ff

DeviceManager: new class
DeviceManager represents a generic class for implementation of
managers of physical devices. All sensoric-networks are expected
to implement its own DeviceManager solving the specific issues
of the target technology.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/DeviceManager.h
@@ -1,0 +1,61 @@
+#ifndef BEEEON_DEVICE_MANAGER_H
+#define BEEEON_DEVICE_MANAGER_H
+
+#include <Poco/AtomicCounter.h>
+
+#include "core/AnswerQueue.h"
+#include "core/CommandHandler.h"
+#include "core/CommandSender.h"
+#include "core/Distributor.h"
+#include "loop/StoppableRunnable.h"
+
+namespace BeeeOn {
+
+/**
+ * All classes that manage devices should inherit from this
+ * abstract class. It provides a common functionality for this
+ * purpose.
+ *
+ * There is usually a main thread that performs communication
+ * with the physical devices and translates the specific device
+ * communication protocol into the Command & Answer interface
+ * or into SensorData interface.
+ *
+ * Communication in the direction to physical devices is served
+ * via the CommandHandler interface. By accepting Commands asking
+ * for specific tasks, the physical devices can be queried as
+ * expected by the server.
+ */
+class DeviceManager:
+	public CommandHandler,
+	protected Loggable,
+	public StoppableRunnable {
+public:
+	DeviceManager(const std::string &name);
+	virtual ~DeviceManager();
+
+	/**
+	* A generic stop implementation to be used by most DeviceManager
+	* implementations. It just atomically sets the m_stop variable.
+	*/
+	void stop() override;
+
+	void setDistributor(Poco::SharedPtr<Distributor> distributor);
+
+protected:
+	/**
+	* Ship data received from a physical device into a collection point.
+	*/
+	void ship(const SensorData &sensorData);
+
+protected:
+	Poco::AtomicCounter m_stop;
+	Poco::SharedPtr<CommandSender> m_commandSender;
+
+private:
+	Poco::SharedPtr<Distributor> m_distributor;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -28,6 +28,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/CommandProgressHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandRunner.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandSender.cpp
+	${PROJECT_SOURCE_DIR}/core/DeviceManager.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoAnswerImpl.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoCommandDispatcher.cpp

src/core/DeviceManager.cpp
@@ -1,0 +1,29 @@
+#include "core/DeviceManager.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+DeviceManager::DeviceManager(const std::string &name):
+	CommandHandler(name),
+	m_stop(false)
+{
+}
+
+DeviceManager::~DeviceManager()
+{
+}
+
+void DeviceManager::stop()
+{
+	m_stop = true;
+}
+
+void DeviceManager::setDistributor(Poco::SharedPtr<Distributor> distributor)
+{
+	m_distributor = distributor;
+}
+
+void DeviceManager::ship(const SensorData &sensorData)
+{
+	m_distributor->exportData(sensorData);
+}

commit 7c60ab1abd3767f5c9ef289bb4d301dcc3d14882

DeviceManager: fix incorrect use of CommandSender
Fixes: 31adc85c("DeviceManager: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/DeviceManager.h
@@ -28,6 +28,7 @@ namespace BeeeOn {
  */
 class DeviceManager:
 	public CommandHandler,
+	public CommandSender,
 	protected Loggable,
 	public StoppableRunnable {
 public:
@@ -50,7 +51,6 @@ protected:
 
 protected:
 	Poco::AtomicCounter m_stop;
-	Poco::SharedPtr<CommandSender> m_commandSender;
 
 private:
 	Poco::SharedPtr<Distributor> m_distributor;