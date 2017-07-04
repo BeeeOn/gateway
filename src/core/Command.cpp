BeeeOn/gateway

commit 85d64a09b82303b1de5ff432a52bc15c40501b14

CommandSender: new class
CommandSender introduces a common set of methods for implementing a
class that dispatches commands into the system. However, an instance
that sends commands can at the same time implement the CommandHandler
interface. This can lead into a situation when the CommandDispatcher
dispatches a command back to its sender.

Sending commands to itself is considered as an unwanted behaviour.
The CommandSender allows to prevent it safely in its dispatch method.
Thus, all classes that needs to dispatch commands should inherit from
CommandSender.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/Command.cpp
@@ -1,4 +1,5 @@
 #include "core/Command.h"
+#include "core/CommandSender.h"
 
 using namespace BeeeOn;
 using namespace std;
@@ -15,4 +16,14 @@ string Command::name() const
 
 Command::~Command()
 {
+}
+
+void Command::setSendingHandler(CommandHandler *sender)
+{
+	m_sendingHandler = sender;
+}
+
+CommandHandler* Command::sendingHandler() const
+{
+	return m_sendingHandler;
 }

src/CMakeLists.txt
@@ -27,6 +27,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/CommandHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandProgressHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandRunner.cpp
+	${PROJECT_SOURCE_DIR}/core/CommandSender.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoAnswerImpl.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoCommandDispatcher.cpp

src/core/Command.h
@@ -5,10 +5,15 @@
 
 #include <Poco/AutoPtr.h>
 #include <Poco/RefCountedObject.h>
+#include <Poco/SharedPtr.h>
 
 #include "util/Castable.h"
 
 namespace BeeeOn {
+
+class CommandDispatcher;
+class CommandHandler;
+class CommandSender;
 
 /*
  * Abstract class for Commands, which will be sent within the gates.
@@ -17,6 +22,9 @@ namespace BeeeOn {
  * to forbid explicit use of delete.
  */
 class Command : public Poco::RefCountedObject, public Castable {
+	// This way, we are hiding setSendingHandler() from the outer world.
+	friend CommandDispatcher;
+	friend CommandSender;
 public:
 	typedef Poco::AutoPtr<Command> Ptr;
 
@@ -24,11 +32,21 @@ public:
 
 	std::string name() const;
 
+	/**
+	 * Returns CommandHandler that initiates sending of this commands.
+	 * If the sender does not implement the CommandHandler interface,
+	 * it returns NULL.
+	 */
+	CommandHandler* sendingHandler() const;
+
 protected:
+	void setSendingHandler(CommandHandler *sender);
+
 	virtual ~Command();
 
 protected:
 	std::string m_commandName;
+	CommandHandler *m_sendingHandler;
 };
 
 }

src/core/CommandSender.cpp
@@ -1,0 +1,36 @@
+#include "core/CommandDispatcher.h"
+#include "core/CommandSender.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+CommandSender::~CommandSender()
+{
+}
+
+void CommandSender::setCommandDispatcher(
+	SharedPtr<CommandDispatcher> dispatcher)
+{
+	m_commandDispatcher = dispatcher;
+}
+
+void CommandSender::dispatch(AutoPtr<Command> cmd, AutoPtr<Answer> answer)
+{
+	// If the sender does not implement CommandHandler, the handler
+	// is NULL. Otherwise, we set ourself to prevent "dispatch to
+	// itself" issue.
+	CommandHandler *handler = dynamic_cast<CommandHandler *>(this);
+	cmd->setSendingHandler(handler);
+
+	m_commandDispatcher->dispatch(cmd, answer);
+}
+
+AnswerQueue &CommandSender::answerQueue()
+{
+	return m_answerQueue;
+}
+
+SharedPtr<CommandDispatcher> CommandSender::commandDispatcher() const
+{
+	return m_commandDispatcher;
+}

src/core/CommandSender.h
@@ -1,0 +1,45 @@
+#ifndef BEEEON_COMMAND_SENDER_H
+#define BEEEON_COMMAND_SENDER_H
+
+#include <Poco/AutoPtr.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/AnswerQueue.h"
+#include "core/CommandHandler.h"
+
+namespace BeeeOn {
+
+class Answer;
+class Command;
+class CommandDispatcher;
+
+/**
+ * Abstract class that provides a generic way to access a
+ * CommandDispatcher. The CommandSender MUST be used for
+ * dispatching commands.
+ */
+class CommandSender {
+public:
+	virtual ~CommandSender();
+
+	void setCommandDispatcher(Poco::SharedPtr<CommandDispatcher> dispatcher);
+	Poco::SharedPtr<CommandDispatcher> commandDispatcher() const;
+
+	/**
+	 * Dispatches commands via the configured CommandDispatcher.
+	 */
+	void dispatch(Poco::AutoPtr<Command> cmd, Poco::AutoPtr<Answer> answer);
+
+	/**
+	 * Provides an implicit answer queue for the sender.
+	 */
+	AnswerQueue &answerQueue();
+
+private:
+	Poco::SharedPtr<CommandDispatcher> m_commandDispatcher;
+	AnswerQueue m_answerQueue;
+};
+
+}
+
+#endif

src/core/PocoCommandDispatcher.cpp
@@ -20,6 +20,10 @@ void PocoCommandDispatcher::dispatch(Command::Ptr cmd, Answer::Ptr answer)
 	injectImpl(answer, impl);
 
 	for (auto item : m_commandHandlers) {
+		// avoid "dispatch to itself" issue
+		if (item.get() == cmd->sendingHandler())
+			continue;
+
 		if (!item->accept(cmd))
 			continue;
 

test/core/CommandDispatcherTest.cpp
@@ -2,6 +2,7 @@
 
 #include "core/AnswerQueue.h"
 #include "core/PocoCommandDispatcher.h"
+#include "core/CommandSender.h"
 #include "core/Result.h"
 #include "model/DeviceID.h"
 
@@ -11,17 +12,21 @@ class CommandDispatcherTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(CommandDispatcherTest);
 	CPPUNIT_TEST(testSupportedCommand);
 	CPPUNIT_TEST(testUnsupportedCommand);
+	CPPUNIT_TEST(testCommandSender);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
 	void testSupportedCommand();
 	void testUnsupportedCommand();
+	void testCommandSender();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(CommandDispatcherTest);
 
 class FakeCommand : public Command {
 public:
+	typedef Poco::AutoPtr<FakeCommand> Ptr;
+
 	FakeCommand(const DeviceID &deviceID):
 		Command("FakeCommand"),
 		m_deviceID(deviceID)
@@ -31,6 +36,11 @@ public:
 	DeviceID deviceID() const
 	{
 		return m_deviceID;
+	}
+
+	CommandHandler* sendingHandler() const
+	{
+		return Command::sendingHandler();
 	}
 
 private:
@@ -124,6 +134,41 @@ public:
 	}
 };
 
+class FakeCommandSender : public CommandSender, public CommandHandler {
+public:
+	FakeCommandSender(const DeviceID &deviceID):
+		CommandHandler("FakeCommandSender"),
+		m_deviceID(deviceID)
+	{
+	}
+
+	virtual ~FakeCommandSender()
+	{
+	}
+
+	bool accept(Command::Ptr cmd) override
+	{
+		if (cmd->is<FakeCommand>()) {
+			return m_deviceID == cmd.cast<FakeCommand>()->deviceID();
+		}
+
+		return false;
+	}
+
+	void handle(Command::Ptr cmd, Answer::Ptr answer) override
+	{
+		if (cmd->is<FakeCommand>()) {
+			Poco::Thread::sleep(60);
+
+			Result::Ptr result = new Result(answer);
+			result->setStatus(Result::FAILED);
+		}
+	}
+
+private:
+	DeviceID m_deviceID;
+};
+
 /*
  * The waiting for the response to the supported command in the
  * FakeHandler1 and the FakeHandler2.
@@ -201,6 +246,40 @@ void CommandDispatcherTest::testUnsupportedCommand()
 
 	CPPUNIT_ASSERT(queue.wait(1, answerList));
 	CPPUNIT_ASSERT(1 == answerList.size());
+
+	// Answer will not be served, answer doesn't contain any Result
+	CPPUNIT_ASSERT(answer->isEmpty());
+
+	queue.remove(answer);
+}
+
+/*
+ * Sending of command and verification, if handler that sent command
+ * will not process sent command.
+ */
+void CommandDispatcherTest::testCommandSender()
+{
+	AnswerQueue queue;
+	Poco::SharedPtr<CommandDispatcher> dispatcher(new PocoCommandDispatcher());
+
+	DeviceID deviceID = DeviceID(0xfe01020304050607);
+
+	Poco::SharedPtr<FakeCommandSender> commandSender(new FakeCommandSender(deviceID));
+	dispatcher->registerHandler(commandSender);
+
+	FakeCommand::Ptr cmd =  new FakeCommand(deviceID);
+	std::list<Answer::Ptr> answerList;
+	Answer::Ptr answer = new Answer(queue);
+
+	commandSender->setCommandDispatcher(dispatcher);
+	commandSender->dispatch(cmd, answer);
+
+	// Answer was not served
+	CPPUNIT_ASSERT(!answer->isPending());
+
+	CPPUNIT_ASSERT(queue.wait(100, answerList));
+	CPPUNIT_ASSERT(1 == answerList.size());
+	CPPUNIT_ASSERT(0 == answerList.front()->resultsCount());
 
 	// Answer will not be served, answer doesn't contain any Result
 	CPPUNIT_ASSERT(answer->isEmpty());

commit f32b18d896ebb56a3445863cce76207afc7eacb4

Command: fix uninitialized pointer to sendingHandler
Fixes: 85d64a09("CommandSender: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/Command.cpp
@@ -5,7 +5,8 @@ using namespace BeeeOn;
 using namespace std;
 
 Command::Command(const string &commandName):
-	m_commandName(commandName)
+	m_commandName(commandName),
+	m_sendingHandler(NULL)
 {
 }
 