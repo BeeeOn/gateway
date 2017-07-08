9c5635c9b97aaa463500b2757e122ba6f25e70f7
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-08 13:04:25 +0200

    UDevListener: new abstract class
    Provide an interface for handling of udev events. Anybody
    can override its event methods for this purpose.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

---
+++ src/udev/UDevListener.cpp
@@ -0,0 +1,19 @@
+#include "udev/UDevListener.h"
+
+using namespace BeeeOn;
+
+void UDevListener::onAdd(const UDevEvent &)
+{
+}
+
+void UDevListener::onRemove(const UDevEvent &)
+{
+}
+
+void UDevListener::onChange(const UDevEvent &)
+{
+}
+
+void UDevListener::onMove(const UDevEvent &)
+{
+}
