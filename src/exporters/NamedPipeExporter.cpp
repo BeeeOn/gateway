BeeeOn/gateway

commit 155c8449b926133738e49e89b3abd88e7087fa98

NamedPipeExporter: new exporter for named pipe
NamedPipeExporter convert SensorData to some text format and write
to named pipe (earlier "geek_pipe). This class extends Exporter and
using SensorDataFormatter.

Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/exporters/NamedPipeExporter.cpp
@@ -1,0 +1,166 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <Poco/Exception.h>
+
+#include "di/Injectable.h"
+#include "NamedPipeExporter.h"
+#include "util/NullSensorDataFormatter.h"
+#include "util/SensorDataFormatter.h"
+
+#define ATTEMPTS_CREATE_PIPE 3
+
+BEEEON_OBJECT_BEGIN(BeeeOn, NamedPipeExporter)
+BEEEON_OBJECT_CASTABLE(Exporter)
+BEEEON_OBJECT_TEXT("fileName", &NamedPipeExporter::setFileName)
+BEEEON_OBJECT_REF("formatter", &NamedPipeExporter::setFormatter)
+BEEEON_OBJECT_END(BeeeOn, NamedPipeExporter)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+NamedPipeExporter::NamedPipeExporter() :
+	m_formatter(&NullSensorDataFormatter::instance())
+{
+}
+
+NamedPipeExporter::~NamedPipeExporter()
+{
+	if (!m_pipeName.empty())
+		remove(m_pipeName.c_str());
+}
+
+bool NamedPipeExporter::ship(const SensorData &data)
+{
+	int fd = openPipe();
+
+	if (fd < 0 && errno == ENXIO)
+		return true;
+	if (fd < 0 && errno == EINTR)
+		return false;
+
+	poco_assert(fd >= 0);
+
+	try {
+		return writeAndClose(fd, m_formatter->format(data));
+	}
+	catch (...) {
+		close(fd);
+		throw;
+	}
+}
+
+void NamedPipeExporter::setFileName(const string &name)
+{
+	m_pipeName = name;
+}
+
+void NamedPipeExporter::setFormatter(SensorDataFormatter *formatter)
+{
+	m_formatter = formatter;
+}
+
+int NamedPipeExporter::openPipe()
+{
+	unsigned int attempts = ATTEMPTS_CREATE_PIPE;
+	int fd;
+
+	do { // forbid symlinks to be followed
+		fd = open(m_pipeName.c_str(), O_WRONLY | O_NONBLOCK | O_NOFOLLOW);
+		if (fd < 0 && errno == ENOENT) {
+			mode_t permit = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
+			if (mkfifo(m_pipeName.c_str(), permit) < 0) {
+					throw IOException(
+						"failed to initialize mkfifo "
+						+ m_pipeName + ": "
+						+ strerror(errno));
+			}
+			continue;
+		}
+		else if (fd < 0 && errno == ENXIO) {
+			// when there is no reader, drop data
+			return fd;
+		}
+		else if (fd < 0 && errno == EINTR) {
+			// interrupt from user
+			logger().notice("interrupt from user during init",
+				__FILE__, __LINE__);
+			return fd;
+		}
+		else if (fd < 0) {
+			// others unexpected failures
+			throw IOException(
+				"failed to initialize named pipe "
+				+ m_pipeName + ": "
+				+ strerror(errno));
+		}
+	} while (fd < 0 && attempts-- > 0);
+
+	if (fd < 0 && attempts == 0) {
+		throw IOException(
+			"failed to create and open mkfifo "
+			+ m_pipeName + ": "
+			+ strerror(errno));
+	}
+
+	struct stat st;
+	if (fstat(fd, &st) < 0) {
+		// should never happen
+		close(fd);
+		throw IOException(
+			"failed to stat mkfifo "
+			+ m_pipeName + ": "
+			+ strerror(errno));
+	}
+
+	if (!S_ISFIFO(st.st_mode)) {
+		// we opened non-fifo file, a race condition?
+		close(fd);
+		throw IOException(
+			"file '" + m_pipeName
+			+ "'' is not a fifo: "
+			+ strerror(errno));
+	}
+	return fd;
+}
+
+bool NamedPipeExporter::writeAndClose(int fd, const string &msg)
+{
+	ssize_t totalLength = msg.size();
+	ssize_t restLength = 0;
+
+	do { // we are blocking here
+		ssize_t writtenLength = write(fd, msg.c_str() + restLength, totalLength - restLength);
+
+		if (writtenLength < 0 && errno == EINTR) {
+			// interrupt from user
+			logger().notice("interrupt from user during write",
+				__FILE__, __LINE__);
+			break;
+		}
+		if (writtenLength < 0) {
+			close(fd);
+			throw IOException(
+				"failed to write fifo: "
+				+ string(strerror(errno)));
+		}
+
+		restLength += writtenLength;
+	} while(restLength < totalLength);
+
+	if (logger().debug()) {
+		logger().debug(
+			"written " + to_string(restLength) + "/"
+			+ to_string(totalLength) + " bytes",
+			__FILE__, __LINE__);
+	}
+
+	close(fd);
+	return restLength >= totalLength;
+}

conf/config.d/factory-exporter.xml
@@ -1,5 +1,10 @@
 <system>
 	<factory>
+		<instance name="NamedPipeExporter" class="BeeeOn::NamedPipeExporter">
+			<set name="fileName" text="${Exporter.pipe.name}" />
+			<set name="formatter" ref="${Exporter.pipe.format}SensorDataFormatter" />
+		</instance>
+
 		<instance name="CSVSensorDataFormatter" class="BeeeOn::CSVSensorDataFormatter">
 			<set name="separator" text="${Exporter.pipe.csv.separator}" />
 		</instance>

conf/config.d/logging-loggers.ini
@@ -7,3 +7,6 @@ loggers.Application.level = notice
 
 loggers.LoopRunner.name = BeeeOn::LoopRunner
 loggers.LoopRunner.level = notice
+
+loggers.NamedPipeExporter.name = BeeeOn::NamedPipeExporter
+loggers.NamedPipeExporter.level = notice

conf/gateway-startup.ini
@@ -2,4 +2,6 @@
 di.runner = main
 
 [exporter]
+pipe.name = /tmp/beeeon_pipee
+pipe.format = CSV
 pipe.csv.separator = ;

src/CMakeLists.txt
@@ -15,6 +15,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
+	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp

src/exporters/NamedPipeExporter.h
@@ -1,0 +1,58 @@
+#ifndef BEEEON_NAMED_PIPE_EXPORTER_H
+#define BEEEON_NAMED_PIPE_EXPORTER_H
+
+#include <string>
+#include <Poco/Logger.h>
+
+#include "core/Exporter.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class SensorDataFormatter;
+
+class NamedPipeExporter :
+	public Exporter,
+	public Loggable {
+public:
+	NamedPipeExporter();
+	~NamedPipeExporter();
+
+	/**
+	 * Export data to named pipe
+	 */
+	bool ship(const SensorData &data) override;
+
+	/**
+	 * Set name of file of named pipe (mkfifo)
+	 */
+	void setFileName(const std::string &name);
+
+	/**
+	 * Set formatter for type of output
+	 * @param m_formatter
+	 */
+	void setFormatter(SensorDataFormatter * formatter);
+
+private:
+	/**
+	 * Create pipe file (mkfifo)
+	 * @return file descriptor to open mkfifo
+	 * @throw ExporterInitException, when cannot open
+	 */
+	int openPipe();
+
+	/**
+	 * Write SensorData in string format to named pipe
+	 * @return result of the write
+	 * @throw ExporterShipException, when cannot write
+	 */
+	bool writeAndClose(int fd, const std::string &msg);
+
+	std::string m_pipeName;
+	SensorDataFormatter *m_formatter;
+};
+
+}
+
+#endif // BEEEON_NAMED_PIPE_EXPORTER_H

commit fb5fb6ff116b70a46cbd7478fea5181019e71061

fix: character new line in exporters
Character new line removed from formatters on the end of messagge
and added to exporter.

Formatters now don't set the end of the line and
responsibility of output is moved to exporters.

Also removed character new line from test JSONSensorDataFormatterTest.

Fixes: 155c8449b926 ("NamedPipeExporter: new exporter for named pipe")
Fixes: d710e03706ce ("CSVSensorDataFormatter: formatter to csv")
Fixes: abf6c73cfa29 ("JSONSensorDataFormatter: new class")

Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/exporters/NamedPipeExporter.cpp
@@ -47,7 +47,7 @@ bool NamedPipeExporter::ship(const SensorData &data)
 	poco_assert(fd >= 0);
 
 	try {
-		return writeAndClose(fd, m_formatter->format(data));
+		return writeAndClose(fd, m_formatter->format(data) + "\n");
 	}
 	catch (...) {
 		close(fd);

src/util/CSVSensorDataFormatter.cpp
@@ -34,7 +34,6 @@ string CSVSensorDataFormatter::format(const SensorData &data)
 		output += "sensor" + separator() + timestamp + separator() + device + separator();
 		output += item.moduleID().toString() + separator();
 		output += NumberFormatter::format(item.value(), PRECISION_OF_VALUE) + separator();
-		output += "\n";
 	}
 
 	return output;

src/util/JSONSensorDataFormatter.cpp
@@ -56,5 +56,5 @@ string JSONSensorDataFormatter::format(const SensorData &data)
 
 	json.endArray();
 	json.endObject();
-	return output.str() + '\n';
+	return output.str();
 }

test/util/JSONSensorDataFormatterTest.cpp
@@ -41,8 +41,7 @@ void JSONSensorDataFormatterTest::testFormat()
 	Timestamp now;
 	const string expected = R"({"device_id":"0x499602d2","timestamp":)"
 		+ to_string(now.epochTime())
-		+ R"(,"data":[{"module_id":5,"value":4.2},{"module_id":4,"value":0.5}]})"
-		+ "\n";
+		+ R"(,"data":[{"module_id":5,"value":4.2},{"module_id":4,"value":0.5}]})";
 
 	CPPUNIT_ASSERT_EQUAL(str, expected);
 }
@@ -60,8 +59,7 @@ void JSONSensorDataFormatterTest::testFormatNaN()
 	Timestamp now;
 	const string expected = R"({"device_id":"0x499602d3","timestamp":)"
 		+ to_string(now.epochTime())
-		+ R"(,"data":[{"module_id":6,"value":null},{"module_id":2,"value":154454}]})"
-		+ "\n";
+		+ R"(,"data":[{"module_id":6,"value":null},{"module_id":2,"value":154454}]})";
 
 	CPPUNIT_ASSERT_EQUAL(str, expected);
 }
@@ -79,8 +77,7 @@ void JSONSensorDataFormatterTest::testFormatINFINITY()
 	Timestamp now;
 	const string expected = R"({"device_id":"0x499602d3","timestamp":)"
 		+ to_string(now.epochTime())
-		+ R"(,"data":[{"module_id":6,"value":null},{"module_id":2}]})"
-		+ "\n";
+		+ R"(,"data":[{"module_id":6,"value":null},{"module_id":2}]})";
 
 	CPPUNIT_ASSERT_EQUAL(str, expected);
 }
@@ -96,8 +93,7 @@ void JSONSensorDataFormatterTest::testFormatNoValues()
 	Timestamp now;
 	const string expected = R"({"device_id":"0x499602d4","timestamp":)"
 		+ to_string(now.epochTime())
-		+ R"(,"data":[]})"
-		+ "\n";
+		+ R"(,"data":[]})";
 
 	CPPUNIT_ASSERT_EQUAL(str, expected);
 }