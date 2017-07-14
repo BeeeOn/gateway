BeeeOn/gateway

commit 8a8e2c469c4e69c1e478354c7f050585b447fd39

FileCredentialsStorage: new class
Subclass of CredentialsStorage which allows saving credentials to
file and loading them from it.

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/credentials/FileCredentialsStorage.cpp
@@ -1,0 +1,182 @@
+#include <Poco/Path.h>
+#include <Poco/Timer.h>
+#include <Poco/Util/AbstractConfiguration.h>
+
+#include "FileCredentialsStorage.h"
+#include "util/ConfigurationLoader.h"
+#include "util/ConfigurationSaver.h"
+#include "di/Injectable.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, FileCredentialsStorage)
+BEEEON_OBJECT_CASTABLE(CredentialsStorage)
+BEEEON_OBJECT_TEXT("file", &FileCredentialsStorage::setFile)
+BEEEON_OBJECT_TEXT("configurationRoot", &FileCredentialsStorage::setConfigRoot)
+BEEEON_OBJECT_NUMBER("saveDelayTime", &FileCredentialsStorage::setSaveDelay)
+BEEEON_OBJECT_HOOK("done", &FileCredentialsStorage::load)
+BEEEON_OBJECT_END(BeeeOn, FileCredentialsStorage)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Util;
+using namespace std;
+
+#define SAVE_DELAY_TIME_30MIN 1800000
+
+FileCredentialsStorage::FileCredentialsStorage():
+	m_confRoot("credentials"),
+	m_callback(*this, &FileCredentialsStorage::onSaveLater),
+	m_timerRunning(false)
+{
+	m_saveDelayTime = SAVE_DELAY_TIME_30MIN;
+}
+
+FileCredentialsStorage::~FileCredentialsStorage()
+{
+	try {
+		m_timer.stop();
+		save();
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+	}
+	catch (exception &e) {
+		poco_critical(logger(), e.what());
+	}
+	catch(...) {
+		logger().critical("failed to save credentials into " + m_file,
+			__FILE__, __LINE__);
+	}
+}
+
+void FileCredentialsStorage::setSaveDelay(int seconds)
+{
+	RWLock::ScopedWriteLock guard(lock());
+	if (seconds < 0 && m_timerRunning) {
+		m_timer.stop();
+		m_timerRunning = false;
+	}
+	m_saveDelayTime = Timespan(seconds * Timespan::SECONDS);
+}
+
+void FileCredentialsStorage::setFile(const string &path)
+{
+	m_file = path;
+}
+
+void FileCredentialsStorage::setConfigRoot(const string &root)
+{
+	m_confRoot = root;
+}
+
+void FileCredentialsStorage::insertOrUpdate(
+		const DeviceID &device,
+		const SharedPtr<Credentials> credentials)
+{
+	RWLock::ScopedWriteLock guard(lock());
+	insertOrUpdateUnlocked(device, credentials);
+	saveLater();
+}
+
+void FileCredentialsStorage::remove(const DeviceID &device)
+{
+	RWLock::ScopedWriteLock guard(lock());
+	removeUnlocked(device);
+	saveLater();
+}
+
+void FileCredentialsStorage::clear()
+{
+	RWLock::ScopedWriteLock guard(lock());
+	clearUnlocked();
+	saveLater();
+}
+
+void FileCredentialsStorage::load()
+{
+	if (m_file.empty())
+		return;
+
+	try {
+		ConfigurationLoader loader;
+		loader.load(Path(m_file));
+		loader.finished();
+		CredentialsStorage::load(loader.config(), m_confRoot);
+	} catch (const IOException &e) {
+		logger().log(e, __FILE__, __LINE__);
+		logger().warning("could not load credentials due to an I/O error",
+			__FILE__, __LINE__);
+	}
+}
+
+void FileCredentialsStorage::save()
+{
+	RWLock::ScopedWriteLock guard(lock());
+
+	if (m_timerRunning) {
+		m_timer.stop();
+		m_timerRunning = false;
+	}
+	saveUnlocked();
+}
+
+void FileCredentialsStorage::saveUnlocked() const
+{
+	ConfigurationSaver saver(m_file);
+	AutoPtr<AbstractConfiguration> conf = saver.config();
+	CredentialsStorage::save(conf, m_confRoot);
+	saver.save();
+	poco_information(logger(), "credentials saved");
+}
+
+void FileCredentialsStorage::saveLater()
+{
+	// No race-condition is possible here because the saveLater is always
+	// called while holding the write-lock.
+	if (!m_timerRunning && m_saveDelayTime >= Timespan(0)) {
+		m_timerRunning = true;
+		try {
+			m_timer.stop();
+
+			logger().debug("save scheduled in " + to_string(m_saveDelayTime.totalSeconds()) + " s");
+
+			m_timer.setStartInterval(m_saveDelayTime.totalMilliseconds());
+			m_timer.setPeriodicInterval(0);
+			m_timer.start(m_callback);
+		}
+		catch (const Exception &e) {
+			logger().log(e, __FILE__, __LINE__);
+			m_timerRunning = false;
+		}
+		catch (exception &e) {
+			poco_critical(logger(), e.what());
+			m_timerRunning = false;
+		}
+		catch (...) {
+			poco_critical(logger(), "unknown error occured while starting timer");
+			m_timerRunning = false;
+		}
+	}
+}
+
+void FileCredentialsStorage::onSaveLater(Timer &)
+{
+	logger().debug("attempt to autosave");
+
+	try {
+		RWLock::ScopedReadLock guard(lock());
+		saveUnlocked();
+		m_timerRunning = false;
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+		m_timerRunning = false;
+	}
+	catch (exception &e) {
+		poco_critical(logger(), e.what());
+		m_timerRunning = false;
+	}
+	catch (...) {
+		poco_critical(logger(), "unknown error occured while saving");
+		m_timerRunning = false;
+	}
+}

conf/config.d/factory.xml
@@ -54,5 +54,11 @@
 			<set name="commandDispatcher" ref="commandDispatcher" />
 			<set name="console" ref="testingConsole" />
 		</instance>
+
+		<instance name="credentialsStorage" class="BeeeOn::FileCredentialsStorage" init="early">
+			<set name="file" text="${credentials.file}" />
+			<set name="configurationRoot" text="${credentials.configuration.root}" />
+			<set name="saveDelayTime" number="${credentials.save.delay}" />
+		</instance>
 	</factory>
 </system>

conf/config.d/logging-loggers.ini
@@ -25,3 +25,6 @@ loggers.VPTHTTPScanner.level = debug
 
 loggers.TestingCenter.name = BeeeOn::TestingCenter
 loggers.TestingCenter.level = debug
+
+loggers.FileCredentialsStorage.name = BeeeOn::FileCredentialsStorage
+loggers.FileCredentialsStorage.level = information

conf/gateway-startup.ini
@@ -15,3 +15,10 @@ center.tcp.port = 6000
 [ssl]
 # certificate.filePath = /etc/pki/tls/certs/example.crt
 # privateKey.filePath = /etc/pki/tls/private/example.key
+
+[credentials]
+file = /tmp/credentials.properties
+configuration.root = credentials
+save.delay = 1800
+crypto.passphrase = If Purple People Eaters are real where do they find purple people to eat?
+crypto.algorithm = aes256

src/CMakeLists.txt
@@ -40,6 +40,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/VirtualSensor.cpp
 	${PROJECT_SOURCE_DIR}/credentials/Credentials.cpp
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsStorage.cpp
+	${PROJECT_SOURCE_DIR}/credentials/FileCredentialsStorage.cpp
 	${PROJECT_SOURCE_DIR}/credentials/PasswordCredentials.cpp
 	${PROJECT_SOURCE_DIR}/credentials/PinCredentials.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp

src/credentials/FileCredentialsStorage.h
@@ -1,0 +1,67 @@
+#pragma once
+#include <Poco/Timer.h>
+#include <Poco/AtomicCounter.h>
+#include <Poco/Timespan.h>
+
+#include "credentials/CredentialsStorage.h"
+
+namespace BeeeOn{
+
+/**
+ * FileCredentialsStorage is subclass of CredentialsStorage which includes
+ * methods for saving credentials to file and loading them from it.
+ * To load from file, it is necessary to setFile and optionally
+ * to setConfigRoot, then call load.
+ */
+class FileCredentialsStorage : public CredentialsStorage {
+public:
+	FileCredentialsStorage();
+	~FileCredentialsStorage();
+
+	void setFile(const std::string &path);
+
+	/**
+	 * Credentials are saved in configuration in this form:
+	 * <configRoot>.<DeviceID>.<attribute> = <value>.
+	 * Default configRoot is "credentials".
+	 */
+	void setConfigRoot(const std::string &root);
+
+	/**
+	 * If a change occurs in Credentials (inserting, updating, or removing),
+	 * FileCredentialsStorage will be automaticly saved after SaveDelayTime
+	 * (from the first change).
+	 * Default SaveDelayTime is 30min.
+	 * Passing negative number to this funcion causes disabling of autosave (if
+	 * autosave timer is already running, storage is saved).
+	 */
+	void setSaveDelay(int seconds);
+	void load();
+	void save();
+
+	void insertOrUpdate(
+		const DeviceID &device,
+		const Poco::SharedPtr<Credentials> credentials) override;
+
+	void remove(const DeviceID &device) override;
+	void clear() override;
+
+protected:
+	/**
+	 * This method must be always called while holding the write-lock.
+	 */
+	void saveLater();
+	void onSaveLater(Poco::Timer &);
+
+	void saveUnlocked() const;
+
+private:
+	std::string m_file;
+	std::string m_confRoot;
+	Poco::Timer m_timer;
+	Poco::TimerCallback<FileCredentialsStorage> m_callback;
+	Poco::AtomicCounter m_timerRunning;
+	Poco::Timespan m_saveDelayTime;
+};
+
+}

commit 39fe006002b6b12f08091ac4efc406491ab7b445

FileCredentialsStorage: saveDelayTime initialization fix
This change does not affect functionality, it is more like
coding style fix.

Fixes: 8a8e2c469c4e ("FileCredentialsStorage: new class")

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/credentials/FileCredentialsStorage.cpp
@@ -20,14 +20,12 @@ using namespace Poco;
 using namespace Poco::Util;
 using namespace std;
 
-#define SAVE_DELAY_TIME_30MIN 1800000
-
 FileCredentialsStorage::FileCredentialsStorage():
 	m_confRoot("credentials"),
 	m_callback(*this, &FileCredentialsStorage::onSaveLater),
-	m_timerRunning(false)
+	m_timerRunning(false),
+	m_saveDelayTime(30 * Timespan::MINUTES)
 {
-	m_saveDelayTime = SAVE_DELAY_TIME_30MIN;
 }
 
 FileCredentialsStorage::~FileCredentialsStorage()