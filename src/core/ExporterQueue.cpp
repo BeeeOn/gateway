BeeeOn/gateway

commit 4498f57225b11a3bc3cfc4a1f17ef92570114e2b

ExporterQueue: new class
A single ExporterQueue is connected with a single Exporter. The
ExporterQueue is a buffer holding the SensorData that could not
be shipped or that were not yet shipped into the Exporter.

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

src/core/ExporterQueue.cpp
@@ -1,0 +1,147 @@
+#include <exception>
+
+#include <Poco/Exception.h>
+
+#include "core/ExporterQueue.h"
+
+using namespace BeeeOn;
+using namespace std;
+using namespace Poco;
+
+ExporterQueue::ExporterQueue(
+		Poco::SharedPtr<Exporter> exporter,
+		int batchSize,
+		int capacity,
+		int treshold):
+	m_exporter(exporter),
+	m_dropped(0),
+	m_sent(0),
+	m_fails(0),
+	m_treshold(treshold),
+	m_capacity(capacity),
+	m_batchSize(batchSize),
+	m_timeOfFailure(Poco::Timestamp()),
+	m_working(true)
+{
+}
+
+ExporterQueue::~ExporterQueue()
+{
+}
+
+void ExporterQueue::enqueue(const SensorData &sensorData)
+{
+	FastMutex::ScopedLock lock(m_queueMutex);
+
+	if (m_queue.size() >= m_capacity && m_capacity > 0) {
+		m_queue.pop();
+		++m_dropped;
+	}
+
+	m_queue.push(sensorData);
+}
+
+unsigned int ExporterQueue::exportBatch()
+{
+	if (isEmpty())
+		return 0;
+
+	unsigned int i = 0;
+
+	try {
+		for (i = 0; (i < m_batchSize || m_batchSize <= 0) && !isEmpty(); ++i) {
+			if (m_exporter->ship(front())) {
+				++m_sent;
+				pop();
+			}
+			else {
+				break;
+			}
+		}
+	}
+	catch (const Exception &e) {
+		fail();
+		logger().log(e, __FILE__, __LINE__);
+		return i;
+	}
+	catch (exception &e) {
+		fail();
+		poco_critical(logger(), e.what());
+		return i;
+	}
+	catch (...) {
+		fail();
+		poco_critical(logger(), "unknown error occured while shipping data");
+		return i;
+	}
+
+	if (i > 0)
+		m_working = true;
+
+	return i;
+}
+
+void ExporterQueue::fail()
+{
+	logger().debug("ship has failed");
+
+	if (m_treshold < 0)
+		return;
+
+	if (working()) {
+		if (++m_fails >= m_treshold) {
+			m_working = false;
+			m_fails = 0;
+		}
+	}
+	else {
+		m_timeOfFailure = Timestamp();
+	}
+}
+
+
+bool ExporterQueue::canExport(const Timespan &deadTimeout) const
+{
+	if (isEmpty())
+		return false;
+
+	return deadTooLong(deadTimeout);
+}
+
+bool ExporterQueue::working() const
+{
+	return m_working;
+}
+
+bool ExporterQueue::deadTooLong(const Timespan deadTimeout) const
+{
+	return working() || m_timeOfFailure.isElapsed(deadTimeout.totalMicroseconds());
+}
+
+bool ExporterQueue::isEmpty() const
+{
+	FastMutex::ScopedLock lock(m_queueMutex);
+	return m_queue.empty();
+}
+
+unsigned int ExporterQueue::dropped() const
+{
+	return m_dropped;
+}
+
+unsigned int ExporterQueue::sent() const
+{
+	return m_sent;
+}
+
+SensorData &ExporterQueue::front()
+{
+	FastMutex::ScopedLock lock(m_queueMutex);
+	return m_queue.front();
+}
+
+void ExporterQueue::pop()
+{
+	FastMutex::ScopedLock lock(m_queueMutex);
+	m_queue.pop();
+}

src/CMakeLists.txt
@@ -35,6 +35,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/DeviceManager.cpp
 	${PROJECT_SOURCE_DIR}/core/DongleDeviceManager.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
+	${PROJECT_SOURCE_DIR}/core/ExporterQueue.cpp
 	${PROJECT_SOURCE_DIR}/core/GatewayInfo.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoAnswerImpl.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoCommandDispatcher.cpp

src/core/ExporterQueue.h
@@ -1,0 +1,83 @@
+#pragma once
+
+#include <queue>
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/Exporter.h"
+#include "model/SensorData.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class ExporterQueue : protected Loggable {
+public:
+	typedef Poco::SharedPtr<ExporterQueue> Ptr;
+
+	const static int UNLIMITED_BATCH_SIZE = 0;
+	const static int UNLIMITED_CAPACITY = 0;
+	const static int UNLIMITED_THRESHOLD = -1;
+
+	/**
+	 * If batchSize <= 0 then size of batch is unlimited.
+	 * If capacity <= 0 then data count is unlimited.
+	 * If treshold < 0 then treshold is unlimited.
+	 */
+	ExporterQueue(
+		Poco::SharedPtr<Exporter> exporter,
+		int batchSize,
+		int capacity,
+		int treshold);
+
+	~ExporterQueue();
+
+	void enqueue(const SensorData &sensorData);
+	unsigned int exportBatch();
+
+	unsigned int sent() const;
+	unsigned int dropped() const;
+
+	/**
+	 * The method canExport returns true if queue is not empty and at least one
+	 * of following conditions is met:
+	 *	- queue is working
+	 *	- queue is dead too long
+	 */
+	bool canExport(const Poco::Timespan &deadTimeout) const;
+
+	bool working() const;
+
+private:
+	/**
+	* The method deadTooLong returns true if queue is working, or if
+	* deadTimeout is elapsed since queue changed its status to "not working".
+	*/
+	bool deadTooLong(const Poco::Timespan deadTimeout) const;
+
+	void fail();
+	bool isEmpty() const;
+
+	SensorData &front();
+	void pop();
+
+private:
+	mutable Poco::FastMutex m_queueMutex;
+
+	Poco::SharedPtr<Exporter> m_exporter;
+
+	Poco::AtomicCounter m_dropped;
+	Poco::AtomicCounter m_sent;
+	int m_fails;
+	int m_treshold;
+
+	std::queue<SensorData> m_queue;
+	unsigned int m_capacity;
+	unsigned int m_batchSize;
+
+	Poco::Timestamp m_timeOfFailure;
+	Poco::AtomicCounter m_working;
+};
+
+}

commit 2fef032201cbcabefffe0b9147c4e3373a815860

ExporterQueue: fix counting fails
Number of failed attempts is now annuled after any successful export,
not only after treshold is reached, because we want to reach treshold
only in case of multiple fails in a row.

Fixes: 4498f57225b1 ("ExporterQueue: new class")

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/ExporterQueue.cpp
@@ -75,8 +75,10 @@ unsigned int ExporterQueue::exportBatch()
 		return i;
 	}
 
-	if (i > 0)
+	if (i > 0) {
 		m_working = true;
+		m_fails = 0;
+	}
 
 	return i;
 }