BeeeOn/gateway

commit 0719ae1df99ef5c7419bc151a6e9e99585aa444c

CommandHandler: drop name memeber
The name() is useless. There was the only use of this member in
the CommandDispatcher where it reports a duplicate handler
registration. However, the name always informs only about a kind
of class name. But we would rather report the name of the instance.
In fact, this is so minor issue because the number of handlers will
always be very small. So, there is no reason to report any details
about the duplicate problem.

Moreover, It is no more then the name of the inherited class. We
can get this name via ClassInfo if we want. So, there is really no
reason to preserve it anymore.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/CommandHandler.h
@@ -18,16 +18,7 @@ class Answer;
  */
 class CommandHandler {
 public:
-	/*
-	 * CommandHandler with specific name of handler.
-	 * The name is intended for debugging purposes only.
-	 */
-	CommandHandler(const std::string &name);
-
-	/*
-	 * Name of handler.
-	 */
-	std::string name() const;
+	CommandHandler();
 
 	/*
 	 * Returns true if the given command can be handled by this handler.

src/core/CommandDispatcher.cpp
@@ -6,10 +6,8 @@ using namespace Poco;
 void CommandDispatcher::registerHandler(SharedPtr<CommandHandler> handler)
 {
 	for (auto &item : m_commandHandlers) {
-		if (item == handler) {
-			throw Poco::ExistsException("handler " + handler->name()
-				+ " has been registered");
-		}
+		if (item == handler)
+			throw Poco::ExistsException("duplicate handler detected");
 	}
 
 	m_commandHandlers.push_back(handler);

src/core/CommandHandler.cpp
@@ -3,12 +3,6 @@
 using namespace BeeeOn;
 using namespace std;
 
-CommandHandler::CommandHandler(const string &name):
-	m_name(name)
+CommandHandler::CommandHandler()
 {
-}
-
-string CommandHandler::name() const
-{
-	return m_name;
 }

src/core/DeviceManager.cpp
@@ -11,9 +11,7 @@ using namespace std;
 
 const Timespan DeviceManager::DEFAULT_REQUEST_TIMEOUT(5 * Timespan::SECONDS);
 
-DeviceManager::DeviceManager(const std::string &name,
-		const DevicePrefix &prefix):
-	CommandHandler(name),
+DeviceManager::DeviceManager(const DevicePrefix &prefix):
 	m_stop(false),
 	m_prefix(prefix)
 {

src/core/DeviceManager.h
@@ -37,7 +37,7 @@ class DeviceManager:
 	protected Loggable,
 	public StoppableRunnable {
 public:
-	DeviceManager(const std::string &name, const DevicePrefix &prefix);
+	DeviceManager(const DevicePrefix &prefix);
 	virtual ~DeviceManager();
 
 	/**

src/core/DongleDeviceManager.cpp
@@ -8,9 +8,8 @@ using namespace std;
 using namespace Poco;
 using namespace BeeeOn;
 
-DongleDeviceManager::DongleDeviceManager(
-		const string &name, const DevicePrefix &prefix):
-	DeviceManager(name, prefix)
+DongleDeviceManager::DongleDeviceManager(const DevicePrefix &prefix):
+	DeviceManager(prefix)
 {
 }
 

src/core/DongleDeviceManager.h
@@ -11,7 +11,7 @@ namespace BeeeOn {
 
 class DongleDeviceManager : public DeviceManager, public UDevListener {
 public:
-	DongleDeviceManager(const std::string &name, const DevicePrefix &prefix);
+	DongleDeviceManager(const DevicePrefix &prefix);
 
 	void run() override;
 	void stop() override;

src/core/TestingCenter.cpp
@@ -413,7 +413,6 @@ static void credentialsAction(TestingCenter::ActionContext &context)
 }
 
 TestingCenter::TestingCenter():
-	CommandHandler("TestingCenter"),
 	m_stop(0)
 {
 	registerAction("echo", echoAction, "echo arguments to output separated by space");

commit f0e672c6524871800e5b58b5db64975411a8f372

CommandHandler: remove unused m_name
Fixes: 0719ae1df99e ("CommandHandler: drop name memeber")

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/CommandHandler.h
@@ -33,9 +33,6 @@ public:
 	 * For example: Mutex::ScopedLock lock(m_mutex);
 	 */
 	virtual void handle(Command::Ptr cmd, Answer::Ptr answer) = 0;
-
-private:
-	std::string m_name;
 };
 
 }