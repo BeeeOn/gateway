68273e1fc265eb64402b1d8edb4a15f6071044bb
Martin Sakin <xsakin00@stud.fit.vutbr.cz>
2017-08-08 17:21:40 +0200

    HciInterface: new class for bluetooth
    The class HciInterface hides the mechanism of accessing the
    Bluetooth. In this implementation, we execute the subprocess hcitool
    to scan for new devices and to query a specific device:
    
    1. scanning for new devices
    
     $ hcitool scan
        btDev1  00:00:00:00:00:00
        btDev2  00:00:00:00:00:00
    
    2. querying a known device
    
     $ hcitool name 00:00:00:00:00:00
        btDev1Name
    
    If the queried device is unavailable, no output is given.
    
    For turning hcitool on, root permission is needed.
    
    Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

---
+++ src/bluetooth/HciInterface.h
@@ -0,0 +1,72 @@
+#ifndef BEEEON_HCI_INTERFACE_H
+#define BEEEON_HCI_INTERFACE_H
+
+#include <list>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <Poco/Mutex.h>
+
+#include "net/MACAddress.h"
+
+namespace BeeeOn {
+
+class HciInterface {
+public:
+	HciInterface(const std::string &name);
+
+	/**
+	 * Try to set hci interface up
+	 * @throws IOException in case of a failure
+	 */
+	void up() const;
+
+	/**
+	 * Check state of device with MACAddress
+	 * @return true if the device was detected or false
+	 * @throws IOException when the detection fails for some reason
+	 */
+	bool detect(const MACAddress &address) const;
+
+	/**
+	 * Full scan of bluetooth net
+	 * This can find only visible devices
+	 * Result is list of MAC addresses with naming
+	 */
+	std::list<std::pair<std::string, MACAddress>> scan() const;
+
+private:
+	/**
+	 * Command execution utility
+	 * @param command - program
+	 * @param args    - vector of agruments for program
+	 * @param result  - stdout from program in terminal
+	 * @return exit_code
+	 *   return 0     => OK
+	 *   return n > 0 => Some error
+	 */
+	int exec(const std::string &command, const std::vector<std::string> &args,
+		std::string &output) const;
+
+	/**
+	 * Parse and process the stdout message from hcitool scan.
+	 * Example stdout:
+	   Scanning ...
+		   AA:BB:CC:DD:EE:FF	Device_name
+	 */
+	std::list<std::pair<std::string, MACAddress>> parseScan(const std::string &scan) const;
+
+	/**
+	 * Distribution of string into the vector by divide string
+	 */
+	std::vector<std::string> split(const std::string &input, const std::string &div) const;
+
+private:
+	mutable Poco::Mutex m_mutexExec;
+	std::string m_name;
+};
+
+}
+
+#endif // BEEEON_HCI_INTERFACE_H
