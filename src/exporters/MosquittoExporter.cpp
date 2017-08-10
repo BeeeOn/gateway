BeeeOn/gateway

commit 01b235b0571a9481861483c84104abf06d24ee7c

MosquittoExporter: new exporter
Exporter connects during the first call to ship().

When the mosquitto library returns some non-zero code the throw is
called and the MosquittoExporter is disconnected. In next calling
to ship() it tries to connect again.

Some MQTT broker is required for the MosquittoExporter to run. This
exporter is only joining an already running broker.

Be aware that it is not possible to have the same client name for more
exporters connected to the same broker. The provided configuration
would always work with only a single MosquittoExporter.

This patch requires libraries libmosquitto and libmosquittopp.

Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
[fixed commit log]
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/exporters/MosquittoExporter.cpp
@@ -1,0 +1,154 @@
+#include <cstring>
+
+#include <Poco/Exception.h>
+#include <Poco/Net/NetException.h>
+
+#include "di/Injectable.h"
+#include "exporters/MosquittoExporter.h"
+#include "util/NullSensorDataFormatter.h"
+#include "util/SensorDataFormatter.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, MosquittoExporter)
+BEEEON_OBJECT_CASTABLE(Exporter)
+BEEEON_OBJECT_TEXT("host", &MosquittoExporter::setHost)
+BEEEON_OBJECT_NUMBER("port", &MosquittoExporter::setPort)
+BEEEON_OBJECT_TEXT("topic", &MosquittoExporter::setTopic)
+BEEEON_OBJECT_NUMBER("qos", &MosquittoExporter::setQos)
+BEEEON_OBJECT_TEXT("clientName", &MosquittoExporter::setClientName)
+BEEEON_OBJECT_REF("formatter", &MosquittoExporter::setFormatter)
+BEEEON_OBJECT_END(BeeeOn, MosquittoExporter)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+const static string DEFAULT_HOST = "localhost";
+const static int DEFAULT_PORT = 1883;
+const static string DEFAULT_TOPIC = "BeeeOnOut";
+const static int DEFAULT_QOS = 0;
+const static string DEFAULT_CLIENT_NAME = "Gateway";
+const static int KEEPALIVE_SEC = 60;
+const static int QOS_LOW = 0;
+const static int QOS_HIGH = 2;
+
+MosquittoExporter::MosquittoExporter() :
+	m_host(DEFAULT_HOST),
+	m_port(DEFAULT_PORT),
+	m_topic(DEFAULT_TOPIC),
+	m_qos(DEFAULT_QOS),
+	m_clientName(DEFAULT_CLIENT_NAME)
+{
+	mosqpp::lib_init();
+}
+
+MosquittoExporter::~MosquittoExporter()
+{
+	disconnect();
+	mosqpp::lib_cleanup();
+}
+
+void MosquittoExporter::setHost(const string &host)
+{
+	m_host = host;
+}
+
+void MosquittoExporter::setPort(const int port)
+{
+	if (port < 0 || port > 65535)
+		throw InvalidArgumentException("port is out of range");
+	m_port = port;
+}
+
+void MosquittoExporter::setTopic(const string &topic)
+{
+	m_topic = topic;
+}
+
+void MosquittoExporter::setQos(const int qos)
+{
+	if (qos < QOS_LOW || qos > QOS_HIGH)
+		throw InvalidArgumentException("QOS is out of range");
+	m_qos = qos;
+}
+
+void MosquittoExporter::setClientName(const string &clientName)
+{
+	m_clientName = clientName;
+}
+
+void MosquittoExporter::setFormatter(const SharedPtr<SensorDataFormatter> formatter)
+{
+	m_formatter = formatter;
+}
+
+bool MosquittoExporter::ship(const SensorData &data)
+{
+	string msg = m_formatter->format(data);
+
+	if (m_mq.isNull())
+		connect();
+
+	int res = m_mq->publish(NULL, m_topic.c_str(), msg.length(), msg.c_str(), m_qos);
+
+	if (res == MOSQ_ERR_SUCCESS) {
+		return true;
+	}
+	else {
+		disconnect();
+		throwMosquittoError(res);
+	}
+	return false;
+}
+
+void MosquittoExporter::connect()
+{
+	m_mq = new mosqpp::mosquittopp(m_clientName.c_str());
+
+	if (logger().debug()) {
+		logger().debug("Host: " + m_host + ":" + to_string(m_port)
+			+ "  Topic: " + m_topic
+			+ "  Qos: " + to_string(m_qos)
+			+ "  ClientName: " + m_clientName,
+			__FILE__, __LINE__);
+	}
+
+	int res = m_mq->connect_async(m_host.c_str(), m_port, KEEPALIVE_SEC);
+
+	if (res == MOSQ_ERR_SUCCESS) {
+		logger().information("connected to " + m_host + ":" + to_string(m_port));
+	}
+	else {
+		m_mq = nullptr;
+		throwMosquittoError(res);
+	}
+
+}
+
+void MosquittoExporter::disconnect()
+{
+	m_mq->disconnect();
+	m_mq = nullptr;
+}
+
+void MosquittoExporter::throwMosquittoError(int returnCode)
+{
+	switch (returnCode) {
+	case MOSQ_ERR_INVAL:
+		throw InvalidArgumentException("the input parameters were invalid");
+	case MOSQ_ERR_NOMEM:
+		throw OutOfMemoryException("an out of memory condition occurred");
+	case MOSQ_ERR_NO_CONN:
+		throw IOException("the client is not connected to a broker");
+	case MOSQ_ERR_PROTOCOL:
+		throw ProtocolException("there is a protocol error communicating with the broker");
+	case MOSQ_ERR_PAYLOAD_SIZE:
+		throw ProtocolException("payloadlen is too large");
+	case MOSQ_ERR_ERRNO:
+		if (errno == ECONNREFUSED)
+			throw Net::ConnectionRefusedException("failed to connect to mqtt broker");
+		else
+			throw SystemException("system call returned an error: " + string(strerror(errno)));
+	default:
+		throw IllegalStateException("unknown error");
+	}
+}

conf/config.d/factory-exporter.xml
@@ -5,6 +5,15 @@
 			<set name="formatter" ref="${exporter.pipe.format}SensorDataFormatter" />
 		</instance>
 
+		<instance name="mosquittoExporter" class="BeeeOn::MosquittoExporter">
+			<set name="host" text="${exporter.mqtt.host}" />
+			<set name="port" number="${exporter.mqtt.port}" />
+			<set name="topic" text="${exporter.mqtt.topic}" />
+			<set name="qos" number="${exporter.mqtt.qos}" />
+			<set name="clientName" text="${exporter.mqtt.clientName}" />
+			<set name="formatter" ref="${exporter.mqtt.format}SensorDataFormatter" />
+		</instance>
+
 		<instance name="CSVSensorDataFormatter" class="BeeeOn::CSVSensorDataFormatter">
 			<set name="separator" text="${exporter.pipe.csv.separator}" />
 		</instance>

conf/config.d/factory.xml
@@ -15,6 +15,7 @@
 
 		<instance name="distributor" class="BeeeOn::BasicDistributor">
 			<set name="exporter" ref="namedPipeExporter" if-yes="${exporter.pipe.enable}"/>
+			<set name="exporter" ref="mosquittoExporter" if-yes="${exporter.mqtt.enable}"/>
 		</instance>
 
 		<instance name="commandDispatcher" class="BeeeOn::PocoCommandDispatcher">

conf/config.d/logging-loggers.ini
@@ -31,3 +31,6 @@ loggers.FileCredentialsStorage.level = information
 
 loggers.UDevMonitor.name = BeeeOn::UDevMonitor
 loggers.UDevMonitor.level = trace
+
+loggers.MosquittoExporter.name = BeeeOn::MosquittoExporter
+loggers.MosquittoExporter.level = debug

conf/gateway-startup.ini
@@ -7,6 +7,14 @@ pipe.path = /tmp/beeeon_pipe
 pipe.format = CSV
 pipe.csv.separator = ;
 
+mqtt.enable = yes
+mqtt.host = localhost
+mqtt.port = 1883
+mqtt.topic = BeeeOnOut
+mqtt.qos = 0
+mqtt.clientName = Gateway
+mqtt.format = JSON
+
 [testing]
 center.enable = no
 center.tcp.address = 127.0.0.1

src/CMakeLists.txt
@@ -1,6 +1,7 @@
 cmake_minimum_required (VERSION 2.8.11)
 project (gateway-core CXX)
 
+find_library (MOSQUITTO_CPP mosquittopp)
 find_library (POCO_FOUNDATION PocoFoundation)
 find_library (POCO_SSL PocoNetSSL)
 find_library (POCO_CRYPTO PocoCrypto)
@@ -44,6 +45,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/FileCredentialsStorage.cpp
 	${PROJECT_SOURCE_DIR}/credentials/PasswordCredentials.cpp
 	${PROJECT_SOURCE_DIR}/credentials/PinCredentials.cpp
+	${PROJECT_SOURCE_DIR}/exporters/MosquittoExporter.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/net/AbstractHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
@@ -73,6 +75,7 @@ add_library(BeeeOnTesting ${TESTING_SOURCES})
 add_executable(beeeon-gateway main.cpp)
 
 set(LIBS
+	${MOSQUITTO_CPP}
 	${POCO_FOUNDATION}
 	${POCO_SSL}
 	${POCO_CRYPTO}

src/exporters/MosquittoExporter.h
@@ -1,0 +1,61 @@
+#ifndef BEEEON_MQTT_EXPORTER_H
+#define BEEEON_MQTT_EXPORTER_H
+
+#include <string>
+#include <mosquittopp.h>
+
+#include <Poco/Logger.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/Exporter.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class SensorDataFormatter;
+
+class MosquittoExporter :
+	public Exporter,
+	protected Loggable {
+public:
+	MosquittoExporter();
+	~MosquittoExporter();
+
+	bool ship(const SensorData &data) override;
+
+	void setHost(const std::string &host);
+
+	void setPort(int port);
+
+	void setTopic(const std::string &topic);
+
+	void setQos(int qos);
+
+	/*
+	 * Client name have to be unique for every exporter.
+	 * In the case of running multiple exporters with the same client name,
+	 * only one of them will work.
+	 */
+	void setClientName(const std::string &clientName);
+
+	void setFormatter(const Poco::SharedPtr<SensorDataFormatter> formatter);
+
+private:
+	void connect();
+
+	void disconnect();
+
+	void throwMosquittoError(int returnCode);
+
+	std::string m_host;
+	int m_port;
+	std::string m_topic;
+	int m_qos;
+	std::string m_clientName;
+	Poco::SharedPtr<SensorDataFormatter> m_formatter;
+	Poco::SharedPtr<mosqpp::mosquittopp> m_mq;
+};
+
+}
+
+#endif // BEEEON_MQTT_EXPORTER_H

test/CMakeLists.txt
@@ -2,6 +2,7 @@ cmake_minimum_required (VERSION 2.8.11)
 project (gateway-tests CXX)
 
 find_library (CPP_UNIT cppunit)
+find_library (MOSQUITTO_CPP mosquittopp)
 find_library (POCO_FOUNDATION PocoFoundation)
 find_library (POCO_UTIL PocoUtil)
 find_library (POCO_SSL PocoNetSSL)
@@ -36,6 +37,7 @@ add_executable(test-suite-gateway
 )
 
 set(LIBS
+	${MOSQUITTO_CPP}
 	${POCO_FOUNDATION}
 	${POCO_SSL}
 	${POCO_CRYPTO}

commit e0c938d7a8cef43a920a0d28e3b3f84fec3aacea

MosquittoExporter: fix NullPointerException in disconnect()
If the MosquittoExporter does not receive any data,
then the mqtt client cannot be created. To exit (close)
the gateway, disconnect() function should be called to
the nullptr object.

Fixes: 01b235b0 ("MosquittoExporter: new exporter")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Tested-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/exporters/MosquittoExporter.cpp
@@ -126,6 +126,9 @@ void MosquittoExporter::connect()
 
 void MosquittoExporter::disconnect()
 {
+	if (m_mq.isNull())
+		return;
+
 	m_mq->disconnect();
 	m_mq = nullptr;
 }