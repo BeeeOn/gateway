BeeeOn/gateway

commit 9c5635c9b97aaa463500b2757e122ba6f25e70f7

UDevListener: new abstract class
Provide an interface for handling of udev events. Anybody
can override its event methods for this purpose.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/udev/UDevListener.cpp
@@ -1,0 +1,19 @@
+#include "udev/UDevListener.h"
+
+using namespace BeeeOn;
+
+void UDevListener::onAdd(const UDevEvent &)
+{
+}
+
+void UDevListener::onRemove(const UDevEvent &)
+{
+}
+
+void UDevListener::onChange(const UDevEvent &)
+{
+}
+
+void UDevListener::onMove(const UDevEvent &)
+{
+}

src/udev/UDevListener.h
@@ -1,0 +1,22 @@
+#ifndef BEEEON_UDEV_LISTENER_H
+#define BEEEON_UDEV_LISTENER_H
+
+#include <Poco/SharedPtr.h>
+
+namespace BeeeOn {
+
+class UDevEvent;
+
+class UDevListener {
+public:
+	typedef Poco::SharedPtr<UDevListener> Ptr;
+
+	virtual void onAdd(const UDevEvent &event);
+	virtual void onRemove(const UDevEvent &event);
+	virtual void onChange(const UDevEvent &event);
+	virtual void onMove(const UDevEvent &event);
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -52,6 +52,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/net/VPTHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/udev/UDevEvent.cpp
+	${PROJECT_SOURCE_DIR}/udev/UDevListener.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp

commit bb9e9256a34fbc584547a394ea7569a70cda4f55

UDevListener: fix missing virtual destructor
Definition of virtual destructor was missing for UDevListener.
This resulted in segmentation fault when trying to destroy inherited
objects.

As UDevListener class is meant to inherit so this was considered a bug.

Fixes: 9c5635c9b97a ("UDevListener: new abstract class")

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/udev/UDevListener.cpp
@@ -2,6 +2,10 @@
 
 using namespace BeeeOn;
 
+UDevListener::~UDevListener()
+{
+}
+
 void UDevListener::onAdd(const UDevEvent &)
 {
 }

src/udev/UDevListener.h
@@ -11,6 +11,8 @@ class UDevListener {
 public:
 	typedef Poco::SharedPtr<UDevListener> Ptr;
 
+	virtual ~UDevListener();
+
 	virtual void onAdd(const UDevEvent &event);
 	virtual void onRemove(const UDevEvent &event);
 	virtual void onChange(const UDevEvent &event);