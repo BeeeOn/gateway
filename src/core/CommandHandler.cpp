BeeeOn/gateway

commit 968eabc1cbe22724bb1289fc9801f11c360b8abb

CommandHandler: new class
The CommandHandler interface implements procedures to fullfill
requested Commands. Each Command holds information about some action to
be performed. The action itself can be realized in many different ways.
E.g. command that asks to unpair a device is implemented differently
for each sensoric network.

Classes that want to react on commands must implement this interface
and register with a particular instance of CommandDispatcher class
(introduced in the following commits).

Signed-off-by: Petr Kohout <xkohou14@stud.fit.vutbr.cz>
Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/CommandHandler.cpp
@@ -1,0 +1,14 @@
+#include "core/CommandHandler.h"
+
+using namespace BeeeOn;
+using namespace std;
+
+CommandHandler::CommandHandler(const string &name):
+	m_name(name)
+{
+}
+
+string CommandHandler::name() const
+{
+	return m_name;
+}

src/core/CommandHandler.h
@@ -1,0 +1,52 @@
+#ifndef BEEEON_COMMAND_HANDLER_H
+#define BEEEON_COMMAND_HANDLER_H
+
+#include <string>
+
+#include "core/Command.h"
+#include "core/Result.h"
+#include "core/Answer.h"
+
+namespace BeeeOn {
+
+class Answer;
+
+/*
+ * Interface for handling commands. It contains a method to check
+ * whether the command is supported and the method to execute
+ * such command.
+ */
+class CommandHandler {
+public:
+	/*
+	 * CommandHandler with specific name of handler.
+	 * The name is intended for debugging purposes only.
+	 */
+	CommandHandler(const std::string &name);
+
+	/*
+	 * Name of handler.
+	 */
+	std::string name() const;
+
+	/*
+	 * Returns true if the given command can be handled by this handler.
+	 */
+	virtual bool accept(const Command::Ptr cmd) = 0;
+
+	/*
+	 * This method is likely to be called concurrently. It must be
+	 * implemented in a thread-safe way. It must create the Result
+	 * and it must add the Answer.
+	 *
+	 * For example: Mutex::ScopedLock lock(m_mutex);
+	 */
+	virtual void handle(Command::Ptr cmd, Answer::Ptr answer) = 0;
+
+private:
+	std::string m_name;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -13,6 +13,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/AbstractDistributor.cpp
 	${PROJECT_SOURCE_DIR}/core/BasicDistributor.cpp
 	${PROJECT_SOURCE_DIR}/core/Command.cpp
+	${PROJECT_SOURCE_DIR}/core/CommandHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/core/VirtualSensor.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp

commit 01df235a0ab111c1fa01cb4338115e4c10f9cd16

CommandHandler: define virtual destructor
This potentially induces big big crashes during shutdown when casting
SharedPtr<X> to SharedPtr<CommandHandler> that calls destructor of
the CommandHandler class. However, in that case, it does not reflect
the fact that such instance is not CommandHandler but X. Thus, it
calls delete on CommandHandler instead of X. In this case, our nice
toys begin to shout terrible things like the following:

*** Error in `build_x86/src/beeeon-gateway': free(): invalid pointer: 0x0000000001250208 ***
======= Backtrace: =========
/usr/lib/libc.so.6(+0x7254c)[0x7f6ab6bc754c]
/usr/lib/libc.so.6(+0x78bfb)[0x7f6ab6bcdbfb]
/usr/lib/libc.so.6(+0x79fd1)[0x7f6ab6bcefd1]
beeeon-gateway(_ZN4Poco13ReleasePolicyIN6BeeeOn14CommandHandlerEE7releaseEPS2_+0x26)[0x4d0c97]
beeeon-gateway(_ZN4Poco9SharedPtrIN6BeeeOn14CommandHandlerENS_16ReferenceCounterENS_13Release
		PolicyIS2_EEE7releaseEv+0x35)[0x4d0c0b]
beeeon-gateway(_ZN4Poco9SharedPtrIN6BeeeOn14CommandHandlerENS_16ReferenceCounterENS_13Release
		PolicyIS2_EEED1Ev+0x18)[0x4d0bb4]
beeeon-gateway(_ZN9__gnu_cxx13new_allocatorISt10_List_nodeIN4Poco9SharedPtrIN6BeeeOn14Command
		HandlerENS2_16ReferenceCounterENS2_13ReleasePolicyIS5_EEEEEE7destroyIS9_EEvPT
		_+0x1c)[0x4d139c]
beeeon-gateway(_ZNSt16allocator_traitsISaISt10_List_nodeIN4Poco9SharedPtrIN6BeeeOn14Command
		HandlerENS1_16ReferenceCounterENS1_13ReleasePolicyIS4_EEEEEEE7destroyIS8_EE
		vRSA_PT_+0x23)[0x4d1350]
beeeon-gateway(_ZNSt7__cxx1110_List_baseIN4Poco9SharedPtrIN6BeeeOn14CommandHandlerENS1_16
		ReferenceCounterENS1_13ReleasePolicyIS4_EEEESaIS8_EE8_M_clearEv+0x62)[0x4d130a]
beeeon-gateway(_ZNSt7__cxx1110_List_baseIN4Poco9SharedPtrIN6BeeeOn14CommandHandlerENS1_16
		ReferenceCounterENS1_13ReleasePolicyIS4_EEEESaIS8_EED1Ev+0x18)[0x4d121c]
beeeon-gateway(_ZNSt7__cxx114listIN4Poco9SharedPtrIN6BeeeOn14CommandHandlerENS1_16Reference
		CounterENS1_13ReleasePolicyIS4_EEEESaIS8_EED1Ev+0x18)[0x4d1148]
beeeon-gateway(_ZN6BeeeOn17CommandDispatcherD1Ev+0x28)[0x4d1174]
beeeon-gateway(_ZN6BeeeOn21PocoCommandDispatcherD2Ev+0x34)[0x4dd70c]
beeeon-gateway(_ZN4Poco13ReleasePolicyIN6BeeeOn21PocoCommandDispatcherEE7releaseEPS2_+0x1e)
		[0x4dd72d]
beeeon-gateway(_ZN4Poco9SharedPtrIN6BeeeOn21PocoCommandDispatcherENS_16ReferenceCounterENS_13
		ReleasePolicyIS2_EEE7releaseEv+0x35)[0x4dd773]
beeeon-gateway(_ZN4Poco9SharedPtrIN6BeeeOn21PocoCommandDispatcherENS_16ReferenceCounterENS_13
		ReleasePolicyIS2_EEED1Ev+0x18)[0x4dd41e]
beeeon-gateway(_ZN6BeeeOn17AbstractDIWrapperINS_21PocoCommandDispatcherEED2Ev+0x22a)[0x4dd19a]
beeeon-gateway(_ZN6BeeeOn24PocoCommandDispatcherDIWD1Ev+0x31)[0x4dd989]
beeeon-gateway(_ZN6BeeeOn24PocoCommandDispatcherDIWD0Ev+0x18)[0x4dd9aa]
...

Fixes: 968eabc1cbe2 ("CommandHandler: new class")

src/core/CommandHandler.cpp
@@ -6,3 +6,7 @@ using namespace std;
 CommandHandler::CommandHandler()
 {
 }
+
+CommandHandler::~CommandHandler()
+{
+}

src/core/CommandHandler.h
@@ -19,6 +19,7 @@ class Answer;
 class CommandHandler {
 public:
 	CommandHandler();
+	virtual ~CommandHandler();
 
 	/*
 	 * Returns true if the given command can be handled by this handler.