0ef748fe4788e64ad720d45e1c349dadc1fdcded
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-08-16 12:47:41 +0200

    JablotronDeviceManager: fit trace log to a single line
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
    Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- src/jablotron/JablotronDeviceManager.cpp
+++ src/jablotron/JablotronDeviceManager.cpp
@@ -368,9 +368,7 @@ void JablotronDeviceManager::setupDongleDevices()
 		// finding empty slot: SLOT:XX [--------]
 		RegularExpression re(".*[--------].*");
 		if (re.match(buffer)) {
-			logger().trace(
-				"empty slot",
-				__FILE__, __LINE__);
+			logger().trace("empty slot", __FILE__, __LINE__);
 			continue;
 		}
 
