BeeeOn/gateway

commit 5d53328be7c2af9b12a1f7cc073a52d0a822c18b

JablotronDeviceManager: new class
The class implements the management with Jablotron dongle:

 * reading values from the dongle
 * parsing messages from serial port
 * communication with the server

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -1,0 +1,385 @@
+#include <Poco/RegularExpression.h>
+#include <Poco/Thread.h>
+#include <Poco/StringTokenizer.h>
+
+#include "core/CommandDispatcher.h"
+#include "di/Injectable.h"
+#include "io/AutoClose.h"
+#include "jablotron/JablotronDeviceManager.h"
+#include "udev/UDevEvent.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, JablotronDeviceManager)
+BEEEON_OBJECT_CASTABLE(CommandHandler)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_CASTABLE(UDevListener)
+BEEEON_OBJECT_REF("distributor", &JablotronDeviceManager::setDistributor)
+BEEEON_OBJECT_REF("commandDispatcher", &JablotronDeviceManager::setCommandDispatcher)
+BEEEON_OBJECT_END(BeeeOn, JablotronDeviceManager)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+static const string JABLOTRON_VENDOR_ID = "0403";
+static const string JABLOTRON_PRODUCT_ID = "6015";
+static const int BAUD_RATE = 57600;
+static const int MAX_DEVICES_IN_JABLOTRON = 32;
+static const int NUMBER_OF_RETRIES = 3;
+static const int MAX_NUMBER_FAILED_REPEATS = 10;
+static const int RESPONSE_WAIT_MSEC = 5000;
+static const Timespan READ_TIMEOUT = 1 * Timespan::SECONDS;
+static const Timespan SLEEP_AFTER_FAILED = 1 * Timespan::SECONDS;
+static const Timespan DELAY_AFTER_SET_SWITCH = 1 * Timespan::SECONDS;
+static const Timespan DELAY_BEETWEEN_CYCLES = 300 * Timespan::MILLISECONDS;
+
+JablotronDeviceManager::JablotronDeviceManager():
+	DongleDeviceManager(DevicePrefix::PREFIX_JABLOTRON),
+	m_lastResponse(NONE)
+{
+}
+
+void JablotronDeviceManager::initJablotronDongle()
+{
+	m_serial.setBaudRate(BAUD_RATE);
+	m_serial.setStopBits(SerialPort::StopBits::STOPBITS_1);
+	m_serial.setParity(SerialPort::Parity::PARITY_NONE);
+	m_serial.setDataBits(SerialPort::DataBits::DATABITS_8);
+	m_serial.setNonBlocking(true);
+
+	m_serial.setDevicePath(dongleName());
+	m_serial.open();
+	m_serial.flush();
+}
+
+void JablotronDeviceManager::dongleVersion()
+{
+	string message;
+
+	m_serial.write("\x1BWHO AM I?\n");
+
+	if (nextMessage(message) != MessageType::DATA) {
+		logger().warning(
+			"unexpected response: " + message,
+			__FILE__, __LINE__);
+		return;
+	}
+
+	if (wasTurrisDongleVersion(message)) {
+		logger().information(
+			"Jablotron Dongle version: "
+			+ message,
+		__FILE__, __LINE__);
+	}
+	else {
+		logger().warning(
+			"unknown dongle version: " + message,
+		__FILE__, __LINE__);
+	}
+}
+
+void JablotronDeviceManager::dongleAvailable()
+{
+	AutoClose<SerialPort> serial(m_serial);
+	m_devices.clear();
+
+	initJablotronDongle();
+	dongleVersion();
+
+	try {
+		jablotronProcess();
+	}
+	catch (const IOException &ex) {
+		// waiting so that UDevEvent can arrive after exception on serial port
+		Thread::sleep(SLEEP_AFTER_FAILED.totalMilliseconds());
+		ex.rethrow();
+	}
+}
+
+void JablotronDeviceManager::stop()
+{
+	DongleDeviceManager::stop();
+}
+
+void JablotronDeviceManager::jablotronProcess()
+{
+	string message;
+
+	setupDongleDevices();
+	loadDeviceList();
+
+	while (!m_stop) {
+		MessageType response = nextMessage(message);
+
+		if (isResponse(response)) {
+			m_lastResponse = response;
+			m_responseRcv.set();
+			continue;
+		}
+
+		if (response != MessageType::DATA) {
+			logger().warning(
+				"unexpected response: " + message,
+				__FILE__, __LINE__);
+			continue;
+		}
+
+		if (message.empty()) {
+			logger().debug("empty message",
+				__FILE__, __LINE__);
+		}
+
+		DeviceID id = JablotronDevice::buildID(extractSerialNumber(message));
+		Mutex::ScopedLock guard(m_lock);
+
+		auto it = m_devices.find(id);
+		if (!it->second.isNull() && it->second->paired()) {
+			shipMessage(message, it->second);
+		}
+		else {
+			logger().debug(
+				"device " + id.toString()
+				+ " is not paired",
+				__FILE__, __LINE__);
+		}
+	}
+}
+
+bool JablotronDeviceManager::accept(const Command::Ptr cmd)
+{
+	if (cmd->is<DeviceSetValueCommand>())
+		return true;
+
+	return false;
+}
+
+void JablotronDeviceManager::handle(Command::Ptr cmd, Answer::Ptr answer)
+{
+	if (cmd->is<DeviceSetValueCommand>())
+		doSetValue(cmd.cast<DeviceSetValueCommand>(), answer);
+	else
+		throw IllegalStateException("received unaccepted command");
+}
+
+void JablotronDeviceManager::loadDeviceList()
+{
+	set<DeviceID> deviceIDs = deviceList(-1);
+
+	Mutex::ScopedLock guard(m_lock);
+	for (auto &id : deviceIDs) {
+		auto it = m_devices.find(id);
+
+		if (it != m_devices.end()) {
+			try {
+				it->second = JablotronDevice::create(id.ident());
+			}
+			catch (const InvalidArgumentException &ex) {
+				logger().log(ex, __FILE__, __LINE__);
+				continue;
+			}
+
+			it->second->setPaired(true);
+		}
+	}
+}
+
+JablotronDeviceManager::MessageType JablotronDeviceManager::nextMessage(
+			string &message)
+{
+	// find message between newline
+	// message example: \nAC-88 RELAY:1\n
+	const RegularExpression re("(?!\\n)[^\\n]*(?=\\n)");
+
+	while (!re.extract(m_buffer, message) && !m_stop) {
+		try {
+			m_buffer += m_serial.read(READ_TIMEOUT);
+		}
+		catch (const TimeoutException &ex) {
+			// avoid frozen state, allow to test m_stop time after time
+			continue;
+		}
+	}
+
+	// erase matched message from buffer + 2x newline
+	m_buffer.erase(0, message.size() + 2);
+	logger().debug("received data: " + message, __FILE__, __LINE__);
+
+	return messageType(message);
+}
+
+JablotronDeviceManager::MessageType JablotronDeviceManager::messageType(
+	const string &data)
+{
+	if (data == "OK")
+		return MessageType::OK;
+	else if (data == "ERROR")
+		return MessageType::ERROR;
+
+	return MessageType::DATA;
+}
+
+bool JablotronDeviceManager::wasTurrisDongleVersion(
+	const string &message) const
+{
+	const RegularExpression re("TURRIS DONGLE V[0-9].[0-9]*");
+	return re.match(message);
+}
+
+void JablotronDeviceManager::setupDongleDevices()
+{
+	string buffer;
+	for (int i = 0; i < MAX_DEVICES_IN_JABLOTRON; i++) {
+		Mutex::ScopedLock guard(m_lock);
+
+		// we need 2-digits long value (zero-prefixed when needed)
+		m_serial.write("\x1BGET SLOT:" + NumberFormatter::format0(i, 2) + "\n");
+
+		if (nextMessage(buffer) != MessageType::DATA) {
+			logger().error(
+				"unknown message: " + buffer,
+				__FILE__, __LINE__
+			);
+
+			continue;
+		}
+
+		// finding empty slot: SLOT:XX [--------]
+		RegularExpression re(".*[--------].*");
+		if (re.match(buffer)) {
+			logger().debug(
+				"empty slot",
+				__FILE__, __LINE__);
+			continue;
+		}
+
+		try {
+			uint32_t serialNumber = extractSerialNumber(buffer);
+			DeviceID deviceID = JablotronDevice::buildID(serialNumber);
+
+			// create empty JablotronDevice
+			// after the obtain server device list, create JablotronDevice for paired devices
+			m_devices.emplace(deviceID, nullptr);
+
+			logger().debug(
+				"created device: " + to_string(serialNumber)
+				+ " from dongle", __FILE__, __LINE__);
+		}
+		catch (const InvalidArgumentException &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+		}
+		catch (const SyntaxException &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+		}
+	}
+}
+
+uint32_t JablotronDeviceManager::extractSerialNumber(const string &message)
+{
+	RegularExpression re("\\[([0-9]+)\\]");
+	vector<string> tmp;
+
+	if (re.split(message, tmp) == 2)
+		return NumberParser::parseUnsigned(tmp[1]);
+
+	throw InvalidArgumentException(
+		"invalid serial number string: " + message);
+}
+
+void JablotronDeviceManager::shipMessage(
+	const string &message, JablotronDevice::Ptr jablotronDevice)
+{
+	try {
+		ship(jablotronDevice->extractSensorData(message));
+	}
+	catch (const RangeException &ex) {
+		logger().error(
+			"unexpected token index in message: " + message,
+			__FILE__, __LINE__
+		);
+	}
+	catch (const Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+	};
+}
+
+string JablotronDeviceManager::dongleMatch(const UDevEvent &e)
+{
+	const string &productID = e.properties()
+		->getString("tty.ID_MODEL_ID", "");
+
+	const string &vendorID = e.properties()
+		->getString("tty.ID_VENDOR_ID", "");
+
+	if (e.subsystem() == "tty") {
+		if (vendorID == JABLOTRON_VENDOR_ID
+				&& productID == JABLOTRON_PRODUCT_ID)
+			return e.node();
+	}
+
+	return "";
+}
+
+/* Retransmission packet status is recommended to be done 3 times with
+ * a minimum gap 200ms and 500ms maximum space (for an answer) - times
+ * in the space, it is recommended to choose random.
+ */
+bool JablotronDeviceManager::transmitMessage(const string &msg, bool autoResult)
+{
+	string message;
+
+	for (int i = 0; i < NUMBER_OF_RETRIES; i++) {
+		if (!m_serial.write(msg)) {
+			Thread::sleep(SLEEP_AFTER_FAILED.totalMilliseconds());
+			continue;
+		}
+
+		if (!autoResult) {
+			int i = 0;
+			while(!getResponse() && i < NUMBER_OF_RETRIES)
+				i++;
+		}
+
+		if (m_responseRcv.tryWait(RESPONSE_WAIT_MSEC)) {
+			if (m_lastResponse == ERROR) {
+				Thread::sleep(DELAY_BEETWEEN_CYCLES.totalMilliseconds());
+				continue;
+			}
+			Thread::sleep(DELAY_AFTER_SET_SWITCH.totalMilliseconds());
+			return true;
+		}
+		else {
+			poco_error(logger(), "no response received");
+			return false;
+		}
+
+		Thread::sleep(DELAY_BEETWEEN_CYCLES.totalMilliseconds());
+	}
+
+	Thread::sleep(DELAY_AFTER_SET_SWITCH.totalMilliseconds());
+	return false;
+}
+
+bool JablotronDeviceManager::getResponse()
+{
+	string message;
+	MessageType response = nextMessage(message);
+
+	if (isResponse(response)) {
+		m_lastResponse = response;
+		m_responseRcv.set();
+		return true;
+	}
+
+	return false;
+}
+
+bool JablotronDeviceManager::isResponse(MessageType type)
+{
+	return type == OK || type == ERROR;
+}
+
+void JablotronDeviceManager::doSetValue(
+	DeviceSetValueCommand::Ptr cmd, Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+	result->setStatus(Result::FAILED);
+}

conf/config.d/factory.xml
@@ -7,6 +7,7 @@
 			<add name="runnables" ref="virtualSensor3" if-yes="${virtual-sensor3.enable}" />
 			<add name="runnables" ref="testingCenter" if-yes="${testing.center.enable}" />
 			<add name="runnables" ref="udevMonitor" />
+			<add name="runnables" ref="jablotronDeviceManager" if-yes="${jablotron.enable}" />
 		</instance>
 
 		<instance name="gatewayInfo" class="BeeeOn::GatewayInfo" init="early">
@@ -27,10 +28,12 @@
 		<instance name="commandDispatcher" class="BeeeOn::PocoCommandDispatcher">
 			<set name="registerHandler" ref="testingCenter" if-yes="${testing.center.enable}"/>
 			<set name="registerHandler" ref="belkinwemoDeviceManager" if-yes="${belkinwemo.enable}"/>
+			<set name="registerHandler" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 		</instance>
 
 		<instance name="udevMonitor" class="BeeeOn::UDevMonitor">
 			<set name="matches" list="bluetooth,tty" />
+			<set name="listeners" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 		</instance>
 
 		<instance name="belkinwemoDeviceManager" class="BeeeOn::BelkinWemoDeviceManager">
@@ -87,6 +90,11 @@
 		<instance name="cryptoConfig" class="BeeeOn::CryptoConfig">
 			<set name="passphrase" text="${credentials.crypto.passphrase}" />
 			<set name="algorithm" text="${credentials.crypto.algorithm}" />
+		</instance>
+
+		<instance name="jablotronDeviceManager" class="BeeeOn::JablotronDeviceManager">
+			<set name="distributor" ref="distributor" />
+			<set name="commandDispatcher" ref="commandDispatcher" />
 		</instance>
 	</factory>
 </system>

conf/config.d/logging-loggers.ini
@@ -37,3 +37,6 @@ loggers.UDevMonitor.level = information
 
 loggers.MosquittoExporter.name = BeeeOn::MosquittoExporter
 loggers.MosquittoExporter.level = debug
+
+loggers.JablotronDeviceManager.name = BeeeOn::JablotronDeviceManager
+loggers.JablotronDeviceManager.level = debug

conf/gateway-startup.ini
@@ -39,3 +39,6 @@ configuration.root = credentials
 save.delay = 1800
 crypto.passphrase = If Purple People Eaters are real where do they find purple people to eat?
 crypto.algorithm = aes256
+
+[jablotron]
+enable = yes

src/CMakeLists.txt
@@ -53,6 +53,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/exporters/MosquittoExporter.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/jablotron/JablotronDevice.cpp
+	${PROJECT_SOURCE_DIR}/jablotron/JablotronDeviceManager.cpp
 	${PROJECT_SOURCE_DIR}/net/AbstractHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp

src/jablotron/JablotronDeviceManager.h
@@ -1,0 +1,119 @@
+#pragma once
+
+#include <map>
+#include <set>
+
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include "commands/DeviceSetValueCommand.h"
+#include "core/DongleDeviceManager.h"
+#include "io/SerialPort.h"
+#include "jablotron/JablotronDevice.h"
+#include "model/DeviceID.h"
+
+namespace BeeeOn {
+
+/**
+ * JablotronDeviceManager manages a cache of devices associated with the
+ * Jablotron dongle and paired on the server. Devices associated with the
+ * dongle are not necessarily those paired by the user. Thus, the
+ * associated devices are managed in the form <DeviceID, NULL>.
+ *
+ * When a device is paired by its user, the JablotronDeviceManager creates
+ * a new instance of a JablotronDevice and manages such device as
+ * <DeviceID, SharedPtr<JablotronDevice>>.
+ *
+ * Physical Jablotron devices can be associated with the dongle only
+ * in a software-based way. Thus, they are associated by an instance
+ * of the JablotronDeviceManager (and we know about it) or the dongle
+ * must be physically connected to another device. Every time a dongle
+ * is physical reconnected, the JablotronDeviceManager checks the
+ * associated devices and the coherency of the device list thus maintained.
+ *
+ * However, if another process (by mistake or even intentionally) performs
+ * a device reassociation with the dongle directly via the serial port
+ * in parallel while the JablotronDeviceManaher is running, then the
+ * JablotronDeviceManager could behave in an unexpected way. Such
+ * behaviour is undefined.
+ */
+class JablotronDeviceManager : public DongleDeviceManager {
+public:
+	JablotronDeviceManager();
+
+	bool accept(const Command::Ptr cmd) override;
+	void handle(Command::Ptr cmd, Answer::Ptr answer) override;
+
+	std::string dongleMatch(const UDevEvent &e) override;
+	void dongleAvailable() override;
+
+	void stop() override;
+
+private:
+	enum MessageType {
+		OK,
+		ERROR,
+		DATA,
+		NONE,
+	};
+
+	/**
+	 * Load device list from server and create JablotronDevice.
+	 */
+	void loadDeviceList();
+
+	/**
+	 * It shows Jablotron Turris Dongle firmware version.
+	 * Example: TURRIS DONGLE V1.4
+	 */
+	void dongleVersion();
+	bool wasTurrisDongleVersion(const std::string &message) const;
+
+	void initJablotronDongle();
+	void jablotronProcess();
+
+	/**
+	 * It picks next message from buffer and removes picked message from it.
+	 * If buffer is empty, method reads data from serial into the buffer.
+	 */
+	MessageType nextMessage(std::string &message);
+	MessageType messageType(const std::string &data);
+
+	/**
+	 * Extract serial number from string.
+	 * Example of string: SLOT:01 [09491911]
+	 */
+	uint32_t extractSerialNumber(const std::string &message);
+
+	/**
+	 * Load devices from USB Dongle.
+	 */
+	void setupDongleDevices();
+
+	void shipMessage(const std::string &message, JablotronDevice::Ptr jablotronDevice);
+
+	/**
+	 * It sends message to Jablotron dongle and checks if sent
+	 * message is accepted.
+	 *
+	 * @return True if Turris dongle accepts message.
+	 */
+	bool transmitMessage(const std::string &msg, bool autoResult);
+
+	void doSetValue(DeviceSetValueCommand::Ptr cmd, Answer::Ptr answer);
+
+	bool getResponse();
+	bool isResponse(MessageType type);
+
+private:
+	SerialPort m_serial;
+	Poco::Mutex m_lock;
+	std::map<DeviceID, JablotronDevice::Ptr> m_devices;
+	std::string m_buffer;
+
+	MessageType m_lastResponse;
+	Poco::Event m_responseRcv;
+};
+
+}

commit 6b312ae4883080ff5723f9248337ec6302a1697d

JablotronDeviceManager: fix no prefix check at SetValueCommand
JablotronDeviceManager accepted every SetValueCommand without checking
if DeviceID belongs to it or not.

This leads to a situation when all Answers to all SetValueCommands in
the system contain at leas one FAILED result

Check based on prefix comparison added.

Fixes: 5d53328be7c2 ("JablotronDeviceManager: new class")

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -156,7 +156,7 @@ void JablotronDeviceManager::jablotronProcess()
 bool JablotronDeviceManager::accept(const Command::Ptr cmd)
 {
 	if (cmd->is<DeviceSetValueCommand>())
-		return true;
+		return cmd->cast<DeviceSetValueCommand>().deviceID().prefix() == m_prefix;
 	else if (cmd->is<GatewayListenCommand>())
 		return true;
 	else if (cmd->is<DeviceUnpairCommand>()) {