330d33e19023602aa4a1999250cf14276a8edcec
David Bednařík <xbedna62@stud.fit.vutbr.cz>
2017-08-21 15:11:34 +0200

    BelkinWemoStandaloneDevice: extract abstract class
    A new abstract class representing generic BelkinWemo standalone device.
    The class implements sending SOAP messages to control the device.
    Messages include GetMacAddr, GetBinaryState and SetBinaryState requests.
    
    Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

---
+++ src/belkin/BelkinWemoStandaloneDevice.h
@@ -0,0 +1,66 @@
+#ifndef GATEWAY_BELKIN_WEMO_STANDALONE_DEVICE_H
+#define GATEWAY_BELKIN_WEMO_STANDALONE_DEVICE_H
+
+#include <string>
+
+#include <Poco/Timespan.h>
+#include <Poco/URI.h>
+#include <Poco/Net/SocketAddress.h>
+
+#include "belkin/BelkinWemoDevice.h"
+#include "net/HTTPEntireResponse.h"
+#include "net/MACAddress.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Abstract class representing generic BelkinWemo standalone device.
+ * The class implements sending messages to control the device.
+ */
+class BelkinWemoStandaloneDevice : public BelkinWemoDevice {
+public:
+	BelkinWemoStandaloneDevice(const Poco::URI& uri);
+
+	/**
+	 * @brief Prepares SOAP message containing GetMacAddr request
+	 * and sends it to device via HTTP. If the device do not respond
+	 * in specified timeout, Poco::TimeoutException is thrown.
+	 * @return MAC address of the device.
+	 */
+	MACAddress requestMacAddr() const;
+
+	/**
+	 * @brief Prepares SOAP message containing GetBinaryState request
+	 * and sends it to device via HTTP. If the device do not respond
+	 * in specified timeout, Poco::TimeoutException is thrown.
+	 */
+	HTTPEntireResponse requestBinaryState() const;
+
+	/**
+	 * @brief Prepares SOAP message containing SetBinaryState request
+	 * and sends it to device via HTTP. If the device do not respond
+	 * in specified timeout, Poco::TimeoutException is thrown.
+	 * @param &setModuleName name of XML element used in sent message.
+	 * @param &getModuleName name of XML element used for searching in received message.
+	 * @return If the request was successful or not.
+	 */
+	bool requestModifyBinaryState(const std::string& setModuleName,
+		const std::string& getModuleName, const int value) const;
+
+	Poco::Net::SocketAddress address() const;
+	void setAddress(const Poco::Net::SocketAddress& address);
+
+	/**
+	 * @brief Called internally when constructing the instance.
+	 * Creates DeviceID based on Mac address of device.
+	 */
+	void buildDeviceID();
+
+protected:
+	Poco::URI m_uri;
+	Poco::Timespan m_httpTimeout;
+};
+
+}
+
+#endif
--- src/belkin/BelkinWemoSwitch.h
+++ src/belkin/BelkinWemoSwitch.h
@@ -7,10 +7,8 @@
 #include <Poco/Net/SocketAddress.h>
 #include <Poco/SharedPtr.h>
 #include <Poco/Timespan.h>
-#include <Poco/URI.h>
 
-#include "belkin/BelkinWemoDevice.h"
-#include "net/MACAddress.h"
+#include "belkin/BelkinWemoStandaloneDevice.h"
 #include "model/ModuleType.h"
 #include "model/SensorData.h"
 
@@ -20,11 +18,10 @@ namespace BeeeOn {
  * @brief The class represents Belkin WeMo Switch F7C027fr.
  * Provides functions to control the switch. It means turn on, turn off, get state.
  */
-class BelkinWemoSwitch : public BelkinWemoDevice{
+class BelkinWemoSwitch : public BelkinWemoStandaloneDevice {
 public:
 	typedef Poco::SharedPtr<BelkinWemoSwitch> Ptr;
 
-	BelkinWemoSwitch();
 	~BelkinWemoSwitch();
 
 	/**
@@ -52,8 +49,6 @@ public:
 	 */
 	SensorData requestState() override;
 
-	Poco::Net::SocketAddress address() const;
-	void setAddress(const Poco::Net::SocketAddress& address);
 	std::list<ModuleType> moduleTypes() const override;
 	std::string name() const override;
 
@@ -64,43 +59,6 @@ public:
 
 protected:
 	BelkinWemoSwitch(const Poco::Net::SocketAddress& address);
-
-	/**
-	 * @brief Prepares SOAP message containing turn on command
-	 * and sends it to device via HTTP. If the device is not on
-	 * network throws Poco::TimeoutException also in this case
-	 * it is blocking.
-	 * @return If the request was successful or not.
-	 */
-	bool turnOn() const;
-
-	/**
-	 * @brief Prepares SOAP message containing turn off command
-	 * and sends it to device via HTTP. If the device is not on
-	 * network throws Poco::TimeoutException also in this case
-	 * it is blocking.
-	 * @return If the request was successful or not.
-	 */
-	bool turnOff() const;
-
-	/**
-	 * @brief Prepares SOAP message containing request Mac address
-	 * command and sends it to device via HTTP. If the device is not
-	 * on network throws Poco::TimeoutException also in this case
-	 * it is blocking. Request mac address of the device.
-	 * @return Mac address of the device.
-	 */
-	MACAddress requestMacAddr();
-
-	/**
-	 * @brief Called internally when constructing the instance.
-	 * Creates DeviceID based on Mac address of device.
-	 */
-	void buildDeviceID();
-
-private:
-	Poco::URI m_uri;
-	Poco::Timespan m_httpTimeout;
 };
 
 }
