BeeeOn/gateway

commit cbe5f26cfb84d21c575da449b4708d16dbba6941

VirtualDeviceManager: new class
It ensures loading of virtual devices from configuration file.
Virtual device that is loaded has to have "enabled" set to "yes"
in configuration file. It is able to send NewDeviceCommand to
CommandDispatcher. It can also send values from modules of
registered devices. These devices are stored in queue and
calendar serves for planning of value sending.
The virtual device manager is able to react to ListenCommand,
DeviceAcceptCommand, SetValueCommand and UnpairCommand.

Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vdev/VirtualDeviceManager.h
@@ -1,0 +1,208 @@
+#ifndef BEEEON_VIRTUAL_DEVICE_MANAGER_H
+#define BEEEON_VIRTUAL_DEVICE_MANAGER_H
+
+#include <queue>
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timestamp.h>
+#include <Poco/Util/IniFileConfiguration.h>
+
+#include "core/DeviceManager.h"
+#include "vdev/VirtualDevice.h"
+
+namespace BeeeOn {
+
+/**
+ * Represents entry in a calendar. It contains time when entry
+ * was inserted into the calendar and information about device.
+ *
+ * Note: Calendar serves for planning of data sending from modules.
+ */
+class VirtualDeviceEntry {
+public:
+	VirtualDeviceEntry(VirtualDevice::Ptr device);
+
+	/**
+	* Sets time when entry was inserted into a calendar.
+	*/
+	void setInserted(const Poco::Timestamp &t);
+
+	/**
+	* Returns time when entry was inserted into a calendar.
+	*/
+	Poco::Timestamp inserted() const;
+
+	/**
+	* Returns time when entry (device) will be activated
+	* (when data will be sent).
+	 *
+	* activationTime = timeInserted + refreshTime
+	*/
+	Poco::Timestamp activationTime() const;
+
+	/**
+	* Returns information about device.
+	*/
+	VirtualDevice::Ptr device() const;
+
+private:
+	Poco::Timestamp m_inserted;
+	VirtualDevice::Ptr m_device;
+};
+
+/**
+ * Ensures comparison of entries in a calendar.
+ */
+class VirtualDeviceEntryComparator {
+public:
+	/**
+	* Returns entry with lower activation time.
+	*/
+	bool lessThan(const VirtualDeviceEntry &deviceQueue1,
+		const VirtualDeviceEntry &deviceQueue2) const;
+
+	/**
+	* Returns entry with lower activation time.
+	*/
+	bool operator ()(const VirtualDeviceEntry &deviceQueue1,
+		const VirtualDeviceEntry &deviceQueue2) const
+	{
+		return lessThan(deviceQueue1, deviceQueue2);
+	}
+};
+
+/**
+ * Ensures configuration of virtual devices from configuration file
+ * virtual-devices.ini and it is able to send NewDeviceCommand to CommandDispatcher
+ * when device attempts to pair.
+ *
+ * It can send values from modules of registered devices.
+ *
+ * It also ensures reaction to commands sent from server:
+ *
+ * ListenCommand, DeviceAcceptCommand - device attempts to pair
+ * SetValueCommand - modification of module value
+ * UnpairCommand - device attempts to unpair
+ */
+class VirtualDeviceManager : public DeviceManager {
+public:
+	VirtualDeviceManager();
+
+	void run() override;
+	void stop() override;
+
+	/**
+	 * Sets path to configuration file.
+	 */
+	void setConfigFile(const std::string &path);
+
+	/**
+	 * Loads setting of virtual devices from configuration file
+	 * and stores this information.
+	 */
+	void installVirtualDevices();
+
+	/**
+	 * Inserts item to map of virtual devices if there is no virtual
+	 * device with given identifier.
+	 */
+	void registerDevice(const VirtualDevice::Ptr virtualDevice);
+
+	/**
+	 * Ensures sending of NewDeviceCommand to CommandDispatcher.
+	 */
+	void dispatchNewDevice(VirtualDevice::Ptr device);
+
+	/**
+	 * Processes information about virtual device loaded from configuration
+	 * file.
+	 */
+	VirtualDevice::Ptr parseDevice(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> cfg);
+
+	/**
+	 * Processes information about virtual module loaded from configuration
+	 * file.
+	 */
+	VirtualModule::Ptr parseModule(
+		Poco::AutoPtr<Poco::Util::AbstractConfiguration> cfg,
+		const ModuleID &moduleID);
+
+	/**
+	 * Sets devices as paired according to the device list sent from server.
+	 */
+	void setPairedDevices();
+
+	/**
+	 * Plans devices that are in a map of virtual devices and are paired.
+	 */
+	void scheduleAllEntries();
+
+	/**
+	 * Checks if a queue of virtual devices is empty.
+	 */
+	bool isEmptyQueue();
+
+	/**
+	 * Sets time when an entry was inserted into a queue and pushes
+	 * this entry to the queue.
+	 */
+	void scheduleEntryUnlocked(VirtualDeviceEntry entry);
+
+	/**
+	 * Logs information about loaded virtual devices and modules.
+	 * Detail of information can be selected from possibilities:
+	 * information, debug, trace.
+	 */
+	void logDeviceParsed(VirtualDevice::Ptr device);
+
+protected:
+	bool accept(const Command::Ptr cmd) override;
+	void handle(Command::Ptr cmd, Answer::Ptr answer) override;
+
+	/**
+	 * Reacts to ListenCommand. It sends NewDeviceCommand if
+	 * device is not paired.
+	 */
+	void doListenCommand(const GatewayListenCommand::Ptr,
+		const Answer::Ptr answer);
+
+	/**
+	 * Reacts to AcceptCommand. Device has to be stored in map
+	 * of virtual devices and it has to be unpaired. If these conditions
+	 * are fulfilled, method inserts device into a calendar, it sets device
+	 * as paired and it plans next activation (data generation) of this device.
+	 */
+	void doDeviceAcceptCommand(const DeviceAcceptCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+	/**
+	* Reacts to SetValueCommand. Device has to be in map of
+	* virtual devices, it has to be sensor and reaction has to be
+	* set to success.
+	*/
+	void doSetValueCommand(const DeviceSetValueCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+	/**
+	* Reacts to UnpairCommand. Device has to be in map of
+	* virtual devices and it has to be paired.
+	*/
+	void doUnpairCommand(const DeviceUnpairCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+private:
+	std::map<DeviceID, VirtualDevice::Ptr> m_virtualDevicesMap;
+	std::string m_configFile;
+	std::priority_queue<VirtualDeviceEntry,
+		std::vector<VirtualDeviceEntry>,
+		VirtualDeviceEntryComparator> m_virtualDeviceQueue;
+	Poco::Event m_event;
+	bool m_requestDeviceList;
+	Poco::FastMutex m_lock;
+};
+
+}
+
+#endif

conf/config.d/logging-loggers.ini
@@ -40,3 +40,6 @@ loggers.MosquittoExporter.level = debug
 
 loggers.JablotronDeviceManager.name = BeeeOn::JablotronDeviceManager
 loggers.JablotronDeviceManager.level = debug
+
+loggers.VirtualDeviceManager.name = BeeeOn::VirtualDeviceManager
+loggers.VirtualDeviceManager.level = debug

src/CMakeLists.txt
@@ -74,6 +74,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualModule.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualDevice.cpp
+	${PROJECT_SOURCE_DIR}/vdev/VirtualDeviceManager.cpp
 )
 
 file(GLOB TESTING_SOURCES

src/vdev/VirtualDeviceManager.cpp
@@ -1,0 +1,507 @@
+#include <sstream>
+
+#include <Poco/NumberParser.h>
+#include <Poco/Util/AbstractConfiguration.h>
+
+#include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceSetValueCommand.h"
+#include "commands/DeviceUnpairCommand.h"
+#include "commands/GatewayListenCommand.h"
+#include "commands/NewDeviceCommand.h"
+
+#include "core/CommandDispatcher.h"
+#include "core/Distributor.h"
+#include "di/Injectable.h"
+#include "model/SensorData.h"
+#include "vdev/VirtualDeviceManager.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, VirtualDeviceManager)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_CASTABLE(CommandHandler)
+BEEEON_OBJECT_TEXT("file", &VirtualDeviceManager::setConfigFile)
+BEEEON_OBJECT_REF("distributor", &VirtualDeviceManager::setDistributor)
+BEEEON_OBJECT_REF("commandDispatcher", &VirtualDeviceManager::setCommandDispatcher)
+BEEEON_OBJECT_HOOK("done", &VirtualDeviceManager::installVirtualDevices)
+BEEEON_OBJECT_END(BeeeOn, VirtualDeviceManager)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Util;
+using namespace std;
+
+const static unsigned int DEFAULT_REFRESH_SECS = 30;
+
+VirtualDeviceManager::VirtualDeviceManager():
+	DeviceManager(DevicePrefix::PREFIX_VIRTUAL_DEVICE)
+{
+}
+
+void VirtualDeviceManager::registerDevice(
+	const VirtualDevice::Ptr device)
+{
+	if (!m_virtualDevicesMap.emplace(device->deviceID(), device).second) {
+		throw ExistsException("registering duplicate device: "
+			+ device->deviceID().toString());
+	}
+
+	logger().debug(
+		"registering new virtual device "
+		+ device->deviceID().toString(),
+		__FILE__, __LINE__
+	);
+}
+
+void VirtualDeviceManager::logDeviceParsed(VirtualDevice::Ptr device)
+{
+	logger().information(
+		"virtual device: "
+		+ device->deviceID().toString(),
+		__FILE__, __LINE__
+	);
+
+	logger().debug(
+		"virtual device: "
+		+ device->deviceID().toString()
+		+ ", modules: "
+		+ to_string(device->modules().size())
+		+ ", paired: "
+		+ (device->paired() ? "yes" : "no")
+		+ ", refresh: "
+		+ to_string(device->refresh().totalSeconds())
+		+ ", vendor: "
+		+ device->vendorName()
+		+ ", product: "
+		+ device->productName(),
+		__FILE__, __LINE__
+	);
+
+	for (auto &module : device->modules()) {
+		logger().trace(
+			"virtual device: "
+			+ device->deviceID().toString()
+			+ ", module: "
+			+ module->moduleID().toString()
+			+ ", type: "
+			+ module->moduleType().type().toString(),
+			__FILE__, __LINE__
+		);
+	}
+}
+
+VirtualDevice::Ptr VirtualDeviceManager::parseDevice(
+	AutoPtr <AbstractConfiguration> cfg)
+{
+	VirtualDevice::Ptr device = new VirtualDevice;
+
+	DeviceID id = DeviceID::parse(cfg->getString("device_id"));
+	if (id.prefix() != DevicePrefix::PREFIX_VIRTUAL_DEVICE) {
+		device->setDeviceId(
+			DeviceID(DevicePrefix::PREFIX_VIRTUAL_DEVICE, id.ident()));
+
+		logger().warning(
+			"device prefix was wrong, overriding ID to "
+			+ device->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+	}
+	else {
+		device->setDeviceId(id);
+	}
+
+	unsigned int refresh = cfg->getUInt("refresh", DEFAULT_REFRESH_SECS);
+	device->setRefresh(refresh * Timespan::SECONDS);
+
+	device->setPaired(cfg->getBool("paired", false));
+	device->setVendorName(cfg->getString("vendor"));
+	device->setProductName(cfg->getString("product"));
+
+	for (int i = 0; cfg->has("module" + to_string(i) + ".type"); ++i) {
+		try {
+			AutoPtr<AbstractConfiguration> view =
+				cfg->createView("module" + to_string(i));
+			device->addModule(parseModule(view, i));
+		}
+		catch (const Exception &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+			logger().critical(
+				"failed to initialize module "
+				+ id.toString(),
+				__FILE__, __LINE__
+			);
+			break;
+		}
+	}
+	logDeviceParsed(device);
+
+	return device;
+}
+
+VirtualModule::Ptr VirtualDeviceManager::parseModule(
+	AutoPtr<AbstractConfiguration> cfg,
+	const ModuleID &moduleID)
+{
+	ModuleType type = ModuleType::parse(cfg->getString("type"));
+	VirtualModule::Ptr virtualModule = new VirtualModule(type);
+
+	virtualModule->setModuleID(moduleID);
+	virtualModule->setMin(cfg->getDouble("min", 0));
+	virtualModule->setMax(cfg->getDouble("max", 100));
+	virtualModule->setGenerator(cfg->getString("generator", ""));
+	virtualModule->setReaction(cfg->getString("reaction", "none"));
+
+	return virtualModule;
+}
+
+void VirtualDeviceManager::setConfigFile(const string &file)
+{
+	m_configFile = file;
+}
+
+void VirtualDeviceManager::installVirtualDevices()
+{
+	logger().information("loading configuration from: " + m_configFile);
+	AutoPtr<AbstractConfiguration> cfg = new IniFileConfiguration(m_configFile);
+
+	m_requestDeviceList =
+		cfg->getBool("virtual-devices.request.device_list", true);
+
+	for (int i = 0; cfg->has("virtual-device" + to_string(i) + ".enable"); ++i) {
+		const string &prefix = "virtual-device" + to_string(i);
+
+		if (!cfg->getBool(prefix + ".enable", "no"))
+			continue;
+
+		try {
+			VirtualDevice::Ptr device = parseDevice(
+				cfg->createView(prefix));
+			registerDevice(device);
+		}
+		catch (const Exception &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+			logger().error(
+				"virtual device was not parsed or registered successfully",
+				__FILE__, __LINE__
+			);
+			continue;
+		}
+	}
+
+	logger().information(
+		"loaded "
+		+ to_string(m_virtualDevicesMap.size())
+		+ " virtual devices",
+		__FILE__, __LINE__
+	);
+}
+
+bool VirtualDeviceManager::accept(const Command::Ptr cmd)
+{
+	if (cmd->is<GatewayListenCommand>()) {
+		return true;
+	}
+	else if (cmd->is<DeviceSetValueCommand>()) {
+		auto it = m_virtualDevicesMap.find(
+			cmd->cast<DeviceSetValueCommand>().deviceID());
+		return it != m_virtualDevicesMap.end();
+	}
+	else if (cmd->is<DeviceUnpairCommand>()) {
+		auto it = m_virtualDevicesMap.find(
+			cmd->cast<DeviceUnpairCommand>().deviceID());
+		return it != m_virtualDevicesMap.end();
+	}
+	else if (cmd->is<DeviceAcceptCommand>()) {
+		return cmd->cast<DeviceAcceptCommand>().deviceID().prefix()
+			== DevicePrefix::PREFIX_VIRTUAL_DEVICE;
+	}
+
+	return false;
+}
+
+void VirtualDeviceManager::dispatchNewDevice(VirtualDevice::Ptr device)
+{
+	NewDeviceCommand::Ptr cmd = new NewDeviceCommand(
+		device->deviceID(),
+		device->vendorName(),
+		device->productName(),
+		device->moduleTypes(),
+		device->refresh());
+
+	dispatch(cmd);
+}
+
+void VirtualDeviceManager::doListenCommand(
+	const GatewayListenCommand::Ptr, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	for (auto &item : m_virtualDevicesMap) {
+		if (!item.second->paired())
+			dispatchNewDevice(item.second);
+	}
+
+	result->setStatus(Result::SUCCESS);
+}
+
+void VirtualDeviceManager::doDeviceAcceptCommand(
+	const DeviceAcceptCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_virtualDevicesMap.find(cmd->deviceID());
+	if (it == m_virtualDevicesMap.end()) {
+		logger().warning(
+			"no such device "
+			+ cmd->deviceID().toString()
+			+ " to accept",
+			__FILE__, __LINE__
+		);
+
+		result->setStatus(Result::SUCCESS);
+		return;
+	}
+
+	if (it->second->paired()) {
+		logger().warning(
+			"ignoring accept for paired device "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+	}
+
+	const VirtualDeviceEntry entry(it->second);
+	entry.device()->setPaired(true);
+	scheduleEntryUnlocked(entry);
+
+	result->setStatus(Result::SUCCESS);
+}
+
+void VirtualDeviceManager::doUnpairCommand(
+	const DeviceUnpairCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_virtualDevicesMap.find(cmd->deviceID());
+	if (it == m_virtualDevicesMap.end()) {
+		logger().warning(
+			"unpairing device that is not registered: "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+
+		result->setStatus(Result::FAILED);
+		return;
+	}
+
+	if (!it->second->paired()) {
+		logger().warning(
+			"unpairing device that is not paired: "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+	}
+
+	it->second->setPaired(false);
+
+	result->setStatus(Result::SUCCESS);
+}
+
+void VirtualDeviceManager::doSetValueCommand(
+	const DeviceSetValueCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_virtualDevicesMap.find(cmd->deviceID());
+	if (it == m_virtualDevicesMap.end()) {
+		logger().warning(
+			"failed to set value for non-existing device "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+
+		result->setStatus(Result::FAILED);
+		return;
+	}
+
+	for (auto &item : it->second->modules()) {
+		if (item->moduleID() == cmd->moduleID()) {
+			if (item->reaction() == VirtualModule::REACTION_NONE) {
+				logger().warning(
+					"value of module "
+					+ item->moduleType().type().toString()
+					+ " cannot be set");
+
+				result->setStatus(Result::FAILED);
+				return;
+			}
+		}
+	}
+
+	it->second->modifyValue(cmd->moduleID(), cmd->value(), result);
+	logger().debug(
+		"module "
+		+ cmd->moduleID().toString()
+		+ " is set to value "
+		+ to_string(cmd->value()),
+		__FILE__, __LINE__
+	);
+}
+
+void VirtualDeviceManager::handle(Command::Ptr cmd, Answer::Ptr answer)
+{
+	if (cmd->is<GatewayListenCommand>())
+		doListenCommand(cmd.cast<GatewayListenCommand>(), answer);
+	else if (cmd->is<DeviceSetValueCommand>())
+		doSetValueCommand(cmd.cast<DeviceSetValueCommand>(), answer);
+	else if (cmd->is<DeviceUnpairCommand>())
+		doUnpairCommand(cmd.cast<DeviceUnpairCommand>(), answer);
+	else if (cmd->is<DeviceAcceptCommand>())
+		doDeviceAcceptCommand(cmd.cast<DeviceAcceptCommand>(), answer);
+	else
+		throw IllegalStateException("received unaccepted command");
+}
+
+void VirtualDeviceManager::setPairedDevices()
+{
+	if (m_requestDeviceList) {
+		set<DeviceID> deviceIDs = deviceList(-1);
+
+		FastMutex::ScopedLock guard(m_lock);
+		for (auto &pair : m_virtualDevicesMap) {
+			if (deviceIDs.find(pair.first) != deviceIDs.end())
+				continue;
+
+			pair.second->setPaired(true);
+		}
+	}
+	else {
+		logger().information("skipping request of device list");
+	}
+}
+
+void VirtualDeviceManager::scheduleAllEntries()
+{
+	FastMutex::ScopedLock guard(m_lock);
+	for (auto &item : m_virtualDevicesMap) {
+		if (item.second->paired()) {
+			const VirtualDeviceEntry entry(item.second);
+			scheduleEntryUnlocked(VirtualDeviceEntry(item.second));
+		}
+	}
+}
+
+bool VirtualDeviceManager::isEmptyQueue()
+{
+	FastMutex::ScopedLock guard(m_lock);
+	return m_virtualDeviceQueue.empty();
+}
+
+void VirtualDeviceManager::run()
+{
+	setPairedDevices();
+	scheduleAllEntries();
+
+	while (!m_stop) {
+		if (isEmptyQueue()) {
+			logger().debug(
+				"empty queue of devices",
+				__FILE__, __LINE__);
+			m_event.wait();
+			continue;
+		}
+
+		ScopedLockWithUnlock<FastMutex> guard(m_lock);
+		const VirtualDeviceEntry entry = m_virtualDeviceQueue.top();
+
+		if (!entry.device()->paired()) {
+			logger().debug(
+				"unpaired device "
+				+ entry.device()->deviceID().toString()
+				+ " was removed from queue",
+				__FILE__, __LINE__);
+			m_virtualDeviceQueue.pop();
+			continue;
+		}
+
+		Timestamp now;
+		const Timestamp &activationTime = entry.activationTime();
+		const Timespan sleepTime(activationTime - now);
+		if (sleepTime.totalMilliseconds() > 0) {
+			guard.unlock();
+			logger().debug(
+				"device "
+				+ entry.device()->deviceID().toString()
+				+ " will be activated in "
+				+ to_string(sleepTime.totalMilliseconds())
+				+ " milliseconds",
+				__FILE__, __LINE__);
+			m_event.tryWait(sleepTime.totalMilliseconds());
+			continue;
+		}
+
+		m_virtualDeviceQueue.pop();
+
+		logger().debug(
+			"device "
+			+ entry.device()->deviceID().toString()
+			+ " is being processed",
+			__FILE__, __LINE__);
+
+		SensorData sensorData = entry.device()->generate();
+		if (!sensorData.isEmpty())
+			ship(entry.device()->generate());
+		else
+			poco_debug(logger(), "received empty SensorData");
+
+		scheduleEntryUnlocked(entry);
+		m_event.reset();
+	}
+}
+
+void VirtualDeviceManager::stop()
+{
+	m_stop = true;
+	m_event.set();
+}
+
+void VirtualDeviceManager::scheduleEntryUnlocked(VirtualDeviceEntry entry)
+{
+	entry.setInserted(Timestamp());
+	m_virtualDeviceQueue.push(entry);
+	m_event.set();
+}
+
+bool VirtualDeviceEntryComparator::lessThan(
+	const VirtualDeviceEntry &deviceQueue1,
+	const VirtualDeviceEntry &deviceQueue2) const
+{
+	return deviceQueue1.activationTime() > deviceQueue2.activationTime();
+}
+
+VirtualDeviceEntry::VirtualDeviceEntry(VirtualDevice::Ptr device)
+{
+	m_device = device;
+}
+
+void VirtualDeviceEntry::setInserted(const Timestamp &t)
+{
+	m_inserted = t;
+}
+
+Timestamp VirtualDeviceEntry::inserted() const
+{
+	return m_inserted;
+}
+
+VirtualDevice::Ptr VirtualDeviceEntry::device() const
+{
+	return m_device;
+}
+
+Timestamp VirtualDeviceEntry::activationTime() const
+{
+	return inserted() + device()->refresh();
+}

commit 0612e55ce62f43775f5de4d34e21695cb46d86c3

VirtualDeviceManager: fix cross references for doxygen
The exact names of commands are used in doc comments to make possible
for doxygen to generate proper hyperlinks.

Fixes: cbe5f26cfb84 ("VirtualDeviceManager: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vdev/VirtualDeviceManager.h
@@ -81,9 +81,9 @@ public:
  *
  * It also ensures reaction to commands sent from server:
  *
- * ListenCommand, DeviceAcceptCommand - device attempts to pair
- * SetValueCommand - modification of module value
- * UnpairCommand - device attempts to unpair
+ * - GatewayListenCommand, DeviceAcceptCommand - device attempts to pair
+ * - DeviceSetValueCommand - modification of module value
+ * - DeviceUnpairCommand - device attempts to unpair
  */
 class VirtualDeviceManager : public DeviceManager {
 public:
@@ -162,14 +162,14 @@ protected:
 	void handle(Command::Ptr cmd, Answer::Ptr answer) override;
 
 	/**
-	 * Reacts to ListenCommand. It sends NewDeviceCommand if
+	 * Reacts to GatewayListenCommand. It sends NewDeviceCommand if
 	 * device is not paired.
 	 */
 	void doListenCommand(const GatewayListenCommand::Ptr,
 		const Answer::Ptr answer);
 
 	/**
-	 * Reacts to AcceptCommand. Device has to be stored in map
+	 * Reacts to DeviceAcceptCommand. Device has to be stored in map
 	 * of virtual devices and it has to be unpaired. If these conditions
 	 * are fulfilled, method inserts device into a calendar, it sets device
 	 * as paired and it plans next activation (data generation) of this device.
@@ -178,7 +178,7 @@ protected:
 		const Answer::Ptr answer);
 
 	/**
-	* Reacts to SetValueCommand. Device has to be in map of
+	* Reacts to DeviceSetValueCommand. Device has to be in map of
 	* virtual devices, it has to be sensor and reaction has to be
 	* set to success.
 	*/
@@ -186,7 +186,7 @@ protected:
 		const Answer::Ptr answer);
 
 	/**
-	* Reacts to UnpairCommand. Device has to be in map of
+	* Reacts to DeviceUnpairCommand. Device has to be in map of
 	* virtual devices and it has to be paired.
 	*/
 	void doUnpairCommand(const DeviceUnpairCommand::Ptr cmd,