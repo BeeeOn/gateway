BeeeOn/gateway

commit 5941bf91c6ccaa2419782652e3503aeb339ab382

BelkinWemoSwitch: new class
The class represents Belkin WeMo Switch F7C027fr.
Provides functions to control the switch.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/belkin/BelkinWemoSwitch.h
@@ -1,0 +1,115 @@
+#ifndef GATEWAY_BELKIN_WEMO_SWITCH_H
+#define GATEWAY_BELKIN_WEMO_SWITCH_H
+
+#include <list>
+#include <string>
+
+#include <Poco/DOM/NodeFilter.h>
+#include <Poco/DOM/NodeIterator.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Timespan.h>
+#include <Poco/URI.h>
+
+#include "net/MACAddress.h"
+#include "model/ModuleType.h"
+#include "model/DeviceID.h"
+#include "model/SensorData.h"
+#include "net/HTTPEntireResponse.h"
+
+namespace BeeeOn {
+
+/*
+ * @brief The class represents Belkin WeMo Switch F7C027fr.
+ * Provides functions to control the switch. It means turn on, turn off, get state.
+ */
+class BelkinWemoSwitch {
+public:
+	BelkinWemoSwitch();
+	~BelkinWemoSwitch();
+
+	/*
+	 * @brief Creates belkin wemo switch. If the device is not on network
+	 * throws Poco::TimeoutException also in this case it is blocking.
+	 * @param &address IP address and port where the device is listening.
+	 * @param &timeout HTTP timeout.
+	 * @return Belkin WeMo Switch.
+	 */
+	static BelkinWemoSwitch buildDevice(const Poco::Net::SocketAddress& address,
+		const Poco::Timespan& timeout);
+
+	/*
+	 * @brief It sets the switch to the given state.
+	 */
+	bool requestModifyState(const ModuleID& moduleID, const bool value);
+
+	/*
+	 * @brief Prepares SOAP message containing request state
+	 * command and sends it to device via HTTP. If the device is
+	 * not on network throws Poco::TimeoutException also in this
+	 * case it is blocking. Request current state of the device
+	 * and return it as SensorData.
+	 * @return SensorData.
+	 */
+	SensorData requestState() const;
+
+	DeviceID deviceID() const;
+	Poco::Net::SocketAddress address() const;
+	void setAddress(const Poco::Net::SocketAddress& address);
+	std::list<ModuleType> moduleTypes() const;
+	std::string name() const;
+
+	/*
+	 * @brief It compares two switches based on DeviceID.
+	 */
+	bool operator==(const BelkinWemoSwitch& bws) const;
+
+protected:
+	BelkinWemoSwitch(const Poco::Net::SocketAddress& address);
+
+	/*
+	 * @brief Prepares SOAP message containing turn on command
+	 * and sends it to device via HTTP. If the device is not on
+	 * network throws Poco::TimeoutException also in this case
+	 * it is blocking.
+	 * @return If the request was successful or not.
+	 */
+	bool turnOn() const;
+
+	/*
+	 * @brief Prepares SOAP message containing turn off command
+	 * and sends it to device via HTTP. If the device is not on
+	 * network throws Poco::TimeoutException also in this case
+	 * it is blocking.
+	 * @return If the request was successful or not.
+	 */
+	bool turnOff() const;
+
+	/*
+	 * @brief Prepares SOAP message containing request Mac address
+	 * command and sends it to device via HTTP. If the device is not
+	 * on network throws Poco::TimeoutException also in this case
+	 * it is blocking. Request mac address of the device.
+	 * @return Mac address of the device.
+	 */
+	MACAddress requestMacAddr();
+
+	/*
+	 * @brief Called internally when constructing the instance.
+	 * Creates DeviceID based on Mac address of device.
+	 */
+	void buildDeviceID();
+
+	Poco::XML::Node* findNode(Poco::XML::NodeIterator& iterator, const std::string& name) const;
+	HTTPEntireResponse sendHTTPRequest(Poco::Net::HTTPRequest& request, const std::string& msg,
+		const Poco::Timespan& timeout) const;
+
+private:
+	DeviceID m_deviceId;
+	Poco::URI m_uri;
+	Poco::Timespan m_httpTimeout;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -13,6 +13,7 @@ find_library (PTHREAD pthread)
 find_library (UDEV udev)
 
 file(GLOB SOURCES
+	${PROJECT_SOURCE_DIR}/belkin/BelkinWemoSwitch.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceAcceptCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceSetValueCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceUnpairCommand.cpp

src/belkin/BelkinWemoSwitch.cpp
@@ -1,0 +1,327 @@
+#include <Poco/AutoPtr.h>
+#include <Poco/DOM/AutoPtr.h>
+#include <Poco/DOM/Document.h>
+#include <Poco/Exception.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/NumberParser.h>
+#include <Poco/RegularExpression.h>
+#include <Poco/SAX/AttributesImpl.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/XML/XMLWriter.h>
+
+#include "belkin/BelkinWemoSwitch.h"
+#include "model/DevicePrefix.h"
+#include "net/SOAPMessage.h"
+#include "util/SecureXmlParser.h"
+
+#define BELKIN_SWITCH_NAME "Switch F7C027fr"
+#define BELKIN_SWITCH_STATE_ON 1
+#define BELKIN_SWITCH_STATE_OFF 0
+#define BELKIN_SWITCH_MODULE_ID ModuleID(0)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::XML;
+using namespace std;
+
+BelkinWemoSwitch::BelkinWemoSwitch(const SocketAddress& address):
+	m_uri("http://" + address.toString() + "/upnp/control/basicevent1")
+{
+}
+
+BelkinWemoSwitch::BelkinWemoSwitch()
+{
+}
+
+BelkinWemoSwitch::~BelkinWemoSwitch()
+{
+}
+
+BelkinWemoSwitch BelkinWemoSwitch::buildDevice(const SocketAddress& address, const Timespan& timeout)
+{
+	BelkinWemoSwitch device(address);
+	device.m_httpTimeout = timeout;
+	device.buildDeviceID();
+	return device;
+}
+
+void BelkinWemoSwitch::buildDeviceID()
+{
+	m_deviceId = DeviceID(DevicePrefix::PREFIX_BELKIN_WEMO, requestMacAddr());
+}
+
+bool BelkinWemoSwitch::requestModifyState(const ModuleID& moduleID, const bool value)
+{
+	if (moduleID != BELKIN_SWITCH_MODULE_ID)
+		return false;
+
+	if (value)
+		return turnOn();
+	else
+		return turnOff();
+}
+
+bool BelkinWemoSwitch::turnOn() const
+{
+	HTTPRequest request;
+
+	SOAPMessage msg;
+	msg.setAction("\"urn:Belkin:service:basicevent:1#SetBinaryState\"");
+
+	AttributesImpl attrs;
+	XMLWriter& writer = msg.bodyWriter();
+
+	attrs.addAttribute(
+		"",
+		"xmlns:u",
+		"xmlns",
+		"string",
+		"urn:Belkin:service:basicevent:1");
+	writer.startElement(
+		"",
+		"u:SetBinaryState",
+		"SetBinaryState",
+		attrs);
+
+	writer.startElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+	writer.characters(to_string(BELKIN_SWITCH_STATE_ON));
+
+	writer.endElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+	writer.endElement(
+		"",
+		"u:SetBinaryState",
+		"SetBinaryState");
+
+	msg.prepare(request);
+
+	HTTPEntireResponse response = sendHTTPRequest(request, msg.toString(), m_httpTimeout);
+
+	SecureXmlParser parser;
+	AutoPtr<Document> xmlDoc = parser.parse(response.getBody());
+	NodeIterator iterator(xmlDoc, NodeFilter::SHOW_ALL);
+	Node* xmlNode = findNode(iterator, "BinaryState");
+
+	return xmlNode->nodeValue() == "1";
+}
+
+bool BelkinWemoSwitch::turnOff() const
+{
+	HTTPRequest request;
+
+	SOAPMessage msg;
+	msg.setAction("\"urn:Belkin:service:basicevent:1#SetBinaryState\"");
+
+	AttributesImpl attrs;
+	XMLWriter& writer = msg.bodyWriter();
+
+	attrs.addAttribute(
+		"",
+		"xmlns:u",
+		"xmlns",
+		"string",
+		"urn:Belkin:service:basicevent:1");
+	writer.startElement(
+		"",
+		"u:SetBinaryState",
+		"SetBinaryState",
+		attrs);
+
+	writer.startElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+	writer.characters(to_string(BELKIN_SWITCH_STATE_OFF));
+
+	writer.endElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+	writer.endElement(
+		"",
+		"u:SetBinaryState",
+		"SetBinaryState");
+
+	msg.prepare(request);
+
+	HTTPEntireResponse response = sendHTTPRequest(request, msg.toString(), m_httpTimeout);
+
+	SecureXmlParser parser;
+	AutoPtr<Document> xmlDoc = parser.parse(response.getBody());
+	NodeIterator iterator(xmlDoc, NodeFilter::SHOW_ALL);
+	Node* xmlNode = findNode(iterator, "BinaryState");
+
+	return xmlNode->nodeValue() == "0";
+}
+
+SensorData BelkinWemoSwitch::requestState() const
+{
+	HTTPRequest request;
+
+	SOAPMessage msg;
+	msg.setAction("\"urn:Belkin:service:basicevent:1#GetBinaryState\"");
+
+	AttributesImpl attrs;
+	XMLWriter& writer = msg.bodyWriter();
+
+	attrs.addAttribute(
+		"",
+		"xmlns:u",
+		"xmlns",
+		"string",
+		"urn:Belkin:service:basicevent:1");
+	writer.startElement(
+		"",
+		"u:GetBinaryState",
+		"GetBinaryState",
+		attrs);
+
+	writer.startElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+
+	writer.endElement(
+		"",
+		"BinaryState",
+		"BinaryState");
+	writer.endElement(
+		"",
+		"u:GetBinaryState",
+		"GetBinaryState");
+
+	msg.prepare(request);
+
+	HTTPEntireResponse response = sendHTTPRequest(request, msg.toString(), m_httpTimeout);
+
+	SecureXmlParser parser;
+	AutoPtr<Document> xmlDoc = parser.parse(response.getBody());
+	NodeIterator iterator(xmlDoc, NodeFilter::SHOW_ALL);
+	Node* xmlNode = findNode(iterator, "BinaryState");
+
+	SensorData data;
+	data.setDeviceID(m_deviceId);
+	if (xmlNode->nodeValue() == "1")
+		data.insertValue(SensorValue(BELKIN_SWITCH_MODULE_ID, BELKIN_SWITCH_STATE_ON));
+	else
+		data.insertValue(SensorValue(BELKIN_SWITCH_MODULE_ID, BELKIN_SWITCH_STATE_OFF));
+
+	return data;
+}
+
+MACAddress BelkinWemoSwitch::requestMacAddr()
+{
+	HTTPRequest request;
+
+	SOAPMessage msg;
+	msg.setAction("\"urn:Belkin:service:basicevent:1#GetMacAddr\"");
+
+	AttributesImpl attrs;
+	XMLWriter& writer = msg.bodyWriter();
+
+	attrs.addAttribute(
+		"",
+		"xmlns:u",
+		"xmlns",
+		"string",
+		"urn:Belkin:service:basicevent:1");
+	writer.startElement(
+		"",
+		"u:GetMacAddr",
+		"GetMacAddr",
+		attrs);
+
+	writer.endElement(
+		"",
+		"u:GetMacAddr",
+		"GetMacAddr");
+
+	msg.prepare(request);
+
+	HTTPEntireResponse response = sendHTTPRequest(request, msg.toString(), m_httpTimeout);
+
+	SecureXmlParser parser;
+	AutoPtr<Document> xmlDoc = parser.parse(response.getBody());
+	NodeIterator iterator(xmlDoc, NodeFilter::SHOW_ALL);
+	Node* xmlNode = findNode(iterator, "MacAddr");
+
+	return MACAddress(NumberParser::parseHex64(xmlNode->nodeValue()));
+}
+
+Node* BelkinWemoSwitch::findNode(NodeIterator& iterator, const string& name) const
+{
+	Node* xmlNode = iterator.nextNode();
+
+	while (xmlNode) {
+		if (xmlNode->nodeName() == name) {
+			xmlNode = iterator.nextNode();
+			return xmlNode;
+		}
+
+		xmlNode = iterator.nextNode();
+	}
+
+	throw SyntaxException("node " + name + " in XML message from belkin device not found");
+}
+
+DeviceID BelkinWemoSwitch::deviceID() const
+{
+	return m_deviceId;
+}
+
+SocketAddress BelkinWemoSwitch::address() const
+{
+	return SocketAddress(m_uri.getHost(), m_uri.getPort());
+}
+
+void BelkinWemoSwitch::setAddress(const SocketAddress& address)
+{
+	m_uri = URI("http://" + address.toString() + "/upnp/control/basicevent1");
+}
+
+list<ModuleType> BelkinWemoSwitch::moduleTypes() const
+{
+	std::list<ModuleType> moduleTypes;
+	moduleTypes.push_back(
+		ModuleType(
+			ModuleType::Type::TYPE_ON_OFF, std::set<ModuleType::Attribute>()
+		)
+	);
+
+	return moduleTypes;
+}
+
+string BelkinWemoSwitch::name() const
+{
+	return BELKIN_SWITCH_NAME;
+}
+
+bool BelkinWemoSwitch::operator==(const BelkinWemoSwitch& bws) const
+{
+	return bws.deviceID() == m_deviceId;
+}
+
+HTTPEntireResponse BelkinWemoSwitch::sendHTTPRequest(HTTPRequest& request, const string& msg, const Timespan& timeout) const
+{
+	HTTPClientSession http;
+	HTTPEntireResponse response;
+
+	http.setHost(m_uri.getHost());
+	http.setPort(m_uri.getPort());
+	http.setTimeout(timeout);
+
+	request.setURI(m_uri.toString());
+
+	http.sendRequest(request) << msg;
+	istream& input = http.receiveResponse(response);
+	response.readBody(input);
+
+	return response;
+}

commit d88d11f91875dd8cb430fca6842367dfc5151644

belkin: fix doc comments
For documentation purposes the comments must start with /**.

Fixes: 5941bf91c6cc ("BelkinWemoSwitch: new class")
Fixes: f27fca187305 ("BelkinWemoDeviceManager: new class")

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/belkin/BelkinWemoDeviceManager.h
@@ -21,7 +21,7 @@ namespace BeeeOn {
 
 class BelkinWemoDeviceManager : public DeviceManager {
 public:
-	/*
+	/**
 	 * @brief Provides searching belkin wemo devices on network
 	 * in own thread.
 	 */
@@ -56,22 +56,22 @@ protected:
 	void refreshPairedDevices();
 	void searchPairedDevices();
 
-	/*
+	/**
 	 * @brief Processes the listen command.
 	 */
 	void doListenCommand(const Command::Ptr cmd, const Answer::Ptr answer);
 
-	/*
+	/**
 	 * @brief Processes the unpair command.
 	 */
 	void doUnpairCommand(const Command::Ptr cmd, const Answer::Ptr answer);
 
-	/*
+	/**
 	 * @brief Processes the device accept command.
 	 */
 	void doDeviceAcceptCommand(const Command::Ptr cmd, const Answer::Ptr answer);
 
-	/*
+	/**
 	 * @brief Sets the proper device's module to given value.
 	 * @return If the setting was successfull or not.
 	 */

src/belkin/BelkinWemoSwitch.h
@@ -19,7 +19,7 @@
 
 namespace BeeeOn {
 
-/*
+/**
  * @brief The class represents Belkin WeMo Switch F7C027fr.
  * Provides functions to control the switch. It means turn on, turn off, get state.
  */
@@ -28,7 +28,7 @@ public:
 	BelkinWemoSwitch();
 	~BelkinWemoSwitch();
 
-	/*
+	/**
 	 * @brief Creates belkin wemo switch. If the device is not on network
 	 * throws Poco::TimeoutException also in this case it is blocking.
 	 * @param &address IP address and port where the device is listening.
@@ -38,12 +38,12 @@ public:
 	static BelkinWemoSwitch buildDevice(const Poco::Net::SocketAddress& address,
 		const Poco::Timespan& timeout);
 
-	/*
+	/**
 	 * @brief It sets the switch to the given state.
 	 */
 	bool requestModifyState(const ModuleID& moduleID, const bool value);
 
-	/*
+	/**
 	 * @brief Prepares SOAP message containing request state
 	 * command and sends it to device via HTTP. If the device is
 	 * not on network throws Poco::TimeoutException also in this
@@ -59,7 +59,7 @@ public:
 	std::list<ModuleType> moduleTypes() const;
 	std::string name() const;
 
-	/*
+	/**
 	 * @brief It compares two switches based on DeviceID.
 	 */
 	bool operator==(const BelkinWemoSwitch& bws) const;
@@ -67,7 +67,7 @@ public:
 protected:
 	BelkinWemoSwitch(const Poco::Net::SocketAddress& address);
 
-	/*
+	/**
 	 * @brief Prepares SOAP message containing turn on command
 	 * and sends it to device via HTTP. If the device is not on
 	 * network throws Poco::TimeoutException also in this case
@@ -76,7 +76,7 @@ protected:
 	 */
 	bool turnOn() const;
 
-	/*
+	/**
 	 * @brief Prepares SOAP message containing turn off command
 	 * and sends it to device via HTTP. If the device is not on
 	 * network throws Poco::TimeoutException also in this case
@@ -85,7 +85,7 @@ protected:
 	 */
 	bool turnOff() const;
 
-	/*
+	/**
 	 * @brief Prepares SOAP message containing request Mac address
 	 * command and sends it to device via HTTP. If the device is not
 	 * on network throws Poco::TimeoutException also in this case
@@ -94,7 +94,7 @@ protected:
 	 */
 	MACAddress requestMacAddr();
 
-	/*
+	/**
 	 * @brief Called internally when constructing the instance.
 	 * Creates DeviceID based on Mac address of device.
 	 */