BeeeOn/gateway

commit d710e03706ce8400845652e014d87473000b8de1

CSVSensorDataFormatter: formatter to csv
Convert SensorData to csv format for output.
Extension for SensorDataFormatter.

Signed-off-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/CSVSensorDataFormatter.h
@@ -1,0 +1,46 @@
+#ifndef BEEEON_CSV_SENSOR_DATA_FORMATTER_H
+#define BEEEON_CSV_SENSOR_DATA_FORMATTER_H
+
+#include <string>
+
+#include "SensorDataFormatter.h"
+
+namespace BeeeOn {
+
+class SensorData;
+
+class CSVSensorDataFormatter : public SensorDataFormatter {
+public:
+	CSVSensorDataFormatter();
+
+	/**
+	 * Convert data from struct SensorData to csv format
+	 * @return type;timestamp;deviceID;moduleID;value;
+	 * @example sensor;1488879656;0x499602d2;5;4.200000;
+	 */
+	std::string format(const SensorData &data) override;
+
+	/**
+	 * Optional custom separator
+	 */
+	void setSeparator(const std::string &separator)
+	{
+		m_separator = separator;
+	}
+
+	/**
+	 * @brief separator
+	 * @return actual separator for csv
+	 */
+	const std::string separator() const
+	{
+		return m_separator;
+	}
+
+private:
+	std::string m_separator;
+};
+
+}
+
+#endif // BEEEON_CSV_SENSOR_DATA_FORMATTER_H

conf/config.d/factory-exporter.xml
@@ -1,0 +1,7 @@
+<system>
+	<factory>
+		<instance name="CSVSensorDataFormatter" class="BeeeOn::CSVSensorDataFormatter">
+			<set name="separator" text="${Exporter.pipe.csv.separator}" />
+		</instance>
+	</factory>
+</system>

conf/gateway-startup.ini
@@ -1,2 +1,5 @@
 [application]
 di.runner = main
+
+[exporter]
+pipe.csv.separator = ;

src/CMakeLists.txt
@@ -16,6 +16,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
+	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 )
 

src/util/CSVSensorDataFormatter.cpp
@@ -1,0 +1,41 @@
+#include <string>
+
+#include <Poco/NumberFormatter.h>
+#include <Poco/Timestamp.h>
+
+#include "CSVSensorDataFormatter.h"
+#include "di/Injectable.h"
+#include "model/SensorData.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, CSVSensorDataFormatter)
+BEEEON_OBJECT_CASTABLE(SensorDataFormatter)
+BEEEON_OBJECT_TEXT("separator", &CSVSensorDataFormatter::setSeparator)
+BEEEON_OBJECT_END(BeeeOn, CSVSensorDataFormatter)
+
+#define DEFAULT_SEPARATOR ";"
+#define PRECISION_OF_VALUE 2
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+CSVSensorDataFormatter::CSVSensorDataFormatter() :
+	m_separator(DEFAULT_SEPARATOR)
+{
+}
+
+string CSVSensorDataFormatter::format(const SensorData &data)
+{
+	string output = "";
+	string device = data.deviceID().toString();
+	string timestamp = to_string(data.timestamp().value().epochTime());
+
+	for (auto item : data) {
+		output += "sensor" + separator() + timestamp + separator() + device + separator();
+		output += item.moduleID().toString() + separator();
+		output += NumberFormatter::format(item.value(), PRECISION_OF_VALUE) + separator();
+		output += "\n";
+	}
+
+	return output;
+}

commit 04e1ab43b0022dc2fab11e23e6bfc2d0cf6d31d9

CSVSensorDataFormatter: fix doc comment
The tag @example has a different purpose than assumed.

Fixes: d710e03706ce ("CSVSensorDataFormatter: formatter to csv")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/CSVSensorDataFormatter.h
@@ -16,7 +16,9 @@ public:
 	/**
 	 * Convert data from struct SensorData to csv format
 	 * @return type;timestamp;deviceID;moduleID;value;
-	 * @example sensor;1488879656;0x499602d2;5;4.200000;
+	 *
+	 * Example output:
+	 * <pre>sensor;1488879656;0x499602d2;5;4.200000;</pre>
 	 */
 	std::string format(const SensorData &data) override;
 