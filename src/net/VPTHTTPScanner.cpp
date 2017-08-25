BeeeOn/gateway

commit bae6b7aa6cfe3ef23f740dfe85cf73ad7688954d

VPTHTTPScanner: new class
The class provides searching of VPT devices (Regulator VPT LAN v1.0).

Poco JSON library was added to test CMakeLists.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/VPTHTTPScanner.cpp
@@ -1,0 +1,69 @@
+#include <Poco/Logger.h>
+
+#include "net/VPTHTTPScanner.h"
+#include "util/JsonUtil.h"
+
+#define VPT_VENDOR "Thermona"
+#define VPT_DEVICE "Regulator VPT LAN v1.0"
+#define VPT_VERSION 2016021100
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace Poco::Net;
+using namespace std;
+
+VPTHTTPScanner::VPTHTTPScanner(const string& path, UInt16 port, const IPAddress& minNetMask):
+	AbstractHTTPScanner(path, port, minNetMask)
+{
+}
+
+VPTHTTPScanner::~VPTHTTPScanner()
+{
+}
+
+void VPTHTTPScanner::prepareRequest(Poco::Net::HTTPRequest& request)
+{
+	request.setMethod(HTTPRequest::HTTP_GET);
+	request.setURI(path());
+}
+
+bool VPTHTTPScanner::isValidResponse(const string& response)
+{
+	logger().debug(response, __FILE__, __LINE__);
+
+	string vendor;
+	string device;
+	int version;
+	try {
+		Object::Ptr object = JsonUtil::parse(response);
+
+		vendor = object->getValue<string>("vendor");
+		device = object->getValue<string>("device");
+		version = object->getValue<int>("version");
+	}
+	catch (Exception& e) {
+		if (logger().debug())
+			logger().log(e, __FILE__, __LINE__);
+		return false;
+	}
+
+	if (vendor != VPT_VENDOR) {
+		logger().warning("unrecognized vendor " + vendor, __FILE__, __LINE__);
+		return false;
+	}
+
+	if (device != VPT_DEVICE) {
+		logger().warning("unrecognized device type " + device, __FILE__, __LINE__);
+		return false;
+	}
+
+	if (version != VPT_VERSION) {
+		logger().warning("unrecognized version " + to_string(version), __FILE__, __LINE__);
+		return false;
+	}
+
+	logger().notice("found device" + vendor + " " + device, __FILE__, __LINE__);
+
+	return true;
+}

conf/config.d/logging-loggers.ini
@@ -20,5 +20,8 @@ loggers.UPnP.level = debug
 loggers.VirtualSensor.name = BeeeOn::VirtualSensor
 loggers.VirtualSensor.level = debug
 
+loggers.VPTHTTPScanner.name = BeeeOn::VPTHTTPScanner
+loggers.VPTHTTPScanner.level = debug
+
 loggers.TestingCenter.name = BeeeOn::TestingCenter
 loggers.TestingCenter.level = debug

src/CMakeLists.txt
@@ -45,6 +45,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/net/AbstractHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
+	${PROJECT_SOURCE_DIR}/net/VPTHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp

src/net/VPTHTTPScanner.h
@@ -1,0 +1,23 @@
+#ifndef GATEWAY_VPT_HTTP_SCANNER_H
+#define GATEWAY_VPT_HTTP_SCANNER_H
+
+#include <Poco/Net/IPAddress.h>
+#include <Poco/Net/SocketAddress.h>
+
+#include "net/AbstractHTTPScanner.h"
+
+namespace BeeeOn {
+
+class VPTHTTPScanner : public AbstractHTTPScanner {
+public:
+	VPTHTTPScanner(const std::string& path, Poco::UInt16 port, const Poco::Net::IPAddress& minNetMask);
+	~VPTHTTPScanner();
+
+protected:
+	void prepareRequest(Poco::Net::HTTPRequest& request) override;
+	bool isValidResponse(const std::string& response) override;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -7,6 +7,7 @@ find_library (POCO_UTIL PocoUtil)
 find_library (POCO_SSL PocoNetSSL)
 find_library (POCO_CRYPTO PocoCrypto)
 find_library (POCO_NET PocoNet)
+find_library (POCO_JSON PocoJSON)
 find_library (POCO_XML PocoXML)
 find_library (PTHREAD pthread)
 
@@ -37,6 +38,7 @@ set(LIBS
 	${POCO_CRYPTO}
 	${POCO_UTIL}
 	${POCO_NET}
+	${POCO_JSON}
 	${POCO_XML}
 	${CPP_UNIT}
 	${PTHREAD}

commit f1e842580d06612438fe3152636facbbe130c40a

VPTHTTPScanner: fix log message
Fixes: bae6b7aa6cfe ("VPTHTTPScanner: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/VPTHTTPScanner.cpp
@@ -67,7 +67,7 @@ bool VPTHTTPScanner::isValidResponse(const string& response)
 		return false;
 	}
 
-	logger().notice("found device" + vendor + " " + device, __FILE__, __LINE__);
+	logger().notice("found device " + vendor + " " + device, __FILE__, __LINE__);
 
 	return true;
 }