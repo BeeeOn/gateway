BeeeOn/gateway

commit 747ea2d73635d355993074cd40bb5a4f1c387eb7

ExporterQueue: FailDetector extracted
FailDetector is extracted from ExporterQueue and queue is now using it.

Also adapted ExporterQueueTest to work with ExporterQueue using new FailDetector.
ExporterQueue::failsTreshold 0 was equal to 1 in the previous state,
now 0 is equal to inf, so all 0 failsTresholds are now changed to 1.

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/ExporterQueue.h
@@ -9,6 +9,7 @@
 #include "core/Exporter.h"
 #include "model/SensorData.h"
 #include "util/Loggable.h"
+#include "util/FailDetector.h"
 
 namespace BeeeOn {
 
@@ -18,12 +19,12 @@ public:
 
 	const static int UNLIMITED_BATCH_SIZE = 0;
 	const static int UNLIMITED_CAPACITY = 0;
-	const static int UNLIMITED_THRESHOLD = -1;
+	const static int UNLIMITED_THRESHOLD = FailDetector::TRESHOLD_UNLIMITED;
 
 	/**
 	 * If batchSize <= 0 then size of batch is unlimited.
 	 * If capacity <= 0 then data count is unlimited.
-	 * If treshold < 0 then treshold is unlimited.
+	 * If treshold <= 0 then treshold is unlimited.
 	 */
 	ExporterQueue(
 		Poco::SharedPtr<Exporter> exporter,
@@ -69,15 +70,11 @@ private:
 
 	Poco::AtomicCounter m_dropped;
 	Poco::AtomicCounter m_sent;
-	int m_fails;
-	int m_treshold;
 
+	FailDetector m_failDetector;
 	std::queue<SensorData> m_queue;
 	unsigned int m_capacity;
 	unsigned int m_batchSize;
-
-	Poco::Timestamp m_timeOfFailure;
-	Poco::AtomicCounter m_working;
 };
 
 }

src/core/ExporterQueue.cpp
@@ -16,12 +16,9 @@ ExporterQueue::ExporterQueue(
 	m_exporter(exporter),
 	m_dropped(0),
 	m_sent(0),
-	m_fails(0),
-	m_treshold(treshold),
+	m_failDetector(treshold),
 	m_capacity(capacity),
-	m_batchSize(batchSize),
-	m_timeOfFailure(Poco::Timestamp()),
-	m_working(true)
+	m_batchSize(batchSize)
 {
 }
 
@@ -60,45 +57,25 @@ unsigned int ExporterQueue::exportBatch()
 		}
 	}
 	catch (const Exception &e) {
-		fail();
+		m_failDetector.fail();
 		logger().log(e, __FILE__, __LINE__);
 		return i;
 	}
 	catch (exception &e) {
-		fail();
+		m_failDetector.fail();
 		poco_critical(logger(), e.what());
 		return i;
 	}
 	catch (...) {
-		fail();
+		m_failDetector.fail();
 		poco_critical(logger(), "unknown error occured while shipping data");
 		return i;
 	}
 
-	if (i > 0) {
-		m_working = true;
-		m_fails = 0;
-	}
+	if (i > 0)
+		m_failDetector.success();
 
 	return i;
-}
-
-void ExporterQueue::fail()
-{
-	logger().debug("ship has failed");
-
-	if (m_treshold < 0)
-		return;
-
-	if (working()) {
-		if (++m_fails >= m_treshold) {
-			m_working = false;
-			m_fails = 0;
-		}
-	}
-	else {
-		m_timeOfFailure = Timestamp();
-	}
 }
 
 
@@ -112,12 +89,12 @@ bool ExporterQueue::canExport(const Timespan &deadTimeout) const
 
 bool ExporterQueue::working() const
 {
-	return m_working;
+	return !m_failDetector.isFailed();
 }
 
 bool ExporterQueue::deadTooLong(const Timespan deadTimeout) const
 {
-	return working() || m_timeOfFailure.isElapsed(deadTimeout.totalMicroseconds());
+	return working() || m_failDetector.lastFailBefore(deadTimeout);
 }
 
 bool ExporterQueue::isEmpty() const

test/core/ExporterQueueTest.cpp
@@ -100,7 +100,7 @@ void ExporterQueueTest::testExportOk()
 {
 	SharedPtr<Exporter> exporter = new QueueTestingExporter;
 
-	ExporterQueue queue(exporter, 10, 20, 0);
+	ExporterQueue queue(exporter, 10, 20, 1);
 
 	DeviceID id01(0x1111222233334444UL);
 
@@ -152,7 +152,7 @@ void ExporterQueueTest::testQueueOverloaded()
 {
 	SharedPtr<Exporter> exporter = new QueueTestingExporter;
 
-	ExporterQueue queue(exporter, 10, 20, 0);
+	ExporterQueue queue(exporter, 10, 20, 1);
 
 	DeviceID id01(0x1111222233334444UL);
 
@@ -252,13 +252,13 @@ void ExporterQueueTest::testExporterBroken()
 /**
  * The test vertifies that when the used exporter fails to export data
  * because it is full, it does not affect queue working status.
- * Treshold is set to 0 for this test.
+ * Treshold is set to 1 for this test.
  */
 void ExporterQueueTest::testExporterFull()
 {
 	SharedPtr<Exporter> exporter = new QueueTestingExporter(&QueueTestingExporter::shipFull);
 
-	ExporterQueue queue(exporter, 10, 20, 0);
+	ExporterQueue queue(exporter, 10, 20, 1);
 
 	SensorData data;
 	DeviceID id(0x1111222233334444UL);

commit 6331bc1d422aff8e52228914395af8ec8db8f717

ExporterQueue: fix not implemented of fail() method
Fixes: 747ea2d7("ExporterQueue: FailDetector extracted")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/ExporterQueue.h
@@ -57,7 +57,6 @@ private:
 	*/
 	bool deadTooLong(const Poco::Timespan deadTimeout) const;
 
-	void fail();
 	bool isEmpty() const;
 
 	SensorData &front();