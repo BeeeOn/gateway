BeeeOn/gateway

commit 08016d6a4fddea789bd61c6213753b50edf23277

VirtualModule: new class
Virtual module is part of virtual device. It is defined by module
identifier. It has type and attributes alternatively. It is able to
generate values in various way (constant, random and sinus generator are
supported) and in various range. Value can be also set to virtual module.

Types of module
* Module is called sensor if generator is defined and reaction is set
to none.
* Module is called actuator if generator is not enabled and reaction
is set to success or fail.
* Combination of mentioned setting is allowed but it is not supported
within the system.

Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vdev/VirtualModule.h
@@ -1,0 +1,65 @@
+#ifndef BEEEON_VIRTUAL_MODULE_H
+#define BEEEON_VIRTUAL_MODULE_H
+
+#include <list>
+
+#include <Poco/Nullable.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/Result.h"
+#include "model/ModuleID.h"
+#include "model/ModuleType.h"
+#include "util/Loggable.h"
+#include "util/ValueGenerator.h"
+
+namespace BeeeOn {
+
+class VirtualModule {
+public:
+	typedef Poco::SharedPtr<VirtualModule> Ptr;
+
+	enum Reaction {
+		REACTION_SUCCESS,
+		REACTION_FAILURE,
+		REACTION_NONE,
+	};
+
+	VirtualModule(const ModuleType &moduleType);
+	~VirtualModule();
+
+	void setModuleID(const ModuleID &moduleID);
+	ModuleID moduleID() const;
+
+	void setModuleType(const ModuleType &moduleType);
+	ModuleType moduleType() const;
+
+	void setMin(double min);
+	double min() const;
+
+	void setMax(double max);
+	double max() const;
+
+	void modifyValue(double value, Result::Ptr result);
+
+	void setGenerator(const std::string &generator);
+	Poco::SharedPtr<ValueGenerator> generator() const;
+	bool generatorEnabled() const;
+
+	SensorValue generate();
+
+	void setReaction(const std::string &reaction);
+	Reaction reaction() const;
+
+private:
+	ModuleID m_moduleID;
+	ModuleType m_moduleType;
+	Poco::Nullable<double> m_min;
+	Poco::Nullable<double> m_max;
+	std::list<Poco::SharedPtr<ValueGenerator>> m_generators;
+	Reaction m_reaction;
+	double m_value;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -72,6 +72,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
+	${PROJECT_SOURCE_DIR}/vdev/VirtualModule.cpp
 )
 
 file(GLOB TESTING_SOURCES

src/vdev/VirtualModule.cpp
@@ -1,0 +1,132 @@
+#include <string>
+
+#include <Poco/NumberParser.h>
+#include <Poco/Timespan.h>
+
+#include "model/SensorData.h"
+#include "vdev/VirtualModule.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+VirtualModule::VirtualModule(const ModuleType &moduleType):
+	m_moduleType(moduleType)
+{
+}
+
+VirtualModule::~VirtualModule()
+{
+}
+
+SensorValue VirtualModule::generate()
+{
+	return SensorValue(moduleID(), m_generators.back()->next());
+}
+
+void VirtualModule::setMin(double min)
+{
+	m_min = min;
+}
+
+double VirtualModule::min() const
+{
+	return m_min.value();
+}
+
+void VirtualModule::setMax(double max)
+{
+	m_max = max;
+}
+
+double VirtualModule::max() const
+{
+	return m_max.value();
+}
+
+void VirtualModule::setGenerator(const string &generator)
+{
+	if (generator.empty()) {
+		return;
+	}
+	else if (generator == "random") {
+		m_generators.clear();
+		m_generators.emplace_back(new RandomGenerator);
+		m_generators.emplace_back(new RangeGenerator(*m_generators.front(), min(), max()));
+	}
+	else if (generator == "sin") {
+		m_generators.clear();
+		m_generators.emplace_back(new SinGenerator);
+		m_generators.emplace_back(new RangeGenerator(*m_generators.front(), min(), max()));
+	}
+	else {
+		double tmp;
+		if (NumberParser::tryParseFloat(generator, tmp)) {
+			m_generators.clear();
+			m_generators.emplace_back(new ConstGenerator(tmp));
+		}
+		else {
+			throw InvalidArgumentException("invalid generator value: " + generator);
+		}
+	}
+}
+
+bool VirtualModule::generatorEnabled() const
+{
+	return !m_generators.empty();
+}
+
+void VirtualModule::setReaction(const string &reaction)
+{
+	if (reaction == "success")
+		m_reaction = VirtualModule::REACTION_SUCCESS;
+	else if (reaction == "failure")
+		m_reaction = VirtualModule::REACTION_FAILURE;
+	else if (reaction == "none")
+		m_reaction = VirtualModule::REACTION_NONE;
+	else
+		throw InvalidArgumentException("invalid reaction: " + reaction);
+}
+
+VirtualModule::Reaction VirtualModule::reaction() const
+{
+	return m_reaction;
+}
+
+void VirtualModule::setModuleType(const ModuleType &moduleType)
+{
+	m_moduleType = moduleType;
+}
+
+ModuleType VirtualModule::moduleType() const
+{
+	return m_moduleType;
+}
+
+void VirtualModule::setModuleID(const ModuleID &moduleID)
+{
+	m_moduleID = moduleID;
+}
+
+ModuleID VirtualModule::moduleID() const
+{
+	return m_moduleID;
+}
+
+void VirtualModule::modifyValue(double value, Result::Ptr result)
+{
+	if (reaction() == REACTION_SUCCESS) {
+		m_value = value;
+		result->setStatus(Result::SUCCESS);
+	}
+	else if (reaction() == REACTION_FAILURE) {
+		result->setStatus(Result::FAILED);
+	}
+	else {
+		throw IllegalStateException(
+			"module "
+			+ m_moduleID.toString()
+			+ " cannot be set"
+		);
+	}
+}

commit 4c7a807dccb5b6e7374b0a147666deb2dce631a5

VirtualModule: fix not implemented of generator() method
Fixes: 08016d6a ("VirtualModule: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vdev/VirtualModule.h
@@ -42,7 +42,6 @@ public:
 	void modifyValue(double value, Result::Ptr result);
 
 	void setGenerator(const std::string &generator);
-	Poco::SharedPtr<ValueGenerator> generator() const;
 	bool generatorEnabled() const;
 
 	SensorValue generate();