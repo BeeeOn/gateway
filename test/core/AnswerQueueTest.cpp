BeeeOn/gateway

commit 36878d427f74400f660c8c90d27459e7bafd6d23

Answer: new class
The CommandDispatcher provides 1-to-many communication pattern.
Each instance of the Answer class holds one or more results.
The results are inserted into the Answer instance early before
the particular CommandHandlers are invoked. Thus, we know in advance,
how many results we will expect during the asynchronous execution
of multiple different CommandHandler::handle() executions.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/Answer.cpp
@@ -1,0 +1,181 @@
+#include <vector>
+
+#include <Poco/Observer.h>
+
+#include "core/Answer.h"
+#include "core/AnswerQueue.h"
+#include "core/CommandRunner.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+Answer::Answer(AnswerQueue &answerQueue):
+	m_answerQueue(answerQueue),
+	m_dirty(0),
+	m_taskManager(Poco::ThreadPool::defaultPool())
+{
+	answerQueue.add(this);
+	installObservers();
+}
+
+void Answer::setDirty(bool dirty)
+{
+	FastMutex::ScopedLock guard(lock());
+	setDirtyUnlocked(dirty);
+}
+
+void Answer::setDirtyUnlocked(bool dirty)
+{
+	assureLocked();
+	m_dirty = dirty;
+}
+
+bool Answer::isDirty() const
+{
+	FastMutex::ScopedLock guard(lock());
+	return isDirtyUnlocked();
+}
+
+bool Answer::isDirtyUnlocked() const
+{
+	assureLocked();
+	return (bool) m_dirty;
+}
+
+Event &Answer::event()
+{
+	return m_answerQueue.event();
+}
+
+FastMutex &Answer::lock() const
+{
+	return m_lock;
+}
+
+bool Answer::isPending() const
+{
+	FastMutex::ScopedLock guard(m_lock);
+	return isPendingUnlocked();
+}
+
+bool Answer::isPendingUnlocked() const
+{
+	assureLocked();
+
+	if ((unsigned long) m_commands != m_resultList.size())
+		return true;
+
+	if (m_commands == 0)
+		return false;
+
+	for (auto &result : m_resultList) {
+		if (result->statusUnlocked() == Result::PENDING)
+			return true;
+	}
+
+	return false;
+}
+
+void Answer::runCommands()
+{
+	assureLocked();
+
+	for (auto item : m_commandList) {
+		m_taskManager.start(item);
+	}
+
+	m_commandList.clear();
+}
+
+void Answer::installObservers()
+{
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskFinishedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onFinished)
+	);
+
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskFailedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onFailed)
+	);
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskStartedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onStarted)
+	);
+
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskCancelledNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onCancel)
+	);
+}
+
+bool Answer::isEmpty() const
+{
+	return m_commands == 0;
+}
+
+unsigned long Answer::resultsCount() const
+{
+	FastMutex::ScopedLock guard(m_lock);
+	return m_resultList.size();
+}
+
+void Answer::addCommand(Poco::SharedPtr<CommandHandler> handler,
+	Command::Ptr cmd, Answer::Ptr answer)
+{
+	FastMutex::ScopedLock guard(m_lock);
+	m_commandList.push_back(new CommandRunner(cmd, answer, handler));
+	m_commands++;
+}
+
+void Answer::addResult(Result *result)
+{
+	FastMutex::ScopedLock guard(m_lock);
+	m_resultList.push_back(AutoPtr<Result>(result, true));
+}
+
+int Answer::commandsCount() const
+{
+	FastMutex::ScopedLock guard(m_lock);
+	return m_commands;
+}
+
+void Answer::assureLocked() const
+{
+	if (lock().tryLock()) {
+		lock().unlock();
+		throw IllegalStateException(
+			"modifying unlocked Answer");
+	}
+}
+
+void Answer::notifyUpdated()
+{
+	assureLocked();
+	setDirtyUnlocked(true);
+	event().set();
+}
+
+Result::Ptr Answer::at(size_t position)
+{
+	FastMutex::ScopedLock guard(m_lock);
+	return atUnlocked(position);
+}
+
+Result::Ptr Answer::atUnlocked(size_t position)
+{
+	assureLocked();
+	return m_resultList.at(position);
+}
+
+std::vector<Result::Ptr>::iterator Answer::begin()
+{
+	assureLocked();
+	return m_resultList.begin();
+}
+
+std::vector<Result::Ptr>::iterator Answer::end()
+{
+	assureLocked();
+	return m_resultList.end();
+}

src/CMakeLists.txt
@@ -12,6 +12,7 @@ find_library (PTHREAD pthread)
 
 file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/AbstractDistributor.cpp
+	${PROJECT_SOURCE_DIR}/core/Answer.cpp
 	${PROJECT_SOURCE_DIR}/core/BasicDistributor.cpp
 	${PROJECT_SOURCE_DIR}/core/Command.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandHandler.cpp

src/core/Answer.h
@@ -1,0 +1,120 @@
+#ifndef BEEEON_ANSWER_H
+#define BEEEON_ANSWER_H
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/RefCountedObject.h>
+#include <Poco/Task.h>
+#include <Poco/TaskManager.h>
+
+#include "Command.h"
+#include "core/CommandProgressHandler.h"
+#include "core/Result.h"
+
+namespace BeeeOn {
+
+class AnswerQueue;
+class Command;
+class CommandDispatcher;
+class CommandHandler;
+class Result;
+
+/*
+ * During the Answer creation the queue is set. The queue is notified using
+ * the event() in the case of status change of the dirty.
+ * The change of the status in Result causes this notification.
+ *
+ * The Answer and the Result share the common mutex. The operations that
+ * change the status in the Answer and in the Result MUST be locked.
+ */
+class Answer : public Poco::RefCountedObject {
+	friend CommandDispatcher;
+public:
+	typedef Poco::AutoPtr<Answer> Ptr;
+
+	Answer(AnswerQueue &answerQueue);
+
+	/*
+	 * All reference counted objects should have a protected destructor,
+	 * to forbid explicit use of delete.
+	 */
+	Answer(const Answer&) = delete;
+
+	/*
+	 * The status that informs about the change of a Result.
+	 */
+	void setDirty(bool dirty);
+	void setDirtyUnlocked(bool dirty);
+
+	bool isDirty() const;
+	bool isDirtyUnlocked() const;
+
+	/*
+	 * The check if the Result are in the terminal state (SUCCESS/ERROR).
+	 */
+	bool isPending() const;
+	bool isPendingUnlocked() const;
+
+	Poco::Event &event();
+	Poco::FastMutex &lock() const;
+
+	/*
+	 * True if the list of commands is empty.
+	 */
+	bool isEmpty() const;
+
+	unsigned long resultsCount() const;
+	int commandsCount() const;
+
+	void addResult(Result *result);
+
+	/*
+	 * Notifies the waiting queue that this Answer isEmpty().
+	 * The call sets Answer::setDirtyUnlocked(true).
+	 */
+	void notifyUpdated();
+
+	Result::Ptr at(size_t position);
+	Result::Ptr atUnlocked(size_t position);
+
+	std::vector<Result::Ptr>::iterator begin();
+	std::vector<Result::Ptr>::iterator end();
+
+protected:
+	/*
+	 * The check if the operation is locked.
+	 */
+	void assureLocked() const;
+
+	/*
+	 * Run all commands from the Answer.
+	 */
+	void runCommands();
+
+	/*
+	 * Adds the command for running and also creates the result
+	 * for a given result.
+	 */
+	void addCommand(Poco::SharedPtr<CommandHandler> handler,
+		Command::Ptr cmd, Answer::Ptr answer);
+
+	/*
+	 * Registers an observer with the NotificationCenter.
+	 */
+	void installObservers();
+
+private:
+	AnswerQueue &m_answerQueue;
+	Poco::AtomicCounter m_dirty;
+	mutable Poco::FastMutex m_lock;
+	std::vector<Result::Ptr> m_resultList;
+	std::vector<Poco::Task *> m_commandList;
+	Poco::TaskManager m_taskManager;
+	CommandProgressHandler m_commandProgressHandler;
+	Poco::AtomicCounter m_commands;
+};
+
+}
+
+#endif

commit 850529a5678321bc32f14a47845604011ba64c83

Answer: fix missing set of m_handlers default value
Fixes: 36878d42 ("Answer: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/Answer.cpp
@@ -8,7 +8,8 @@ using namespace Poco;
 
 Answer::Answer(AnswerQueue &answerQueue):
 	m_answerQueue(answerQueue),
-	m_dirty(0)
+	m_dirty(0),
+	m_handlers(0)
 {
 	answerQueue.add(this);
 }

test/core/AnswerQueueTest.cpp
@@ -24,6 +24,7 @@ class AnswerQueueTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testDisposeAnswerWithoutResult);
 	CPPUNIT_TEST(testSetResultAfterLock);
 	CPPUNIT_TEST(testCreateAnswerAfterLock);
+	CPPUNIT_TEST(testDisposeUnusedAnswer);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -35,6 +36,7 @@ public:
 	void testDisposeAnswerWithoutResult();
 	void testSetResultAfterLock();
 	void testCreateAnswerAfterLock();
+	void testDisposeUnusedAnswer();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(AnswerQueueTest);
@@ -433,6 +435,22 @@ void AnswerQueueTest::testCreateAnswerAfterLock()
 
 	CPPUNIT_ASSERT_THROW(queue.newAnswer(), IllegalStateException);
 	CPPUNIT_ASSERT_THROW(new Answer(queue), IllegalStateException);
+}
+
+void AnswerQueueTest::testDisposeUnusedAnswer()
+{
+	TestableAnswerQueue queue;
+	Answer::Ptr answer = queue.newAnswer();
+
+	CPPUNIT_ASSERT(1 == queue.size());
+	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->resultsCount());
+	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->handlersCount());
+
+	queue.dispose();
+
+	CPPUNIT_ASSERT(1 == queue.size());
+	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->resultsCount());
+	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->handlersCount());
 }
 
 }