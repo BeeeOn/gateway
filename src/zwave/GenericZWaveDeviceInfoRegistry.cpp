BeeeOn/gateway

commit 8f533cd4d0a32c2a7ad7804678badc9942d56175

GenericZWaveDeviceInfoRegistry: new class
It enables to register of individual vendors that select a product.
If no vendor is found, default register is used.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/zwave/GenericZWaveDeviceInfoRegistry.cpp
@@ -1,0 +1,54 @@
+#include <string>
+
+#include "di/Injectable.h"
+#include "zwave/GenericZWaveDeviceInfoRegistry.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+BEEEON_OBJECT_BEGIN(BeeeOn, GenericZWaveDeviceInfoRegistry)
+BEEEON_OBJECT_CASTABLE(ZWaveDeviceInfoRegistry)
+BEEEON_OBJECT_REF("registers", &GenericZWaveDeviceInfoRegistry::registerVendor)
+BEEEON_OBJECT_REF("defaultRegistry", &GenericZWaveDeviceInfoRegistry::registerDefault)
+BEEEON_OBJECT_END(BeeeOn, GenericZWaveDeviceInfoRegistry)
+
+ZWaveDeviceInfo::Ptr GenericZWaveDeviceInfoRegistry::find(
+		uint32_t vendor, uint32_t product)
+{
+	auto search = m_vendors.find(vendor);
+	if (search != m_vendors.end())
+		return search->second->find(vendor, product);
+
+	logger().debug(
+		+ "vendor " + to_string(vendor)
+		+ " is not registered",
+		__FILE__, __LINE__);
+
+	if (m_defaultVendor.isNull()) {
+		throw IllegalStateException(
+			"default ZWaveDeviceInfoRegistry is not registered");
+	}
+
+	return m_defaultVendor->find(vendor, product);
+}
+
+void GenericZWaveDeviceInfoRegistry::registerVendor(
+	VendorZWaveDeviceInfoRegistry::Ptr factory)
+{
+	auto it = m_vendors.emplace(factory->vendor(), factory);
+
+	if (!it.second) {
+		throw ExistsException(
+			"vendor "
+			+ to_string(factory->vendor())
+			+ " is already registered"
+		);
+	}
+}
+
+void GenericZWaveDeviceInfoRegistry::registerDefault(
+	ZWaveDeviceInfoRegistry::Ptr factory)
+{
+	m_defaultVendor = factory;
+}

conf/config.d/factory.xml
@@ -113,5 +113,8 @@
 			<set name="distributor" ref="distributor" />
 			<set name="commandDispatcher" ref="commandDispatcher" />
 		</instance>
+
+		<instance name="zwaveDeviceInfoRegistry" class="BeeeOn::GenericZWaveDeviceInfoRegistry">
+		</instance>
 	</factory>
 </system>

src/CMakeLists.txt
@@ -81,6 +81,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/vdev/VirtualModule.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualDevice.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualDeviceManager.cpp
+	${PROJECT_SOURCE_DIR}/zwave/GenericZWaveDeviceInfoRegistry.cpp
 	${PROJECT_SOURCE_DIR}/zwave/VendorZWaveDeviceInfoRegistry.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveClassRegistry.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveDeviceInfo.cpp

src/zwave/GenericZWaveDeviceInfoRegistry.h
@@ -1,0 +1,33 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/SharedPtr.h>
+
+#include "zwave/VendorZWaveDeviceInfoRegistry.h"
+#include "zwave/ZWaveDeviceInfoRegistry.h"
+
+namespace BeeeOn {
+
+class GenericZWaveDeviceInfoRegistry:
+	public ZWaveDeviceInfoRegistry,
+	public Loggable {
+public:
+	ZWaveDeviceInfo::Ptr find(
+		uint32_t vendor, uint32_t product) override;
+
+	/**
+	 * Register Z-Wave vendor to factory using vendor id.
+	 * @param factory vendor object
+	 */
+	void registerVendor(
+		VendorZWaveDeviceInfoRegistry::Ptr factory);
+
+	void registerDefault(ZWaveDeviceInfoRegistry::Ptr factory);
+
+private:
+	std::map<uint32_t, VendorZWaveDeviceInfoRegistry::Ptr> m_vendors;
+	ZWaveDeviceInfoRegistry::Ptr m_defaultVendor;
+};
+
+}

commit 00c3d99635d2922df5e2b55eb26c70405a8ff7b0

GenericZWaveDeviceInfoRegistry: fix reduntant '+' character
Fixes: 8f533cd4 ("GenericZWaveDeviceInfoRegistry: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/zwave/GenericZWaveDeviceInfoRegistry.cpp
@@ -21,7 +21,7 @@ ZWaveDeviceInfo::Ptr GenericZWaveDeviceInfoRegistry::find(
 		return search->second->find(vendor, product);
 
 	logger().debug(
-		+ "vendor " + to_string(vendor)
+		"vendor " + to_string(vendor)
 		+ " is not registered",
 		__FILE__, __LINE__);
 