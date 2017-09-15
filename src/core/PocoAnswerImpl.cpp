BeeeOn/gateway

commit e9d8f2492fbe4610b6665534def6834d747ec00c

PocoAnswerImpl: extract poco-specific details
To decouple poco-specific logic from the Answer class, the
PocoAnswerImpl is provided here. Later, the PocoAnswerImpl is
generalized even more to make the Answer independent on the backend
implementation.

Tasks are now managed by AutoPtr. The PocoAnswerImpl is an owner of all
its tasks. We can avoid clearing the task list - it is now valid all the
time until the PocoAnswerImpl is destroyed.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/PocoAnswerImpl.cpp
@@ -1,0 +1,55 @@
+#include <Poco/Observer.h>
+
+#include "core/CommandRunner.h"
+#include "core/PocoAnswerImpl.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+
+PocoAnswerImpl::PocoAnswerImpl():
+	m_taskManager(Poco::ThreadPool::defaultPool())
+{
+}
+
+void PocoAnswerImpl::runTasks()
+{
+	for (auto &item : m_taskList)
+		m_taskManager.start(item);
+}
+
+void PocoAnswerImpl::installObservers()
+{
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskFinishedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onFinished)
+	);
+
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskFailedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onFailed)
+	);
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskStartedNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onStarted)
+	);
+
+	m_taskManager.addObserver(
+		Observer<CommandProgressHandler, Poco::TaskCancelledNotification>
+			(m_commandProgressHandler, &CommandProgressHandler::onCancel)
+	);
+}
+
+void PocoAnswerImpl::addTask(Poco::SharedPtr<CommandHandler> handler, Command::Ptr cmd, Answer::Ptr answer)
+{
+	// Increment reference counter of the runner. Thus, TaskManager
+	// would not destroy it and the destruction is invoked in the
+	// destructor of PocoAnswerImpl.
+	AutoPtr<CommandRunner> ptr(new CommandRunner(cmd, answer, handler), true);
+	m_taskList.push_back(ptr);
+}
+
+unsigned long PocoAnswerImpl::tasks() const
+{
+	return m_taskList.size();
+}

src/CMakeLists.txt
@@ -27,6 +27,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/CommandProgressHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandRunner.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
+	${PROJECT_SOURCE_DIR}/core/PocoAnswerImpl.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoCommandDispatcher.cpp
 	${PROJECT_SOURCE_DIR}/core/Result.cpp
 	${PROJECT_SOURCE_DIR}/core/VirtualSensor.cpp

src/core/Answer.cpp
@@ -1,21 +1,16 @@
 #include <vector>
 
-#include <Poco/Observer.h>
-
 #include "core/Answer.h"
 #include "core/AnswerQueue.h"
-#include "core/CommandRunner.h"
 
 using namespace BeeeOn;
 using namespace Poco;
 
 Answer::Answer(AnswerQueue &answerQueue):
 	m_answerQueue(answerQueue),
-	m_dirty(0),
-	m_taskManager(Poco::ThreadPool::defaultPool())
+	m_dirty(0)
 {
 	answerQueue.add(this);
-	installObservers();
 }
 
 void Answer::setDirty(bool dirty)
@@ -76,39 +71,6 @@ bool Answer::isPendingUnlocked() const
 	return false;
 }
 
-void Answer::runCommands()
-{
-	assureLocked();
-
-	for (auto item : m_commandList) {
-		m_taskManager.start(item);
-	}
-
-	m_commandList.clear();
-}
-
-void Answer::installObservers()
-{
-	m_taskManager.addObserver(
-		Observer<CommandProgressHandler, Poco::TaskFinishedNotification>
-			(m_commandProgressHandler, &CommandProgressHandler::onFinished)
-	);
-
-	m_taskManager.addObserver(
-		Observer<CommandProgressHandler, Poco::TaskFailedNotification>
-			(m_commandProgressHandler, &CommandProgressHandler::onFailed)
-	);
-	m_taskManager.addObserver(
-		Observer<CommandProgressHandler, Poco::TaskStartedNotification>
-			(m_commandProgressHandler, &CommandProgressHandler::onStarted)
-	);
-
-	m_taskManager.addObserver(
-		Observer<CommandProgressHandler, Poco::TaskCancelledNotification>
-			(m_commandProgressHandler, &CommandProgressHandler::onCancel)
-	);
-}
-
 bool Answer::isEmpty() const
 {
 	return m_commands == 0;
@@ -123,14 +85,6 @@ unsigned long Answer::resultsCount() const
 unsigned long Answer::resultsCountUnlocked() const
 {
 	return m_resultList.size();
-}
-
-void Answer::addCommand(Poco::SharedPtr<CommandHandler> handler,
-	Command::Ptr cmd, Answer::Ptr answer)
-{
-	FastMutex::ScopedLock guard(m_lock);
-	m_commandList.push_back(new CommandRunner(cmd, answer, handler));
-	m_commands++;
 }
 
 void Answer::addResult(Result *result)

src/core/Answer.h
@@ -90,31 +90,11 @@ protected:
 	 */
 	void assureLocked() const;
 
-	/*
-	 * Run all commands from the Answer.
-	 */
-	void runCommands();
-
-	/*
-	 * Adds the command for running and also creates the result
-	 * for a given result.
-	 */
-	void addCommand(Poco::SharedPtr<CommandHandler> handler,
-		Command::Ptr cmd, Answer::Ptr answer);
-
-	/*
-	 * Registers an observer with the NotificationCenter.
-	 */
-	void installObservers();
-
 private:
 	AnswerQueue &m_answerQueue;
 	Poco::AtomicCounter m_dirty;
 	mutable Poco::FastMutex m_lock;
 	std::vector<Result::Ptr> m_resultList;
-	std::vector<Poco::Task *> m_commandList;
-	Poco::TaskManager m_taskManager;
-	CommandProgressHandler m_commandProgressHandler;
 	Poco::AtomicCounter m_commands;
 };
 

src/core/CommandRunner.cpp
@@ -14,5 +14,9 @@ CommandRunner::CommandRunner(Command::Ptr cmd, Answer::Ptr answer,
 
 void CommandRunner::runTask()
 {
+	/*
+	 * The TaskManager takes ownership of the Task object
+	 * and deletes it when it it finished.
+	 */
 	m_handler->handle(m_cmd, m_answer);
 }

src/core/PocoAnswerImpl.h
@@ -1,0 +1,51 @@
+#ifndef BEEEON_POCO_ANSWER_IMPL_H
+#define BEEEON_POCO_ANSWER_IMPL_H
+
+#include <Poco/AutoPtr.h>
+#include <Poco/RefCountedObject.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/Answer.h"
+#include "core/Command.h"
+
+namespace BeeeOn {
+
+class CommandHandler;
+
+class PocoAnswerImpl {
+public:
+	typedef Poco::SharedPtr<PocoAnswerImpl> Ptr;
+
+	PocoAnswerImpl();
+
+	/**
+	 * Run all commands from the Answer.
+	 */
+	void runTasks();
+
+	/**
+	 * Registers an observer with the NotificationCenter.
+	 */
+	void installObservers();
+
+	/**
+	 * @return number of tasks to be executed
+	 */
+	unsigned long tasks() const;
+
+	/**
+	 * Creates a new Poco::Task for the given arguments. The task
+	 * implements calling the associated handler in a separate thread.
+	 */
+	void addTask(Poco::SharedPtr<CommandHandler> handler,
+		Command::Ptr cmd, Answer::Ptr answer);
+
+private:
+	Poco::TaskManager m_taskManager;
+	CommandProgressHandler m_commandProgressHandler;
+	std::vector<Poco::AutoPtr<Poco::Task>> m_taskList;
+};
+
+}
+
+#endif

commit 30d1adb19881effbbd868653c03cc02dc32c8c64

PocoAnswerImpl: fix missing header file
Fixes: e9d8f249 ("PocoAnswerImpl: extract poco-specific details")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/PocoAnswerImpl.cpp
@@ -1,4 +1,5 @@
 #include <Poco/Observer.h>
+#include <Poco/ThreadPool.h>
 
 #include "core/CommandRunner.h"
 #include "core/PocoAnswerImpl.h"