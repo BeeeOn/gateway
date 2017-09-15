BeeeOn/gateway

commit 9b7507bf6b3ccb5747aae5d02cc7bbee4dd8f2de

AbstractHTTPScanner: new class
The abstract class contains core of HTTP scanning
of network. Derivated classes will have to implement
methods to prepare HTTP request and if the response
is from correct device.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/AbstractHTTPScanner.cpp
@@ -1,0 +1,200 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/URI.h>
+
+#include "net/AbstractHTTPScanner.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace std;
+
+AbstractHTTPScanner::AbstractHTTPScanner(const string& path, UInt16 port, const IPAddress& minNetMask):
+	m_path(path),
+	m_port(port),
+	m_minNetMask(minNetMask)
+{
+}
+
+AbstractHTTPScanner::~AbstractHTTPScanner()
+{
+}
+
+void AbstractHTTPScanner::setPingTimeout(const Poco::Timespan& pingTimeout)
+{
+	m_pingTimeout = pingTimeout;
+}
+
+void AbstractHTTPScanner::setHTTPTimeout(const Poco::Timespan& httpTimeout)
+{
+	m_httpTimeout = httpTimeout;
+}
+
+void AbstractHTTPScanner::setBlackList(const set<string>& blackList)
+{
+	m_blackList = blackList;
+}
+
+string AbstractHTTPScanner::path()
+{
+	return m_path;
+}
+
+UInt16 AbstractHTTPScanner::port()
+{
+	return m_port;
+}
+
+vector<SocketAddress> AbstractHTTPScanner::scan(const uint32_t maxResponseLength)
+{
+	vector<NetworkInterface> listOfNetworkInterfaces = listNetworkInterfaces();
+	vector<SocketAddress> devices;
+
+	for (auto &interface : listOfNetworkInterfaces)
+		probeInterface(interface, devices, maxResponseLength);
+
+	if (devices.empty())
+		logger().notice("no device found", __FILE__, __LINE__);
+
+	return devices;
+}
+
+void AbstractHTTPScanner::probeInterface(const NetworkInterface& interface,
+	vector<SocketAddress>& devices,
+	const Int64 maxResponseLength)
+{
+	logger().notice("probing interface " + interface.adapterName(),
+		__FILE__, __LINE__);
+
+	for (auto &addressTuple : interface.addressList()) {
+		if (addressIncompatible(addressTuple)) {
+			logger().debug("incompatible address " + addressTuple.get<NetworkInterface::IP_ADDRESS>().toString());
+			continue;
+		}
+
+		IPAddress networkAddress = addressTuple.get<NetworkInterface::IP_ADDRESS>();
+		IPAddress netMask = addressTuple.get<NetworkInterface::SUBNET_MASK>();
+		networkAddress.mask(netMask);
+
+		if (netMask < m_minNetMask) {
+			netMask = m_minNetMask;
+			logger().warning("truncate scanning range to: " + m_minNetMask.toString(),
+				__FILE__, __LINE__);
+		}
+
+		IPAddressRange range(networkAddress, netMask);
+		probeAddressRange(range, devices, maxResponseLength);
+	}
+}
+
+void AbstractHTTPScanner::probeAddressRange(const IPAddressRange& range,
+	vector<SocketAddress>& devices,
+	const Int64 maxResponseLength)
+{
+	StreamSocket ping;
+	IPAddress ip;
+
+	for (auto& ip : range) {
+		SocketAddress socketAddress(ip, m_port);
+
+		try {
+			ping.connect(socketAddress, m_pingTimeout);
+		}
+		catch (Exception& e) {
+			ping.close();
+			continue;
+		}
+
+		ping.close();
+		logger().debug("service detected at " + ip.toString() + ":" + to_string(m_port));
+
+		HTTPEntireResponse response;
+		try {
+			response = sendRequest(socketAddress, maxResponseLength);
+		}
+		catch (TimeoutException& e) {
+			logger().debug("timeout expired", __FILE__, __LINE__);
+			continue;
+		}
+		catch (Exception& e) {
+			if (logger().debug())
+				logger().log(e, __FILE__, __LINE__);
+			continue;
+		}
+
+		if (response.getStatus() != 200) {
+			logger().warning("drop response " + to_string(response.getStatus()), __FILE__, __LINE__);
+			continue;
+		}
+
+		if (isValidResponse(response.getBody()))
+			devices.push_back(socketAddress);
+	}
+}
+
+HTTPEntireResponse AbstractHTTPScanner::sendRequest(const SocketAddress& socketAddress, const Int64 maxResponseLength)
+{
+	HTTPClientSession http;
+	HTTPEntireResponse response;
+	HTTPRequest request;
+
+	http.setHost(socketAddress.host().toString());
+	http.setPort(socketAddress.port());
+	http.setTimeout(m_httpTimeout);
+
+	prepareRequest(request);
+
+	logger().information("request: " + socketAddress.toString() + request.getURI(), __FILE__, __LINE__);
+
+	http.sendRequest(request);
+
+	istream &input = http.receiveResponse(response);
+
+	if (response.getContentLength64() > maxResponseLength)
+		throw RangeException("too long response");
+
+	response.readBody(input);
+	const int status = response.getStatus();
+	if (status >= 400)
+		logger().warning("response: " + to_string(status), __FILE__, __LINE__);
+	else
+		logger().information("response: " + to_string(status), __FILE__, __LINE__);
+
+	return response;
+}
+
+bool AbstractHTTPScanner::addressIncompatible(const NetworkInterface::AddressTuple& addressTuple) const
+{
+	const auto &family = addressTuple.get<NetworkInterface::IP_ADDRESS>().family();
+	return family != IPAddress::Family::IPv4 || addressTuple.length != 3;
+}
+
+vector<NetworkInterface> AbstractHTTPScanner::listNetworkInterfaces()
+{
+	vector<NetworkInterface> list;
+	NetworkInterface::NetworkInterfaceList listOfNetworkInterfaces = NetworkInterface::list();
+
+	for (auto &interface : listOfNetworkInterfaces) {
+		if (interface.isPointToPoint() || interface.isLoopback()) {
+			logger().debug("auto skipping interface " + interface.adapterName());
+			continue;
+		}
+
+		if (m_blackList.find(interface.adapterName()) != m_blackList.end()) {
+			logger().information("skipping blacklisted interface " + interface.adapterName());
+			continue;
+		}
+
+		list.push_back(interface);
+	}
+
+	if (list.empty())
+		logger().warning("no valid interface found", __FILE__, __LINE__);
+
+	return list;
+}

conf/config.d/logging-loggers.ini
@@ -2,6 +2,9 @@
 loggers.root.channel = console
 loggers.root.level = information
 
+loggers.AbstractHTTPScanner.name = BeeeOn::AbstractHTTPScanner
+loggers.AbstractHTTPScanner.level = debug
+
 loggers.Application.name = Application
 loggers.Application.level = notice
 

src/CMakeLists.txt
@@ -42,6 +42,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
+	${PROJECT_SOURCE_DIR}/net/AbstractHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp

src/net/AbstractHTTPScanner.h
@@ -1,0 +1,108 @@
+#ifndef GATEWAY_ABSTRACT_HTTP_SCANNER_H
+#define GATEWAY_ABSTRACT_HTTP_SCANNER_H
+
+#include <set>
+
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/IPAddress.h>
+#include <Poco/Net/NetworkInterface.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Timespan.h>
+
+#include "net/HTTPEntireResponse.h"
+#include "net/IPAddressRange.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/*
+ * @brief The abstract class contains core of HTTP scanning
+ * of network. Derivated classes will have to implement
+ * methods to prepare HTTP request and if the response
+ * is from right device.
+ */
+class AbstractHTTPScanner : protected Loggable {
+public:
+	/*
+	 * @param path Defines path of HTTP request.
+	 * @param port Defines receiver's port.
+	 * @param minNetMask Defines minimal range of exploring IP address.
+	 */
+	AbstractHTTPScanner(const std::string& path, Poco::UInt16 port, const Poco::Net::IPAddress& minNetMask);
+	~AbstractHTTPScanner();
+
+	/*
+	 * @brief It executes the scan of proper network interfaces.
+	 * @param maxResponseLength Defines maximal length of response message which
+	 * will be process.
+	 * @return Vector of SocketAddress belongs to found devices.
+	 */
+	std::vector<Poco::Net::SocketAddress> scan(const uint32_t maxResponseLength);
+
+	void setPingTimeout(const Poco::Timespan& pingTimeout);
+	void setHTTPTimeout(const Poco::Timespan& httpTimeout);
+	void setBlackList(const std::set<std::string>& set);
+
+	std::string path();
+	Poco::UInt16 port();
+
+protected:
+	virtual void prepareRequest(Poco::Net::HTTPRequest& request) = 0;
+	/*
+	 * @brief It defines if the response is valid or not.
+	 * @param response Response message.
+	 * @return If the response is valid or not.
+	 */
+	virtual bool isValidResponse(const std::string& response) = 0;
+
+	/*
+	 * @brief It explores network interface.
+	 * @param interafce Exploring interface.
+	 * @param devices Structure where are found devices saved.
+	 * @param maxResponseLength Defines maximal length of response message which
+	 * will be process.
+	 */
+	void probeInterface(const Poco::Net::NetworkInterface& interface,
+		std::vector<Poco::Net::SocketAddress>& devices,
+		const Poco::Int64 maxResponseLength);
+
+	/*
+	 * @brief It explores IP address range.
+	 * @param range Exploring IP address range.
+	 * @param devices Structure where are found devices saved.
+	 * @param maxResponseLength Defines maximal length of response message which
+	 * will be process.
+	 */
+	void probeAddressRange(const IPAddressRange& range,
+		std::vector<Poco::Net::SocketAddress>& devices,
+		const Poco::Int64 maxResponseLength);
+
+	/*
+	 * @brief It sends HTTP request.
+	 * @param socketAddress Address of receiver.
+	 * @param maxResponseLength Defines maximal length of response message which
+	 * will be process.
+	 * @return HTTP response.
+	 */
+	HTTPEntireResponse sendRequest(const Poco::Net::SocketAddress& socketAddress,
+		const Poco::Int64 maxResponseLength);
+
+	bool addressIncompatible(const Poco::Net::NetworkInterface::AddressTuple& addressTuple) const;
+
+	/*
+	 * @return Vector of active network interfaces.
+	 */
+	std::vector<Poco::Net::NetworkInterface> listNetworkInterfaces();
+
+private:
+	std::string m_path;
+	Poco::UInt16 m_port;
+	Poco::Net::IPAddress m_minNetMask;
+	Poco::Timespan m_pingTimeout;
+	Poco::Timespan m_httpTimeout;
+	std::set<std::string> m_blackList;
+};
+
+}
+
+#endif

commit a5befc0e0324348d9fbf40105c1b884e53476899

AbstractHTTPScanner: remove unused variable
Fixes: 9b7507bf ("AbstractHTTPScanner: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/AbstractHTTPScanner.cpp
@@ -134,7 +134,6 @@ void AbstractHTTPScanner::probeAddressRange(const IPAddressRange& range,
 	const Int64 maxResponseLength)
 {
 	StreamSocket ping;
-	IPAddress ip;
 
 	for (auto& ip : range) {
 		SocketAddress socketAddress(ip, m_port);