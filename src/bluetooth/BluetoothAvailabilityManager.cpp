BeeeOn/gateway

commit 4e36ca273e52f2484c3315cef81157ebf1e68c8e

DongleDeviceManager: detect failing dongle
When the dongleAvailable() method throws an exception and the dongle
is still connected, we are quickly calling the dongleAvailable() method
again. This however does not improve the sitation as we are now looping
fast inside the DongleDeviceManager without any possibility to recover.

Instead, utilize the FailDetector to detect a number of fails following
quickly. If the dongle fails multiple times quickly, then prevent calling
dongleAvailable() for a while. Instead, the method dongleFailed is called.
Its purpose is to delay the next attempt to call dongleAvailable() or
to wait until the dongle is removed from the device.

The patch has been tested manually on a PC without hciconfig where the
BluetoothAvailabilityManager fails to initialize the HCI interface.
Without this patch, we get a very high rate of failures many times
per second:

15:35:39.191 21281 [Debug] registering dongle bluetooth,,host,hci0,
15:35:39.191 21281 [Information] dongle is available: hci0
15:35:39.193 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.193 21281 [Information] dongle is available: hci0
15:35:39.194 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.194 21281 [Information] dongle is available: hci0
15:35:39.195 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.195 21281 [Information] dongle is available: hci0
15:35:39.196 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.196 21281 [Information] dongle is available: hci0
15:35:39.197 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.197 21281 [Information] dongle is available: hci0
15:35:39.198 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.198 21281 [Information] dongle is available: hci0
15:35:39.199 21281 [Error] I/O error: hciconfig hci0 up has failed: 72
15:35:39.199 21281 [Information] dongle is available: hci0

src/bluetooth/BluetoothAvailabilityManager.cpp
@@ -25,6 +25,9 @@ BEEEON_OBJECT_CASTABLE(HotplugListener)
 BEEEON_OBJECT_NUMBER("wakeUpTime", &BluetoothAvailabilityManager::setWakeUpTime)
 BEEEON_OBJECT_REF("distributor", &BluetoothAvailabilityManager::setDistributor)
 BEEEON_OBJECT_REF("commandDispatcher", &BluetoothAvailabilityManager::setCommandDispatcher)
+BEEEON_OBJECT_NUMBER("attemptsCount", &BluetoothAvailabilityManager::setAttemptsCount)
+BEEEON_OBJECT_NUMBER("retryTimeout", &BluetoothAvailabilityManager::setRetryTimeout)
+BEEEON_OBJECT_NUMBER("failTimeout", &BluetoothAvailabilityManager::setFailTimeout)
 BEEEON_OBJECT_END(BeeeOn, BluetoothAvailabilityManager)
 
 using namespace BeeeOn;

src/core/DongleDeviceManager.cpp
@@ -3,19 +3,59 @@
 
 #include "core/DongleDeviceManager.h"
 #include "hotplug/HotplugEvent.h"
+#include "util/FailDetector.h"
 
 using namespace std;
 using namespace Poco;
 using namespace BeeeOn;
 
 DongleDeviceManager::DongleDeviceManager(const DevicePrefix &prefix):
-	DeviceManager(prefix)
+	DeviceManager(prefix),
+	m_attemptsCount(3),
+	m_retryTimeout(10 * Timespan::SECONDS),
+	m_failTimeout(1 * Timespan::SECONDS)
 {
+}
+
+void DongleDeviceManager::setAttemptsCount(const int count)
+{
+	if (count <= 0)
+		throw InvalidAccessException("attemptsCount must be greater then 0");
+
+	m_attemptsCount = count;
+}
+
+void DongleDeviceManager::setRetryTimeout(const int msecs)
+{
+	m_retryTimeout = msecs * Timespan::MILLISECONDS;
+}
+
+void DongleDeviceManager::setFailTimeout(const int msecs)
+{
+	if (msecs < 0)
+		throw InvalidArgumentException("failTimeout must not be negative");
+
+	m_failTimeout = msecs * Timespan::MILLISECONDS;
 }
 
 bool DongleDeviceManager::dongleMissing()
 {
 	return true;
+}
+
+void DongleDeviceManager::dongleFailed(const FailDetector &)
+{
+	logger().critical("dongle seems to be failing",
+			__FILE__, __LINE__);
+
+	if (m_retryTimeout < 0) {
+		// wait indefinitely until an event occures
+		event().wait();
+	}
+	else {
+		// wait for the retry timeout
+		(void) event().tryWait(m_retryTimeout.totalMilliseconds());
+	}
 }
 
 string DongleDeviceManager::dongleName(bool failWhenMissing) const
@@ -86,6 +126,8 @@ void DongleDeviceManager::run()
 	logger().information("device manager is starting",
 			     __FILE__, __LINE__);
 
+	FailDetector dongleStatus(m_attemptsCount);
+
 	while (!m_stop) {
 		while (!m_stop && dongleName(false).empty()) {
 			logger().information("no appropriate dongle is available",
@@ -93,6 +135,8 @@ void DongleDeviceManager::run()
 
 			if (dongleMissing())
 				event().wait();
+
+			dongleStatus.success();
 		}
 
 		if (m_stop)
@@ -108,15 +152,19 @@ void DongleDeviceManager::run()
 		}
 		catch (const Exception &e) {
 			logger().log(e, __FILE__, __LINE__);
-			continue;
 		}
 		catch (const exception &e) {
 			logger().critical(e.what(), __FILE__, __LINE__);
-			continue;
 		}
 		catch (...) {
 			logger().critical("unknown error", __FILE__, __LINE__);
-			continue;
+		}
+
+		dongleStatus.fail();
+
+		if (dongleStatus.isFailed()) {
+			dongleFailed(dongleStatus);
+			dongleStatus.success();
 		}
 	}
 

src/core/DongleDeviceManager.h
@@ -3,11 +3,14 @@
 
 #include <Poco/Event.h>
 #include <Poco/Mutex.h>
+#include <Poco/Timespan.h>
 
 #include "core/DeviceManager.h"
 #include "hotplug/HotplugListener.h"
 
 namespace BeeeOn {
+
+class FailDetector;
 
 class DongleDeviceManager : public DeviceManager, public HotplugListener {
 public:
@@ -18,6 +21,29 @@ public:
 
 	void onAdd(const HotplugEvent &e) override;
 	void onRemove(const HotplugEvent &e) override;
+
+	/**
+	 * Set how many attempts of dongleAvailable() should be done before it
+	 * is considered as failing.
+	 */
+	void setAttemptsCount(const int count);
+
+	/**
+	 * Set the timeout to retry the count of attempts to call dongleAvailable()
+	 * successfully. If the timeout is negative, do not repeat any attempt unless
+	 * some event occures.
+	 */
+	void setRetryTimeout(const int msecs);
+
+	/**
+	 * Set the timeout to detect a failure. If the dongleAvailable() fails
+	 * multiple times in a row within the given number of milliseconds,
+	 * it is considered as failing and dongleFailed() might be called.
+	 * If there are multiple fails within a greater time interval, then
+	 * we do not considered to be failing as it might be just a dongle
+	 * removal event.
+	 */
+	void setFailTimeout(const int msecs);
 
 protected:
 	/**
@@ -52,6 +78,13 @@ protected:
 	virtual bool dongleMissing();
 
 	/**
+	 * Called when dongleAvailable() fails multiple times (according
+	 * to the FailDetector instance). The default implementation just
+	 * sleeps for a while.
+	 */
+	virtual void dongleFailed(const FailDetector &dongleStatus);
+
+	/**
 	 * Return the name of associated dongle.
 	 * If the failWhenMissing is true, then it throws an exception
 	 * when no such dongle name is available (disconnected).
@@ -69,6 +102,9 @@ private:
 	mutable Poco::FastMutex m_lock;
 	Poco::Event m_event;
 	std::string m_dongleName;
+	unsigned int m_attemptsCount;
+	Poco::Timespan m_retryTimeout;
+	Poco::Timespan m_failTimeout;
 };
 
 }

src/jablotron/JablotronDeviceManager.cpp
@@ -17,6 +17,9 @@ BEEEON_OBJECT_CASTABLE(StoppableRunnable)
 BEEEON_OBJECT_CASTABLE(HotplugListener)
 BEEEON_OBJECT_REF("distributor", &JablotronDeviceManager::setDistributor)
 BEEEON_OBJECT_REF("commandDispatcher", &JablotronDeviceManager::setCommandDispatcher)
+BEEEON_OBJECT_NUMBER("attemptsCount", &JablotronDeviceManager::setAttemptsCount)
+BEEEON_OBJECT_NUMBER("retryTimeout", &JablotronDeviceManager::setRetryTimeout)
+BEEEON_OBJECT_NUMBER("failTimeout", &JablotronDeviceManager::setFailTimeout)
 BEEEON_OBJECT_END(BeeeOn, JablotronDeviceManager)
 
 using namespace BeeeOn;

commit c1ac52ca94d1bca1caf0f2ed6237e10b60444da5

DongleDeviceManager: remove unused failTimeout
The failTimeout was finally never used and thus it can be dropped.

Fixes: 4e36ca273e52 ("DongleDeviceManager: detect failing dongle")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/bluetooth/BluetoothAvailabilityManager.cpp
@@ -28,7 +28,6 @@ BEEEON_OBJECT_REF("distributor", &BluetoothAvailabilityManager::setDistributor)
 BEEEON_OBJECT_REF("commandDispatcher", &BluetoothAvailabilityManager::setCommandDispatcher)
 BEEEON_OBJECT_NUMBER("attemptsCount", &BluetoothAvailabilityManager::setAttemptsCount)
 BEEEON_OBJECT_NUMBER("retryTimeout", &BluetoothAvailabilityManager::setRetryTimeout)
-BEEEON_OBJECT_NUMBER("failTimeout", &BluetoothAvailabilityManager::setFailTimeout)
 BEEEON_OBJECT_TIME("statisticsInterval", &BluetoothAvailabilityManager::setStatisticsInterval)
 BEEEON_OBJECT_REF("executor", &BluetoothAvailabilityManager::setExecutor)
 BEEEON_OBJECT_REF("listeners", &BluetoothAvailabilityManager::registerListener)

src/core/DongleDeviceManager.cpp
@@ -12,8 +12,7 @@ using namespace BeeeOn;
 DongleDeviceManager::DongleDeviceManager(const DevicePrefix &prefix):
 	DeviceManager(prefix),
 	m_attemptsCount(3),
-	m_retryTimeout(10 * Timespan::SECONDS),
-	m_failTimeout(1 * Timespan::SECONDS)
+	m_retryTimeout(10 * Timespan::SECONDS)
 {
 }
 
@@ -28,14 +27,6 @@ void DongleDeviceManager::setAttemptsCount(const int count)
 void DongleDeviceManager::setRetryTimeout(const int msecs)
 {
 	m_retryTimeout = msecs * Timespan::MILLISECONDS;
-}
-
-void DongleDeviceManager::setFailTimeout(const int msecs)
-{
-	if (msecs < 0)
-		throw InvalidArgumentException("failTimeout must not be negative");
-
-	m_failTimeout = msecs * Timespan::MILLISECONDS;
 }
 
 bool DongleDeviceManager::dongleMissing()

src/core/DongleDeviceManager.h
@@ -35,16 +35,6 @@ public:
 	 */
 	void setRetryTimeout(const int msecs);
 
-	/**
-	 * Set the timeout to detect a failure. If the dongleAvailable() fails
-	 * multiple times in a row within the given number of milliseconds,
-	 * it is considered as failing and dongleFailed() might be called.
-	 * If there are multiple fails within a greater time interval, then
-	 * we do not considered to be failing as it might be just a dongle
-	 * removal event.
-	 */
-	void setFailTimeout(const int msecs);
-
 protected:
 	/**
 	 * If the event represents an appropriate dongle, it should
@@ -114,7 +104,6 @@ private:
 	std::string m_dongleName;
 	unsigned int m_attemptsCount;
 	Poco::Timespan m_retryTimeout;
-	Poco::Timespan m_failTimeout;
 };
 
 }

src/jablotron/JablotronDeviceManager.cpp
@@ -19,7 +19,6 @@ BEEEON_OBJECT_REF("distributor", &JablotronDeviceManager::setDistributor)
 BEEEON_OBJECT_REF("commandDispatcher", &JablotronDeviceManager::setCommandDispatcher)
 BEEEON_OBJECT_NUMBER("attemptsCount", &JablotronDeviceManager::setAttemptsCount)
 BEEEON_OBJECT_NUMBER("retryTimeout", &JablotronDeviceManager::setRetryTimeout)
-BEEEON_OBJECT_NUMBER("failTimeout", &JablotronDeviceManager::setFailTimeout)
 BEEEON_OBJECT_END(BeeeOn, JablotronDeviceManager)
 
 using namespace BeeeOn;