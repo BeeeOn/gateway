BeeeOn/gateway

commit f548baa8000fd1d9a12b5ba393e63a53322bea6a

AnswerQueue: implement dispose()
The AnswerQueue::dispose() performs a cleanup of the pending results in
case of exiting application or a thread. This ensures that all threads
waiting on AnswerQueue::event() would be woken up and could exit
gracefully. Such situation happens when some CommandHandler fails and
never adds a result or never updates it into a terminating state.
Thus, usually, threads would call AnswerQueue::dispose() while leaving
their run(). This method can also by called from stop().

The AnswerQueue::dispose() creates the missing Results and sets all
pending results into the state FAILED. This ensures that all waiting
threads can finish.

Example output:

finalizing Answer 0x7F2D680008C0, missing result 2/2
created result for Answer 0x7F2D680008C0, 1/2 missing result
created result for Answer 0x7F2D680008C0, 2/2 missing result
result 1/2 for Answer 0x7F2D680008C0 done: FAILED
result 2/2 for Answer 0x7F2D680008C0 done: FAILED

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/AnswerQueue.cpp
@@ -1,15 +1,23 @@
+#include <Poco/NumberFormatter.h>
+
 #include "core/AnswerQueue.h"
 
 using namespace BeeeOn;
 using namespace Poco;
 using namespace std;
 
-AnswerQueue::AnswerQueue()
+AnswerQueue::AnswerQueue():
+	m_disposed(false)
 {
 }
 
 Answer::Ptr AnswerQueue::newAnswer()
 {
+	if (m_disposed) {
+		throw IllegalStateException(
+			"creating Answer for a disposed AnswerQueue");
+	}
+
 	return new Answer(*this);
 }
 
@@ -24,7 +32,7 @@ bool AnswerQueue::wait(const Timespan &timeout, list<Answer::Ptr> &dirtyList)
 			dirtyList = tmpList;
 			return true;
 		}
-	} while (block(timeout));
+	} while (block(timeout) && !m_disposed);
 
 	return false;
 }
@@ -45,6 +53,11 @@ void AnswerQueue::listDirty(list<Answer::Ptr> &dirtyList) const
 void AnswerQueue::add(Answer *answer)
 {
 	FastMutex::ScopedLock lock(m_mutex);
+
+	if (m_disposed) {
+		throw IllegalStateException(
+			"adding Answer into a disposed AnswerQueue");
+	}
 
 	m_answerList.push_back(AutoPtr<Answer>(answer, true));
 }
@@ -80,4 +93,69 @@ unsigned long AnswerQueue::size() const
 {
 	FastMutex::ScopedLock lock(m_mutex);
 	return m_answerList.size();
+}
+
+void AnswerQueue::dispose()
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	for (auto &answer : m_answerList) {
+		FastMutex::ScopedLock guard(answer->lock());
+
+		int resultCount = answer->resultsCountUnlocked();
+		const int handlersCount = answer->handlersCountUnlocked();
+		const int missingCount = handlersCount - resultCount;
+
+		string answerAddr =
+			NumberFormatter::formatHex(reinterpret_cast<size_t>(answer.get()), true);
+
+		if (missingCount > 0) {
+			logger().debug(
+				"finalizing Answer "
+				+ answerAddr
+				+ ", missing result "
+				+ to_string(missingCount)
+				+ "/"
+				+ to_string(handlersCount),
+				__FILE__, __LINE__);
+		}
+
+		for (int i = 0; i < missingCount; ++i) {
+			new Result(answer, true);
+
+			logger().debug(
+				"created result for Answer "
+				+ answerAddr
+				+ ", "
+				+ to_string(i + 1)
+				+ "/"
+				+ to_string(missingCount)
+				+ " missing result",
+				__FILE__, __LINE__);
+		}
+
+		resultCount = answer->resultsCountUnlocked();
+		for (int i = 0; i < resultCount; ++i) {
+			if (answer->atUnlocked(i)->statusUnlocked() == Result::Status::PENDING)
+				answer->atUnlocked(i)->setStatusUnlocked(Result::Status::FAILED);
+
+			logger().debug(
+				"result "
+				+ to_string(i + 1)
+				+ "/"
+				+ to_string(resultCount)
+				+ " for Answer "
+				+ answerAddr
+				+ " done: "
+				+ answer->atUnlocked(i)->statusUnlocked(),
+				__FILE__, __LINE__);
+		}
+	}
+
+	m_disposed = true;
+}
+
+bool AnswerQueue::isDisposed() const
+{
+	return m_disposed;
 }

test/core/AnswerQueueTest.cpp
@@ -1,11 +1,14 @@
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <Poco/Event.h>
 #include <Poco/Timer.h>
 #include <Poco/Timespan.h>
 
 #include "cppunit/BetterAssert.h"
 
 #include "core/AnswerQueue.h"
+#include "core/CommandHandler.h"
+#include "core/PocoCommandDispatcher.h"
 
 using namespace Poco;
 
@@ -17,6 +20,10 @@ class AnswerQueueTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testWaitTimeout);
 	CPPUNIT_TEST(testRemove);
 	CPPUNIT_TEST(testResultUpdated);
+	CPPUNIT_TEST(testDisposePendingAnswerResult);
+	CPPUNIT_TEST(testDisposeAnswerWithoutResult);
+	CPPUNIT_TEST(testSetResultAfterLock);
+	CPPUNIT_TEST(testCreateAnswerAfterLock);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -24,6 +31,10 @@ public:
 	void testWaitTimeout();
 	void testRemove();
 	void testResultUpdated();
+	void testDisposePendingAnswerResult();
+	void testDisposeAnswerWithoutResult();
+	void testSetResultAfterLock();
+	void testCreateAnswerAfterLock();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(AnswerQueueTest);
@@ -100,6 +111,60 @@ public:
 
 private:
 	bool m_dirty;
+	Answer::Ptr m_answer;
+};
+
+class TestableCommand : public Command {
+public:
+	typedef AutoPtr<TestableCommand> Ptr;
+
+	TestableCommand():
+		Command("TestableCommand")
+	{
+	}
+};
+
+class HandlerWithSetResultLater : public CommandHandler {
+public:
+	typedef SharedPtr<HandlerWithSetResultLater> Ptr;
+
+	HandlerWithSetResultLater()
+	{
+	}
+
+	bool accept(Command::Ptr cmd) override
+	{
+		return cmd->is<TestableCommand>();
+	}
+
+	void handle(Command::Ptr cmd, Answer::Ptr answer) override
+	{
+		if (cmd->is<TestableCommand>()) {
+			m_answer = answer;
+			m_eventHandle.set();
+		}
+	}
+
+	void setResult(const Result::Status &status)
+	{
+		Result::Ptr result = new Result(m_answer);
+		result->setStatus(status);
+		m_eventSetResult.set();
+	}
+
+	bool waitForRunHandle(const Timespan &timeout)
+	{
+		return m_eventHandle.tryWait(timeout.totalMilliseconds());
+	}
+
+	bool waitForCreateResult(const Timespan &timeout)
+	{
+		return m_eventSetResult.tryWait(timeout.totalMilliseconds());
+	}
+
+private:
+	Poco::Event m_eventHandle;
+	Poco::Event m_eventSetResult;
 	Answer::Ptr m_answer;
 };
 
@@ -266,6 +331,106 @@ void AnswerQueueTest::testResultUpdated()
 
 	queue.remove(answer1);
 	CPPUNIT_ASSERT(0 == queue.size());
+}
+
+/**
+ * It checks if dispose is able to set status of Result to FAILED,
+ * condition: Result is created and its state is PENDING.
+ */
+void AnswerQueueTest::testDisposePendingAnswerResult()
+{
+	PocoCommandDispatcher dispatcher;
+	AnswerQueue queue;
+
+	TestableCommand::Ptr cmd = new TestableCommand();
+	Answer::Ptr answer = queue.newAnswer();
+
+	HandlerWithSetResultLater::Ptr handler = new HandlerWithSetResultLater;
+	dispatcher.registerHandler(handler);
+
+	dispatcher.dispatch(cmd, answer);
+	CPPUNIT_ASSERT(handler->waitForRunHandle(5 * Timespan::SECONDS));
+
+	handler->setResult(Result::Status::PENDING);
+	CPPUNIT_ASSERT(handler->waitForCreateResult(5 * Timespan::SECONDS));
+
+	CPPUNIT_ASSERT_EQUAL(1, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+	CPPUNIT_ASSERT(answer->isPending());
+
+	queue.dispose();
+	CPPUNIT_ASSERT(!answer->isPending());
+	CPPUNIT_ASSERT_EQUAL(1, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+	CPPUNIT_ASSERT(Result::Status::FAILED == answer->at(0)->status());
+}
+
+/**
+ * It checks if dispose is able to create a Result that is set to FAILED.
+ */
+void AnswerQueueTest::testDisposeAnswerWithoutResult()
+{
+	PocoCommandDispatcher dispatcher;
+	AnswerQueue queue;
+
+	TestableCommand::Ptr cmd = new TestableCommand();
+	Answer::Ptr answer = queue.newAnswer();
+
+	HandlerWithSetResultLater::Ptr handler = new HandlerWithSetResultLater;
+	dispatcher.registerHandler(handler);
+
+	dispatcher.dispatch(cmd, answer);
+	CPPUNIT_ASSERT(handler->waitForRunHandle(5 * Timespan::SECONDS));
+
+	//empty result list in Answer
+	CPPUNIT_ASSERT_EQUAL(0, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+	CPPUNIT_ASSERT(answer->isPending());
+
+	queue.dispose();
+	CPPUNIT_ASSERT(!answer->isPending());
+	CPPUNIT_ASSERT_EQUAL(1, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+	CPPUNIT_ASSERT(Result::Status::FAILED == answer->at(0)->status());
+}
+
+/**
+ * Test that no other Result can be created after the queue is disposed.
+ */
+void AnswerQueueTest::testSetResultAfterLock()
+{
+	PocoCommandDispatcher dispatcher;
+	AnswerQueue queue;
+
+	TestableCommand::Ptr cmd = new TestableCommand;
+	Answer::Ptr answer = queue.newAnswer();
+
+	HandlerWithSetResultLater::Ptr handler = new HandlerWithSetResultLater;
+	dispatcher.registerHandler(handler);
+
+	dispatcher.dispatch(cmd, answer);
+	CPPUNIT_ASSERT(handler->waitForRunHandle(5 * Timespan::SECONDS));
+
+	//empty result list in Answer
+	CPPUNIT_ASSERT_EQUAL(0, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+
+	queue.dispose();
+	CPPUNIT_ASSERT_THROW(handler->setResult(Result::Status::PENDING), IllegalStateException);
+	CPPUNIT_ASSERT_EQUAL(1, answer->resultsCount());
+	CPPUNIT_ASSERT_EQUAL(1, answer->handlersCount());
+}
+
+/**
+ * Test that no more answers can be created after the queue is locked.
+ */
+void AnswerQueueTest::testCreateAnswerAfterLock()
+{
+	AnswerQueue queue;
+	queue.dispose();
+
+	CPPUNIT_ASSERT_THROW(queue.newAnswer(), IllegalStateException);
+	CPPUNIT_ASSERT_THROW(new Answer(queue), IllegalStateException);
 }
 
 }

conf/config.d/logging-loggers.ini
@@ -61,3 +61,6 @@ loggers.ZWaveDeviceManager.level = debug
 
 loggers.OpenZWaveLibrary.name = OpenZWaveLibrary
 loggers.OpenZWaveLibrary.level = error
+
+loggers.AnswerQueue.name = BeeeOn::AnswerQueue
+loggers.AnswerQueue.level = debug

src/core/Answer.cpp
@@ -90,6 +90,16 @@ unsigned long Answer::resultsCountUnlocked() const
 void Answer::addResult(Result *result)
 {
 	FastMutex::ScopedLock guard(m_lock);
+	addResultUnlocked(result);
+}
+
+void Answer::addResultUnlocked(Result *result)
+{
+	if (m_answerQueue.isDisposed()) {
+		throw IllegalStateException(
+			"inserting result into a disposed AnswerQueue");
+	}
+
 	m_resultList.push_back(AutoPtr<Result>(result, true));
 }
 

src/core/Answer.h
@@ -73,6 +73,7 @@ public:
 	void setHandlersCount(unsigned long counter);
 
 	void addResult(Result *result);
+	void addResultUnlocked(Result *result);
 
 	/*
 	 * Notifies the waiting queue that this Answer isEmpty().

src/core/AnswerQueue.h
@@ -7,6 +7,7 @@
 #include <Poco/Timespan.h>
 
 #include "core/Answer.h"
+#include "util/Loggable.h"
 
 namespace BeeeOn {
 
@@ -19,7 +20,7 @@ namespace BeeeOn {
  * wait(Timespan, dirtyList). After a given time Answers with the set dirty
  * (the status Response was set to the Answers) are stored to the dirtyList.
  */
-class AnswerQueue {
+class AnswerQueue : public Loggable {
 	friend Answer;
 public:
 	AnswerQueue();
@@ -40,8 +41,16 @@ public:
 
 	unsigned long size() const;
 
+	/**
+	 * It processes all instances of Answer, adds their Results
+	 * and sets them as FAILED.
+	 */
+	void dispose();
+
 protected:
 	void add(Answer *answer);
+
+	bool isDisposed() const;
 
 	bool block(const Poco::Timespan &timeout);
 
@@ -54,6 +63,7 @@ protected:
 	std::list<Answer::Ptr> m_answerList;
 	Poco::Event m_event;
 	mutable Poco::FastMutex m_mutex;
+	Poco::AtomicCounter m_disposed;
 };
 
 }

src/core/Result.cpp
@@ -5,11 +5,14 @@
 using namespace BeeeOn;
 using namespace Poco;
 
-Result::Result(Poco::AutoPtr<Answer> answer):
+Result::Result(Poco::AutoPtr<Answer> answer, bool locked):
 	m_status(Status::PENDING),
 	m_answer(*answer)
 {
-	m_answer.addResult(this);
+	if (locked)
+		m_answer.addResultUnlocked(this);
+	else
+		m_answer.addResult(this);
 }
 
 Result::~Result()

src/core/Result.h
@@ -45,7 +45,7 @@ public:
 
 	typedef Enum<StatusEnum> Status;
 
-	Result(Poco::AutoPtr<Answer> answer);
+	Result(Poco::AutoPtr<Answer> answer, bool locked = false);
 
 	void setStatus(const Status status);
 

commit 1246b404ea99be1dba5f715ed144ecdc9fa56b19

AnswerQueue: fix missing clear answer list after dispose
Fixes: f548baa8 ("AnswerQueue: implement dispose()")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/AnswerQueue.cpp
@@ -152,6 +152,7 @@ void AnswerQueue::dispose()
 		}
 	}
 
+	m_answerList.clear();
 	m_disposed = true;
 }
 

test/core/AnswerQueueTest.cpp
@@ -443,9 +443,8 @@ void AnswerQueueTest::testDisposeUnusedAnswer()
 
 	queue.dispose();
 
-	CPPUNIT_ASSERT(1 == queue.size());
-	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->resultsCount());
-	CPPUNIT_ASSERT(0 == queue.m_answerList.front()->handlersCount());
+	CPPUNIT_ASSERT(0 == queue.size());
+
 }
 
 }