8da63e9440249ebd9ac4d24c619a03e90cc5fdc5
Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
2017-10-17 13:18:35 +0200

    ServerAnswer: new class
    Extends the Answer class with unique id of the received request.
    
    Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
    Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/server/ServerAnswer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Poco/AutoPtr.h>
+
+#include "core/Answer.h"
+#include "model/GlobalID.h"
+#include "server/GWMessageContext.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief ServerAnswer extends regular Answer with ID of
+ * corresponding received GWMessage, which was translated to dispatched
+ * Command. This ID is used to create GWMessage to inform server
+ * about execution status of this Command.
+ */
+class ServerAnswer : public Answer {
+public:
+	typedef Poco::AutoPtr<ServerAnswer> Ptr;
+
+	ServerAnswer(AnswerQueue &answerQueue, const GlobalID &id);
+
+	ServerAnswer(const ServerAnswer&) = delete;
+
+	void setID(const GlobalID &id);
+	void setIDUnlocked(const GlobalID &id);
+
+	/**
+	 * Convert Answer to appropriate GWResponseWithAckContext if possible.
+	 */
+	GWResponseWithAckContext::Ptr toResponse(const GWResponse::Status &status) const;
+
+	GlobalID id() const;
+	GlobalID idUnlocked() const;
+
+private:
+	GlobalID m_id;
+};
+
+}
