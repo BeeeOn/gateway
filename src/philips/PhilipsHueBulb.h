69921290e6e4e1b91695f8683f43cffb9ebcdb34
David Bednařík <xbedna62@stud.fit.vutbr.cz>
2017-11-27 11:38:24 +0100

    PhilipsHueBulb: new abstract class
    Abstract class representing generic Philips Hue bulb.
    
    Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

---
+++ src/philips/PhilipsHueBulb.h
@@ -0,0 +1,57 @@
+#ifndef GATEWAY_PHILIPS_BULB_H
+#define GATEWAY_PHILIPS_BULB_H
+
+#include <list>
+#include <string>
+
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include "model/DeviceID.h"
+#include "model/ModuleID.h"
+#include "model/ModuleType.h"
+#include "model/SensorData.h"
+#include "philips/PhilipsHueBridge.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Abstract class representing generic Philips Hue bulb.
+ */
+class PhilipsHueBulb : protected Loggable {
+public:
+	typedef Poco::SharedPtr<PhilipsHueBulb> Ptr;
+
+	static const double MAX_DIM;
+
+	/**
+	 * @brief The DeviceID is created based on a bulb's 64-bit identifier,
+	 * where DevicePrefix is given on 8th byte.
+	 */
+	PhilipsHueBulb(const uint32_t ordinalNumber, const PhilipsHueBridge::BulbID bulbId, const PhilipsHueBridge::Ptr bridge);
+	~PhilipsHueBulb();
+
+	virtual bool requestModifyState(const ModuleID& moduleID, const double value) = 0;
+	virtual SensorData requestState() = 0;
+
+	DeviceID deviceID();
+	virtual std::list<ModuleType> moduleTypes() const = 0;
+	virtual std::string name() const = 0;
+	Poco::FastMutex& lock();
+
+	PhilipsHueBridge::Ptr bridge();
+
+protected:
+	int dimToPercentage(const double value);
+	int dimFromPercentage(const double percents);
+
+protected:
+	DeviceID m_deviceID;
+	uint32_t m_ordinalNumber;
+	PhilipsHueBridge::Ptr m_bridge;
+};
+
+}
+
+#endif
