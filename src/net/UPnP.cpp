BeeeOn/gateway

commit de9a2c16ff3cc27aae3f5991054edc3a8d49981f

UPnP: new class
Provide searching devices on network by UPnP protocol
using ssdp(Simple Service Discovery Protocol).

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
[moved into net/]
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/net/UPnP.cpp
@@ -1,0 +1,77 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Net/DatagramSocket.h>
+#include <Poco/RegularExpression.h>
+
+#include "net/UPnP.h"
+
+#define BUFFER_LENGTH 256
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace std;
+
+UPnP::UPnP(const SocketAddress& address)
+{
+	m_multicastAddress = address;
+}
+
+list<SocketAddress> UPnP::discover(const Timespan& timeout, const string& deviceType)
+{
+	DatagramSocket socket;
+	socket.setBroadcast(true);
+
+	const string msg = "M-SEARCH * HTTP/1.1\r\n"
+	                   "HOST: " + m_multicastAddress.toString() + "\r\n"
+	                   "MAN: \"ssdp:discover\"\r\n"
+	                   "MX: " + to_string(timeout.totalSeconds()) + "\r\n"
+	                   "ST: " + deviceType + "\r\n\r\n";
+
+	socket.sendTo(msg.data(), msg.size(), m_multicastAddress);
+
+	if (logger().debug()) {
+		logger().debug("sent " + to_string(msg.size()) + " bytes",
+			__FILE__, __LINE__);
+	}
+
+	socket.setReceiveTimeout(timeout);
+	char buffer[BUFFER_LENGTH];
+	SocketAddress device;
+	list<SocketAddress> listOfDevices;
+
+	logger().information("starting to looking for devices " + deviceType, __FILE__, __LINE__);
+
+	while (true) {
+		int sizeOfResponse;
+
+		try {
+			sizeOfResponse = socket.receiveFrom(buffer, sizeof(buffer), device);
+		}
+		catch (const TimeoutException e) {
+			break;
+		}
+
+		const string response(buffer, sizeOfResponse);
+		RegularExpression::MatchVec matches;
+		RegularExpression reLocation(
+			"(?i)(location): "
+			"http://([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}:[0-9]{1,5})"
+			"/[a-zA-z]*\\.xml\r\n");
+
+		if (reLocation.match(response, 0, matches) == 0)
+			continue;
+
+		SocketAddress searchingDevice(response.substr(matches[2].offset, matches[2].length));
+		listOfDevices.push_front(searchingDevice);
+	}
+
+	logger().information("found " + to_string(listOfDevices.size()) + " device(s) " + deviceType, __FILE__, __LINE__);
+
+	if (logger().debug()) {
+		for (const auto &address : listOfDevices)
+			logger().debug("found device at " + address.toString());
+	}
+	
+	return listOfDevices;
+}

conf/config.d/logging-loggers.ini
@@ -11,5 +11,8 @@ loggers.LoopRunner.level = notice
 loggers.NamedPipeExporter.name = BeeeOn::NamedPipeExporter
 loggers.NamedPipeExporter.level = notice
 
+loggers.UPnP.name = BeeeOn::UPnP
+loggers.UPnP.level = debug
+
 loggers.VirtualSensor.name = BeeeOn::VirtualSensor
 loggers.VirtualSensor.level = debug

src/CMakeLists.txt
@@ -33,6 +33,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
+	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp

src/net/UPnP.h
@@ -1,0 +1,31 @@
+#ifndef BEEEON_UPNP_H
+#define BEEEON_UPNP_H
+
+#include <list>
+#include <string>
+
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Timespan.h>
+
+#include "util/Loggable.h"
+
+// Documentation for UPnP protocol
+// http://upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.1.pdf
+// Section 1 Discovery
+#define UPNP_MULTICAST_IP "239.255.255.250"
+#define UPNP_PORT 1900
+
+namespace BeeeOn {
+
+class UPnP : protected Loggable {
+public:
+	UPnP(const Poco::Net::SocketAddress& address = Poco::Net::SocketAddress(UPNP_MULTICAST_IP, UPNP_PORT));
+	std::list<Poco::Net::SocketAddress> discover(const Poco::Timespan& timeout, const std::string& deviceType);
+
+private:
+	Poco::Net::SocketAddress m_multicastAddress;
+};
+
+}
+
+#endif

commit 61f1bba6285b8b7b727744e7ac376c8bc10ea417

UPnP: fix accepting of a device
It was added a check of the value of the ST header if it occurs in the received message.
If the value is the same as method's parametr deviceType accepts the device.
This is added due to Philips, because it responds to searching by messages containing the
ST header with all the values it knows. And it was also added to avoiding the return of
the same device multiple times.

ST header is more described in http://www.upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.0-20080424.pdf
on the page 19.

Fixes: de9a2c16 ("UPnP: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/UPnP.cpp
@@ -52,8 +52,17 @@ list<SocketAddress> UPnP::discover(const Timespan& timeout, const string& device
 			break;
 		}
 
-		const string response(buffer, sizeOfResponse);
+		const string response = string(buffer, sizeOfResponse) + "\r\n";
+
 		RegularExpression::MatchVec matches;
+		RegularExpression reST("\r\n(?i)(st): (.*)\r\n");
+
+		if (reST.match(response, 0, matches)) {
+			if (response.substr(matches[2].offset, matches[2].length) != deviceType) {
+				continue;
+			}
+		}
+
 		RegularExpression reLocation(
 			"(?i)(location): "
 			"http://([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}:[0-9]{1,5})"
@@ -63,7 +72,10 @@ list<SocketAddress> UPnP::discover(const Timespan& timeout, const string& device
 			continue;
 
 		SocketAddress searchingDevice(response.substr(matches[2].offset, matches[2].length));
-		listOfDevices.push_front(searchingDevice);
+
+		auto it = find(listOfDevices.begin(), listOfDevices.end(), searchingDevice);
+		if (it == listOfDevices.end())
+			listOfDevices.push_front(searchingDevice);
 	}
 
 	logger().information("found " + to_string(listOfDevices.size()) + " device(s) " + deviceType, __FILE__, __LINE__);