BeeeOn/gateway

commit 1d76d9a9ba620f7fe58e29ffc5ad73c41e4a9026

tools: generate Gateway certificate and key-pair
The gateway application requires to have a valid private key for self
identification and a certificate proving that it has the ID it proclaims
to have. This mechanism is used for deploying gateways however it makes
testing complicated.

To simplify the situation, this script generates the appropriate key-pair
and certificate. There are two usual use cases:

1. I want to quickly test the gateway in a testing environment and I have
   no requirements on the certificate. Solution (I need to have some valid
   gateway ID):

   $ tools/gateway-gencert.sh -G <ID>
   $ ls
   gateway.pem  gateway.pair ...

2. I have set up the testing environment (with a server) and I need a
   certificate that is acceptable by the testing server. The server
   trusts to a certification authority represented by a given key-pair.
   Solution:

   $ tools/gateway-gencert.sh -G <ID> -c ca.key -k gateway.pubkey
   $ ls
   gateway.pem  gateway.pair  gateway.pubkey ...

   The public key is likely unnecessary and can be used to verify signitures
   coming from such gateway or to encrypt data for the specific gateway.

The gateway ID must be generated by some external tool at the moment.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

tools/gateway-gencert.sh
@@ -1,0 +1,207 @@
+test -z "${C}"  && C="CZ"
+test -z "${ST}" && ST="Czech Republic"
+test -z "${L}"  && L="Brno"
+test -z "${O}"  && O="BeeeOn"
+test -z "${EMAIL}" && EMAIL="testing@beeeon.org"
+
+test -z "${RSA_SIZE}" && RSA_SIZE=4096
+test -z "${CERT_DAYS}" && CERT_DAYS=1
+
+# Prepare string to include as subject in the generated gateway certificate.
+# Only the Gateway ID is an input argument. The rest can be overriden from
+# the environment.
+cert_subj()
+{
+	id="$1"
+
+	echo "/CN=${id}/C=${C}/ST=${ST}/L=${L}/O=${O}/emailAddress=${EMAIL}"
+}
+
+# Generate an RSA key-pair into the given file.
+rsa_gen_pair()
+{
+	file="$1"
+	size="$2"
+
+	openssl genrsa -out "${file}" ${size}
+}
+
+# Extract the public key from the given RSA key-pair.
+rsa_extract_public()
+{
+	file="$1"
+	out="$2"
+
+	openssl rsa -in "${file}" -pubout -out "${out}"
+}
+
+# Generate certificate signing request for a given key-pair and certificate
+# subject. The subject is likely to be generated by cert_subj().
+cert_request()
+{
+	key_pair="$1"
+	subj="$2"
+
+	openssl req -sha256 -new -key "${key_pair}" -nodes -subj "${subj}"
+}
+
+# Sign the request by the key-pair from a selected CA. Self-signing is also
+# possible by passing the same key-pair that has been used for the CSR generation.
+# Validity is given in a number of days.
+cert_sign()
+{
+	request="$1"
+	key_pair="$2"
+	days="$3"
+	out="$4"
+
+	openssl x509 -in "${request}" -out "${out}" -req -signkey "${key_pair}" -days ${days}
+}
+
+# Compute SHA-256 digest of the given file.
+mk_digest()
+{
+	file="$1"
+
+	openssl dgst -sha256 "${file}"
+}
+
+# Compute SHA-256 fingerprint of the given certificate.
+cert_fingerprint()
+{
+	file="$1"
+
+	openssl x509 -sha256 -in "${file}" -noout -fingerprint
+}
+
+die()
+{
+	echo "$@" >&2
+	exit 1
+}
+
+# ==============================
+
+opts=`getopt -o "hfp:c:G:o:" -- "$@"`
+if [ "$?" != 0 ]; then
+	die "invalid options given: $@"
+fi
+
+eval set -- "${opts}"
+
+while [ -n "$1" ]; do
+	case "$1" in
+	-h)
+		echo "$0 [-h] [-f] options -G <id>"
+		echo "  -p <file> - provide a gateway key-pair, otherwise a new one is generated"
+		echo "  -k <file> - file where to extract the gateway public key"
+		echo "  -c <file> - provide a CA key-pair, otherwise the gateway key-pair is used (self-signed)"
+		echo "  -o <file> - target file containing the signed gateway certificate"
+		echo "  -f        - force overriding of the existing signed gateway certificate"
+		echo "  -G <id>   - provide gateway ID (mandatory)"
+
+		exit 0
+	;;
+	-p)
+		gateway_pair="$2"
+		shift
+		shift
+		;;
+	-k)
+		gateway_pubkey="$2"
+		shift
+		shift
+		;;
+	-c)
+		ca_pair="$2"
+		shift
+		shift
+		;;
+	-G)
+		gateway_id="$2"
+		shift
+		shift
+		;;
+	-o)
+		gateway_cert="$2"
+		shift
+		shift
+		;;
+	-f)
+		force=y
+		shift
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		break
+		;;
+	esac
+done
+
+# Gateway ID is mandatory and must always be given.
+if [ -z "${gateway_id}" ]; then
+	die "missing mandatory gateway ID (option -G)"
+fi
+
+if [ -z "${gateway_cert}" ]; then
+	gateway_cert="gateway.pem"
+fi
+
+# File where to store the generated and signed gateway certificate
+# must not exists unless -f option is set.
+if [ -f "${gateway_cert}" ]; then
+	if [ "${force}" != "y" ]; then
+		die "certificate '${gateway_cert}' already exists"
+	else
+		echo "Overriding existing '${gateway_cert}'" >&2
+	fi
+fi
+
+if [ -z "${gateway_pair}" ]; then
+	gateway_pair="gateway.pair"
+fi
+
+# Generate gateway RSA key-pair if it does not exist.
+if [ ! -f "${gateway_pair}" ]; then
+	rsa_gen_pair "${gateway_pair}" ${RSA_SIZE} \
+		|| die "failed to generate gateway key-pair"
+fi
+
+# Public key is generated alone if requested.
+if [ -n "${gateway_pubkey}" ]; then
+	rsa_extract_public "${gateway_pair}" "${gateway_pubkey}"
+fi
+
+# Signing is possible by a CA key-pair or if it is not given
+# the gateway key-pair is used (leads to a self-signed certificate).
+if [ -z "${ca_pair}" ]; then
+	ca_pair="${gateway_pair}"
+elif [ ! -f "${ca_pair}" ]; then
+	ca_pair="ca.pair"
+	rsa_gen_pair "${ca_pair}" ${RSA_SIZE} || die "failed to generate CA key-pair"
+fi
+
+csr="${gateway_cert}.csr"
+
+cert_request "${gateway_pair}" `cert_subj "${gateway_id}"` > "${csr}" \
+	|| die "failed to create gateway certificate signing request"
+
+cert_sign "${csr}" "${ca_pair}" ${CERT_DAYS} "${gateway_cert}" \
+	|| die "failed to sign gateway certificate"
+
+# List digests and fingerprints of the resulted files.
+mk_digest "${gateway_pair}"
+
+if [ -n "${gateway_pubkey}" ]; then
+	mk_digest "${gateway_pubkey}"
+fi
+
+mk_digest "${gateway_cert}"
+cert_fingerprint "${gateway_cert}"
+
+if [ "${gateway_pair}" != "${ca_pair}" ]; then
+	mk_digest "${ca_pair}"
+fi

commit e5e6d5d324c8b334b5a2a5b14d51dac69917ae34

tools: fix subject in generated certificate
If a field in the subject contains a space, the generated certificate
would contain a wrong contents like:

"/CN=1806037847304877/C=CZ/ST=Czech"

instead of

"/CN=1806037847304877/C=CZ/ST=Czech Republic/L=Brno/O=BeeeOn
	/emailAddress=testing@beeeon.org"

Fixes: 1d76d9a9ba62 ("tools: generate Gateway certificate and key-pair")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

tools/gateway-gencert.sh
@@ -202,7 +202,7 @@ fi
 
 csr="${gateway_cert}.csr"
 
-cert_request "${gateway_pair}" `cert_subj "${gateway_id}"` > "${csr}" \
+cert_request "${gateway_pair}" "`cert_subj "${gateway_id}"`" > "${csr}" \
 	|| die "failed to create gateway certificate signing request"
 
 cert_sign "${csr}" "${ca_pair}" ${CERT_DAYS} "${gateway_cert}" "${ca_cert}" \