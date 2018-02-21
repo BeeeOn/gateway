BeeeOn/gateway

commit dcf222fed451e5f510fbb06f142f2183ce0eeb7b

JablotronDevice: new class
JablotronDevice is a common ancestor of classes that represent
particular Jablotron devices. The devices uses different messages
for communication and provides different types of data. For this
purpose, the methods extractSensorData(), moduleTypes() are to be
implemented.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/jablotron/JablotronDevice.cpp
@@ -1,0 +1,72 @@
+#include <Poco/NumberParser.h>
+#include <Poco/StringTokenizer.h>
+
+#include "jablotron/JablotronDevice.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+static const int LOW_BATTERY = 5;
+static const int FULL_BATTERY = 100;
+
+const Timespan JablotronDevice::REFRESH_TIME_SUPPORTED_BEACON = -1;
+const Timespan JablotronDevice::REFRESH_TIME_UNSUPPORTED_BEACON = 9 * Timespan::MINUTES;
+
+JablotronDevice::~JablotronDevice()
+{
+}
+
+JablotronDevice::Ptr JablotronDevice::create(uint32_t serialNumber)
+{
+	throw InvalidArgumentException(
+		"unsupported device: " + to_string(serialNumber));
+}
+
+JablotronDevice::JablotronDevice(
+		const DeviceID &deviceID, const string &name):
+	m_deviceID(deviceID),
+	m_name(name)
+{
+}
+
+int JablotronDevice::parseValue(const string &msg) const
+{
+	StringTokenizer token(msg, ":");
+	return NumberParser::parse(token[1]);
+}
+
+void JablotronDevice::setPaired(bool paired)
+{
+	m_paired = paired;
+}
+
+bool JablotronDevice::paired() const
+{
+	return m_paired;
+}
+
+DeviceID JablotronDevice::buildID(uint32_t serialNumber)
+{
+	return DeviceID(DevicePrefix::PREFIX_JABLOTRON, serialNumber);
+}
+
+DeviceID JablotronDevice::deviceID() const
+{
+	return m_deviceID;
+}
+
+int JablotronDevice::extractBatteryLevel(const string &battery)
+{
+	return parseValue(battery) ? LOW_BATTERY : FULL_BATTERY;
+}
+
+string JablotronDevice::name() const
+{
+	return m_name;
+}
+
+Timespan JablotronDevice::refreshTime()
+{
+	return REFRESH_TIME_UNSUPPORTED_BEACON;
+}

src/CMakeLists.txt
@@ -52,6 +52,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/PinCredentials.cpp
 	${PROJECT_SOURCE_DIR}/exporters/MosquittoExporter.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
+	${PROJECT_SOURCE_DIR}/jablotron/JablotronDevice.cpp
 	${PROJECT_SOURCE_DIR}/net/AbstractHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp

src/jablotron/JablotronDevice.h
@@ -1,0 +1,83 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+#include <Poco/SharedPtr.h>
+
+#include "model/ModuleType.h"
+#include "model/DeviceID.h"
+#include "model/SensorData.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class JablotronDevice : public Loggable {
+public:
+	typedef Poco::SharedPtr<JablotronDevice> Ptr;
+
+	virtual ~JablotronDevice();
+
+	void setPaired(bool paired);
+	bool paired() const;
+
+	DeviceID deviceID() const;
+
+	static DeviceID buildID(uint32_t serialNumber);
+	static JablotronDevice::Ptr create(uint32_t serialNumber);
+
+	/**
+	 * It should extract the device-specific values and DeviceID from
+	 * Jablotron message and creates SensorData.
+	 * Example of Jablotron message: [XXXXXXXX] JA-81M SENSOR LB:? ACT:1
+	 *
+	 * See: https://www.turris.cz/gadgets/manual
+	 */
+	virtual SensorData extractSensorData(const std::string &message) = 0;
+
+	/**
+	 * List of supported value types with attributes.
+	 */
+	virtual std::list<ModuleType> moduleTypes() = 0;
+
+	/**
+	 * Name of Jablotron device.
+	 */
+	std::string name() const;
+
+	virtual Poco::Timespan refreshTime();
+
+protected:
+	JablotronDevice(const DeviceID &deviceID, const std::string &name);
+
+	/**
+	 * It divides a string consisting of two parts. Delimiter of these
+	 * two parts is colon. The first part contains value type and
+	 * the second part contains value.
+	 * Example: ACT:0 or LB:0
+	 */
+	int parseValue(const std::string &msg) const;
+
+	/**
+	 * Extracts battery level from string. String contains two parts.
+	 * The first part is "LB" (value type) and the second part is
+	 * battery level. Delimiter of these two parts is colon.
+	 * Battery level is 0 or 1. 0 doesn't indicate low battery level (100%),
+	 * and 1 indicates low battery level (5%).
+	 * Example of input string: LB:0 or LB:1
+	 *
+	 * See: https://www.turris.cz/gadgets/manual
+	 */
+	int extractBatteryLevel(const std::string &battery);
+
+protected:
+	static const Poco::Timespan REFRESH_TIME_SUPPORTED_BEACON;
+	static const Poco::Timespan REFRESH_TIME_UNSUPPORTED_BEACON;
+
+private:
+	DeviceID m_deviceID;
+	bool m_paired;
+	std::string m_name;
+};
+
+}

commit 3a0a1edbcb61dce68bbd3bc628ce709333565cfa

JablotronDevice: fix refresh time
Jablotron device that supports beacon meesage has refresh time
set to 9 minutes, the same refresh time is set also in BeeeOn
system. If device does not support refresh time, refresh time
in the BeeeOn system is set to -1. These constants were mistaken.

Fixes: dcf222fe ("JablotronDevice: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/jablotron/JablotronDevice.cpp
@@ -17,8 +17,8 @@ using namespace std;
 static const int LOW_BATTERY = 5;
 static const int FULL_BATTERY = 100;
 
-const Timespan JablotronDevice::REFRESH_TIME_SUPPORTED_BEACON = -1;
-const Timespan JablotronDevice::REFRESH_TIME_UNSUPPORTED_BEACON = 9 * Timespan::MINUTES;
+const Timespan JablotronDevice::REFRESH_TIME_SUPPORTED_BEACON = 9 * Timespan::MINUTES;
+const Timespan JablotronDevice::REFRESH_TIME_UNSUPPORTED_BEACON = -1;
 
 JablotronDevice::~JablotronDevice()
 {