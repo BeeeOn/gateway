BeeeOn/gateway

commit 01aa86e5a2ea0eb47d693830b85a2adaf31a3ba8

NewDeviceCommand: new class
The command is sent after listen command for informing of server
about new devices and data types, which sensor sends to server.

Each new device contains DeviceID, vendor, product name, data types
and default refresh time.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/commands/NewDeviceCommand.cpp
@@ -1,0 +1,52 @@
+#include <Poco/Nullable.h>
+
+#include "commands/NewDeviceCommand.h"
+
+using namespace BeeeOn;
+using namespace std;
+using namespace Poco;
+
+NewDeviceCommand::NewDeviceCommand(const DeviceID &deviceID, const string &vendor,
+	const string &productName, list<ModuleType> &dataTypes, Timespan refresh_time):
+		Command("NewDeviceCommand"),
+		m_deviceID(deviceID),
+		m_vendor(vendor),
+		m_productName(productName),
+		m_dataTypes(dataTypes),
+		m_refreshTime(refresh_time)
+{
+}
+
+NewDeviceCommand::~NewDeviceCommand()
+{
+}
+
+DeviceID NewDeviceCommand::deviceID() const
+{
+	return DeviceID();
+}
+
+string NewDeviceCommand::vendor() const
+{
+	return m_vendor;
+}
+
+string NewDeviceCommand::productName() const
+{
+	return m_productName;
+}
+
+list<ModuleType> NewDeviceCommand::dataTypes() const
+{
+	return m_dataTypes;
+}
+
+bool NewDeviceCommand::supportRefreshTime() const
+{
+	return m_refreshTime.totalSeconds() < 0;
+}
+
+Timespan NewDeviceCommand::refreshtime() const
+{
+	return m_refreshTime;
+}

src/commands/NewDeviceCommand.h
@@ -1,0 +1,46 @@
+#ifndef BEEEON_NEW_DEVICE_COMMAND_H
+#define BEEEON_NEW_DEVICE_COMMAND_H
+
+#include <list>
+
+#include <Poco/Timespan.h>
+
+#include "core/Command.h"
+#include "core/ModuleType.h"
+#include "model/DeviceID.h"
+
+namespace BeeeOn {
+
+/*
+ * The command that informs the server about new device and
+ * data types, which it sends.
+ */
+class NewDeviceCommand : public Command {
+public:
+	typedef Poco::AutoPtr<NewDeviceCommand> Ptr;
+
+	NewDeviceCommand(const DeviceID &deviceID, const std::string &vendor,
+		const std::string &productName, std::list<ModuleType> &dataTypes,
+		Poco::Timespan refresh_time = -1);
+
+	DeviceID deviceID() const;
+	std::string vendor() const;
+	std::string productName() const;
+	std::list<ModuleType> dataTypes() const;
+	bool supportRefreshTime() const;
+	Poco::Timespan refreshtime() const;
+
+protected:
+	~NewDeviceCommand();
+
+private:
+	DeviceID m_deviceID;
+	std::string m_vendor;
+	std::string m_productName;
+	std::list<ModuleType> m_dataTypes;
+	Poco::Timespan m_refreshTime;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -14,6 +14,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/commands/DeviceSetValueCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceUnpairCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/GatewayListenCommand.cpp
+	${PROJECT_SOURCE_DIR}/commands/NewDeviceCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/ServerDeviceListCommand.cpp
 	${PROJECT_SOURCE_DIR}/commands/ServerDeviceListResult.cpp
 	${PROJECT_SOURCE_DIR}/commands/ServerLastValueCommand.cpp

src/core/TestingCenter.cpp
@@ -1,5 +1,6 @@
 #include <Poco/NumberFormatter.h>
 #include <Poco/NumberParser.h>
+#include <Poco/StringTokenizer.h>
 
 #include "di/Injectable.h"
 #include "commands/DeviceSetValueCommand.h"

commit b54967a4fae2aa53495fc630ba351b2b99cdfa19

NewDeviceCommand: fix name of method refreshtime()
The method refreshtime() follows wrong naming conventions.

Fixes: 01aa86e5a2ea ("NewDeviceCommand: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/commands/NewDeviceCommand.cpp
@@ -45,7 +45,7 @@ bool NewDeviceCommand::supportRefreshTime() const
 	return m_refreshTime.totalSeconds() < 0;
 }
 
-Timespan NewDeviceCommand::refreshtime() const
+Timespan NewDeviceCommand::refreshTime() const
 {
 	return m_refreshTime;
 }

src/commands/NewDeviceCommand.h
@@ -28,7 +28,7 @@ public:
 	std::string productName() const;
 	std::list<ModuleType> dataTypes() const;
 	bool supportRefreshTime() const;
-	Poco::Timespan refreshtime() const;
+	Poco::Timespan refreshTime() const;
 
 	std::string toString() const override;
 

src/server/GWServerConnector.cpp
@@ -562,7 +562,7 @@ void GWServerConnector::doNewDeviceCommand(NewDeviceCommand::Ptr cmd,
 	request->setDeviceID(cmd->deviceID());
 	request->setProductName(cmd->productName());
 	request->setVendor(cmd->vendor());
-	request->setRefreshTime(cmd->refreshtime());
+	request->setRefreshTime(cmd->refreshTime());
 	request->setModuleTypes(cmd->dataTypes());
 
 	m_outputQueue.enqueue(new GWRequestContext(request, result));