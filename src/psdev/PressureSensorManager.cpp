BeeeOn/gateway

commit c47e7068cffda8954cb58bd618c90f6e6f5cf7ad

PressureSensorManager: new class
New class for managing BeeeOn gateway's internal air pressure sensor.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/psdev/PressureSensorManager.cpp
@@ -1,0 +1,221 @@
+#include <fstream>
+
+#include <Poco/Exception.h>
+#include <Poco/FileStream.h>
+#include <Poco/Hash.h>
+
+#include "commands/NewDeviceCommand.h"
+#include "core/CommandDispatcher.h"
+#include "di/Injectable.h"
+#include "model/SensorData.h"
+#include "model/SensorValue.h"
+#include "psdev/PressureSensorManager.h"
+#include "util/IncompleteTimestamp.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, PressureSensorManager)
+BEEEON_OBJECT_CASTABLE(CommandHandler)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_REF("distributor", &PressureSensorManager::setDistributor)
+BEEEON_OBJECT_REF("commandDispatcher", &PressureSensorManager::setCommandDispatcher)
+BEEEON_OBJECT_TIME("refresh", &PressureSensorManager::setRefresh)
+BEEEON_OBJECT_TEXT("path", &PressureSensorManager::setPath)
+BEEEON_OBJECT_TEXT("vendor", &PressureSensorManager::setVendor)
+BEEEON_OBJECT_END(BeeeOn, PressureSensorManager)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+#define PRODUCT "Air Pressure Sensor"
+
+static const list<ModuleType> TYPES =
+	{
+		{ModuleType::Type::TYPE_PRESSURE}
+	};
+
+PressureSensorManager::PressureSensorManager():
+	DeviceManager(DevicePrefix::PREFIX_PRESSURE_SENSOR),
+	m_paired(false),
+	m_refresh(15 * Timespan::SECONDS),
+	m_vendor("BeeeOn")
+{
+}
+
+PressureSensorManager::~PressureSensorManager()
+{
+}
+
+void PressureSensorManager::run()
+{
+	poco_information(logger(), "pressure sensor started");
+
+	initialize();
+
+	while(!m_stop) {
+		if (!m_paired) {
+			m_event.wait();
+			continue;
+		}
+
+		shipValue();
+		m_event.tryWait(m_refresh.totalMilliseconds());
+	}
+
+	poco_information(logger(), "pressure sensor finished");
+	m_stop = false;
+}
+
+void PressureSensorManager::stop()
+{
+	m_stop = true;
+	m_event.set();
+}
+
+void PressureSensorManager::initialize()
+{
+	set<DeviceID> devices;
+	try {
+		devices = deviceList(-1);
+	}
+	catch (const Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+	}
+
+	if (devices.size() > 1) {
+		poco_warning(logger(),
+			"obtained more than one paired sensor: "
+			+ to_string(devices.size()));
+	}
+
+	if (devices.find(pairedID()) != devices.end())
+		m_paired = true;
+}
+
+bool PressureSensorManager::accept(const Command::Ptr cmd)
+{
+	if (cmd->is<GatewayListenCommand>())
+		return true;
+	if (cmd->is<DeviceAcceptCommand>())
+		return cmd->cast<DeviceAcceptCommand>().deviceID().prefix() == DevicePrefix::PREFIX_PRESSURE_SENSOR;
+	if (cmd->is<DeviceUnpairCommand>())
+		return cmd->cast<DeviceUnpairCommand>().deviceID().prefix() == DevicePrefix::PREFIX_PRESSURE_SENSOR;
+
+	return false;
+}
+
+void PressureSensorManager::handle(const Command::Ptr cmd, Answer::Ptr answer)
+{
+	if (cmd->is<GatewayListenCommand>())
+		handleListenCommand(cmd->cast<GatewayListenCommand>(), answer);
+	else if (cmd->is<DeviceAcceptCommand>())
+		handleAcceptCommand(cmd->cast<DeviceAcceptCommand>(), answer);
+	else if (cmd->is<DeviceUnpairCommand>())
+		handleUnpairCommand(cmd->cast<DeviceUnpairCommand>(), answer);
+	else
+		throw IllegalStateException("received unaccepted command");
+}
+
+void PressureSensorManager::handleListenCommand(const GatewayListenCommand &, Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	if (!m_paired) {
+		dispatch(new NewDeviceCommand(
+			pairedID(),
+			m_vendor,
+			PRODUCT,
+			TYPES,
+			m_refresh));
+	}
+	result->setStatus(Result::Status::SUCCESS);
+}
+
+void PressureSensorManager::handleAcceptCommand(const DeviceAcceptCommand &cmd, Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	if (cmd.deviceID() != pairedID()) {
+		poco_warning(logger(), "not accepting device with unknown id: "
+			+ cmd.deviceID().toString());
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	if (!m_paired) {
+		m_paired = true;
+		m_event.set();
+	}
+	else {
+		poco_warning(logger(), "ignoring accept of already paired device");
+	}
+
+	result->setStatus(Result::Status::SUCCESS);
+}
+
+void PressureSensorManager::handleUnpairCommand(const DeviceUnpairCommand &cmd, Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	if (cmd.deviceID() != pairedID()) {
+		poco_warning(logger(), "not unpairing device with unknown id: "
+			+ cmd.deviceID().toString());
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	if (m_paired) {
+		m_paired = false;
+		result->setStatus(Result::Status::SUCCESS);
+	}
+	else {
+		poco_warning(logger(), "ignoring unpair of not paired device");
+		result->setStatus(Result::Status::FAILED);
+	}
+}
+
+void PressureSensorManager::setRefresh(const Timespan &refresh)
+{
+	if (refresh < 0)
+		throw InvalidArgumentException("refresh time must be positive");
+
+	m_refresh = refresh;
+}
+
+void PressureSensorManager::setPath(const string &path)
+{
+	m_path = path;
+}
+
+void PressureSensorManager::setVendor(const string &vendor)
+{
+	m_vendor = vendor;
+}
+
+void PressureSensorManager::shipValue()
+{
+	SensorData data;
+
+	data.setDeviceID(pairedID());
+
+	try {
+		double value;
+		FileInputStream fStream(m_path);
+		fStream >> value;
+		data.insertValue(SensorValue(ModuleID(0), value));
+		ship(data);
+	}
+	catch (const Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+	}
+}
+
+DeviceID PressureSensorManager::pairedID()
+{
+	return buildID(m_path);
+}
+
+DeviceID PressureSensorManager::buildID(const string &path)
+{
+	const uint64_t h = Poco::hash(path);
+	return DeviceID(DevicePrefix::PREFIX_PRESSURE_SENSOR, h & DeviceID::IDENT_MASK);
+}

CMakeLists.txt
@@ -33,6 +33,7 @@ option(ENABLE_MOSQUITTO_EXPORTER "Enable support of Mosquitto Exporting" ON)
 option(ENABLE_BASIC_DISTRIBUTOR "Enable support of Basic Distributor" ON)
 option(ENABLE_QUEUING_DISTRIBUTOR "Enable support of Queuing Distributor" ON)
 option(ENABLE_TESTING_CENTER "Enable support of Testing Center" ON)
+option(ENABLE_PRESSURE_SENSOR "Enable support of internal air pressure sensor" ON)
 
 add_subdirectory(src)
 add_subdirectory(base)

conf/config.d/factory.xml
@@ -2,6 +2,7 @@
 	<factory>
 		<instance name="main" class="BeeeOn::LoopRunner">
 			<add name="loops" ref="gwServerConnector" if-yes="${gws.enable}"/>
+			<add name="runnables" ref="pressureSensorManager" if-yes="${psdev.enable}" />
 			<add name="runnables" ref="belkinwemoDeviceManager" if-yes="${belkinwemo.enable}" />
 			<add name="runnables" ref="bluetoothAvailability" if-yes="${bluetooth.enable}" />
 			<add name="runnables" ref="testingCenter" if-yes="${testing.center.enable}" />
@@ -36,6 +37,7 @@
 			<add name="handlers" ref="virtualDeviceManager" if-yes="${vdev.enable}"/>
 			<add name="handlers" ref="vptDeviceManager" if-yes="${vpt.enable}"/>
 			<add name="handlers" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
+			<add name="handlers" ref="pressureSensorManager" if-yes="${psdev.enable}"/>
 		</instance>
 
 		<instance name="gwServerConnector" class="BeeeOn::GWServerConnector">
@@ -63,6 +65,14 @@
 			<add name="listeners" ref="bluetoothAvailability" if-yes="${bluetooth.enable}" />
 			<set name="listeners" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 			<set name="listeners" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
+		</instance>
+
+		<instance name="pressureSensorManager" class="BeeeOn::PressureSensorManager">
+			<set name="refresh" time="${psdev.refresh}" />
+			<set name="path" text="${psdev.path}" />
+			<set name="vendor" text="${psdev.vendor}" />
+			<set name="distributor" ref="distributor" />
+			<set name="commandDispatcher" ref="commandDispatcher" />
 		</instance>
 
 		<instance name="belkinwemoDeviceManager" class="BeeeOn::BelkinWemoDeviceManager">

conf/gateway-startup.ini
@@ -1,6 +1,12 @@
 [application]
 di.runner = main
 
+[psdev]
+enable = yes
+path = /sys/devices/platform/soc@01c00000/1c2b400.i2c/i2c-2/2-0077/pressure0_input
+vendor = BeeeOn
+refresh = 30 s
+
 [belkinwemo]
 enable = yes
 upnp.timeout = 5

src/CMakeLists.txt
@@ -202,6 +202,15 @@ else()
 	message(STATUS "Testing Center support is disabled")
 endif()
 
+if (ENABLE_PRESSURE_SENSOR)
+	file(GLOB PRESSURE_SENSOR_SOURCES
+		${PROJECT_SOURCE_DIR}/psdev/PressureSensorManager.cpp
+	)
+	list(APPEND SOURCES ${PRESSURE_SENSOR_SOURCES})
+else()
+	message(STATUS "Internal air pressure sensor support is disabled")
+endif()
+
 include_directories(
 	${PROJECT_SOURCE_DIR}
 	${CMAKE_SOURCE_DIR}/base/src

src/psdev/PressureSensorManager.h
@@ -1,0 +1,85 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Event.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceUnpairCommand.h"
+#include "commands/GatewayListenCommand.h"
+#include "core/DeviceManager.h"
+#include "model/DeviceID.h"
+#include "model/ModuleType.h"
+
+namespace BeeeOn {
+
+/**
+ * Manager for BeeeOn gateway's internal air pressure sensor.
+ * Actuall value is obtained by reading from specified file typically
+ * located in sysfs.
+ *
+ * This class instance is restricted for managing only one physical device
+ * with one module representing pressure.
+ *
+ * Measured values are reported periodically with specified refresh time.
+ *
+ * This file is typically named "pressure0_input"
+ */
+class PressureSensorManager : public DeviceManager {
+public:
+
+	PressureSensorManager();
+	~PressureSensorManager();
+
+	void run() override;
+	void stop() override;
+
+	bool accept(const Command::Ptr cmd) override;
+	void handle(const Command::Ptr cmd, Answer::Ptr answer) override;
+
+	void setRefresh(const Poco::Timespan &refresh);
+
+	/**
+	 * @brief sets the Path to Air Pressure Sensor device
+	 * entry output file. Reading from this file invoke measuring
+	 * of sensoric value and returns current measured Air pressure.
+	 */
+	void setPath(const std::string &path);
+	void setVendor(const std::string &vendor);
+
+private:
+	/**
+	 * @brief obtain list of paired devices from a server and
+	 * eventually sets the state of this instance to paired.
+	 */
+	void initialize();
+
+	/**
+	 * @brief read value of air pressure and ship it to the Distributor.
+	 */
+	void shipValue();
+	DeviceID pairedID();
+
+	/**
+	 * @brief create DeviceID with PREFIX_PRESSURE_SENSOR. Suffix
+	 * is calculated by hash of specified path to device output file.
+	 */
+	DeviceID buildID(const std::string &path);
+
+	void handleListenCommand(const GatewayListenCommand &cmd, Answer::Ptr answer);
+	void handleAcceptCommand(const DeviceAcceptCommand &cmd, Answer::Ptr answer);
+	void handleUnpairCommand(const DeviceUnpairCommand &cmd, Answer::Ptr answer);
+
+private:
+	Poco::AtomicCounter m_paired;
+	Poco::Timespan m_refresh;
+	std::string m_vendor;
+	std::string m_path;
+	Poco::Event m_event;
+};
+
+}

commit 1e358e3cf9d34b96d210ca2af8de65506d6a41d3

PressureSensorManager: fix incorrect value
The ModuleType of pressure value is in [hPa]. Value is
extracted in [Pa] from the sensor. This conversion was
missing.

Fixes: c47e7068cffd ("PressureSensorManager: new class")

Reported-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/psdev/PressureSensorManager.cpp
@@ -201,6 +201,7 @@ void PressureSensorManager::shipValue()
 		double value;
 		FileInputStream fStream(m_path);
 		fStream >> value;
+		value = value / 100.0; // convert to hPa
 		data.insertValue(SensorValue(ModuleID(0), value));
 		ship(data);
 	}