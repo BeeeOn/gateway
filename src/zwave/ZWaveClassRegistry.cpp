BeeeOn/gateway

commit 68a6b345e12050f9105c3aed9c4eaebe5f76c281

ZWaveRegistry: new class
Each value (OpenZWave::ValueID) from Z-Wave device contains
information about CommnadClass and index. CommnadClass marks
group of value, for example:

 * COMMAND_CLASS_SENSOR_MULTILEVEL - measured values (temperature,
   humidity, luminance)
 * COMMAND_CLASS_BATTERY - battery level

The index identifies measured value within group (CommandClass),
for example for COMMAND_CLASS_SENSOR_MULTILEVEL:

 * 0x01 - air temperature
 * 0x03 - luminance
 * 0x05 - humidity
 * 0x3C - resiratory rate

Some CommandClasses enable to define meaning of individual values
(indexes), for example:

 * COMMAND_CLASS_CONFIGURATION

This implementation supports:

 * 0x25 - COMMAND_CLASS_SWITCH_BINARY

 * 0x31 - COMMAND_CLASS_SENSOR_MULTILEVEL
   - 0x01 - air temperature
   - 0x03 - luminance
   - 0x05 - humidity
   - 0x1b - ultraviolet light

 * 0x80 - COMMAND_CLASS_BATTERY
   - 0x80 - battery level

The ZWaveRegistry enables to define key (CommandClass:Index)
for mapping OpenZWave::ValeID to ModuleType.

The ZWaveCommonRegistry contains CommandClasses and indexes
for all devices for mapping of ModuleType.

The ZWaveProductRegistry contains of specific product
CommandClasses and indexes for mapping of ModuleType.

This implementation does not support command classes and indexes
that map bitmaps. Implementation does not support mapping of command
class and index to various values that can be received.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/zwave/ZWaveClassRegistry.cpp
@@ -1,0 +1,91 @@
+#include <Poco/SingletonHolder.h>
+
+#include "zwave/ZWaveClassRegistry.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+
+static ZWaveCommandClassMap COMMON_TYPES = {
+	{{37, 0x00}, {ModuleType::Type::TYPE_ON_OFF}},
+	{{49, 0x01}, {ModuleType::Type::TYPE_TEMPERATURE}},
+	{{49, 0x03}, {ModuleType::Type::TYPE_LUMINANCE}},
+	{{49, 0x05}, {ModuleType::Type::TYPE_HUMIDITY}},
+	{{49, 0x1b}, {ModuleType::Type::TYPE_ULTRAVIOLET}},
+	{{128, 0x00}, {ModuleType::Type::TYPE_BATTERY}},
+};
+
+ZWaveClassRegistry::~ZWaveClassRegistry()
+{
+}
+
+ZWaveCommonClassRegistry::ZWaveCommonClassRegistry() :
+	m_impl(COMMON_TYPES)
+{
+}
+
+ZWaveCommonClassRegistry &ZWaveCommonClassRegistry::instance()
+{
+	static SingletonHolder<ZWaveCommonClassRegistry> sh;
+	return *sh.get();
+}
+
+ModuleType ZWaveCommonClassRegistry::find(
+	uint8_t commandClass, uint8_t index)
+{
+	return m_impl.find(commandClass, index);
+}
+
+bool ZWaveCommonClassRegistry::contains(
+	uint8_t commandClass, uint8_t index)
+{
+	return m_impl.contains(commandClass, index);
+}
+
+ZWaveGenericClassRegistry::ZWaveGenericClassRegistry(
+		const ZWaveCommandClassMap &map):
+	m_map(map)
+{
+}
+
+ModuleType ZWaveGenericClassRegistry::find(
+	uint8_t commandClass, uint8_t index)
+{
+	auto it = m_map.find({commandClass, index});
+	if (it != m_map.end()) {
+		return it->second;
+	}
+
+	throw InvalidArgumentException("ModuleType is not registered");
+}
+
+bool ZWaveGenericClassRegistry::contains(
+	uint8_t commandClass, uint8_t index)
+{
+	return m_map.find({commandClass, index}) != m_map.end();
+}
+
+ZWaveProductClassRegistry::ZWaveProductClassRegistry(
+		const ZWaveCommandClassMap &map):
+	m_impl(map)
+{
+}
+
+ModuleType ZWaveProductClassRegistry::find(
+	uint8_t commandClass, uint8_t index)
+{
+	if (m_impl.contains(commandClass, index))
+		return m_impl.find(commandClass, index);
+
+	return ZWaveCommonClassRegistry::instance()
+		.find(commandClass, index);
+}
+
+bool ZWaveProductClassRegistry::contains(
+	uint8_t commandClass, uint8_t index)
+{
+	if (m_impl.contains(commandClass, index))
+		return true;
+
+	return ZWaveCommonClassRegistry::instance()
+		.contains(commandClass, index);
+}

src/CMakeLists.txt
@@ -81,6 +81,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/vdev/VirtualModule.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualDevice.cpp
 	${PROJECT_SOURCE_DIR}/vdev/VirtualDeviceManager.cpp
+	${PROJECT_SOURCE_DIR}/zwave/ZWaveClassRegistry.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveDriver.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWavePocoLoggerAdapter.cpp
 )

src/zwave/ZWaveClassRegistry.h
@@ -1,0 +1,84 @@
+#pragma once
+
+#include <Poco/SharedPtr.h>
+
+#include "model/ModuleType.h"
+
+namespace BeeeOn {
+
+struct ZWaveCommandClassKey {
+	uint8_t commandClassID;
+	uint8_t index;
+
+	bool operator <(const ZWaveCommandClassKey &id) const
+	{
+		return commandClassID < id.commandClassID || index < id.index;
+	}
+};
+
+typedef std::map<ZWaveCommandClassKey, ModuleType> ZWaveCommandClassMap;
+
+/**
+ * Interface for querying ModuleType based on Z-Wave data types.
+ */
+class ZWaveClassRegistry {
+public:
+	typedef Poco::SharedPtr<ZWaveClassRegistry> Ptr;
+
+	virtual ~ZWaveClassRegistry();
+
+	virtual ModuleType find(
+		uint8_t commandClass, uint8_t index) = 0;
+
+	virtual bool contains(
+		uint8_t commandClass, uint8_t index) = 0;
+};
+
+/**
+ * It ensures to find and check of ModuleType in map.
+ */
+class ZWaveGenericClassRegistry : public ZWaveClassRegistry {
+public:
+	ZWaveGenericClassRegistry(const ZWaveCommandClassMap &map);
+
+	ModuleType find(uint8_t commandClass, uint8_t index) override;
+	bool contains(uint8_t commandClass, uint8_t index) override;
+
+private:
+	ZWaveCommandClassMap m_map;
+};
+
+/**
+ * Implementation of common Z-Wave command classes,
+ * common indexes and their ModuleType for all devices.
+ */
+class ZWaveCommonClassRegistry : public ZWaveClassRegistry {
+public:
+	ZWaveCommonClassRegistry();
+
+	ModuleType find(uint8_t commandClass, uint8_t index) override;
+	bool contains(uint8_t commandClass, uint8_t index) override;
+
+	static ZWaveCommonClassRegistry& instance();
+
+private:
+	ZWaveGenericClassRegistry m_impl;
+};
+
+/**
+ * Implementation of specific product of Z-Wave command classes,
+ * its indexes and their ModuleType.
+ */
+class ZWaveProductClassRegistry : public ZWaveClassRegistry {
+public:
+	ZWaveProductClassRegistry(const ZWaveCommandClassMap &map);
+
+	ModuleType find(uint8_t commandClass, uint8_t index) override;
+
+	bool contains(uint8_t commandClass, uint8_t index) override;
+
+private:
+	ZWaveGenericClassRegistry m_impl;
+};
+
+}

test/CMakeLists.txt
@@ -25,6 +25,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsStorageTest.cpp
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsTest.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatterTest.cpp
+	${PROJECT_SOURCE_DIR}/zwave/ZWaveClassRegistryTest.cpp
 )
 
 include_directories(

test/zwave/ZWaveClassRegistryTest.cpp
@@ -1,0 +1,59 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "zwave/ZWaveClassRegistry.h"
+
+using namespace Poco;
+
+namespace BeeeOn {
+
+class ZWaveClassRegistryTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(ZWaveClassRegistryTest);
+	CPPUNIT_TEST(testCommonCommandClass);
+	CPPUNIT_TEST(testProductCommandClass);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void testCommonCommandClass();
+	void testProductCommandClass();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ZWaveClassRegistryTest);
+
+void ZWaveClassRegistryTest::testCommonCommandClass()
+{
+	ZWaveCommandClassMap emptyMap;
+	ZWaveClassRegistry::Ptr zwaveRegistry(new ZWaveProductClassRegistry(emptyMap));
+
+	CPPUNIT_ASSERT(zwaveRegistry->contains(49, 0x01));
+	CPPUNIT_ASSERT(!zwaveRegistry->contains(0, 0x00));
+
+	CPPUNIT_ASSERT_EQUAL(
+		zwaveRegistry->find(49, 1).type(),
+		ModuleType::Type::TYPE_TEMPERATURE
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		zwaveRegistry->find(0, 0).type(),
+		InvalidArgumentException
+	);
+}
+
+void ZWaveClassRegistryTest::testProductCommandClass()
+{
+	ZWaveCommandClassMap productMap = {
+		{{200,0x00}, {ModuleType::Type::TYPE_TEMPERATURE}},
+	};
+
+	ZWaveClassRegistry::Ptr zwaveRegistry(new ZWaveProductClassRegistry(productMap));
+
+	CPPUNIT_ASSERT(zwaveRegistry->contains(200, 0x00));
+	CPPUNIT_ASSERT(!zwaveRegistry->contains(200, 0x01));
+
+	CPPUNIT_ASSERT_EQUAL(
+		zwaveRegistry->find(200, 0).type(),
+		ModuleType::Type::TYPE_TEMPERATURE
+	);
+}
+
+}

commit d68995a5bdb764cba12edffd871d6b7ea94cb084

ZWaveRegistry: mark Type::TYPE_ON_OFF as controllable
Fixes: 68a6b345 ("ZWaveRegistry: new class")
Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/ZWaveClassRegistry.cpp
@@ -7,7 +7,7 @@ using namespace BeeeOn;
 using namespace Poco;
 
 static const ZWaveCommandClassMap COMMON_TYPES = {
-	{{37, 0x00}, {ModuleType::Type::TYPE_ON_OFF}},
+	{{37, 0x00}, {ModuleType::Type::TYPE_ON_OFF, {ModuleType::Attribute::ATTR_CONTROLLABLE}}},
 	{{49, 0x01}, {ModuleType::Type::TYPE_TEMPERATURE}},
 	{{49, 0x03}, {ModuleType::Type::TYPE_LUMINANCE}},
 	{{49, 0x05}, {ModuleType::Type::TYPE_HUMIDITY}},