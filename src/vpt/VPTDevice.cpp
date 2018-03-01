BeeeOn/gateway

commit 8befa91cb3c9e7ee87d84e1ff4167435ef77b9f0

VPTDevice: new class and tests
The class represents Regulator VPT LAN v1.0. It provides
controlling of the device. Also gathering of data from
sensors is managed.

Also incluldes unit tests for this class.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -1,0 +1,518 @@
+#include <Poco/DigestStream.h>
+#include <Poco/Event.h>
+#include <Poco/NumberFormatter.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/RegularExpression.h>
+#include <Poco/SHA1Engine.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/String.h>
+
+#include "model/DevicePrefix.h"
+#include "util/JsonUtil.h"
+#include "vpt/VPTBoilerModuleType.h"
+#include "vpt/VPTDevice.h"
+#include "vpt/VPTValuesParser.h"
+#include "vpt/VPTZoneModuleType.h"
+
+#define VPT_VENDOR "Thermona"
+#define EXTRACT_ZONE_MASK 0x0007000000000000
+#define OMIT_SUBDEVICE_MASK 0xff00ffffffffffff
+#define MAX_ATTEMPTS 3
+#define SETTING_DELAY_MS 15000
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace Poco::Net;
+using namespace std;
+
+/*
+ * Registers belong gradually to zones 1, 2, 3, 4.
+ */
+const vector<string> VPTDevice::REG_BOILER_OPER_TYPE =
+	{"PE040","PE041","PE042","PE043"};
+
+const vector<string> VPTDevice::REG_BOILER_OPER_MODE =
+	{"PE044","PE045","PE046","PE047"};
+
+const vector<string> VPTDevice::REG_MAN_ROOM_TEMP =
+	{"PE086","PE087","PE088","PE089"};
+
+const vector<string> VPTDevice::REG_MAN_WATER_TEMP =
+	{"PE094","PE095","PE096","PE097"};
+
+const vector<string> VPTDevice::REG_MAN_TUV_TEMP =
+	{"PE098","PE099","PE100","PE101"};
+
+const vector<string> VPTDevice::REG_MOD_WATER_TEMP =
+	{"PE075","PE076","PE077","PE078"};
+
+const std::list<ModuleType> VPTDevice::ZONE_MODULE_TYPES = {
+	// MOD_BOILER_OPERATION_TYPE
+	ModuleType(ModuleType::Type::TYPE_ENUM,
+		VPTZoneModuleType(VPTZoneModuleType::MOD_BOILER_OPERATION_TYPE).toString()),
+	// MOD_BOILER_OPERATION_MODE
+	ModuleType(ModuleType::Type::TYPE_ENUM,
+		VPTZoneModuleType(VPTZoneModuleType::MOD_BOILER_OPERATION_MODE).toString()),
+	// MOD_REQUESTED_ROOM_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_CURRENT_ROOM_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_REQUESTED_WATER_TEMPERATURE_SET
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_CURRENT_WATER_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MANUAL_REQUESTED_ROOM_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MANUAL_REQUESTED_WATER_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MANUAL_REQUESTED_TUV_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE)
+};
+
+const std::list<ModuleType> VPTDevice::BOILER_MODULE_TYPES = {
+	// MOD_BOILER_STATUS
+	ModuleType(ModuleType::Type::TYPE_ENUM,
+		VPTBoilerModuleType(VPTBoilerModuleType::MOD_BOILER_STATUS).toString()),
+	// MOD_BOILER_MODE
+	ModuleType(ModuleType::Type::TYPE_ENUM,
+		VPTBoilerModuleType(VPTBoilerModuleType::MOD_BOILER_MODE).toString()),
+	// MOD_CURRENT_WATER_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_CURRENT_OUTSIDE_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_AVERAGE_OUTSIDE_TEMPERATURE
+	ModuleType(ModuleType::Type::TYPE_TEMPERATURE),
+	// MOD_CURRENT_BOILER_PERFORMANCE
+	ModuleType(ModuleType::Type::TYPE_PERFORMANCE),
+	// MOD_CURRENT_BOILER_PRESSURE
+	ModuleType(ModuleType::Type::TYPE_PRESSURE),
+	// MOD_CURRENT_BOILER_ERROR
+	ModuleType(ModuleType::Type::TYPE_BITMAP,
+		VPTBoilerModuleType(VPTBoilerModuleType::MOD_CURRENT_BOILER_ERROR).toString()),
+};
+
+const int VPTDevice::COUNT_OF_ZONES = 4;
+
+
+VPTDevice::VPTDevice(const SocketAddress& address):
+	m_address(address),
+	m_executor(*this)
+{
+}
+
+VPTDevice::VPTDevice():
+	m_executor(*this)
+{
+}
+
+DeviceID VPTDevice::deviceID() const
+{
+	return m_deviceId;
+}
+
+SocketAddress VPTDevice::address() const
+{
+	return m_address;
+}
+
+void VPTDevice::setAddress(const SocketAddress& address)
+{
+	m_address = address;
+}
+
+void VPTDevice::setPassword(const string& pwd)
+{
+	m_password = pwd;
+}
+
+FastMutex& VPTDevice::lock()
+{
+	return m_lock;
+}
+
+bool VPTDevice::operator==(const VPTDevice& other) const
+{
+	return other.deviceID() == m_deviceId;
+}
+
+VPTDevice::Ptr VPTDevice::buildDevice(const SocketAddress& address, const Timespan& timeout)
+{
+	VPTDevice::Ptr device = new VPTDevice(address);
+	device->m_httpTimeout = timeout;
+	device->buildDeviceID();
+	return device;
+}
+
+void VPTDevice::buildDeviceID()
+{
+	HTTPRequest request;
+	request.setMethod(HTTPRequest::HTTP_GET);
+	request.setURI("/values.json");
+	HTTPEntireResponse response = sendRequest(request, m_httpTimeout);
+
+	Object::Ptr object = JsonUtil::parse(response.getBody());
+	string mac = object->getValue<string>("id");
+	m_deviceId = DeviceID(DevicePrefix::PREFIX_VPT, NumberParser::parseHex64(mac));
+}
+
+void VPTDevice::requestModifyState(const DeviceID& id, const ModuleID module,
+	const double value, Result::Ptr result)
+{
+	const int zone = VPTDevice::extractSubdeviceFromDeviceID(id);
+	if (zone == 0) {
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	switch (module.value()) {
+	case VPTZoneModuleType::MOD_BOILER_OPERATION_TYPE:
+		if (!m_executor.isRunning()) {
+			try {
+				m_executor.setZone(zone);
+				m_executor.setValue(value);
+				m_executor.setResult(result);
+				m_executor.start();
+			}
+			catch (const Exception &e) {
+				logger().log(e, __FILE__, __LINE__);
+				logger().critical("command executor thread failed to start", __FILE__, __LINE__);
+				result->setStatus(Result::Status::FAILED);
+			}
+		}
+		else {
+			logger().debug("command executor thread seems to be running already, dropping request to set",
+				__FILE__, __LINE__);
+			result->setStatus(Result::Status::FAILED);
+		}
+		break;
+	case VPTZoneModuleType::MOD_BOILER_OPERATION_MODE:
+		requestSetModBoilerOperationMode(zone, value, result);
+		break;
+	case VPTZoneModuleType::MANUAL_REQUESTED_ROOM_TEMPERATURE:
+		requestSetManualRoomTemperature(zone, value, result);
+		break;
+	case VPTZoneModuleType::MANUAL_REQUESTED_WATER_TEMPERATURE:
+		requestSetManualWaterTemperature(zone, value, result);
+		break;
+	case VPTZoneModuleType::MANUAL_REQUESTED_TUV_TEMPERATURE:
+		requestSetManualTUVTemperature(zone, value, result);
+		break;
+	case VPTZoneModuleType::MOD_REQUESTED_WATER_TEMPERATURE_SET:
+		requestSetModWaterTemperature(zone, value, result);
+		break;
+	}
+}
+
+void VPTDevice::requestSetModBoilerOperationType(const int zone, const int value, Result::Ptr result)
+{
+	string registr = REG_BOILER_OPER_TYPE.at(zone - 1);
+
+	string strValue;
+	for (auto item : VPTValuesParser::BOILER_OPERATION_TYPE) {
+		if (item.second == value)
+			strValue = item.first;
+	}
+
+	prepareAndSendRequest(registr, strValue);
+
+	/*
+	 * The success of request is probed in iterations with
+	 * some delay because the setting time of request can be long.
+	 */
+	for (int i = 0; i < MAX_ATTEMPTS; i++) {
+		Event event;
+		event.tryWait(SETTING_DELAY_MS / MAX_ATTEMPTS);
+
+		HTTPRequest request;
+		request.setMethod(HTTPRequest::HTTP_GET);
+		request.setURI("/values.json");
+		HTTPEntireResponse response = sendSetRequest(request);
+
+		string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+				VPTZoneModuleType(VPTZoneModuleType::MOD_BOILER_OPERATION_TYPE).toString());
+
+		if (strValue == newValue) {
+			result->setStatus(Result::Status::SUCCESS);
+			return;
+		}
+	}
+
+	result->setStatus(Result::Status::FAILED);
+}
+
+void VPTDevice::requestSetModBoilerOperationMode(const int zone, const int value, Result::Ptr result)
+{
+	string registr = REG_BOILER_OPER_MODE.at(zone - 1);
+
+	string strValue;
+	for (auto item : VPTValuesParser::BOILER_OPERATION_MODE) {
+		if (item.second == value)
+			strValue = item.first;
+	}
+
+	HTTPEntireResponse response = prepareAndSendRequest(registr, strValue);
+
+	string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+			VPTZoneModuleType(VPTZoneModuleType::MOD_BOILER_OPERATION_MODE).toString());
+
+	if (strValue == newValue)
+		result->setStatus(Result::Status::SUCCESS);
+	else
+		result->setStatus(Result::Status::FAILED);
+}
+
+void VPTDevice::requestSetManualRoomTemperature(const int zone, const double value, Result::Ptr result)
+{
+	string registr = REG_MAN_ROOM_TEMP.at(zone - 1);
+	string strValue = NumberFormatter::format(value, 1);
+	strValue = replace(strValue, ".", "%2C");
+
+	HTTPEntireResponse response = prepareAndSendRequest(registr, strValue);
+
+	string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+			VPTZoneModuleType(VPTZoneModuleType::MANUAL_REQUESTED_ROOM_TEMPERATURE).toString());
+
+	if (value == NumberParser::parseFloat(newValue, ',', '.'))
+		result->setStatus(Result::Status::SUCCESS);
+	else
+		result->setStatus(Result::Status::FAILED);
+}
+
+void VPTDevice::requestSetManualWaterTemperature(const int zone, const double value, Result::Ptr result)
+{
+	string registr = REG_MAN_WATER_TEMP.at(zone - 1);
+	string strValue = NumberFormatter::format(value, 0);
+
+	HTTPEntireResponse response = prepareAndSendRequest(registr, strValue);
+
+	string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+			VPTZoneModuleType(VPTZoneModuleType::MANUAL_REQUESTED_WATER_TEMPERATURE).toString());
+
+	if (value == NumberParser::parseFloat(newValue, ',', '.'))
+		result->setStatus(Result::Status::SUCCESS);
+	else
+		result->setStatus(Result::Status::FAILED);
+}
+
+void VPTDevice::requestSetManualTUVTemperature(const int zone, const double value, Result::Ptr result)
+{
+	string registr = REG_MAN_TUV_TEMP.at(zone - 1);
+	string strValue = NumberFormatter::format(value, 0);
+
+	HTTPEntireResponse response = prepareAndSendRequest(registr, strValue);
+
+	string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+			VPTZoneModuleType(VPTZoneModuleType::MANUAL_REQUESTED_TUV_TEMPERATURE).toString());
+
+	if (value == NumberParser::parseFloat(newValue, ',', '.'))
+		result->setStatus(Result::Status::SUCCESS);
+	else
+		result->setStatus(Result::Status::FAILED);
+}
+
+void VPTDevice::requestSetModWaterTemperature(const int zone, const double value, Result::Ptr result)
+{
+	string registr = REG_MOD_WATER_TEMP.at(zone - 1);
+	string strValue = NumberFormatter::format(value, 0);
+
+	HTTPEntireResponse response = prepareAndSendRequest(registr, strValue);
+
+	string newValue = parseZoneAttrFromJson(response.getBody(), zone,
+			VPTZoneModuleType(VPTZoneModuleType::MOD_REQUESTED_WATER_TEMPERATURE_SET).toString());
+
+	if (value == NumberParser::parseFloat(newValue, ',', '.'))
+		result->setStatus(Result::Status::SUCCESS);
+	else
+		result->setStatus(Result::Status::FAILED);
+}
+
+HTTPEntireResponse VPTDevice::prepareAndSendRequest(const string& registr, const string& value)
+{
+	HTTPRequest request;
+	request.setMethod(HTTPRequest::HTTP_GET);
+	request.setURI("/values.json?" + registr + "=" + value);
+	return sendSetRequest(request);
+}
+
+HTTPEntireResponse VPTDevice::sendSetRequest(HTTPRequest& request)
+{
+	HTTPEntireResponse response = sendRequest(request, m_httpTimeout);
+
+	try {
+		JsonUtil::parse(response.getBody());
+	}
+	catch (SyntaxException& e) {
+		request.setURI(request.getURI() + "&__HOSTPWD=" +
+			VPTDevice::generateHashPassword(m_password, VPTDevice::extractNonce(response.getBody())));
+		response = sendRequest(request, m_httpTimeout);
+	}
+
+	return response;
+}
+
+string VPTDevice::parseZoneAttrFromJson(const string& json, const int zone, const string& key)
+{
+	try {
+		Object::Ptr object = JsonUtil::parse(json);
+		object = object->getObject("sensors");
+		Object::Ptr sensor = object->getObject("ZONE_" + to_string(zone));
+		return sensor->getValue<string>(key);
+	}
+	catch (SyntaxException& e) {
+		logger().log(e, __FILE__, __LINE__);
+		logger().warning("can not retrieve data " + key, __FILE__, __LINE__);
+		return "";
+	}
+}
+
+DeviceID VPTDevice::createSubdeviceID(const int zone, const DeviceID& id)
+{
+	return id | ((uint64_t)zone << 48);
+}
+
+DeviceID VPTDevice::omitSubdeviceFromDeviceID(const DeviceID& id)
+{
+	VPTDevice::extractSubdeviceFromDeviceID(id);
+
+	return id & OMIT_SUBDEVICE_MASK;
+}
+
+int VPTDevice::extractSubdeviceFromDeviceID(const DeviceID& id)
+{
+	int zone = (id & EXTRACT_ZONE_MASK) >> 48;
+
+	if (0 <= zone && zone <= 4)
+		return zone;
+
+	throw InvalidArgumentException("invalid subdevice number " + to_string(zone));
+}
+
+string VPTDevice::extractNonce(const string& response)
+{
+	RegularExpression re("var randnum = ([0-9]+)");
+	RegularExpression::MatchVec matches;
+	string nonce;
+
+	if (re.match(response, 0, matches) != 0)
+		nonce = response.substr(matches[1].offset, matches[1].length);
+
+	return nonce;
+}
+
+string VPTDevice::generateHashPassword(const string& pwd, const string& random)
+{
+	SHA1Engine sha1;
+	DigestOutputStream str(sha1);
+
+	str << random + pwd;
+	str.flush();
+
+	const DigestEngine::Digest & digest = sha1.digest();
+	return DigestEngine::digestToHex(digest);
+}
+
+vector<SensorData> VPTDevice::requestValues()
+{
+	HTTPRequest request;
+	request.setMethod(HTTPRequest::HTTP_GET);
+	request.setURI("/values.json");
+
+	HTTPEntireResponse response = sendRequest(request, m_httpTimeout);
+
+	VPTValuesParser parser;
+	return parser.parse(m_deviceId, response.getBody());
+}
+
+vector<NewDeviceCommand::Ptr> VPTDevice::createNewDeviceCommands()
+{
+	vector<NewDeviceCommand::Ptr> vector;
+
+	std::list<ModuleType> zoneModules = VPTDevice::ZONE_MODULE_TYPES;
+	for (int i = 1; i <= COUNT_OF_ZONES; i++) {
+		vector.push_back(
+			new NewDeviceCommand(
+				VPTDevice::createSubdeviceID(i, m_deviceId),
+				VPT_VENDOR,
+				"Zone " + to_string(i),
+				zoneModules));
+	}
+
+	std::list<ModuleType> boilerModules = VPTDevice::BOILER_MODULE_TYPES;
+	vector.push_back(
+		new NewDeviceCommand(
+			m_deviceId,
+			VPT_VENDOR,
+			"Boiler",
+			boilerModules));
+
+	return vector;
+}
+
+HTTPEntireResponse VPTDevice::sendRequest(HTTPRequest& request, const Timespan& timeout) const
+{
+	HTTPClientSession http;
+	HTTPEntireResponse response;
+
+	http.setHost(m_address.host().toString());
+	http.setPort(m_address.port());
+	http.setTimeout(timeout);
+
+	logger().information("request: " + m_address.toString() + request.getURI(), __FILE__, __LINE__);
+
+	http.sendRequest(request);
+
+	istream &input = http.receiveResponse(response);
+	response.readBody(input);
+
+	const int status = response.getStatus();
+	if (status >= 400)
+		logger().warning("response: " + to_string(status), __FILE__, __LINE__);
+	else
+		logger().information("response: " + to_string(status), __FILE__, __LINE__);
+
+	return response;
+}
+
+VPTDevice::VPTCommandExecutor::VPTCommandExecutor(VPTDevice& parent):
+	m_parent(parent),
+	m_activity(this, &VPTDevice::VPTCommandExecutor::executeCommand)
+{
+}
+
+VPTDevice::VPTCommandExecutor::~VPTCommandExecutor()
+{
+	m_activity.stop();
+	m_activity.wait(SETTING_DELAY_MS);
+}
+
+void VPTDevice::VPTCommandExecutor::setZone(const int zone)
+{
+	m_zone = zone;
+}
+
+void VPTDevice::VPTCommandExecutor::setValue(const double value)
+{
+	m_value = value;
+}
+
+void VPTDevice::VPTCommandExecutor::setResult(const Result::Ptr result)
+{
+	m_result = result;
+}
+
+void VPTDevice::VPTCommandExecutor::start()
+{
+	m_activity.start();
+}
+
+void VPTDevice::VPTCommandExecutor::executeCommand()
+{
+	m_parent.requestSetModBoilerOperationType(m_zone, m_value, m_result);
+}
+
+bool VPTDevice::VPTCommandExecutor::isRunning()
+{
+	return m_activity.isRunning();
+}

src/CMakeLists.txt
@@ -131,6 +131,7 @@ endif()
 if(ENABLE_VPT)
 	file(GLOB VPT_SOURCES
 		${PROJECT_SOURCE_DIR}/vpt/VPTBoilerModuleType.cpp
+		${PROJECT_SOURCE_DIR}/vpt/VPTDevice.cpp
 		${PROJECT_SOURCE_DIR}/vpt/VPTValuesParser.cpp
 		${PROJECT_SOURCE_DIR}/vpt/VPTZoneModuleType.cpp
 	)

src/vpt/VPTDevice.h
@@ -1,0 +1,187 @@
+#ifndef BEEEON_VPT_DEVICE_H
+#define BEEEON_VPT_DEVICE_H
+
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <Poco/Activity.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/Mutex.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "commands/NewDeviceCommand.h"
+#include "core/Result.h"
+#include "model/DeviceID.h"
+#include "model/ModuleID.h"
+#include "model/ModuleType.h"
+#include "model/SensorData.h"
+#include "net/HTTPEntireResponse.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The class represents Thermona Regulator VPT LAN v1.0.
+ * It provides functions to control the VPT and to gather data from
+ * its sensors. Each VPT regulator consists of up to 4 zones and 1 boiler.
+ * This means one instance of VPTDevice takes care of 5 devices. Each subdevice
+ * has own DeviceID.
+ */
+class VPTDevice : protected Loggable {
+public:
+	/**
+	 * @brief Allows to execute a command in a separate thread.
+	 */
+	class VPTCommandExecutor {
+	public:
+		VPTCommandExecutor(VPTDevice& parent);
+		~VPTCommandExecutor();
+
+		void setZone(const int zone);
+		void setValue(const double value);
+		void setResult(const Result::Ptr result);
+
+		void start();
+		void executeCommand();
+		bool isRunning();
+
+	private:
+		VPTDevice& m_parent;
+
+		Poco::Activity<VPTCommandExecutor> m_activity;
+
+		int m_zone;
+		double m_value;
+		Result::Ptr m_result;
+	};
+
+	typedef Poco::SharedPtr<VPTDevice> Ptr;
+
+	static const std::vector<std::string> REG_BOILER_OPER_TYPE;
+	static const std::vector<std::string> REG_BOILER_OPER_MODE;
+	static const std::vector<std::string> REG_MAN_ROOM_TEMP;
+	static const std::vector<std::string> REG_MAN_WATER_TEMP;
+	static const std::vector<std::string> REG_MAN_TUV_TEMP;
+	static const std::vector<std::string> REG_MOD_WATER_TEMP;
+
+	static const std::list<ModuleType> ZONE_MODULE_TYPES;
+	static const std::list<ModuleType> BOILER_MODULE_TYPES;
+
+	static const int COUNT_OF_ZONES;
+
+private:
+	VPTDevice(const Poco::Net::SocketAddress& address);
+
+public:
+	VPTDevice();
+
+	/**
+	 * @brief Connects to specified address to fetch information for creating VPT Device.
+	 * If the device do not respond in specified timeout, Poco::TimeoutException is thrown.
+	 * @param &address IP address and port where the device is listening.
+	 * @param timeout HTTP timeout.
+	 * @return VPTDevice.
+	 */
+	static VPTDevice::Ptr buildDevice(const Poco::Net::SocketAddress& address,
+		const Poco::Timespan& timeout);
+
+	DeviceID deviceID() const;
+	Poco::Net::SocketAddress address() const;
+	void setAddress(const Poco::Net::SocketAddress& address);
+	void setPassword(const std::string& pwd);
+	Poco::FastMutex& lock();
+
+	/**
+	 * @param id Subdevice of VPT.
+	 * @param module Module of subdevice.
+	 * @param value Requested value.
+	 * @param result Will contains information about
+	 * the success of the request
+	 */
+	void requestModifyState(const DeviceID& id, const ModuleID module,
+		const double value, Result::Ptr result);
+
+	/**
+	 * @brief Gathers data from all sensors of subdevices.
+	 * @return List of SensorData.
+	 */
+	std::vector<SensorData> requestValues();
+
+	/**
+	 * @brief Returns list of new device commands of all subdevices.
+	 */
+	std::vector<NewDeviceCommand::Ptr> createNewDeviceCommands();
+
+	/**
+	 * @brief Compares two VPTs based on DeviceID.
+	 */
+	bool operator==(const VPTDevice& other) const;
+
+	/**
+	 * @brief Creates DeviceID from VPT DeviceID and
+	 * number of zone.
+	 */
+	static DeviceID createSubdeviceID(const int zone, const DeviceID& id);
+
+	/**
+	 * @brief Returns DeviceID of real VPT.
+	 */
+	static DeviceID omitSubdeviceFromDeviceID(const DeviceID& id);
+
+	/**
+	 * @brief Extracts subdevice number from DeviceID.
+	 */
+	static int extractSubdeviceFromDeviceID(const DeviceID& id);
+
+	static std::string extractNonce(const std::string& response);
+	static std::string generateHashPassword(const std::string& pwd, const std::string& response);
+
+private:
+	/**
+	 * @brief Creates DeviceID based on Mac address of device.
+	 */
+	void buildDeviceID();
+
+	void requestSetModBoilerOperationType(const int zone, const int value, Result::Ptr result);
+	void requestSetModBoilerOperationMode(const int zone, const int value, Result::Ptr result);
+	void requestSetManualRoomTemperature(const int zone, const double value, Result::Ptr result);
+	void requestSetManualWaterTemperature(const int zone, const double value, Result::Ptr result);
+	void requestSetManualTUVTemperature(const int zone, const double value, Result::Ptr result);
+	void requestSetModWaterTemperature(const int zone, const double value, Result::Ptr result);
+
+	std::string parseZoneAttrFromJson(const std::string& json, const int zone, const std::string& key);
+
+	HTTPEntireResponse prepareAndSendRequest(const std::string& registr, const std::string& value);
+
+	/**
+	 * @brief It sends the set HTTP request. If the password is required
+	 * it sends request with set password.
+	 * @param request HTTP request.
+	 * @return HTTP response.
+	 */
+	HTTPEntireResponse sendSetRequest(Poco::Net::HTTPRequest& request);
+	HTTPEntireResponse sendRequest(Poco::Net::HTTPRequest& request, const Poco::Timespan& timeout) const;
+
+private:
+	/**
+	 * DeviceIDs of subdevices are created by this DeviceID.
+	 * Also it is used to search a password to access in credentials storage.
+	 */
+	DeviceID m_deviceId;
+	Poco::Net::SocketAddress m_address;
+	std::string m_password;
+
+	Poco::Timespan m_httpTimeout;
+
+	VPTDevice::VPTCommandExecutor m_executor;
+	Poco::FastMutex m_lock;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -33,6 +33,13 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatterTest.cpp
 )
 
+if(ENABLE_VPT)
+	file(GLOB VPT_TEST_SOURCES
+		${PROJECT_SOURCE_DIR}/vpt/VPTDeviceTest.cpp
+	)
+	list(APPEND TEST_SOURCES ${VPT_TEST_SOURCES})
+endif()
+
 if(ENABLE_ZWAVE)
 	file(GLOB ZWAVE_TEST_SOURCES
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveClassRegistryTest.cpp

test/vpt/VPTDeviceTest.cpp
@@ -1,0 +1,163 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <string>
+
+#include <Poco/Exception.h>
+
+#include "cppunit/BetterAssert.h"
+
+#include "model/DeviceID.h"
+#include "model/DevicePrefix.h"
+#include "vpt/VPTDevice.h"
+
+using namespace Poco;
+using namespace std;
+
+namespace BeeeOn {
+
+class VPTDeviceTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(VPTDeviceTest);
+	CPPUNIT_TEST(testCreateSubdeviceID);
+	CPPUNIT_TEST(testOmitSubdeviceFromDeviceID);
+	CPPUNIT_TEST(testExtractSubdeviceFromDeviceID);
+	CPPUNIT_TEST(testExtractNonce);
+	CPPUNIT_TEST(testGenerateHashPassword);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void testCreateSubdeviceID();
+	void testOmitSubdeviceFromDeviceID();
+	void testExtractSubdeviceFromDeviceID();
+	void testExtractNonce();
+	void testGenerateHashPassword();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(VPTDeviceTest);
+
+/*
+ * @brief Test of creating DeviceIDs for all subdevices (4 zones + boiler).
+ */
+void VPTDeviceTest::testCreateSubdeviceID()
+{
+	DeviceID realVPTid(DevicePrefix::PREFIX_VPT, 0x0000112233445566);
+
+	// DeviceID for boiler
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::createSubdeviceID(0, realVPTid));
+	// DeviceID for zone 1
+	CPPUNIT_ASSERT(0xa401112233445566 == VPTDevice::createSubdeviceID(1, realVPTid));
+	// DeviceID for zone 2
+	CPPUNIT_ASSERT(0xa402112233445566 == VPTDevice::createSubdeviceID(2, realVPTid));
+	// DeviceID for zone 3
+	CPPUNIT_ASSERT(0xa403112233445566 == VPTDevice::createSubdeviceID(3, realVPTid));
+	// DeviceID for zone 4
+	CPPUNIT_ASSERT(0xa404112233445566 == VPTDevice::createSubdeviceID(4, realVPTid));
+}
+
+/*
+ * @brief Test of omitting subdevice number from DeviceID. Also
+ * it checks if it throws exception when the subdevice number is
+ * out of range.
+ */
+void VPTDeviceTest::testOmitSubdeviceFromDeviceID()
+{
+	DeviceID boilerID(DevicePrefix::PREFIX_VPT, 0x0000112233445566);
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::omitSubdeviceFromDeviceID(boilerID));
+
+	DeviceID zone1ID(DevicePrefix::PREFIX_VPT, 0x0001112233445566);
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::omitSubdeviceFromDeviceID(zone1ID));
+
+	DeviceID zone2ID(DevicePrefix::PREFIX_VPT, 0x0002112233445566);
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::omitSubdeviceFromDeviceID(zone2ID));
+
+	DeviceID zone3ID(DevicePrefix::PREFIX_VPT, 0x0003112233445566);
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::omitSubdeviceFromDeviceID(zone3ID));
+
+	DeviceID zone4ID(DevicePrefix::PREFIX_VPT, 0x0004112233445566);
+	CPPUNIT_ASSERT(0xa400112233445566 == VPTDevice::omitSubdeviceFromDeviceID(zone4ID));
+
+	DeviceID wrong1ID(DevicePrefix::PREFIX_VPT, 0x0005112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 5",
+		VPTDevice::omitSubdeviceFromDeviceID(wrong1ID),
+		InvalidArgumentException);
+
+	DeviceID wrong2ID(DevicePrefix::PREFIX_VPT, 0x000f112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 15",
+		VPTDevice::omitSubdeviceFromDeviceID(wrong2ID),
+		InvalidArgumentException);
+
+	DeviceID wrong3ID(DevicePrefix::PREFIX_VPT, 0x00ff112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 255",
+		VPTDevice::omitSubdeviceFromDeviceID(wrong3ID),
+		InvalidArgumentException);
+}
+
+/*
+ * @brief Test of extracting subdevice number from DeviceID. Also
+ * it checks if it throws exception when the subdevice number is
+ * out of range.
+ */
+void VPTDeviceTest::testExtractSubdeviceFromDeviceID()
+{
+	DeviceID boilerID(DevicePrefix::PREFIX_VPT, 0x0000112233445566);
+	CPPUNIT_ASSERT(0 == VPTDevice::extractSubdeviceFromDeviceID(boilerID));
+
+	DeviceID zone1ID(DevicePrefix::PREFIX_VPT, 0x0001112233445566);
+	CPPUNIT_ASSERT(1 == VPTDevice::extractSubdeviceFromDeviceID(zone1ID));
+
+	DeviceID zone2ID(DevicePrefix::PREFIX_VPT, 0x0002112233445566);
+	CPPUNIT_ASSERT(2 == VPTDevice::extractSubdeviceFromDeviceID(zone2ID));
+
+	DeviceID zone3ID(DevicePrefix::PREFIX_VPT, 0x0003112233445566);
+	CPPUNIT_ASSERT(3 == VPTDevice::extractSubdeviceFromDeviceID(zone3ID));
+
+	DeviceID zone4ID(DevicePrefix::PREFIX_VPT, 0x0004112233445566);
+	CPPUNIT_ASSERT(4 == VPTDevice::extractSubdeviceFromDeviceID(zone4ID));
+
+	DeviceID wrong1ID(DevicePrefix::PREFIX_VPT, 0x0005112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 5",
+		VPTDevice::extractSubdeviceFromDeviceID(wrong1ID),
+		InvalidArgumentException);
+
+	DeviceID wrong2ID(DevicePrefix::PREFIX_VPT, 0x000f112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 15",
+		VPTDevice::extractSubdeviceFromDeviceID(wrong2ID),
+		InvalidArgumentException);
+
+	DeviceID wrong3ID(DevicePrefix::PREFIX_VPT, 0x00ff112233445566);
+	CPPUNIT_ASSERT_THROW_MESSAGE("invalid subdevice number 255",
+		VPTDevice::extractSubdeviceFromDeviceID(wrong3ID),
+		InvalidArgumentException);
+}
+
+/*
+ * @brief Test of extracting random number from text.
+ */
+void VPTDeviceTest::testExtractNonce()
+{
+	string text1 = R"(text text text text text)"
+	               R"(text text var randnum = 42 text text)"
+	               R"(text text text text text)";
+	CPPUNIT_ASSERT("42" == VPTDevice::extractNonce(text1));
+
+	string text2 = R"(text text text text text\n)"
+	               R"(text text text text text\n)"
+	               R"(var randnum = 42\n)"
+	               R"(text text text text)";
+	CPPUNIT_ASSERT("42" == VPTDevice::extractNonce(text2));
+
+	string text3 = R"(var randnum = 42 text text text text)"
+	               R"(text text text text text)"
+	               R"(text text text text text)";
+	CPPUNIT_ASSERT("42" == VPTDevice::extractNonce(text3));
+}
+
+/*
+ * @brief Test of generating sha1 hash of password + random number.
+ */
+void VPTDeviceTest::testGenerateHashPassword()
+{
+	CPPUNIT_ASSERT("3c7b3a2ffe38d977df9d6fa4455c9f8403ce374f" == VPTDevice::generateHashPassword("password", "125"));
+	CPPUNIT_ASSERT("e9dee14b78ab69de5c91a0c7bc216c7652953b54" == VPTDevice::generateHashPassword("strongerpassword", "1025"));
+	CPPUNIT_ASSERT("4ede3da98ce049c6cedea83fa4b159aa84a6ef1c" == VPTDevice::generateHashPassword("theultrastrongestpassword", "10025"));
+}
+
+}

commit 389f9ec27428d59c38bc72104e3dca87b3aa058e

VPTDevice: fix invalid BOILER_OPERATION_TYPE set attempt
Fixes: 8befa91cb3c9 ("VPTDevice: new class and tests")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -280,6 +280,12 @@ void VPTDevice::requestSetModBoilerOperationType(const int zone, const int value
 			strValue = item.first;
 	}
 
+	if (strValue.empty()) {
+		throw InvalidArgumentException(
+			"value " + to_string(value)
+			+ " is invalid for BOILER_OPERATION_TYPE");
+	}
+
 	prepareAndSendRequest(registr, strValue);
 
 	stampVPT(Action::SET);