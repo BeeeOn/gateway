BeeeOn/gateway

commit 58bec2230bfe61ed44e5468cfe5ce9962b912ec5

GWServerConnector: implement handling of incoming messages
GWServerConnector is now able to handle incoming messages and
dispatching appropriate gateway commands to CommandDispatcher.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/GWServerConnector.cpp
@@ -3,10 +3,16 @@
 #include <Poco/Net/HTTPResponse.h>
 #include <Poco/Net/NetException.h>
 
+#include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceSetValueCommand.h"
+#include "commands/DeviceUnpairCommand.h"
+#include "commands/GatewayListenCommand.h"
 #include "commands/ServerDeviceListResult.h"
 #include "commands/ServerLastValueResult.h"
+#include "core/CommandDispatcher.h"
 #include "di/Injectable.h"
 #include "gwmessage/GWDeviceListRequest.h"
+#include "gwmessage/GWDeviceListResponse.h"
 #include "gwmessage/GWGatewayAccepted.h"
 #include "gwmessage/GWGatewayRegister.h"
 #include "gwmessage/GWLastValueRequest.h"
@@ -15,6 +21,7 @@
 #include "gwmessage/GWResponseWithAck.h"
 #include "gwmessage/GWSensorDataConfirm.h"
 #include "server/GWServerConnector.h"
+#include "server/ServerAnswer.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, GWServerConnector)
 BEEEON_OBJECT_CASTABLE(StoppableLoop)
@@ -30,6 +37,7 @@ BEEEON_OBJECT_TIME("resendTimeoutTimeout", &GWServerConnector::setResendTimeout)
 BEEEON_OBJECT_NUMBER("maxMessageSize", &GWServerConnector::setMaxMessageSize)
 BEEEON_OBJECT_REF("sslConfig", &GWServerConnector::setSSLConfig)
 BEEEON_OBJECT_REF("gatewayInfo", &GWServerConnector::setGatewayInfo)
+BEEEON_OBJECT_REF("commandDispatcher", &GWServerConnector::setCommandDispatcher)
 BEEEON_OBJECT_END(BeeeOn, GWServerConnector)
 
 using namespace std;
@@ -207,10 +215,8 @@ void GWServerConnector::runReceiver()
 				continue;
 
 			GWMessage::Ptr msg = receiveMessageUnlocked();
-			if (!msg.isNull()) {
-				poco_information(logger(), "received new message of type: "
-						+ msg->type().toString());
-			}
+			if (!msg.isNull())
+				handleMessage(msg);
 		}
 		catch (const Exception &e) {
 			logger().log(e, __FILE__, __LINE__);
@@ -512,4 +518,142 @@ void GWServerConnector::doLastValueCommand(
 	request->setModuleID(cmd->moduleID());
 
 	m_outputQueue.enqueue(new GWRequestContext(request, result));
+}
+
+void GWServerConnector::handleMessage(GWMessage::Ptr msg)
+{
+	if (!msg.cast<GWRequest>().isNull())
+		handleRequest(msg.cast<GWRequest>());
+	else if (!msg.cast<GWResponse>().isNull())
+		handleResponse(msg.cast<GWResponse>());
+	else if (!msg.cast<GWAck>().isNull())
+		handleAck(msg.cast<GWAck>());
+	else if (!msg.cast<GWSensorDataConfirm>().isNull())
+		handleSensorDataConfirm(msg.cast<GWSensorDataConfirm>());
+	else
+		throw InvalidArgumentException("bad message type " + msg->type().toString());
+}
+
+void GWServerConnector::handleRequest(GWRequest::Ptr request)
+{
+	switch (request->type()) {
+	case GWMessageType::DEVICE_ACCEPT_REQUEST:
+		handleDeviceAcceptRequest(request.cast<GWDeviceAcceptRequest>());
+		break;
+	case GWMessageType::LISTEN_REQUEST:
+		handleListenRequest(request.cast<GWListenRequest>());
+		break;
+	case GWMessageType::SET_VALUE_REQUEST:
+		handleSetValueRequest(request.cast<GWSetValueRequest>());
+		break;
+	case GWMessageType::UNPAIR_REQUEST:
+		handleUnpairRequest(request.cast<GWUnpairRequest>());
+		break;
+	default:
+		throw InvalidArgumentException("bad request type "+ request->type().toString());
+	}
+}
+
+void GWServerConnector::handleDeviceAcceptRequest(
+	GWDeviceAcceptRequest::Ptr request)
+{
+	DeviceAcceptCommand::Ptr command = new DeviceAcceptCommand(request->deviceID());
+
+	dispatchServerCommand(command, request->id(), request->derive());
+}
+
+void GWServerConnector::handleListenRequest(GWListenRequest::Ptr request)
+{
+	GatewayListenCommand::Ptr command =
+		new GatewayListenCommand(request->duration());
+
+	dispatchServerCommand(command, request->id(), request->derive());
+}
+
+void GWServerConnector::handleSetValueRequest(
+	GWSetValueRequest::Ptr request)
+{
+	DeviceSetValueCommand::Ptr command =
+		new DeviceSetValueCommand(
+			request->deviceID(),
+			request->moduleID(),
+			request->value(),
+			request->timeout());
+
+	dispatchServerCommand(command, request->id(), request->derive());
+}
+
+void GWServerConnector::handleUnpairRequest(GWUnpairRequest::Ptr request)
+{
+	DeviceUnpairCommand::Ptr command =
+		new DeviceUnpairCommand(request->deviceID());
+
+	dispatchServerCommand(command, request->id(), request->derive());
+}
+
+void GWServerConnector::dispatchServerCommand(
+		Command::Ptr cmd,
+		const GlobalID &id,
+		GWResponse::Ptr response)
+{
+	response->setStatus(GWResponse::Status::ACCEPTED);
+	GWResponseContext::Ptr context = new GWResponseContext(response);
+	m_outputQueue.enqueue(context);
+	dispatch(cmd, new ServerAnswer(answerQueue(), id));
+}
+
+void GWServerConnector::handleResponse(GWResponse::Ptr response)
+{
+	GWRequestContext::Ptr context = m_contextPoll.remove(response->id()).cast<GWRequestContext>();
+
+	if (context.isNull()) {
+		poco_warning(logger(), "no coresponding request found, dropping response of type "
+				+ response->toString()
+				+ " with id: " + response->id().toString());
+		return;
+	}
+
+	auto result = context->result();
+
+	if (response->status() == GWResponse::Status::SUCCESS) {
+		switch (response->type()) {
+		case GWMessageType::GENERIC_RESPONSE:
+			break;
+		case GWMessageType::DEVICE_LIST_RESPONSE:
+		{
+			ServerDeviceListResult::Ptr deviceListResult = result.cast<ServerDeviceListResult>();
+			if (deviceListResult.isNull())
+				throw IllegalStateException("request result do not match with response result");
+			deviceListResult->setDeviceList(response.cast<GWDeviceListResponse>()->devices());
+			break;
+		}
+		case GWMessageType::LAST_VALUE_RESPONSE:
+		{
+			ServerLastValueResult::Ptr lastValueResult = result.cast<ServerLastValueResult>();
+			if (lastValueResult.isNull())
+				throw IllegalStateException("request result do not match with response result");
+			lastValueResult->setValue(response.cast<GWLastValueResponse>()->value());
+			break;
+		}
+		default:
+			result->setStatus(Result::Status::FAILED);
+			throw InvalidArgumentException("bad response type "
+				+ response->type().toString());
+		}
+
+		result->setStatus(Result::Status::SUCCESS);
+	}
+	else {
+		result->setStatus(Result::Status::FAILED);
+	}
+}
+
+void GWServerConnector::handleSensorDataConfirm(GWSensorDataConfirm::Ptr confirm)
+{
+	m_contextPoll.remove(confirm->id());
+}
+
+void GWServerConnector::handleAck(GWAck::Ptr ack)
+{
+	m_contextPoll.remove(ack->id());
 }

src/server/GWServerConnector.h
@@ -15,8 +15,18 @@
 #include "commands/ServerDeviceListCommand.h"
 #include "commands/ServerLastValueCommand.h"
 #include "core/CommandHandler.h"
+#include "core/CommandSender.h"
 #include "core/GatewayInfo.h"
+#include "gwmessage/GWDeviceAcceptRequest.h"
+#include "gwmessage/GWListenRequest.h"
 #include "gwmessage/GWMessage.h"
+#include "gwmessage/GWMessage.h"
+#include "gwmessage/GWPingRequest.h"
+#include "gwmessage/GWRequest.h"
+#include "gwmessage/GWResponse.h"
+#include "gwmessage/GWSensorDataConfirm.h"
+#include "gwmessage/GWSetValueRequest.h"
+#include "gwmessage/GWUnpairRequest.h"
 #include "loop/StoppableLoop.h"
 #include "server/GWMessageContext.h"
 #include "server/GWSOutputQueue.h"
@@ -37,6 +47,7 @@ namespace BeeeOn {
  */
 class GWServerConnector :
 	public StoppableLoop,
+	public CommandSender,
 	public CommandHandler,
 	protected Loggable {
 public:
@@ -139,6 +150,43 @@ private:
 	void doNewDeviceCommand(NewDeviceCommand::Ptr cmd, Answer::Ptr answer);
 	void doDeviceListCommand(ServerDeviceListCommand::Ptr cmd, Answer::Ptr answer);
 	void doLastValueCommand(ServerLastValueCommand::Ptr cmd, Answer::Ptr answer);
+
+	void dispatchServerCommand(Command::Ptr cmd, const GlobalID &id, GWResponse::Ptr response);
+
+	/**
+	 * Handle request received from server. one of the specific "handle*Reqest()" method
+	 * is called to process request. An appropriate Command is dispatched eventually.
+	 */
+	void handleRequest(GWRequest::Ptr request);
+
+	void handleDeviceAcceptRequest(GWDeviceAcceptRequest::Ptr request);
+	void handleListenRequest(GWListenRequest::Ptr request);
+	void handleSetValueRequest(GWSetValueRequest::Ptr request);
+	void handleUnpairRequest(GWUnpairRequest::Ptr request);
+
+	/**
+	 * Handle received response message, corresponding request is first
+	 * found in GWContextPoll and its result is set.
+	 */
+	void handleResponse(GWResponse::Ptr response);
+
+	/**
+	 * Handle Ack message, corresponding context is found
+	 * in GWContextPoll and remove it from m_contextPoll.
+	 */
+	void handleAck(GWAck::Ptr ack);
+
+	/**
+	 * Handle confirmation of receiving exported SensorData, this will remove
+	 * message from GWContextPoll so its not resend.
+	 */
+	void handleSensorDataConfirm(GWSensorDataConfirm::Ptr confirm);
+
+	/**
+	 * Handle generic message from server, specific handle*() method is
+	 * invoked based on message type.
+	 */
+	void handleMessage(GWMessage::Ptr msg);
 
 	GWMessage::Ptr receiveMessageUnlocked();
 

conf/config.d/factory.xml
@@ -54,6 +54,7 @@
 			<set name="maxMessageSize" number="${gws.maxMessageSize}" />
 			<set name="gatewayInfo" ref="gatewayInfo" />
 			<set name="sslConfig" ref="gwsSSLClient" if-yes="${gws.ssl.enable}"/>
+			<set name="commandDispatcher" ref="commandDispatcher"/>
 		</instance>
 
 		<instance name="gwsSSLClient" class="BeeeOn::SSLClient">

commit b4f8a5abd03b2a9f4a5a6ab980b536a9e13bec0e

GWServerConnector: fix race condition in dispatching commands
There was a race condition when a Command accepted from server was
dispatched to system. Because dispatch() operation initializes answer previously
inserted into AnswerQueue without any locking mechanism, another thread can
possibly consider this uninitialized answer for completed with zero handlers.

Atomicity of this operations is secured by mutex.

Fixes: 58bec2230bfe ("GWServerConnector: implement handling of incoming messages")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/server/GWServerConnector.cpp
@@ -246,6 +246,7 @@ void GWServerConnector::markDisconnected()
 
 void GWServerConnector::enqueueFinishedAnswers()
 {
+	FastMutex::ScopedLock guard(m_dispatchLock);
 	list<Answer::Ptr> finishedAnswers = answerQueue().finishedAnswers();
 
 	for (auto answer : finishedAnswers) {
@@ -675,6 +676,7 @@ void GWServerConnector::dispatchServerCommand(
 		const GlobalID &id,
 		GWResponse::Ptr response)
 {
+	FastMutex::ScopedLock guard(m_dispatchLock);
 	response->setStatus(GWResponse::Status::ACCEPTED);
 	GWResponseContext::Ptr context = new GWResponseContext(response);
 	m_outputQueue.enqueue(context);

src/server/GWServerConnector.h
@@ -223,6 +223,8 @@ private:
 	Poco::FastMutex m_lastReceivedMutex;
 	int m_inactiveMultiplier;
 
+	Poco::FastMutex m_dispatchLock;
+
 	Poco::Buffer<char> m_receiveBuffer;
 	Poco::SharedPtr<Poco::Net::WebSocket> m_socket;
 	Poco::FastMutex m_receiveMutex;