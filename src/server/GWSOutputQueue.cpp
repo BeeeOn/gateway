BeeeOn/gateway

commit dd5af377b1959a8044295d35bbcb0ffba5636f6b

GWSOutputQueue: new class
Queue for all outgoing messages. Dequeue message order is based
on specified priority of specific GWMessageContext.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/GWSOutputQueue.cpp
@@ -1,0 +1,50 @@
+#include "server/GWSOutputQueue.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+GWSOutputQueue::GWSOutputQueue(Event &enqueueEvent):
+	m_enqueueEvent(enqueueEvent)
+{
+}
+
+GWSOutputQueue::~GWSOutputQueue()
+{
+	clear();
+}
+
+void GWSOutputQueue::enqueue(GWMessageContext::Ptr context)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	m_queue.push(context);
+	m_enqueueEvent.set();
+}
+
+GWMessageContext::Ptr GWSOutputQueue::dequeue()
+{
+	GWMessageContext::Ptr context;
+
+	FastMutex::ScopedLock guard(m_mutex);
+
+	if (!m_queue.empty()) {
+		context = m_queue.top();
+		m_queue.pop();
+	}
+
+	return context;
+}
+
+void GWSOutputQueue::clear()
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	poco_debug(logger(), "clearing queue with " + to_string(m_queue.size()) + " contexts enqueued");
+
+	while(!m_queue.empty()) {
+		GWMessageContext::Ptr context = m_queue.top();
+		poco_debug(logger(), "clearing context id: " + context->id().toString()
+				+ " type: " + context->message()->type().toString());
+		m_queue.pop();
+	}
+}

src/CMakeLists.txt
@@ -73,6 +73,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/net/VPTHTTPScanner.cpp
 	${PROJECT_SOURCE_DIR}/server/GWMessageContext.cpp
+	${PROJECT_SOURCE_DIR}/server/GWSOutputQueue.cpp
 	${PROJECT_SOURCE_DIR}/server/GWServerConnector.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatter.cpp

src/server/GWSOutputQueue.h
@@ -1,0 +1,38 @@
+#pragma once
+
+#include <queue>
+
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include "server/GWMessageContext.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Queue for all outgoing messages. Must be initialized with
+ * Poco::Event reference, which is notified on item enqueue.
+ */
+class GWSOutputQueue : public Loggable {
+public:
+	GWSOutputQueue(Poco::Event &enqueueEvent);
+	virtual ~GWSOutputQueue();
+
+	void enqueue(GWMessageContext::Ptr context);
+
+	GWMessageContext::Ptr dequeue();
+
+	void clear();
+
+private:
+	std::priority_queue<GWMessageContext::Ptr,
+			std::vector<GWMessageContext::Ptr>,
+			ContextPriorityComparator> m_queue;
+
+	Poco::FastMutex m_mutex;
+	Poco::Event &m_enqueueEvent;
+};
+
+}

commit c9949e77e370289259d286084b0fbe6a14022a52

GWSOutputQueue: fix set requests FAILED in case of clear()
Fixes: dd5af377b195 ("GWSOutputQueue: new class")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/server/GWSOutputQueue.cpp
@@ -45,6 +45,14 @@ void GWSOutputQueue::clear()
 		GWMessageContext::Ptr context = m_queue.top();
 		poco_debug(logger(), "clearing context id: " + context->id().toString()
 				+ " type: " + context->message()->type().toString());
+
+		GWRequestContext::Ptr requestContext = context.cast<GWRequestContext>();
+		if (!requestContext.isNull()) {
+			poco_debug(logger(), "setting status FAILED for context id: " + context->id().toString()
+					+ " type: " + context->message()->type().toString());
+			requestContext->result()->setStatus(Result::Status::FAILED);
+		}
+
 		m_queue.pop();
 	}
 }