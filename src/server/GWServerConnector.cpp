BeeeOn/gateway

commit d2767d2b4eb558e5e06da763527d6bdb852b1ef3

GWServerConnector: implement message sending
GWServerConnector is now capable of sending messages to server.
Messages of any kind inserted to its dedicated output queue are
now forwarded to the server.

Any responses or received requests are ignored until received message handling
is implemented in upcomming patches.

If a message requires to receive an answer in some specific timeout, its resend
is planned using Poco::Util::Timer.

Introducing m_readyToSendEvent, which is just temporary solution and will be replaced
with shared Event from AnswerQueue in upcomming patches.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/GWServerConnector.cpp
@@ -16,6 +16,8 @@ BEEEON_OBJECT_TIME("pollTimeout", &GWServerConnector::setPollTimeout)
 BEEEON_OBJECT_TIME("receiveTimeout", &GWServerConnector::setReceiveTimeout)
 BEEEON_OBJECT_TIME("sendTimeout", &GWServerConnector::setSendTimeout)
 BEEEON_OBJECT_TIME("retryConnectTimeout", &GWServerConnector::setRetryConnectTimeout)
+BEEEON_OBJECT_TIME("busySleep", &GWServerConnector::setBusySleep)
+BEEEON_OBJECT_TIME("resendTimeoutTimeout", &GWServerConnector::setResendTimeout)
 BEEEON_OBJECT_NUMBER("maxMessageSize", &GWServerConnector::setMaxMessageSize)
 BEEEON_OBJECT_REF("sslConfig", &GWServerConnector::setSSLConfig)
 BEEEON_OBJECT_REF("gatewayInfo", &GWServerConnector::setGatewayInfo)
@@ -26,18 +28,19 @@ using namespace Poco;
 using namespace Poco::Net;
 using namespace BeeeOn;
 
-static const Timespan RECONNECT_TIMEOUT = Timespan::SECONDS * 10;
-
 GWServerConnector::GWServerConnector():
 	m_port(0),
 	m_pollTimeout(250 * Timespan::MILLISECONDS),
 	m_receiveTimeout(3 * Timespan::SECONDS),
 	m_sendTimeout(1 * Timespan::SECONDS),
 	m_retryConnectTimeout(1 * Timespan::SECONDS),
+	m_busySleep(30 * Timespan::SECONDS),
+	m_resendTimeout(20 * Timespan::SECONDS),
 	m_maxMessageSize(4096),
 	m_receiveBuffer(m_maxMessageSize),
 	m_isConnected(false),
-	m_stop(false)
+	m_stop(false),
+	m_outputQueue(readyToSendEvent())
 {
 }
 
@@ -55,12 +58,17 @@ void GWServerConnector::stop()
 {
 	m_stop = true;
 	m_stopEvent.set();
+	readyToSendEvent().set();
 	m_connectedEvent.set();
 
 	m_senderThread.join();
 	m_receiverThread.join();
 
 	disconnectUnlocked();
+
+	m_timer.cancel(true);
+	m_outputQueue.clear();
+	m_contextPoll.clear();
 }
 
 void GWServerConnector::startSender()
@@ -79,7 +87,65 @@ void GWServerConnector::runSender()
 			continue;
 		}
 
-		m_stopEvent.tryWait(RECONNECT_TIMEOUT.totalMilliseconds());
+		forwardOutputQueue();
+	}
+}
+
+void GWServerConnector::forwardOutputQueue()
+{
+	try {
+		GWMessageContext::Ptr context = m_outputQueue.dequeue();
+		if (!context.isNull()) {
+			forwardContext(context);
+		}
+		else {
+			if (!readyToSendEvent().tryWait(m_busySleep.totalMilliseconds()))
+				sendPing();
+		}
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+		markDisconnected();
+	}
+	catch (const exception &e) {
+		logger().critical(e.what(), __FILE__, __LINE__);
+		markDisconnected();
+	}
+	catch (...) {
+		logger().critical("unknown error", __FILE__, __LINE__);
+		markDisconnected();
+	}
+}
+
+void GWServerConnector::forwardContext(GWMessageContext::Ptr context)
+{
+	if (!context.cast<GWTimedContext>().isNull()) {
+		GWTimedContext::Ptr timedContext = context.cast<GWTimedContext>();
+
+		const GlobalID &id = timedContext->id();
+
+		LambdaTimerTask::Ptr task = new LambdaTimerTask(
+			[this, id](){
+				GWMessageContext::Ptr context = m_contextPoll.remove(id);
+				if (!context.isNull())
+					m_outputQueue.enqueue(context);
+			}
+		);
+
+		timedContext->setMissingResponseTask(task);
+		m_contextPoll.insert(timedContext);
+
+		try {
+			sendMessage(timedContext->message());
+		} catch (const Exception &e) {
+			m_contextPoll.remove(id);
+			e.rethrow();
+		}
+
+		m_timer.schedule(task, Timestamp() + m_resendTimeout);
+	}
+	else if (!context.isNull()) {
+		sendMessage(context->message());
 	}
 }
 
@@ -89,6 +155,11 @@ void GWServerConnector::sendPing()
 	poco_trace(logger(), "sending ping frame");
 
 	m_socket->sendFrame("echo", 4, WebSocket::FRAME_OP_PING);
+}
+
+Event &GWServerConnector::readyToSendEvent()
+{
+	return m_readyToSendEvent;
 }
 
 void GWServerConnector::reconnect()
@@ -330,6 +401,22 @@ void GWServerConnector::setRetryConnectTimeout(const Timespan &timeout)
 		throw InvalidArgumentException("retryConnectTimeout must be non negative");
 
 	m_retryConnectTimeout = timeout;
+}
+
+void GWServerConnector::setBusySleep(const Poco::Timespan &busySleep)
+{
+	if (busySleep < 0)
+		throw InvalidArgumentException("retryConnectTimeout must be non negative");
+
+	m_busySleep = busySleep;
+}
+
+void GWServerConnector::setResendTimeout(const Poco::Timespan &timeout)
+{
+	if (timeout < 0)
+		throw InvalidArgumentException("retryConnectTimeout must be non negative");
+
+	m_resendTimeout = timeout;
 }
 
 void GWServerConnector::setMaxMessageSize(int size)

src/server/GWServerConnector.h
@@ -9,10 +9,14 @@
 #include <Poco/Thread.h>
 #include <Poco/Timespan.h>
 #include <Poco/Timestamp.h>
+#include <Poco/Util/Timer.h>
 
 #include "core/GatewayInfo.h"
 #include "gwmessage/GWMessage.h"
 #include "loop/StoppableLoop.h"
+#include "server/GWMessageContext.h"
+#include "server/GWSOutputQueue.h"
+#include "server/GWContextPoll.h"
 #include "ssl/SSLClient.h"
 #include "util/Loggable.h"
 
@@ -44,6 +48,8 @@ public:
 	void setReceiveTimeout(const Poco::Timespan &timeout);
 	void setSendTimeout(const Poco::Timespan &timeout);
 	void setRetryConnectTimeout(const Poco::Timespan &timeout);
+	void setBusySleep(const Poco::Timespan &timeout);
+	void setResendTimeout(const Poco::Timespan &timeout);
 	void setMaxMessageSize(int size);
 	void setGatewayInfo(Poco::SharedPtr<GatewayInfo> info);
 	void setSSLConfig(Poco::SharedPtr<SSLClient> config);
@@ -108,10 +114,23 @@ private:
 	 * Send ping frame to server.
 	 */
 	void sendPing();
+
+	/**
+	 * Forward given context to server. If nullptr is given, waits
+	 * for readyToSendEvent() to be notified or send ping frame after timeout.
+	 */
+	void forwardContext(GWMessageContext::Ptr context);
+
+	/**
+	 * Dequeue context from outputQueue and forward is to server.
+	 */
+	void forwardOutputQueue();
 	void sendMessage(const GWMessage::Ptr message);
 	void sendMessageUnlocked(const GWMessage::Ptr message);
 
 	GWMessage::Ptr receiveMessageUnlocked();
+
+	Poco::Event &readyToSendEvent();
 
 private:
 	std::string m_host;
@@ -120,6 +139,8 @@ private:
 	Poco::Timespan m_receiveTimeout;
 	Poco::Timespan m_sendTimeout;
 	Poco::Timespan m_retryConnectTimeout;
+	Poco::Timespan m_busySleep;
+	Poco::Timespan m_resendTimeout;
 	size_t m_maxMessageSize;
 	Poco::SharedPtr<GatewayInfo> m_gatewayInfo;
 	Poco::SharedPtr<SSLClient> m_sslConfig;
@@ -137,6 +158,11 @@ private:
 
 	Poco::AtomicCounter m_stop;
 	Poco::Event m_stopEvent;
+
+	Poco::Event m_readyToSendEvent;
+	GWContextPoll m_contextPoll;
+	GWSOutputQueue m_outputQueue;
+	Poco::Util::Timer m_timer;
 };
 
 }

commit 7f4ac00a3c28787a6c809aedec6355c9c0df9f9f

GWServerConnector: fix potential message drop
Due to an NetException (indicating connection error) raised in sending message to server,
the message was dropped completely. Instead of dropping, we schedule to send it again.
Message is still dropped if another type Exception is caught.

Fixes: d2767d2b4eb5 ("GWServerConnector: implement message sending")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/server/GWServerConnector.cpp
@@ -160,6 +160,10 @@ void GWServerConnector::forwardContext(GWMessageContext::Ptr context)
 
 		try {
 			sendMessage(timedContext->message());
+		} catch (const NetException &e) {
+			m_contextPoll.remove(id);
+			m_outputQueue.enqueue(context);
+			e.rethrow();
 		} catch (const Exception &e) {
 			m_contextPoll.remove(id);
 			e.rethrow();