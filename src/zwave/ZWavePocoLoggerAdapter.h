f45f84bb8d3a3c1303e2d70083e860fabad23778
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-03-07 00:01:37 +0100

    ZWavePocoLoggerAdapter: define method fromPocoLevel()
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
    Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

--- src/zwave/ZWavePocoLoggerAdapter.h
+++ src/zwave/ZWavePocoLoggerAdapter.h
@@ -7,6 +7,26 @@
 
 namespace BeeeOn {
 
+/**
+ * @brief ZWavePocoLoggerAdapter adapts the the logging infrastructure
+ * of the OpenZWave library to the Poco::Logger. It converts the
+ * OpenZWave logging levels as follows:
+ *
+ * - Fatal    - PRIO_FATAL
+ * - Error    - PRIO_ERROR
+ * - Warning  - PRIO_WARNING
+ * - Alert    - PRIO_NOTICE
+ * - Always   - PRIO_INFORMATION
+ * - Info     - PRIO_INFORMATION
+ * - Detail   - PRIO_DEBUG
+ * - Debug    - PRIO_DEBUG
+ * - StreamDetail - PRIO_TRACE
+ * - Internal - PRIO_TRACE
+ *
+ * Each created Poco::Message would also contain named parameter "node"
+ * holding the node ID. This can be used for better formatting of log
+ * messages.
+ */
 class ZWavePocoLoggerAdapter : public OpenZWave::i_LogImpl {
 public:
 	ZWavePocoLoggerAdapter(Poco::Logger& logger);
@@ -21,14 +41,35 @@ public:
 	void Write(OpenZWave::LogLevel logLevel, uint8 const nodeId,
 		char const* format, va_list args) override;
 
+	/**
+	 * @brief The implementation does nothing.
+	 */
 	void QueueDump() override;
+
+	/**
+	 * @brief The implementation does nothing.
+	 */
 	void QueueClear() override;
 
+	/**
+	 * @brief The implementation does nothing.
+	 */
 	void SetLoggingState(OpenZWave::LogLevel saveLevel,
 		OpenZWave::LogLevel queueLevel,
 		OpenZWave::LogLevel dumpTrigger) override;
 
+	/**
+	 * @brief The implementation does nothing.
+	 */
 	void SetLogFileName(const std::string& filename) override;
+
+	static OpenZWave::LogLevel fromPocoLevel(int prio)
+	{
+		return fromPocoLevel(
+			static_cast<Poco::Message::Priority>(prio));
+	}
+
+	static OpenZWave::LogLevel fromPocoLevel(Poco::Message::Priority prio);
 
 private:
 	Poco::Logger& logger;
