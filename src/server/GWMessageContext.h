BeeeOn/gateway

commit 84402961e968307935ce5c95bb3b2726d0f6a150

GWMessageContext: new classes
Introducing new classes for encapsulating outgoing
messages types and extending them with additional data.

GWMessageContext: base class, which contains unique
message id and message. It is meant to be derived by specific
generic contexts.

GWTimedContext: extending base GWMessageContext class by
generic task, which is typically used for messages awaiting
response to notify about missing response. Context for these
kind of messages should derive this class.

Implementations for direct use: GWRequestContext, GWResponseContext,
GWResponseWithAckContext, GWSensorDataExportContext.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/GWMessageContext.h
@@ -1,0 +1,142 @@
+#pragma once
+
+#include <Poco/SharedPtr.h>
+
+#include "core/Result.h"
+#include "gwmessage/GWRequest.h"
+#include "gwmessage/GWResponse.h"
+#include "gwmessage/GWResponseWithAck.h"
+#include "gwmessage/GWSensorDataExport.h"
+#include "model/GlobalID.h"
+#include "util/LambdaTimerTask.h"
+
+namespace BeeeOn {
+
+/**
+ * Priority for messages, message types with higher priority are defined
+ * lower in this enum.
+ */
+enum GWMessagePriority {
+	DEFAULT_PRIO = 0,
+	REQUEST_PRIO = 10,
+	RESPONSE_PRIO = 20,
+	RESPONSEWITHACK_PRIO = 30,
+	DATA_PRIO = 40,
+};
+
+/**
+ * Encapsulate GWMessage with its priority for sending. Higher priority
+ * means message is forwarded sooner.
+ *
+ * This is typically used for messages forwarded to server to hold an additional
+ * information needed in case of response will arrive or, in case its needed, to resend
+ * the message.
+ *
+ * This class is meant to be derived by specific message type context.
+ */
+class GWMessageContext {
+public:
+	typedef Poco::SharedPtr<GWMessageContext> Ptr;
+
+	GWMessageContext(GWMessagePriority priority);
+
+	virtual ~GWMessageContext();
+
+	GWMessage::Ptr message();
+	void setMessage(GWMessage::Ptr msg);
+
+	int priority() const;
+
+	GlobalID id() const;
+
+protected:
+	int m_priority;
+	GWMessage::Ptr m_message;
+};
+
+/**
+ * GWTimedContext extends base GWMessageContext with LambdaTimerTask.
+ * This class is meant to be derived by specific message type
+ * context, which expects response to be received in given time,
+ * otherwise the given task is executed.
+ */
+class GWTimedContext : public GWMessageContext {
+public:
+	typedef Poco::SharedPtr<GWTimedContext> Ptr;
+
+	GWTimedContext(GWMessagePriority priority);
+
+	LambdaTimerTask::Ptr missingResponseTask();
+	void setMissingResponseTask(LambdaTimerTask::Ptr task);
+
+private:
+	LambdaTimerTask::Ptr m_missingResponseTask;
+};
+
+/**
+ * GWRequestContext contains Result::Ptr of the BeeeOn Command
+ * executed by this request.
+ */
+class GWRequestContext : public GWTimedContext {
+public:
+	typedef Poco::SharedPtr<GWRequestContext> Ptr;
+
+	GWRequestContext();
+	GWRequestContext(GWRequest::Ptr request, Result::Ptr result);
+
+	Result::Ptr result();
+	void setResult(Result::Ptr result);
+private:
+	Result::Ptr m_result;
+};
+
+/**
+ * GWResponseContext is used to store GWResponse message with GWResponse priority.
+ */
+class GWResponseContext : public GWMessageContext {
+public:
+	typedef Poco::SharedPtr<GWResponseContext> Ptr;
+
+	GWResponseContext();
+	GWResponseContext(GWResponse::Ptr response);
+};
+
+/**
+ * GWResponseWithAckContext extends GWTimedContext with status of the
+ * running task, which was invoked by by a matching GWRequest message.
+ * Context of this type with status ACCEPTED has automatically higher priority,
+ * as its expected to be sent before FAILED or SUCCESS status.
+ */
+class GWResponseWithAckContext : public GWTimedContext {
+public:
+	typedef Poco::SharedPtr<GWResponseWithAckContext> Ptr;
+
+	GWResponseWithAckContext(GWResponse::Status status);
+
+	GWResponse::Status status();
+	void setStatus(GWResponse::Status);
+
+private:
+	GWResponse::Status m_status;
+};
+
+/**
+ * GWSensorDataExportContext holds data message.
+ */
+class GWSensorDataExportContext : public GWTimedContext {
+public:
+	typedef Poco::SharedPtr<GWSensorDataExportContext> Ptr;
+
+	GWSensorDataExportContext();
+};
+
+/**
+ * Comparator for GWMessageContext, which can be usued to construct priority_queue of
+ * GWMessageContext items, so the dequeue() operation always returns the highest priority message.
+ */
+class ContextPriorityComparator {
+public:
+	bool operator()(const GWMessageContext::Ptr a, const GWMessageContext::Ptr b);
+};
+
+}

src/CMakeLists.txt
@@ -72,6 +72,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/net/VPTHTTPScanner.cpp
+	${PROJECT_SOURCE_DIR}/server/GWMessageContext.cpp
 	${PROJECT_SOURCE_DIR}/server/GWServerConnector.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatter.cpp

src/server/GWMessageContext.cpp
@@ -1,0 +1,110 @@
+#include <Poco/Exception.h>
+
+#include "server/GWMessageContext.h"
+
+using namespace BeeeOn;
+
+GWMessageContext::GWMessageContext(GWMessagePriority priority):
+	m_priority(priority)
+{
+}
+
+GWMessageContext::~GWMessageContext()
+{
+}
+
+GWMessage::Ptr GWMessageContext::message()
+{
+	return m_message;
+}
+
+void GWMessageContext::setMessage(GWMessage::Ptr msg)
+{
+	m_message = msg;
+}
+
+int GWMessageContext::priority() const
+{
+	return m_priority;
+}
+
+GlobalID GWMessageContext::id() const
+{
+	if (m_message.isNull())
+		throw Poco::IllegalStateException("message not set");
+
+	return m_message->id();
+}
+
+GWTimedContext::GWTimedContext(GWMessagePriority priority):
+	GWMessageContext(priority)
+{
+}
+
+LambdaTimerTask::Ptr GWTimedContext::missingResponseTask()
+{
+	return m_missingResponseTask;
+}
+
+void GWTimedContext::setMissingResponseTask(LambdaTimerTask::Ptr task)
+{
+	m_missingResponseTask = task;
+}
+
+GWRequestContext::GWRequestContext():
+	GWTimedContext(REQUEST_PRIO)
+{
+}
+
+GWRequestContext::GWRequestContext(GWRequest::Ptr request, Result::Ptr result):
+	GWTimedContext(REQUEST_PRIO),
+	m_result(result)
+{
+	setMessage(request);
+}
+
+Result::Ptr GWRequestContext::result()
+{
+	return m_result;
+}
+
+void GWRequestContext::setResult(Result::Ptr result)
+{
+	m_result = result;
+}
+GWResponseContext::GWResponseContext():
+	GWMessageContext(RESPONSE_PRIO)
+{
+}
+
+GWResponseContext::GWResponseContext(GWResponse::Ptr response):
+	GWMessageContext(RESPONSE_PRIO)
+{
+	setMessage(response);
+}
+
+GWResponseWithAckContext::GWResponseWithAckContext(GWResponse::Status status):
+	GWTimedContext(RESPONSEWITHACK_PRIO),
+	m_status(status)
+{
+}
+
+GWResponse::Status GWResponseWithAckContext::status()
+{
+	return m_status;
+}
+
+void GWResponseWithAckContext::setStatus(GWResponse::Status status)
+{
+	m_status = status;
+}
+
+GWSensorDataExportContext::GWSensorDataExportContext():
+	GWTimedContext(DATA_PRIO)
+{
+}
+
+bool ContextPriorityComparator::operator()(const GWMessageContext::Ptr a, const GWMessageContext::Ptr b)
+{
+	return a->priority() < b->priority();
+}

commit d03b29fb57feda62980dd0226028e03b65ec6e56

GWMessageContext: fix incorrect context type priority
Because data messages had highest priority, it would be
possible to starve control messages, which are more important.
Also response type messages typically serve to instantly inform
the server, that gateway accepted incoming request.

Our desired, more logical priority is now:
DATA < REQUEST < RESPONSEWITHACK < RESPONSE

Fixes: 84402961e968 ("GWMessageContext: new classes")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/server/GWMessageContext.h
@@ -18,10 +18,10 @@ namespace BeeeOn {
  */
 enum GWMessagePriority {
 	DEFAULT_PRIO = 0,
-	REQUEST_PRIO = 10,
-	RESPONSE_PRIO = 20,
+	DATA_PRIO = 10,
+	REQUEST_PRIO = 20,
 	RESPONSEWITHACK_PRIO = 30,
-	DATA_PRIO = 40,
+	RESPONSE_PRIO = 40,
 };
 
 /**