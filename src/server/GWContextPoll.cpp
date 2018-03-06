BeeeOn/gateway

commit 951e210c3e189269e5fd40bb463b4916ed7c738d

GWContextPoll: new class
This class is used for storing record of messages succesfully sent to server,
so they can be later matched with response.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/server/GWContextPoll.cpp
@@ -1,0 +1,55 @@
+#include "server/GWContextPoll.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+GWContextPoll::~GWContextPoll()
+{
+	clear();
+}
+
+void GWContextPoll::insert(GWMessageContext::Ptr context)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+	m_messages.emplace(context->id(), context);
+}
+
+GWMessageContext::Ptr GWContextPoll::remove(const GlobalID &id)
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	GWMessageContext::Ptr context;
+
+	auto it = m_messages.find(id);
+	if (it != m_messages.end()) {
+		GWTimedContext::Ptr timedContext;
+		timedContext = it->second.cast<GWTimedContext>();
+		if (!timedContext.isNull())
+			timedContext->missingResponseTask()->cancel();
+
+		context = it->second;
+		m_messages.erase(it);
+	}
+
+	return context;
+}
+
+void GWContextPoll::clear()
+{
+	FastMutex::ScopedLock guard(m_mutex);
+
+	if (m_messages.size()>0) {
+		poco_warning(logger(), "clearing "
+				+ to_string(m_messages.size())
+				+ " of messages still in poll");
+	}
+
+	for (auto message : m_messages) {
+		GWTimedContext::Ptr timedContext = message.second.cast<GWTimedContext>();
+		if (!timedContext.isNull())
+			timedContext->missingResponseTask()->cancel();
+	}
+
+	m_messages.clear();
+}

src/CMakeLists.txt
@@ -72,6 +72,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/net/SOAPMessage.cpp
 	${PROJECT_SOURCE_DIR}/net/UPnP.cpp
 	${PROJECT_SOURCE_DIR}/net/VPTHTTPScanner.cpp
+	${PROJECT_SOURCE_DIR}/server/GWContextPoll.cpp
 	${PROJECT_SOURCE_DIR}/server/GWMessageContext.cpp
 	${PROJECT_SOURCE_DIR}/server/GWSOutputQueue.cpp
 	${PROJECT_SOURCE_DIR}/server/GWServerConnector.cpp

src/server/GWContextPoll.h
@@ -1,0 +1,32 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/Mutex.h>
+
+#include "server/GWMessageContext.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief GWContextPoll stores contexts of sent messages. This is used
+ * for messages that expects the answer, so they can be matched. All
+ * supported operations are thread-safe.
+ */
+class GWContextPoll : public Loggable {
+public:
+	virtual ~GWContextPoll();
+
+	void insert(GWMessageContext::Ptr context);
+
+	GWMessageContext::Ptr remove(const GlobalID &id);
+
+	void clear();
+
+private:
+	std::map<GlobalID, GWMessageContext::Ptr> m_messages;
+	Poco::FastMutex m_mutex;
+};
+
+}

commit 1b256f3ad80b7bc70cc06916b66040a7fe97ce83

GWContextPoll: fix set request status FAILED on clear()
In case of clear() operation, we are responsible for setting status
of any dropped request as FAILED.

Fixes: 951e210c3e18 ("GWContextPoll: new class")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/server/GWContextPoll.cpp
@@ -49,6 +49,13 @@ void GWContextPoll::clear()
 		GWTimedContext::Ptr timedContext = message.second.cast<GWTimedContext>();
 		if (!timedContext.isNull())
 			timedContext->missingResponseTask()->cancel();
+
+		GWRequestContext::Ptr requestContext = message.second.cast<GWRequestContext>();
+		if (!requestContext.isNull()) {
+			requestContext->result()->setStatus(Result::Status::FAILED);
+			poco_warning(logger(), "dropping request: " + requestContext->message()->type().toString()
+						+ " , with id: " + requestContext->id().toString());
+		}
 	}
 
 	m_messages.clear();