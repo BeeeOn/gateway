BeeeOn/gateway

commit fb4fa2cce03240159271f3e194bd8fc0f13e178f

VPTDevice: throw exception for denial of access
Catch and handle exception thrown due to a bad password.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -423,7 +423,13 @@ HTTPEntireResponse VPTDevice::sendSetRequest(HTTPRequest& request)
 	catch (SyntaxException& e) {
 		request.setURI(request.getURI() + "&__HOSTPWD=" +
 			VPTDevice::generateHashPassword(m_password, VPTDevice::extractNonce(response.getBody())));
-		response = sendRequest(request, m_httpTimeout);
+
+		try {
+			response = sendRequest(request, m_httpTimeout);
+		}
+		catch (SyntaxException& e) {
+			throw InvalidAccessException("denied access due to bad password", e);
+		}
 	}
 
 	return response;

commit 6b2ccc09565d59f382f4183eee949bca29627901

VPTDevice: fix detection of denial of access
Denial of access to VPT can be detected only from the content of the
response and not from sending the request. The access is denied when
the response is not in JSON format. The sendRequest method never
could throw SyntaxException.

Fixes: fb4fa2cc ("VPTDevice: throw exception for denial of access")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -422,9 +422,10 @@ HTTPEntireResponse VPTDevice::sendSetRequest(HTTPRequest& request)
 
 		request.setURI(request.getURI() + "&__HOSTPWD=" +
 			VPTDevice::generateHashPassword(m_password, nonce));
+		response = sendRequest(request, m_httpTimeout);
 
 		try {
-			response = sendRequest(request, m_httpTimeout);
+			JsonUtil::parse(response.getBody());
 		}
 		catch (SyntaxException& e) {
 			throw InvalidAccessException("denied access due to bad password", e);