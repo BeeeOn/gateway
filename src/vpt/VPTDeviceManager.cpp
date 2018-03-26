BeeeOn/gateway

commit 8facc283a94ac40e6dc6c389655dc3f5f41ad923

VPTDeviceManager: extract method findPassword
The process of retrieving a password from credentials storage is extracted
to a single method. The reason is the need to search the password in more
situations.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDeviceManager.cpp
@@ -295,19 +295,15 @@ void VPTDeviceManager::doDeviceAcceptCommand(const DeviceAcceptCommand::Ptr cmd,
 	 * The password is searched only when the first subdevice is accepted.
 	 */
 	if (noSubdevicePaired(VPTDevice::omitSubdeviceFromDeviceID(cmd->deviceID()))) {
-		SharedPtr<Credentials> credential = m_credentialsStorage->find(
-			VPTDevice::omitSubdeviceFromDeviceID(cmd->deviceID()));
+		try {
+			string password = findPassword(
+				VPTDevice::omitSubdeviceFromDeviceID(cmd->deviceID()));
 
-		if (!credential.isNull()) {
-			CipherKey key = m_cryptoConfig->createKey(credential->params());
-			Cipher *cipher = CipherFactory::defaultFactory().createCipher(key);
-
-			if (!credential.cast<PasswordCredentials>().isNull()) {
-				ScopedLock<FastMutex> guard(it->second->lock());
-
-				SharedPtr<PasswordCredentials> password = credential.cast<PasswordCredentials>();
-				it->second->setPassword(password->password(cipher));
-			}
+			ScopedLock<FastMutex> guard(it->second->lock());
+			it->second->setPassword(password);
+		}
+		catch (NotFoundException& e) {
+			logger().log(e, __FILE__, __LINE__);
 		}
 	}
 
@@ -413,6 +409,23 @@ void VPTDeviceManager::processNewDevice(VPTDevice::Ptr newDevice)
 		if (pairedDevice == m_pairedDevices.end())
 			dispatch(cmd);
 	}
+}
+
+string VPTDeviceManager::findPassword(const DeviceID& id)
+{
+	SharedPtr<Credentials> credential = m_credentialsStorage->find(id);
+
+	if (!credential.isNull()) {
+		CipherKey key = m_cryptoConfig->createKey(credential->params());
+		Cipher *cipher = CipherFactory::defaultFactory().createCipher(key);
+
+		if (!credential.cast<PasswordCredentials>().isNull()) {
+			SharedPtr<PasswordCredentials> password = credential.cast<PasswordCredentials>();
+			return password->password(cipher);
+		}
+	}
+
+	throw NotFoundException("password not found for VPT " + id.toString());
 }
 
 VPTDeviceManager::VPTSeeker::VPTSeeker(VPTDeviceManager& parent):

src/vpt/VPTDeviceManager.h
@@ -133,6 +133,14 @@ protected:
 	 */
 	void processNewDevice(Poco::SharedPtr<VPTDevice> newDevice);
 
+	/**
+	 * @brief Tries to find password credential for VPT by the given DeviceID from
+	 * the credentials storage. If the password is not found the NotFoundException
+	 * is thrown.
+	 * @param id DeviceID of real VPT
+	 */
+	std::string findPassword(const DeviceID& id);
+
 private:
 	VPTDeviceManager::VPTSeeker m_seeker;
 	VPTHTTPScanner m_scanner;

commit 0d8378dad605ce0343af78b5d5c3bb01d07f1e10

VPTDeviceManager: fix leaking cipher
Fixes: 8facc283a94a ("VPTDeviceManager: extract method findPassword")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDeviceManager.cpp
@@ -1,3 +1,4 @@
+#include <Poco/AutoPtr.h>
 #include <Poco/Timestamp.h>
 #include <Poco/Crypto/Cipher.h>
 #include <Poco/Crypto/CipherFactory.h>
@@ -443,7 +444,7 @@ string VPTDeviceManager::findPassword(const DeviceID& id)
 
 	if (!credential.isNull()) {
 		CipherKey key = m_cryptoConfig->createKey(credential->params());
-		Cipher *cipher = CipherFactory::defaultFactory().createCipher(key);
+		AutoPtr<Cipher> cipher = CipherFactory::defaultFactory().createCipher(key);
 
 		if (!credential.cast<PasswordCredentials>().isNull()) {
 			SharedPtr<PasswordCredentials> password = credential.cast<PasswordCredentials>();