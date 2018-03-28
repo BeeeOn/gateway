BeeeOn/gateway

commit 152a39f4a08de69b1cb728b75de3f06034dfc078

FitpDevice: new class
It keeps information (deviceID, type (end device, coordinator))
about devices that are paired within fitp network or are also
in the BeeeOn system.

If device exists, it is paired within fitp network. If device
has also set paired flag to true, it is paired in the BeeeOn system.

It also processes received data from coordinator or end device
and it is able to send module types of coordinator or end device.

Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

src/fitp/FitpDevice.cpp
@@ -1,0 +1,266 @@
+#include <cmath>
+#include <Poco/Logger.h>
+
+#include "fitp/FitpDevice.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+#define BATTERY_PAYLOAD_SIZE      3
+#define TEMPERATURE_PAYLOAD_SIZE  5
+#define HUMIDITY_PAYLOAD_SIZE     5
+#define RSSI_PAYLOAD_SIZE         1
+
+#define SKIP_INFO                 5
+
+#define UNAVAILABLE_MODULE        0x7f
+
+#define U0   1800 //drained batteries [mV]
+
+#define Umax 3200 //new batteries [mV]
+
+static const uint8_t FITP_BATTERY_ID = 0;
+static const uint8_t FITP_TEMPERATURE_INNER_ID = 1;
+static const uint8_t FITP_TEMPERATURE_OUTER_ID = 2;
+static const uint8_t FITP_HUMIDITY_ID = 3;
+static const uint8_t FITP_ED_RSSI_ID = 4;
+static const uint8_t FITP_COORD_RSSI_ID = 2;
+
+static const uint8_t COORD_PREFIX = 0xec;
+
+static const list<ModuleType> MODULES_COORD = {
+	{ModuleType::Type::TYPE_BATTERY, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_RSSI},
+};
+
+static const list<ModuleType> MODULES_ED = {
+	{ModuleType::Type::TYPE_BATTERY, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_OUTER}},
+	{ModuleType::Type::TYPE_HUMIDITY, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_RSSI},
+};
+
+FitpDevice::FitpDevice(DeviceID id, bool paired):
+	m_deviceID(id),
+	m_paired(paired)
+{
+	if (((id & 0xFF000000) >> 24) == COORD_PREFIX)
+		m_type = COORDINATOR;
+	else
+		m_type = END_DEVICE;
+}
+
+FitpDevice::~FitpDevice()
+{
+}
+
+void FitpDevice::setDeviceID(const DeviceID &deviceId)
+{
+	m_deviceID = deviceId;
+}
+
+DeviceID FitpDevice::deviceID() const
+{
+	return m_deviceID;
+}
+
+FitpDevice::DeviceType FitpDevice::type() const
+{
+	return m_type;
+}
+
+void FitpDevice::setPaired(const bool paired)
+{
+	m_paired = paired;
+}
+
+bool FitpDevice::paired()
+{
+	return m_paired;
+}
+
+list<ModuleType> FitpDevice::modules(const DeviceType &type)
+{
+	if (type == END_DEVICE)
+		return MODULES_ED;
+	else
+		return MODULES_COORD;
+}
+
+size_t FitpDevice::moduleEDOffset(const uint8_t &id)
+{
+	size_t count = 0;
+
+	switch (id) {
+	case FITP_BATTERY_ID:
+		count = BATTERY_PAYLOAD_SIZE;
+		break;
+	case FITP_TEMPERATURE_INNER_ID:
+	case FITP_TEMPERATURE_OUTER_ID:
+		count = TEMPERATURE_PAYLOAD_SIZE;
+		break;
+	case FITP_HUMIDITY_ID:
+		count = HUMIDITY_PAYLOAD_SIZE;
+		break;
+	case FITP_ED_RSSI_ID:
+		count = RSSI_PAYLOAD_SIZE;
+		break;
+	default:
+		logger().error(
+			"module identifier is invalid",
+			__FILE__, __LINE__
+		);
+		break;
+	}
+
+	return count;
+}
+
+size_t FitpDevice::moduleCOORDOffset(const uint8_t &id)
+{
+	size_t count = 0;
+
+	switch (id) {
+	case FITP_BATTERY_ID:
+		count = BATTERY_PAYLOAD_SIZE;
+		break;
+	case FITP_TEMPERATURE_INNER_ID:
+		count = TEMPERATURE_PAYLOAD_SIZE;
+		break;
+	case FITP_COORD_RSSI_ID:
+		count = RSSI_PAYLOAD_SIZE;
+		break;
+	default:
+		logger().error(
+			"module identifier is invalid",
+			__FILE__, __LINE__
+		);
+		break;
+	}
+
+	return count;
+}
+
+double FitpDevice::voltsToPercentage(double milivolts)
+{
+	double percent;
+
+	if (milivolts > (U0 + 10))
+		percent = (milivolts - U0) / (Umax - U0) * 100;
+	else
+		percent = 1.0;
+
+	if (percent > 100.0)
+		percent = 100.0;
+
+	return percent;
+}
+
+double FitpDevice::moduleValue(uint8_t id, const vector<uint8_t> &data)
+{
+	switch (id) {
+	case FITP_BATTERY_ID:
+		return voltsToPercentage(extractValue(data));
+	case FITP_HUMIDITY_ID:
+	case FITP_TEMPERATURE_INNER_ID:
+	case FITP_TEMPERATURE_OUTER_ID:
+		return extractValue(data) / 100.0;
+	default:
+		return NAN;
+	}
+}
+
+double FitpDevice::extractValue(const vector<uint8_t> &values)
+{
+	if (values.empty())
+		throw InvalidArgumentException("missing values of module in data message");
+
+	if (values.at(0) == UNAVAILABLE_MODULE)
+		throw InvalidArgumentException("unavailable module");
+
+	int32_t data = 0;
+	for (const uint8_t value : values) {
+		data <<= 8;
+		data |= value;
+	}
+
+	return double(data);
+}
+
+SensorData FitpDevice::parseMessage(const vector<uint8_t> &data, DeviceID deviceID)
+{
+
+	SensorData sensorData;
+	sensorData.setDeviceID(deviceID);
+
+	for (auto it = data.begin() + SKIP_INFO; it != data.end();) {
+		const uint8_t id = *it;
+
+		ModuleID moduleID;
+		size_t count;
+		try {
+			if (m_type == END_DEVICE) {
+				moduleID = deriveEDModuleID(id);
+				count = moduleEDOffset(id);
+			}
+			else {
+				moduleID = deriveCOORDModuleID(id);
+				count = moduleCOORDOffset(id);
+			}
+		}
+		catch (const Exception &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+			break;
+		}
+
+		if (count == 0)
+			break;
+
+		try {
+			double value = moduleValue(id, {it + 1, it + count});
+			sensorData.insertValue(SensorValue(moduleID, value));
+		}
+		catch (const Exception &ex) {
+			sensorData.insertValue(SensorValue(moduleID));
+		}
+
+		it += count;
+	}
+
+	return sensorData;
+}
+
+ModuleID FitpDevice::deriveEDModuleID(const uint8_t id)
+{
+	switch (id) {
+	case FITP_BATTERY_ID:
+		 return ModuleID(0);
+	case FITP_TEMPERATURE_INNER_ID:
+		return ModuleID(1);
+	case FITP_TEMPERATURE_OUTER_ID:
+		return ModuleID(2);
+	case FITP_HUMIDITY_ID:
+		return ModuleID(3);
+	case FITP_ED_RSSI_ID:
+		return ModuleID(4);
+	default:
+		throw InvalidArgumentException("invalid ED module: " + to_string(id));
+	}
+}
+
+ModuleID FitpDevice::deriveCOORDModuleID(const uint8_t id)
+{
+	switch (id) {
+	case FITP_BATTERY_ID:
+		return ModuleID(0);
+	case FITP_TEMPERATURE_INNER_ID:
+		return ModuleID(1);
+	case FITP_COORD_RSSI_ID:
+		return ModuleID(2);
+	default:
+		throw InvalidArgumentException("invalid COORD module: " + to_string(id));
+	}
+}

CMakeLists.txt
@@ -35,6 +35,7 @@ option(ENABLE_BASIC_DISTRIBUTOR "Enable support of Basic Distributor" ON)
 option(ENABLE_QUEUING_DISTRIBUTOR "Enable support of Queuing Distributor" ON)
 option(ENABLE_TESTING_CENTER "Enable support of Testing Center" ON)
 option(ENABLE_PRESSURE_SENSOR "Enable support of internal air pressure sensor" ON)
+option(ENABLE_FITP "Enable support of FITP" OFF)
 
 add_subdirectory(src)
 add_subdirectory(base)

src/CMakeLists.txt
@@ -27,6 +27,10 @@ if(ENABLE_ZWAVE)
 	include_directories(${OPENZWAVE_INCLUDE_DIR})
 endif()
 
+if(ENABLE_FITP)
+	find_library (FITP_LIB fitp)
+endif()
+
 file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/bluetooth/BluetoothListener.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceAcceptCommand.cpp
@@ -244,6 +248,15 @@ else()
 	message(STATUS "Internal air pressure sensor support is disabled")
 endif()
 
+if(ENABLE_FITP)
+	file(GLOB FITP_SOURCES
+		${PROJECT_SOURCE_DIR}/fitp/FitpDevice.cpp
+	)
+	list(APPEND SOURCES ${FITP_SOURCES})
+else()
+	message(STATUS "Fitp support is disabled")
+endif()
+
 configure_file(core/Version.cpp.in ${PROJECT_BINARY_DIR}/core/Version.cpp)
 list(APPEND SOURCES ${PROJECT_BINARY_DIR}/core/Version.cpp)
 
@@ -269,6 +282,7 @@ set(LIBS
 	${PTHREAD}
 	${UDEV}
 	${OPENZWAVE_LIBRARY}
+	${FITP_LIB}
 )
 
 target_link_libraries(beeeon-gateway

src/fitp/FitpDevice.h
@@ -1,0 +1,87 @@
+#pragma once
+
+#include <Poco/SharedPtr.h>
+
+#include "core/DeviceManager.h"
+#include "model/DeviceID.h"
+#include "model/ModuleType.h"
+#include "model/SensorData.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * Keeps information about devices that are paired within the BeeeOn system.
+ * If device has set paired flag to true, it is paired in the BeeeOn system.
+ */
+class FitpDevice : public Loggable {
+public:
+	enum DeviceType {
+		// end device - it can send values and it is not able to route data
+		END_DEVICE,
+		// coordinator - it can send values and it is able to route data
+		COORDINATOR,
+	};
+	typedef Poco::SharedPtr<FitpDevice> Ptr;
+
+	FitpDevice(DeviceID id, bool paired = false);
+	virtual ~FitpDevice();
+
+	DeviceID deviceID() const;
+	void setDeviceID(const DeviceID &deviceId);
+
+	DeviceType type() const;
+
+	/**
+	 * Returns module types of device.
+	 * ED - battery, inner temperature, outer temperature, humidity, RSSI module.
+	 * COORD - battery, inner temperature, RSSI module.
+	 */
+	std::list<ModuleType> modules(const DeviceType &type);
+
+	/**
+	 * Returns count of bytes that has to be skipped in case of end device.
+	 */
+	size_t moduleEDOffset(const uint8_t &id);
+
+	/**
+	 * Returns count of bytes that has to be skipped in case of coordinator.
+	 */
+	size_t moduleCOORDOffset(const uint8_t &id);
+
+	/**
+	 * Processes data received from device. It is able to recognize a particular
+	 * module and its value.
+	 */
+	SensorData parseMessage(const std::vector<uint8_t> &data, DeviceID deviceID);
+
+	/**
+	 * Returns module value.
+	 */
+	static double extractValue(const std::vector<uint8_t> &values);
+	static double voltsToPercentage(double milivolts);
+
+	/**
+	 * Returns modified module value.
+	 */
+	static double moduleValue(uint8_t id, const std::vector<uint8_t> &data);
+
+	/**
+	 * Returns end device (ED) module ID.
+	 */
+	static ModuleID deriveEDModuleID(const uint8_t id);
+
+	/**
+	 * Returns coordinator (COORD) module ID.
+	 */
+	static ModuleID deriveCOORDModuleID(const uint8_t id);
+
+	void setPaired(bool paired);
+	bool paired();
+private:
+	DeviceID m_deviceID;
+	DeviceType m_type;
+	bool m_paired;
+};
+
+}

test/CMakeLists.txt
@@ -28,6 +28,10 @@ if(ENABLE_ZWAVE)
 	include_directories(${OPENZWAVE_INCLUDE_DIR})
 endif()
 
+if(ENABLE_FITP)
+	find_library (FITP_LIB fitplib)
+endif()
+
 file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/core/AnswerQueueTest.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandDispatcherTest.cpp
@@ -63,6 +67,13 @@ if (ENABLE_QUEUING_DISTRIBUTOR)
 	list(APPEND TEST_SOURCES ${QUEUING_DISTRIBUTOR_TEST_SOURCES})
 endif()
 
+if(ENABLE_FITP)
+	file(GLOB FITP_TEST_SOURCES
+		${PROJECT_SOURCE_DIR}/fitp/FitpDeviceTest.cpp
+	)
+	list(APPEND TEST_SOURCES ${FITP_TEST_SOURCES})
+endif()
+
 include_directories(
 	${PROJECT_SOURCE_DIR}
 	${PROJECT_SOURCE_DIR}/../base/src
@@ -89,6 +100,7 @@ set(LIBS
 	${PTHREAD}
 	${UDEV}
 	${OPENZWAVE_LIBRARY}
+	${FITP_LIB}
 )
 
 target_link_libraries(test-suite-gateway

test/fitp/FitpDeviceTest.cpp
@@ -1,0 +1,95 @@
+#include <cmath>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestFixture.h>
+
+#include "cppunit/BetterAssert.h"
+#include "fitp/FitpDevice.h"
+
+using namespace Poco;
+using namespace std;
+
+namespace BeeeOn {
+
+class FitpDeviceTest : public CppUnit::TestFixture {
+CPPUNIT_TEST_SUITE(FitpDeviceTest);
+	CPPUNIT_TEST(testParseMessage);
+	CPPUNIT_TEST(testModuleValueInvalid);
+	CPPUNIT_TEST(testParseValue);
+	CPPUNIT_TEST(testNegativeValue);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+void testParseMessage();
+void testModuleValueInvalid();
+void testParseValue();
+void testNegativeValue();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FitpDeviceTest);
+
+/**
+ * Parse message containing 4 data payloads giving:
+ *
+ * Temperature INNER: 30.00 °C
+ * Temperature OUTER: 25.47 °C
+ * Humidity: 44.53 %
+ * Battery: 100 %
+ */
+void FitpDeviceTest::testParseMessage()
+{
+	Timestamp now;
+	DeviceID deviceID = 0xa1000000ed000004;
+	FitpDevice device(deviceID);
+
+	const vector<uint8_t> values = {0x92, 0x01, 0x00, 0x00, 0x05, 0x00, 0x0b, 0xb8,
+				0x01, 0x00, 0x00, 0x09, 0xf3, 0x02, 0x00, 0x00, 0x11, 0x65,
+				0x03, 0x00, 0x00, 0x0e, 0x7c, 0x05, 0x00, 0x1e};
+	SensorData sensorData = device.parseMessage(values, deviceID);
+
+	CPPUNIT_ASSERT(now <= sensorData.timestamp());
+	CPPUNIT_ASSERT_EQUAL(4, sensorData.size());
+
+	CPPUNIT_ASSERT(sensorData.at(0).moduleID().value() == 1);      //BATTERY
+	CPPUNIT_ASSERT_EQUAL(sensorData.at(0).value(), 100);
+
+	CPPUNIT_ASSERT(sensorData.at(1).moduleID().value() == 2);      //TEMPERATURE INNER, SHT21
+	CPPUNIT_ASSERT_EQUAL(sensorData.at(1).value(), 25.47);
+
+	CPPUNIT_ASSERT(sensorData.at(2).moduleID().value() == 3);       //TEMPERATURE OUTER, Ds18b20
+	CPPUNIT_ASSERT_EQUAL(sensorData.at(2).value(), 44);
+
+	CPPUNIT_ASSERT(sensorData.at(3).moduleID().value() == 4);      //HUMIDITY, SHT21
+	CPPUNIT_ASSERT_EQUAL(sensorData.at(3).value(), 37);
+}
+
+void FitpDeviceTest::testParseValue()
+{
+	const vector<uint8_t> values = {0x0b, 0xb8};
+
+	double value = FitpDevice::extractValue(values);
+
+	CPPUNIT_ASSERT(3000 == value);
+}
+
+void FitpDeviceTest::testNegativeValue()
+{
+	const vector<uint8_t> values = {0xff, 0xff, 0xff, 0xff};
+
+	double value = FitpDevice::extractValue(values);
+
+	CPPUNIT_ASSERT(-1 == value);
+}
+
+void FitpDeviceTest::testModuleValueInvalid()
+{
+	DeviceID deviceID = 0xa1000000ed000004;
+	FitpDevice device(deviceID);
+
+	const vector<uint8_t> values = {0x92, 0x01, 0x00, 0x00, 0x05, 0x00, 0x7f, 0xff};
+	SensorData sensorData = device.parseMessage(values, deviceID);
+
+	CPPUNIT_ASSERT(!sensorData.at(0).isValid());
+	CPPUNIT_ASSERT(std::isnan(sensorData.at(0).value()));
+}
+
+}

commit a65bd9389cfb6240da9d473ba706b3402ca37ecf

FitpDevice: fix TYPE_BATTERY to not use attributes
TYPE_BATTERY with ATTR_INNER does not make much sense. The INNER
and OUTER attributes describes measuring inside (a building) or
outside (in the wild). In case of batteries, we might consider
something like INTERNAL vs. EXTERNAL but there are no such attributes
defined and we do not need them now.

Fixes: 152a39f4a08d ("FitpDevice: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/fitp/FitpDevice.cpp
@@ -30,13 +30,13 @@ static const uint8_t FITP_COORD_RSSI_ID = 2;
 static const uint8_t COORD_PREFIX = 0xec;
 
 static const list<ModuleType> MODULES_COORD = {
-	{ModuleType::Type::TYPE_BATTERY, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_BATTERY},
 	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_INNER}},
 	{ModuleType::Type::TYPE_RSSI},
 };
 
 static const list<ModuleType> MODULES_ED = {
-	{ModuleType::Type::TYPE_BATTERY, {ModuleType::Attribute::ATTR_INNER}},
+	{ModuleType::Type::TYPE_BATTERY},
 	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_INNER}},
 	{ModuleType::Type::TYPE_TEMPERATURE, {ModuleType::Attribute::ATTR_OUTER}},
 	{ModuleType::Type::TYPE_HUMIDITY, {ModuleType::Attribute::ATTR_INNER}},