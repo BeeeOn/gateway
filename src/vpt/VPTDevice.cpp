BeeeOn/gateway

commit 4d3afe55b51998ee847d1f53dfec27452917f83c

VPTDevice: add updating of BEEE0 register
Add the BEEE0 register update after accessing the device. Access may be due to
reading values, changing value or device pairing.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -1,8 +1,14 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 #include <Poco/DigestStream.h>
 #include <Poco/Event.h>
 #include <Poco/NumberFormatter.h>
+#include <Poco/Timestamp.h>
 #include <Poco/Net/HTTPClientSession.h>
 #include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/IPAddress.h>
+#include <Poco/Net/StreamSocket.h>
 #include <Poco/RegularExpression.h>
 #include <Poco/SHA1Engine.h>
 #include <Poco/StreamCopier.h>
@@ -20,6 +26,7 @@
 #define OMIT_SUBDEVICE_MASK 0xff00ffffffffffff
 #define MAX_ATTEMPTS 3
 #define SETTING_DELAY_MS 15000
+#define GATEWAY_ID_MASK 0x00ffffffffffffffUL
 
 using namespace BeeeOn;
 using namespace Poco;
@@ -138,15 +145,66 @@ FastMutex& VPTDevice::lock()
 	return m_lock;
 }
 
+string VPTDevice::generateStamp(const Action action)
+{
+	string stamp;
+	StreamSocket ping;
+
+	struct in_addr *addr = NULL;
+	uint32_t ipAddress = 0;
+
+	try {
+		ping.connect(m_address, m_pingTimeout);
+
+		addr = (struct in_addr *) ping.address().host().addr();
+		ipAddress = (uint32_t) addr->s_addr;
+	}
+	catch (const Exception& e) {
+		logger().warning("unable to get IP address of proper gateway's interface, IP 0.0.0.0 is used",
+			__FILE__, __LINE__);
+	}
+	ping.close();
+
+	const uint64_t id = m_gatewayID.data() & GATEWAY_ID_MASK;
+
+	const uint32_t time = (uint32_t) Timestamp().epochTime();
+
+	//Format: 4B - GATEWAY IP, 7B - GATEWAY ID, 4B - TIME, 1B - ACTION
+	//Hex: 8 + 14 + 8 + 2 = 32 characters
+	NumberFormatter::appendHex(stamp, ipAddress, 8);
+	NumberFormatter::appendHex(stamp, id, 14);
+	NumberFormatter::appendHex(stamp, time, 8);
+	NumberFormatter::appendHex(stamp, action, 2);
+
+	return stamp;
+}
+
+void VPTDevice::stampVPT(const Action action)
+{
+	string stamp = generateStamp(action);
+
+	try {
+		prepareAndSendRequest("BEEE0", stamp);
+
+		logger().debug("update register BEEE0 to " + stamp, __FILE__, __LINE__);
+	}
+	catch (const Exception& e) {
+		logger().warning("the BEEE0 register update failed due to network", __FILE__, __LINE__);
+	}
+}
+
 bool VPTDevice::operator==(const VPTDevice& other) const
 {
 	return other.deviceID() == m_deviceId;
 }
 
-VPTDevice::Ptr VPTDevice::buildDevice(const SocketAddress& address, const Timespan& timeout)
+VPTDevice::Ptr VPTDevice::buildDevice(const SocketAddress& address,
+	const Timespan& httpTimeout, const Timespan& pingTimeout, const GatewayID& id)
 {
 	VPTDevice::Ptr device = new VPTDevice(address);
-	device->m_httpTimeout = timeout;
+	device->m_httpTimeout = httpTimeout;
+	device->m_pingTimeout = pingTimeout;
+	device->m_gatewayID = id;
 	device->buildDeviceID();
 	return device;
 }
@@ -161,6 +219,8 @@ void VPTDevice::buildDeviceID()
 	Object::Ptr object = JsonUtil::parse(response.getBody());
 	string mac = object->getValue<string>("id");
 	m_deviceId = DeviceID(DevicePrefix::PREFIX_VPT, NumberParser::parseHex64(mac));
+
+	stampVPT(Action::PAIR);
 }
 
 void VPTDevice::requestModifyState(const DeviceID& id, const ModuleID module,
@@ -223,6 +283,8 @@ void VPTDevice::requestSetModBoilerOperationType(const int zone, const int value
 
 	prepareAndSendRequest(registr, strValue);
 
+	stampVPT(Action::SET);
+
 	/*
 	 * The success of request is probed in iterations with
 	 * some delay because the setting time of request can be long.
@@ -267,6 +329,8 @@ void VPTDevice::requestSetModBoilerOperationMode(const int zone, const int value
 		result->setStatus(Result::Status::SUCCESS);
 	else
 		result->setStatus(Result::Status::FAILED);
+
+	stampVPT(Action::SET);
 }
 
 void VPTDevice::requestSetManualRoomTemperature(const int zone, const double value, Result::Ptr result)
@@ -284,6 +348,8 @@ void VPTDevice::requestSetManualRoomTemperature(const int zone, const double val
 		result->setStatus(Result::Status::SUCCESS);
 	else
 		result->setStatus(Result::Status::FAILED);
+
+	stampVPT(Action::SET);
 }
 
 void VPTDevice::requestSetManualWaterTemperature(const int zone, const double value, Result::Ptr result)
@@ -300,6 +366,8 @@ void VPTDevice::requestSetManualWaterTemperature(const int zone, const double va
 		result->setStatus(Result::Status::SUCCESS);
 	else
 		result->setStatus(Result::Status::FAILED);
+
+	stampVPT(Action::SET);
 }
 
 void VPTDevice::requestSetManualTUVTemperature(const int zone, const double value, Result::Ptr result)
@@ -316,6 +384,8 @@ void VPTDevice::requestSetManualTUVTemperature(const int zone, const double valu
 		result->setStatus(Result::Status::SUCCESS);
 	else
 		result->setStatus(Result::Status::FAILED);
+
+	stampVPT(Action::SET);
 }
 
 void VPTDevice::requestSetModWaterTemperature(const int zone, const double value, Result::Ptr result)
@@ -332,6 +402,8 @@ void VPTDevice::requestSetModWaterTemperature(const int zone, const double value
 		result->setStatus(Result::Status::SUCCESS);
 	else
 		result->setStatus(Result::Status::FAILED);
+
+	stampVPT(Action::SET);
 }
 
 HTTPEntireResponse VPTDevice::prepareAndSendRequest(const string& registr, const string& value)
@@ -426,6 +498,8 @@ vector<SensorData> VPTDevice::requestValues()
 	request.setURI("/values.json");
 
 	HTTPEntireResponse response = sendRequest(request, m_httpTimeout);
+
+	stampVPT(Action::READ);
 
 	VPTValuesParser parser;
 	return parser.parse(m_deviceId, response.getBody());

conf/config.d/factory.xml
@@ -145,6 +145,7 @@
 			<set name="minNetMask" text="${vpt.min.net.mask}" />
 			<set name="distributor" ref="distributor" />
 			<set name="commandDispatcher" ref="commandDispatcher" />
+			<set name="gatewayInfo" ref="gatewayInfo" />
 			<set name="credentialsStorage" ref="credentialsStorage" />
 			<set name="cryptoConfig" ref="cryptoConfig" />
 		</instance>

src/vpt/VPTDevice.h
@@ -17,6 +17,7 @@
 #include "commands/NewDeviceCommand.h"
 #include "core/Result.h"
 #include "model/DeviceID.h"
+#include "model/GatewayID.h"
 #include "model/ModuleID.h"
 #include "model/ModuleType.h"
 #include "model/SensorData.h"
@@ -74,6 +75,12 @@ public:
 
 	static const int COUNT_OF_ZONES;
 
+	enum Action {
+		PAIR = 0x01,
+		READ,
+		SET
+	};
+
 private:
 	VPTDevice(const Poco::Net::SocketAddress& address);
 
@@ -84,17 +91,32 @@ public:
 	 * @brief Connects to specified address to fetch information for creating VPT Device.
 	 * If the device do not respond in specified timeout, Poco::TimeoutException is thrown.
 	 * @param &address IP address and port where the device is listening.
-	 * @param timeout HTTP timeout.
+	 * @param httpTimeout HTTP timeout.
+	 * @param pingTimeout ping timeout used to obtain the IP address of gateway's interface
+	 * from which the VPT is available.
+	 * @param id Gateway id used in generating of stamp.
 	 * @return VPTDevice.
 	 */
 	static VPTDevice::Ptr buildDevice(const Poco::Net::SocketAddress& address,
-		const Poco::Timespan& timeout);
+		const Poco::Timespan& httpTimeout, const Poco::Timespan& pingTimeout, const GatewayID& id);
 
 	DeviceID deviceID() const;
 	Poco::Net::SocketAddress address() const;
 	void setAddress(const Poco::Net::SocketAddress& address);
 	void setPassword(const std::string& pwd);
 	Poco::FastMutex& lock();
+
+	/**
+	 * @brief Creates a stamp that consists of gateway id, IP address of gateway's interface
+	 * from whitch the VPT is available, time, and action.
+	 */
+	std::string generateStamp(const Action action);
+
+	/**
+	 * @brief Sends stamp with aciton to the VPT. It serves to verify that
+	 * the gateway communicates with the VPT.
+	 */
+	void stampVPT(const Action action);
 
 	/**
 	 * @param id Subdevice of VPT.
@@ -176,8 +198,10 @@ private:
 	Poco::Net::SocketAddress m_address;
 	std::string m_password;
 
+	Poco::Timespan m_pingTimeout;
 	Poco::Timespan m_httpTimeout;
 
+	GatewayID m_gatewayID;
 	VPTDevice::VPTCommandExecutor m_executor;
 	Poco::FastMutex m_lock;
 };

src/vpt/VPTDeviceManager.cpp
@@ -24,6 +24,7 @@ BEEEON_OBJECT_NUMBER("maxMsgSize", &VPTDeviceManager::setMaxMsgSize)
 BEEEON_OBJECT_TEXT("path", &VPTDeviceManager::setPath)
 BEEEON_OBJECT_NUMBER("port", &VPTDeviceManager::setPort)
 BEEEON_OBJECT_TEXT("minNetMask", &VPTDeviceManager::setMinNetMask)
+BEEEON_OBJECT_REF("gatewayInfo", &VPTDeviceManager::setGatewayInfo)
 BEEEON_OBJECT_REF("credentialsStorage", &VPTDeviceManager::setCredentialsStorage)
 BEEEON_OBJECT_REF("cryptoConfig", &VPTDeviceManager::setCryptoConfig)
 BEEEON_OBJECT_END(BeeeOn, VPTDeviceManager)
@@ -140,6 +141,11 @@ void VPTDeviceManager::setPort(const int port)
 void VPTDeviceManager::setMinNetMask(const string& minNetMask)
 {
 	m_scanner.setMinNetMask(IPAddress(minNetMask));
+}
+
+void VPTDeviceManager::setGatewayInfo(SharedPtr<GatewayInfo> gatewayInfo)
+{
+	m_gatewayInfo = gatewayInfo;
 }
 
 void VPTDeviceManager::setCredentialsStorage(SharedPtr<CredentialsStorage> storage)
@@ -360,7 +366,8 @@ vector<VPTDevice::Ptr> VPTDeviceManager::seekDevices()
 
 		VPTDevice::Ptr newDevice;
 		try {
-			newDevice = VPTDevice::buildDevice(address, m_httpTimeout);
+			newDevice = VPTDevice::buildDevice(address, m_httpTimeout,
+				m_pingTimeout, m_gatewayInfo->gatewayID());
 		}
 		catch (Exception& e) {
 			logger().warning("found device has disconnected", __FILE__, __LINE__);

src/vpt/VPTDeviceManager.h
@@ -19,6 +19,7 @@
 #include "commands/NewDeviceCommand.h"
 #include "core/Answer.h"
 #include "core/DeviceManager.h"
+#include "core/GatewayInfo.h"
 #include "credentials/CredentialsStorage.h"
 #include "loop/StoppableRunnable.h"
 #include "model/DeviceID.h"
@@ -73,6 +74,7 @@ public:
 	void setPath(const std::string& path);
 	void setPort(const int port);
 	void setMinNetMask(const std::string& minNetMask);
+	void setGatewayInfo(Poco::SharedPtr<GatewayInfo> gatewayInfo);
 	void setCredentialsStorage(Poco::SharedPtr<CredentialsStorage> storage);
 	void setCryptoConfig(Poco::SharedPtr<CryptoConfig> config);
 
@@ -152,6 +154,7 @@ private:
 	 */
 	std::map<DeviceID, VPTDevice::Ptr> m_devices;
 
+	Poco::SharedPtr<GatewayInfo> m_gatewayInfo;
 	Poco::SharedPtr<CredentialsStorage> m_credentialsStorage;
 	Poco::SharedPtr<CryptoConfig> m_cryptoConfig;
 };

commit f58e357a0790735558305aeea8dc9314047692eb

VPTDevice: fix stamping logging
The logging of failed stamping is fixed. The failure can also occur
due to other ways then network failure. For this reason is error
specified by an exception.

Fixes: 4d3afe5 ("VPTDevice: add updating of BEEE0 register")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/vpt/VPTDevice.cpp
@@ -186,7 +186,9 @@ void VPTDevice::stampVPT(const Action action)
 		logger().debug("update register BEEE0 to " + stamp, __FILE__, __LINE__);
 	}
 	catch (const Exception& e) {
-		logger().warning("the BEEE0 register update failed due to network", __FILE__, __LINE__);
+		logger().log(e, __FILE__, __LINE__);
+		logger().warning("the BEEE0 register of VPT " + m_boilerId.toString() + " update failed",
+			__FILE__, __LINE__);
 	}
 }
 