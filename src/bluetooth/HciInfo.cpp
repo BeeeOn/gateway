BeeeOn/gateway

commit ecc4926f3550724e45eb47b064b20100ec9596d8

HciInfo: new class
The HciInfo holds information about a single HCI interface. The statistics
are retreived from structs hci_dev_info and hci_dev_stats which are not
documented.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/bluetooth/HciInfo.cpp
@@ -1,0 +1,107 @@
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+
+#include "bluetooth/HciInfo.h"
+
+using namespace std;
+using namespace BeeeOn;
+
+HciInfo::HciInfo(const struct hci_dev_info &info):
+	m_name(info.name),
+	m_address(info.bdaddr.b),
+	m_aclMtu(info.acl_mtu),
+	m_scoMtu(info.sco_mtu),
+	m_aclPackets(info.acl_pkts),
+	m_scoPackets(info.sco_pkts),
+	m_rxErrors(info.stat.err_rx),
+	m_txErrors(info.stat.err_tx),
+	m_rxEvents(info.stat.evt_rx),
+	m_txCmds(info.stat.cmd_tx),
+	m_rxAcls(info.stat.acl_rx),
+	m_txAcls(info.stat.acl_tx),
+	m_rxScos(info.stat.sco_rx),
+	m_txScos(info.stat.sco_tx),
+	m_rxBytes(info.stat.byte_rx),
+	m_txBytes(info.stat.byte_tx)
+{
+}
+
+string HciInfo::name() const
+{
+	return m_name;
+}
+
+MACAddress HciInfo::address() const
+{
+	return m_address;
+}
+
+uint32_t HciInfo::aclMtu() const
+{
+	return m_aclMtu;
+}
+
+uint32_t HciInfo::aclPackets() const
+{
+	return m_aclPackets;
+}
+
+uint32_t HciInfo::scoMtu() const
+{
+	return m_scoMtu;
+}
+
+uint32_t HciInfo::scoPackets() const
+{
+	return m_scoPackets;
+}
+
+uint32_t HciInfo::rxErrors() const
+{
+	return m_rxEvents;
+}
+
+uint32_t HciInfo::txErrors() const
+{
+	return m_txErrors;
+}
+
+uint32_t HciInfo::rxEvents() const
+{
+	return m_rxEvents;
+}
+
+uint32_t HciInfo::txCmds() const
+{
+	return m_txCmds;
+}
+
+uint32_t HciInfo::rxAcls() const
+{
+	return m_rxAcls;
+}
+
+uint32_t HciInfo::txAcls() const
+{
+	return m_txAcls;
+}
+
+uint32_t HciInfo::rxScos() const
+{
+	return m_rxScos;
+}
+
+uint32_t HciInfo::txScos() const
+{
+	return m_txScos;
+}
+
+uint32_t HciInfo::rxBytes() const
+{
+	return m_rxBytes;
+}
+
+uint32_t HciInfo::txBytes() const
+{
+	return m_txBytes;
+}

src/CMakeLists.txt
@@ -97,6 +97,7 @@ if(ENABLE_BLUETOOTH_AVAILABILITY)
 	file(GLOB BLUETOOTH_AVAILABILITY_SOURCES
 		${PROJECT_SOURCE_DIR}/bluetooth/BluetoothAvailabilityManager.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/BluetoothDevice.cpp
+		${PROJECT_SOURCE_DIR}/bluetooth/HciInfo.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/HciInterface.cpp
 	)
 	list(APPEND SOURCES ${BLUETOOTH_AVAILABILITY_SOURCES})

src/bluetooth/HciInfo.h
@@ -1,0 +1,65 @@
+#ifndef BEEEON_HCI_INFO_H
+#define BEEEON_HCI_INFO_H
+
+#include <string>
+
+#include "net/MACAddress.h"
+
+// forward-declaration
+struct hci_dev_info;
+
+namespace BeeeOn {
+
+/**
+ * Provides information about a hci interface.
+ */
+class HciInfo {
+public:
+	HciInfo(const struct hci_dev_info &info);
+
+	std::string name() const;
+	MACAddress address() const;
+
+	uint32_t aclMtu() const;
+	uint32_t aclPackets() const;
+
+	uint32_t scoMtu() const;
+	uint32_t scoPackets() const;
+
+	uint32_t rxErrors() const;
+	uint32_t txErrors() const;
+
+	uint32_t rxEvents() const;
+	uint32_t txCmds() const;
+
+	uint32_t rxAcls() const;
+	uint32_t txAcls() const;
+
+	uint32_t rxScos() const;
+	uint32_t txScos() const;
+
+	uint32_t rxBytes() const;
+	uint32_t txBytes() const;
+
+private:
+	std::string m_name;
+	MACAddress m_address;
+	uint32_t m_aclMtu;
+	uint32_t m_scoMtu;
+	uint32_t m_aclPackets;
+	uint32_t m_scoPackets;
+	uint32_t m_rxErrors;
+	uint32_t m_txErrors;
+	uint32_t m_rxEvents;
+	uint32_t m_txCmds;
+	uint32_t m_rxAcls;
+	uint32_t m_txAcls;
+	uint32_t m_rxScos;
+	uint32_t m_txScos;
+	uint32_t m_rxBytes;
+	uint32_t m_txBytes;
+};
+
+}
+
+#endif

commit 13eb4a8cfe33878ec0ee00db4b2fa96348fc3224

HciInfo: fix rxErrors getter
Typo in rxErrors getter caused to return rxEvents attribute instead.

Fixes: ecc4926f3550 ("HciInfo: new class")
Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/HciInfo.cpp
@@ -58,7 +58,7 @@ uint32_t HciInfo::scoPackets() const
 
 uint32_t HciInfo::rxErrors() const
 {
-	return m_rxEvents;
+	return m_rxErrors;
 }
 
 uint32_t HciInfo::txErrors() const