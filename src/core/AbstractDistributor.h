BeeeOn/gateway

commit 66cb42dc0bc407af3f814f59d57d3522be28e9c6

AbstractDistributor: new class for exporter registration handling
Most Distributor implementations would need to register one or more
Exporter instances for distribution and logging capabilities. The
AbstractDistributor implements those generic features.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/AbstractDistributor.h
@@ -1,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include <Poco/SharedPtr.h>
+
+#include "core/Distributor.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class Exporter;
+class SensorData;
+
+class AbstractDistributor : public Distributor, public Loggable {
+public:
+	/*
+	 * Register exporter. Received messages are resent to
+	 * all registered exporters.
+	*/
+	virtual void registerExporter(Poco::SharedPtr<Exporter> exporter);
+	/*
+	 * Export data to all registered exporters.
+	 */
+	virtual void exportData(const SensorData &sensorData) = 0;
+
+protected:
+	std::vector<Poco::SharedPtr<Exporter>> m_exporters;
+};
+
+}

src/CMakeLists.txt
@@ -14,6 +14,7 @@ add_definitions(-std=c++11)
 file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
+	${PROJECT_SOURCE_DIR}/core/AbstractDistributor.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp

src/core/AbstractDistributor.cpp
@@ -1,0 +1,13 @@
+#include <Poco/Logger.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/AbstractDistributor.h"
+#include "core/Exporter.h"
+
+using namespace BeeeOn;
+
+void AbstractDistributor::registerExporter(Poco::SharedPtr<Exporter> exporter)
+{
+	poco_debug(logger(), "registering new exporter");
+	m_exporters.push_back(exporter);
+}

commit 1b322ee228b93213f0eec7adb2308482fa1f2e82

AbstractDistributor: remove redundand declaration of exportData()
Fixes: 66cb42dc0bc4 ("AbstractDistributor: new class for exporter registration handling")
Signed-off-by: Lubomir Svehla <xsvehl09@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/AbstractDistributor.h
@@ -22,11 +22,6 @@ public:
 	*/
 	virtual void registerExporter(Poco::SharedPtr<Exporter> exporter);
 
-	/*
-	 * Export data to all registered exporters.
-	 */
-	virtual void exportData(const SensorData &sensorData) = 0;
-
 	void registerListener(DistributorListener::Ptr listener);
 
 	/*