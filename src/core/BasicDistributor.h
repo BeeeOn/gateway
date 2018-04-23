BeeeOn/gateway

commit 8ece7fa264250af9170dec9a136e828d09e65990

BasicDistributor: new basic Distributor implementation
BasicDistributor implements simple messages transporting.
Any data message received from any DeviceManager is sequentially
resent to all registered Exporters without buffering.

Exporting is secured by lock so only one thread at a time can be
exporting data.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/BasicDistributor.h
@@ -1,0 +1,21 @@
+#pragma once
+
+#include <Poco/Mutex.h>
+
+#include "core/AbstractDistributor.h"
+
+namespace BeeeOn {
+
+class SensorData;
+
+class BasicDistributor : public AbstractDistributor {
+public:
+	/*
+	 * Export data to all registered exporters.
+	 */
+	void exportData(const SensorData &sensorData);
+private:
+	Poco::FastMutex m_exportMutex;
+};
+
+}

src/CMakeLists.txt
@@ -15,6 +15,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 	${PROJECT_SOURCE_DIR}/core/AbstractDistributor.cpp
+	${PROJECT_SOURCE_DIR}/core/BasicDistributor.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/exporters/NamedPipeExporter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp

src/core/BasicDistributor.cpp
@@ -1,0 +1,41 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+
+#include <exception>
+#include <string>
+
+#include "di/Injectable.h"
+#include "core/BasicDistributor.h"
+#include "core/Exporter.h"
+#include "model/SensorData.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, BasicDistributor)
+BEEEON_OBJECT_CASTABLE(Distributor)
+BEEEON_OBJECT_REF("exporter", &BasicDistributor::registerExporter)
+BEEEON_OBJECT_END(BeeeOn, BasicDistributor)
+
+using namespace BeeeOn;
+
+void BasicDistributor::exportData(const SensorData &sensorData)
+{
+	Poco::FastMutex::ScopedLock lock(m_exportMutex);
+
+	for (Poco::SharedPtr<Exporter> exporter : m_exporters) {
+		try {
+			exporter->ship(sensorData);
+			poco_debug(logger(), "Data shipped successfully");
+
+		} catch (Poco::Exception &ex) {
+			poco_error(logger(), "Data failed to ship: " + ex.displayText());
+
+		} catch (std::exception &ex) {
+			poco_critical(logger(), "Data failed to ship: " + std::string(ex.what()));
+
+		} catch (...) {
+			poco_critical(logger(), "Unknown error occured when shipping data");
+
+		}
+	}
+}

commit b4d0b94bad2d9a9e43f005c51c9088ce056c8cf8

BasicDistributor: fix missing keyword override
Fixes: 8ece7fa26425 ("BasicDistributor: new basic Distributor implementation")
Signed-off-by: Lubomir Svehla <xsvehl09@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/BasicDistributor.h
@@ -13,7 +13,7 @@ public:
 	/*
 	 * Export data to all registered exporters.
 	 */
-	void exportData(const SensorData &sensorData);
+	void exportData(const SensorData &sensorData) override;
 private:
 	Poco::FastMutex m_exportMutex;
 };