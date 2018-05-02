BeeeOn/gateway

commit a46d2a42700288c47682f77f6cbd61afd4440fa7

FitpDeviceManager: new class
Ensures FIT protocol (fitp) initialization.
Notice: NID (network identifier) is derived from GatewayID.

Sends NewDeviceCommand to CommandDispatcher when device
attempts to join.

Ensures reaction to commands sent from server:
- GatewayListenCommand, DeviceAcceptCommand - device attempts to join
- DeviceUnpairCommand - device attempts to unpair

Loads device list to obtain already paired devices.
Processes DATA message received from paired end device and then
ships sensor data to Distributor. Processes JOIN REQUEST
messsage that is sent by end device if it wants to join. In this case
device type is read from the message and FitpDevice is created.

Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

src/fitp/FitpDeviceManager.cpp
@@ -1,0 +1,562 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceSetValueCommand.h"
+#include "commands/DeviceUnpairCommand.h"
+#include "commands/GatewayListenCommand.h"
+#include "commands/NewDeviceCommand.h"
+#include "core/CommandDispatcher.h"
+#include "core/Distributor.h"
+#include "di/Injectable.h"
+#include "fitp/FitpDevice.h"
+#include "fitp/FitpDeviceManager.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, FitpDeviceManager)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_CASTABLE(CommandHandler)
+BEEEON_OBJECT_PROPERTY("file", &FitpDeviceManager::setConfigPath)
+BEEEON_OBJECT_PROPERTY("noiseMin", &FitpDeviceManager::setNoiseMin)
+BEEEON_OBJECT_PROPERTY("noiseMax", &FitpDeviceManager::setNoiseMax)
+BEEEON_OBJECT_PROPERTY("bitrate", &FitpDeviceManager::setBitrate)
+BEEEON_OBJECT_PROPERTY("band", &FitpDeviceManager::setBand)
+BEEEON_OBJECT_PROPERTY("channel", &FitpDeviceManager::setChannel)
+BEEEON_OBJECT_PROPERTY("power", &FitpDeviceManager::setPower)
+BEEEON_OBJECT_PROPERTY("txRetries", &FitpDeviceManager::setTxRetries)
+BEEEON_OBJECT_PROPERTY("distributor", &FitpDeviceManager::setDistributor)
+BEEEON_OBJECT_PROPERTY("gatewayInfo", &FitpDeviceManager::setGatewayInfo)
+BEEEON_OBJECT_PROPERTY("commandDispatcher", &FitpDeviceManager::setCommandDispatcher)
+BEEEON_OBJECT_HOOK("done", &FitpDeviceManager::initFitp)
+BEEEON_OBJECT_END(BeeeOn, FitpDeviceManager)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+#define COORDINATOR_READY    0xcc
+#define END_DEVICE_READY     0x00
+#define END_DEVICE_SLEEPY    0xff
+
+#define EDID_LENGTH          4
+#define EDID_OFFSET          2
+#define EDID_START           2
+#define EDID_END             6
+#define EDID_MASK            0xffffffff
+
+#define SKIP_INFO            6
+#define FITP_DATA_OFFSET     6
+#define JOIN_REQUEST_LENGTH  6
+
+#define DEFAULT_REFRESH_TIME 60 * Timespan::SECONDS
+
+#define PRODUCT_COORDINATOR "Temperature sensor"
+#define PRODUCT_END_DEVICE  "Temperature and humidity sensor"
+#define VENDOR              "BeeeOn"
+#define FITP_CONFIG_PATH    "/var/cache/beeeon/gateway/fitp.devices"
+
+FitpDeviceManager::FitpDeviceManager():
+	DeviceManager(DevicePrefix::PREFIX_FITPROTOCOL),
+	m_configFile(FITP_CONFIG_PATH),
+	m_listening(false),
+	m_listenCallback(*this, &FitpDeviceManager::stopListen),
+	m_listenTimer(0, 0)
+{
+}
+
+FitpDeviceManager::~FitpDeviceManager()
+{
+	fitp_deinit();
+}
+
+void FitpDeviceManager::doListenCommand(
+	const GatewayListenCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	if (m_listening) {
+		logger().debug(
+			"listening is already in progress",
+			__FILE__, __LINE__
+		);
+		return;
+	}
+	m_listening = true;
+
+	Result::Ptr result = new Result(answer);
+
+	if (cmd->duration().totalSeconds() < 1) {
+		logger().error(
+			"listening duration is too short: "
+			+ to_string(cmd->duration().totalMilliseconds())
+			+ " ms"
+			__FILE__, __LINE__
+		);
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	logger().debug(
+		"starting listening mode",
+		__FILE__, __LINE__
+	);
+
+	fitp_listen(cmd->duration().totalSeconds());
+
+	m_listenTimer.stop();
+	m_listenTimer.setStartInterval(cmd->duration().totalMilliseconds());
+	m_listenTimer.start(m_listenCallback);
+
+	result->setStatus(Result::Status::SUCCESS);
+}
+
+void FitpDeviceManager::stopListen(Timer &)
+{
+	fitp_joining_disable();
+	m_listening = false;
+
+	logger().debug(
+		"listening mode has finished",
+		__FILE__, __LINE__
+	);
+}
+
+void FitpDeviceManager::doDeviceAcceptCommand(
+	const DeviceAcceptCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_devices.find(cmd->deviceID());
+	if (it == m_devices.end()) {
+		logger().warning(
+			"no such device "
+			+ cmd->deviceID().toString()
+			+ " to accept",
+			__FILE__, __LINE__
+		);
+
+		result->setStatus(Result::Status::SUCCESS);
+		return;
+	}
+
+	if (it->second->paired()) {
+		logger().warning(
+			"ignoring accept for paired device "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+		result->setStatus(Result::Status::SUCCESS);
+		return;
+	}
+
+	logger().debug(
+		"accept command received successfully",
+		__FILE__, __LINE__
+	);
+
+	result->setStatus(Result::Status::SUCCESS);
+	it->second->setPaired(true);
+
+	logger().notice(
+		"device "
+		+ cmd->deviceID().toString()
+		+ " was successfully paired"
+		__FILE__, __LINE__
+	);
+}
+
+void FitpDeviceManager::doUnpairCommand(
+	const DeviceUnpairCommand::Ptr cmd, const Answer::Ptr answer)
+{
+	Result::Ptr result = new Result(answer);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_devices.find(cmd->deviceID());
+	if (it == m_devices.end() || !it->second->paired()) {
+		logger().warning(
+			"unpairing device that is not registered: "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	FitpDeviceManager::EDID edid  = deriveEDID(cmd->deviceID());
+
+	if (!fitp_unpair(edid)) {
+		logger().error(
+			"failed to unpair device "
+			+ cmd->deviceID().toString(),
+			__FILE__, __LINE__
+		);
+		result->setStatus(Result::Status::FAILED);
+		return;
+	}
+
+	logger().notice(
+		"device "
+		+ cmd->deviceID().toString()
+		+ " was successfully unpaired"
+			__FILE__, __LINE__
+	);
+	m_devices.erase(cmd->deviceID());
+
+	result->setStatus(Result::Status::SUCCESS);
+}
+
+bool FitpDeviceManager::accept(const Command::Ptr cmd)
+{
+	if (cmd->is<GatewayListenCommand>()) {
+		return true;
+	}
+	else if (cmd->is<DeviceUnpairCommand>()) {
+		return cmd->cast<DeviceUnpairCommand>().deviceID().prefix()
+			   == DevicePrefix::PREFIX_FITPROTOCOL;
+	}
+	else if (cmd->is<DeviceAcceptCommand>()) {
+		return cmd->cast<DeviceAcceptCommand>().deviceID().prefix()
+			   == DevicePrefix::PREFIX_FITPROTOCOL;
+	}
+
+	return false;
+}
+
+void FitpDeviceManager::handle(Command::Ptr cmd, Answer::Ptr answer)
+{
+	if (cmd->is<GatewayListenCommand>())
+		doListenCommand(cmd.cast<GatewayListenCommand>(), answer);
+	else if (cmd->is<DeviceUnpairCommand>())
+		doUnpairCommand(cmd.cast<DeviceUnpairCommand>(), answer);
+	else if (cmd->is<DeviceAcceptCommand>())
+		doDeviceAcceptCommand(cmd.cast<DeviceAcceptCommand>(), answer);
+	else
+		throw IllegalStateException("received unaccepted command: " + cmd->toString());
+}
+
+void FitpDeviceManager::dispatchNewDevice(FitpDevice::Ptr device)
+{
+	NewDeviceCommand::Ptr cmd;
+
+	if (device->type() == FitpDevice::END_DEVICE) {
+		cmd = new NewDeviceCommand(
+			device->deviceID(),
+			VENDOR,
+			PRODUCT_END_DEVICE,
+			device->modules(FitpDevice::END_DEVICE),
+			DEFAULT_REFRESH_TIME);
+	}
+	else if (device->type() == FitpDevice::COORDINATOR) {
+		cmd = new NewDeviceCommand(
+			device->deviceID(),
+			VENDOR,
+			PRODUCT_COORDINATOR,
+			device->modules(FitpDevice::COORDINATOR),
+			DEFAULT_REFRESH_TIME);
+	}
+	else {
+		throw InvalidArgumentException("invalid device type");
+	}
+
+	dispatch(cmd);
+}
+
+void FitpDeviceManager::setConfigPath(const string &configPath)
+{
+	m_configFile = configPath;
+}
+
+void FitpDeviceManager::setNoiseMin(int min)
+{
+	if (min < 0) {
+		throw InvalidArgumentException(
+			"invalid min: " + to_string(min));
+	}
+	m_phyParams.cca_noise_threshold_min = min;
+}
+
+void FitpDeviceManager::setNoiseMax(int max)
+{
+	if (max < 0) {
+		throw InvalidArgumentException(
+			"invalid max: " + to_string(max));
+	}
+	m_phyParams.cca_noise_threshold_max = max;
+}
+
+void FitpDeviceManager::setBitrate(int bitrate)
+{
+	if (bitrate < 0 || bitrate > 7) {
+		throw InvalidArgumentException(
+			"invalid bitrate: " + to_string(bitrate));
+	}
+	m_phyParams.bitrate = bitrate;
+}
+
+void FitpDeviceManager::setBand(int band)
+{
+	if (band < 0 || band > 3) {
+		throw InvalidArgumentException(
+			"invalid band: " + to_string(band));
+	}
+	m_phyParams.band = band;
+}
+
+int FitpDeviceManager::channelCnt()
+{
+	if ((m_phyParams.band == 0 || m_phyParams.band == 1) && (m_phyParams.bitrate == 6 || m_phyParams.bitrate == 7))
+		return 24;
+	else
+		return 31;
+}
+
+void FitpDeviceManager::setChannel(int channel)
+{
+	if (channelCnt() < channel) {
+			throw InvalidArgumentException(
+				"invalid channel: " + to_string(channel));
+	}
+
+	m_phyParams.channel = channel;
+}
+
+void FitpDeviceManager::setPower(int power)
+{
+	if (power < 0 || power > 7) {
+		throw InvalidArgumentException(
+			"invalid TX power: " + to_string(power));
+	}
+	m_phyParams.power = power;
+}
+
+void FitpDeviceManager::setTxRetries(int retries)
+{
+	if (retries < 0) {
+		throw InvalidArgumentException(
+			"invalid retries: " + to_string(retries));
+	}
+	m_linkParams.tx_max_retries = retries;
+}
+
+void FitpDeviceManager::initFitp()
+{
+	logger().information(
+		"configuration file path: "
+		+ m_configFile,
+		__FILE__, __LINE__
+	);
+	fitp_set_config_path(m_configFile);
+
+	logger().debug(
+		"acceptable noise: "
+		+ to_string(m_phyParams.cca_noise_threshold_min)
+		+ "-" + to_string(m_phyParams.cca_noise_threshold_max)
+		+ ", bitrate: "
+		+ to_string(m_phyParams.bitrate)
+		+ ", band: "
+		+ to_string(m_phyParams.band)
+		+ ", channel: "
+		+ to_string(m_phyParams.channel)
+		+ ", TX power: "
+		+ to_string(m_phyParams.power)
+		+ ", TX retries: "
+		+ to_string(m_linkParams.tx_max_retries),
+		__FILE__, __LINE__
+	);
+	fitp_init(&m_phyParams, &m_linkParams);
+}
+
+void FitpDeviceManager::loadDeviceList()
+{
+	set<DeviceID> deviceIDs;
+
+	try {
+		deviceIDs = deviceList(-1);
+	}
+	catch (const Poco::Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+		return;
+	}
+
+	FastMutex::ScopedLock guard(m_lock);
+	for (auto &item : deviceIDs) {
+		FitpDevice::Ptr device = new FitpDevice(item, true);
+		m_devices.emplace(item, device);
+	}
+}
+
+DeviceID FitpDeviceManager::buildID(FitpDeviceManager::EDID edid)
+{
+	return DeviceID(DevicePrefix::PREFIX_FITPROTOCOL, edid);
+}
+
+FitpDeviceManager::EDID FitpDeviceManager::deriveEDID(const DeviceID &id)
+{
+	return id.ident() & EDID_MASK;
+}
+
+FitpDeviceManager::EDID FitpDeviceManager::parseEDID(const vector<uint8_t> &id)
+{
+	if (id.size() < EDID_LENGTH)
+		throw InvalidArgumentException("invalid end device id length");
+
+	EDID edid = 0;
+	for (int i = 0; i < EDID_LENGTH; i++) {
+		edid <<= 8;
+		edid |= id.at(i);
+	}
+	return edid;
+}
+
+void FitpDeviceManager::processDataMsg(const vector<uint8_t> &data)
+{
+	logger().dump(
+		"received data",
+		data.data(),
+		data.size(),
+		Message::PRIO_TRACE
+	);
+
+	EDID edid = parseEDID({data.begin() + EDID_OFFSET, data.begin() + EDID_OFFSET + EDID_LENGTH});
+	DeviceID deviceID = buildID(edid);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_devices.find(deviceID);
+	if (it != m_devices.end() && it->second->paired()) {
+		try {
+			const vector<uint8_t> &values = {data.begin() + FITP_DATA_OFFSET, data.end()};
+			SensorData sensorData = it->second->parseMessage(values, deviceID);
+			ship(sensorData);
+		}
+		catch(const Exception &ex) {
+			logger().log(ex, __FILE__, __LINE__);
+		}
+	}
+	else {
+		logger().warning(
+			"data cannot be shipped, device "
+			+ deviceID.toString()
+			+ " is not paired",
+			__FILE__, __LINE__
+		);
+	}
+}
+
+void FitpDeviceManager::processJoinMsg(const vector<uint8_t> &data)
+{
+	logger().dump(
+		"join request",
+		data.data(),
+		data.size()
+	);
+
+	if(!m_listening) {
+		logger().warning(
+			"received join message out of the listen mode, ignoring...",
+			__FILE__, __LINE__
+		);
+		return;
+	}
+
+	if (data.size() != JOIN_REQUEST_LENGTH) {
+		logger().error(
+			"invalid join request length",
+			__FILE__, __LINE__
+		);
+		return;
+	}
+
+	EDID edid = parseEDID({data.begin() + EDID_OFFSET, data.begin() + EDID_OFFSET + EDID_LENGTH});
+	DeviceID deviceID = buildID(edid);
+
+	FastMutex::ScopedLock guard(m_lock);
+	auto it = m_devices.find(deviceID);
+	if (it == m_devices.end() || !it->second->paired()) {
+		FitpDevice::Ptr device;
+
+		switch (data[1]) {
+		case END_DEVICE_READY:
+		case END_DEVICE_SLEEPY:
+		case COORDINATOR_READY:
+			device = new FitpDevice(deviceID);
+			break;
+		default:
+			logger().warning(
+				"invalid device type "
+				+ to_string(data[1]),
+				__FILE__, __LINE__
+			);
+			break;
+		}
+
+		if (data[1] == END_DEVICE_READY || data[1] == END_DEVICE_SLEEPY || data[1] == COORDINATOR_READY) {
+			device = new FitpDevice(deviceID);
+		}
+		else {
+			logger().warning(
+				"invalid device type",
+				__FILE__, __LINE__
+			);
+			return;
+		}
+
+		device->setDeviceID(deviceID);
+
+		fitp_accepted_device({data.begin() + EDID_START, data.begin() + EDID_END});
+		m_devices.emplace(device->deviceID(), device);
+		dispatchNewDevice(device);
+	}
+	else {
+		logger().warning(
+			"device "
+			+ deviceID.toString()
+			+ " has been already paired",
+			__FILE__, __LINE__
+		);
+	}
+}
+
+void FitpDeviceManager::setGatewayInfo(Poco::SharedPtr<GatewayInfo> info)
+{
+	m_gatewayInfo = info;
+}
+
+void FitpDeviceManager::run()
+{
+	loadDeviceList();
+
+	logger().information(
+		"starting fitp device manager, fitplib version: "
+		+ fitp_version(),
+		__FILE__, __LINE__
+	);
+
+	fitp_set_nid((uint32_t) m_gatewayInfo->gatewayID().data());
+
+	while (!m_stop) {
+		vector<uint8_t> data;
+
+		fitp_received_data(data);
+		if (!data.empty()) {
+			if (isDataMessage(data)) {
+				processDataMsg(data);
+			}
+			else if (isJoinMessage(data)) {
+				processJoinMsg(data);
+			}
+		}
+	}
+
+}
+
+void FitpDeviceManager::stop()
+{
+	m_listenTimer.stop();
+	if (m_listening) {
+		fitp_joining_disable();
+		m_listening = false;
+	}
+	m_stop = true;
+	m_event.set();
+}
+

CMakeLists.txt
@@ -64,3 +64,5 @@ install(FILES systemd/beeeon-gateway.service
 	DESTINATION lib/systemd/system)
 install(DIRECTORY udev/rules.d
 	DESTINATION etc/udev)
+install(DIRECTORY
+	DESTINATION var/cache/beeeon/gateway)

conf/config.d/factory.xml
@@ -12,6 +12,7 @@
 			<add name="runnables" ref="virtualDeviceManager" if-yes="${vdev.enable}" />
 			<add name="runnables" ref="vptDeviceManager" if-yes="${vpt.enable}" />
 			<add name="runnables" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
+			<add name="runnables" ref="fitpDeviceManager" if-yes="${fitp.enable}" />
 			<add name="runnables" ref="asyncExecutor" />
 		</instance>
 
@@ -45,6 +46,7 @@
 			<add name="handlers" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
 			<add name="handlers" ref="pressureSensorManager" if-yes="${psdev.enable}"/>
 			<add name="listeners" ref="loggingCollector" if-yes="${testing.collector.enable}" />
+			<add name="handlers" ref="fitpDeviceManager" if-yes="${fitp.enable}"/>
 		</instance>
 
 		<instance name="gwServerConnector" class="BeeeOn::GWServerConnector">
@@ -187,6 +189,20 @@
 			<set name="commandDispatcher" ref="commandDispatcher" />
 			<set name="credentialsStorage" ref="credentialsStorage" />
 			<set name="cryptoConfig" ref="cryptoConfig" />
+		</instance>
+
+		<instance name="fitpDeviceManager" class="BeeeOn::FitpDeviceManager">
+			<set name="distributor" ref="distributor"/>
+			<set name="commandDispatcher" ref="commandDispatcher"/>
+			<set name="gatewayInfo" ref="gatewayInfo"/>
+			<set name="file" text="${fitp.config.path}"/>
+			<set name="noiseMin" number="${fitp.noise.min}"/>
+			<set name="noiseMax" number="${fitp.noise.max}"/>
+			<set name="bitrate" number="${fitp.bitrate}"/>
+			<set name="band" number="${fitp.band}"/>
+			<set name="channel" number="${fitp.channel}"/>
+			<set name="power" number="${fitp.power}"/>
+			<set name="txRetries" number="${fitp.tx.retries}"/>
 		</instance>
 	</factory>
 </system>

conf/config.d/logging-loggers.ini
@@ -73,3 +73,6 @@ loggers.PocoCommandDispatcher.level = debug
 
 loggers.GWServerConnector.name = BeeeOn::GWServerConnector
 loggers.GWServerConnector.level = debug
+
+loggers.FitpDeviceManager.name = BeeeOn::FitpDeviceManager
+loggers.FitpDeviceManager.level = debug

conf/gateway-startup.ini
@@ -110,3 +110,14 @@ enable = yes
 upnp.timeout = 5 s
 http.timeout = 3 s
 refresh = 10 s
+
+[fitp]
+enable = no
+config.path = /var/cache/beeeon/gateway/fitp.devices
+noise.min = 15
+noise.max = 30
+bitrate = 5
+band = 0
+channel = 15
+power = 0
+tx.retries = 4

conf/gateway-testing.ini
@@ -110,3 +110,14 @@ enable = yes
 upnp.timeout = 5 s
 http.timeout = 3 s
 refresh = 10 s
+
+[fitp]
+enable = no
+config.path = ${application.configDir}/../fitp.devices
+noise.min = 15
+noise.max = 30
+bitrate = 5
+band = 0
+channel = 15
+power = 0
+tx.retries = 4

src/CMakeLists.txt
@@ -251,6 +251,7 @@ endif()
 if(ENABLE_FITP)
 	file(GLOB FITP_SOURCES
 		${PROJECT_SOURCE_DIR}/fitp/FitpDevice.cpp
+		${PROJECT_SOURCE_DIR}/fitp/FitpDeviceManager.cpp
 	)
 	list(APPEND SOURCES ${FITP_SOURCES})
 else()

src/fitp/FitpDeviceManager.h
@@ -1,0 +1,248 @@
+#ifndef BEEEON_FITPROTOCOL_DEVICE_MANAGER_H
+#define BEEEON_FITPROTOCOL_DEVICE_MANAGER_H
+
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timer.h>
+#include <Poco/Timestamp.h>
+
+#include <fitp/fitp.h>
+
+#include "core/DeviceManager.h"
+#include "core/GatewayInfo.h"
+#include "fitp/FitpDevice.h"
+#include "model/DeviceID.h"
+
+namespace BeeeOn {
+
+/**
+ * Ensures initialization of FIT protocol (fitp). It is able to send
+ * NewDeviceCommand to CommandDispatcher when device attempts to pair.
+ *
+ * It also ensures reaction to commands sent from server:
+ *
+ * - GatewayListenCommand, DeviceAcceptCommand - device attempts to pair
+ * - DeviceUnpairCommand - device attempts to unpair
+ *
+ * It ships measured data to Distributor.
+ *
+ * It processes DATA and JOIN REQUEST messages.
+ *
+ * Generally, message has the following format:
+ * - message type [1 B] - DATA or JOIN REQUEST message
+ * - device type [1 B] - end device or coordinator
+ * - edid [4 B] - end device identifier
+ * - data [x B] - data on application layer
+ *
+ * Format of application data:
+ * - command [1 B] - FROM_SENSOR_MSG
+ * - version [1 B] - application protocol version
+ * - device ID [2 B] - device identifier
+ * - pairs [1 B] - count of pairs: module ID, value
+ * - module ID [1 B] - module identifier
+ * - value [2 B/4 B] - value of module, length of value depends on module type
+ * -- 2 B - battery
+ * -- 4 B - inner/outer temperature, humidity
+ *
+ * Note: RSSI is obtained in FitpDeviceManager, value has 1 B.
+ *
+ * Note: Only one instance of FitpDeviceManager is possible, because
+ * fitp library does not support more instances.
+ */
+class FitpDeviceManager : public DeviceManager {
+public:
+	typedef Poco::SharedPtr<FitpDeviceManager> Ptr;
+	typedef uint32_t EDID;
+
+	FitpDeviceManager();
+	~FitpDeviceManager();
+
+	void run() override;
+	void stop() override;
+
+	/**
+	 * Sets devices as available and paired according to the device list sent from server
+	 * and the map of devices. A device has to be in the map of devices
+	 * so that it can be loaded from server.
+	 */
+	void loadDeviceList();
+
+	/**
+	 * Sets path to the configuration file from which device table on PAN coordinator is filled.
+	 * If device table is not filled, PAN coordinator is not able to react to the DATA messages from
+	 * a device. If a device wants to join to the network, it sends the JOIN REQUEST message that
+	 * PAN coordinator is able to receive and process. After successful joining process, this device will
+	 * be stored into the device table and saved to the file.
+	 */
+	void setConfigPath(const std::string &configPath);
+
+	/**
+	 * Sets minimum value of noise threshold.
+	 * It is necessary to set so that data can be sent.
+	 * Condition: min >= 0
+	 */
+	void setNoiseMin(int min);
+
+	/**
+	 * Sets maximum value of noise threshold.
+	 * It is necessary to set so that data can be sent.
+	 * Condition: max >= 0
+	 */
+	void setNoiseMax(int max);
+
+	/**
+	 * Sets bitrate.
+	 * It is possible to change bitrate.
+	 * 0x00 - DATA_RATE_5
+	 * 0x01 - DATA_RATE_10
+	 * 0x02 - DATA_RATE_20
+	 * 0x03 - DATA_RATE_40
+	 * 0x04 - DATA_RATE_50
+	 * 0x05 - DATA_RATE_66
+	 * 0x06 - DATA_RATE_100
+	 * 0x07 - DATA_RATE_200
+	 */
+	void setBitrate(int bitrate);
+
+
+	/**
+	 * Sets band.
+	 * It is possible to change band.
+	 * Following values are supported:
+	 * - 0 - band 863-870 MHz
+	 * - 1 - band 950-960 MHz
+	 * - 2 - band 902-915 MHz
+	 * - 3 - band 915-928 MHz
+	 */
+	void setBand(int band);
+
+	/**
+	 * Sets channel.
+	 * It is possible to change channel.
+	 * Case 1: (band = 0 or band = 1) and (bitrate = 6 or bitrate = 7)
+	 *         values 0-24 are valid
+	 * Case 2: otherwise
+	 *         values 0-31 are valid
+	 *
+	 * Note: Band and bitrate have to be set before setting a channel.
+	 * It is used during joining process when channel scanning is performed.
+	 */
+	void setChannel(int channel);
+
+	/**
+	 * Sets transmission power.
+	 * It is possible to change TX power.
+	 * The TX power specifies the strength of the signal that device produces during
+	 * data transmitting.
+	 * Following values are supported:
+	 * 0x00: 13 dBm
+	 * 0x01: 10 dBm
+	 * 0x02: 7 dBm
+	 * 0x03: 4 dBm
+	 * 0x04: 1 dBm
+	 * 0x05: -2 dBm
+	 * 0x06: -5 dBm
+	 * 0x07: -8 dBm
+	 *
+	 * Note: If TX power is set to 7 dBm, device is able to communicate with near devices.
+	 * If TX power is set to 13 dBm, device is able to communicate with more distant devices.
+	 */
+	void setPower(int power);
+
+	/**
+	 * Sets count of attempts to resend a packet.
+	 * Packet can be resent if devices communicate using four-way handshake.
+	 * Condition: retries >= 0
+	 */
+	void setTxRetries(int retries);
+
+	void setGatewayInfo(Poco::SharedPtr<GatewayInfo> info);
+
+	/**
+	 * Initializes the fitp.
+	 */
+	void initFitp();
+
+	/**
+	* Converts end device identifier (EDID) to device ID.
+	* EDID is array of four uint8_t values.
+	* EDID is stored in lower 32 bits of DeviceID from the index 0
+	* to index 4.
+	 * Example:
+	 * 0 0 0 0 edid[0] edid[1] edid[2] edid[3]
+	*/
+	static DeviceID buildID(EDID edid);
+
+	/**
+	* Converts device ID to end device identifier (EDID).
+	*/
+	static EDID deriveEDID(const DeviceID &id);
+
+	/**
+	* Parses EDID from received data.
+	*/
+	EDID parseEDID(const std::vector<uint8_t> &id);
+
+	/**
+	* Processes data message sent by device.
+	*/
+	void processDataMsg(const std::vector<uint8_t> &data);
+
+	/**
+	* Processes join request message sent by device.
+	*/
+	void processJoinMsg(const std::vector<uint8_t> &data);
+
+protected:
+	bool accept(const Command::Ptr cmd) override;
+	void handle(Command::Ptr cmd, Answer::Ptr answer) override;
+
+	/**
+	 * Reacts to ListenCommand. It sends NewDeviceCommand if
+	 * device is not paired or is not available.
+	 */
+	void doListenCommand(const GatewayListenCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+	/**
+	 * Reacts to AcceptCommand. Device has to be stored in map
+	 * of devices, it has to be available and it has to be unpaired.
+	 * If these conditions are fulfilled, method inserts device into a map of devices,
+	 * and it sets device as paired.
+	 */
+	void doDeviceAcceptCommand(const DeviceAcceptCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+	/**
+	* Reacts to UnpairCommand. Device has to be in map of
+	* devices, it has to be paired and available.
+	*/
+	void doUnpairCommand(const DeviceUnpairCommand::Ptr cmd,
+		const Answer::Ptr answer);
+
+	/**
+	 * Ensures sending of NewDeviceCommand to CommandDispatcher.
+	 */
+	void dispatchNewDevice(FitpDevice::Ptr device);
+
+private:
+	void stopListen(Poco::Timer &timer);
+	int channelCnt();
+
+private:
+	std::map<DeviceID, FitpDevice::Ptr> m_devices;
+	std::string m_configFile;
+	PHY_init_t m_phyParams;
+	LINK_init_t m_linkParams;
+	bool m_listening;
+	Poco::TimerCallback<FitpDeviceManager> m_listenCallback;
+	Poco::Timer m_listenTimer;
+	Poco::Event m_event;
+	Poco::FastMutex m_lock;
+	Poco::SharedPtr<GatewayInfo> m_gatewayInfo;
+};
+
+}
+
+#endif

commit 1136485e31fa871642a299daa3a829bd2f6c2201

FitpDeviceManager: fix set missing result
The result for the answer to Listen Command was not set
in the case listen was still in progress.

Fixes: a46d2a427002 ("FitpDeviceManager: new class")

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/fitp/FitpDeviceManager.cpp
@@ -72,16 +72,17 @@ FitpDeviceManager::~FitpDeviceManager()
 void FitpDeviceManager::doListenCommand(
 	const GatewayListenCommand::Ptr cmd, const Answer::Ptr answer)
 {
+	Result::Ptr result = new Result(answer);
+
 	if (m_listening) {
 		logger().debug(
 			"listening is already in progress",
 			__FILE__, __LINE__
 		);
+		result->setStatus(Result::Status::SUCCESS);
 		return;
 	}
 	m_listening = true;
-
-	Result::Ptr result = new Result(answer);
 
 	if (cmd->duration().totalSeconds() < 1) {
 		logger().error(