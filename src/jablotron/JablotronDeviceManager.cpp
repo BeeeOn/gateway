BeeeOn/gateway

commit dfdb7d9f41eec51af0ff45d7fe165425bfe38175

JablotronDeviceManager: migrate to generic handleUnpair()
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -9,6 +9,7 @@
 #include "jablotron/JablotronDeviceAC88.h"
 #include "jablotron/JablotronDeviceManager.h"
 #include "hotplug/HotplugEvent.h"
+#include "util/BlockingAsyncWork.h"
 #include "util/DelayedAsyncWork.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, JablotronDeviceManager)
@@ -158,8 +159,6 @@ void JablotronDeviceManager::handleGeneric(Command::Ptr cmd, Result::Ptr result)
 {
 	if (cmd->is<DeviceSetValueCommand>())
 		doSetValue(cmd.cast<DeviceSetValueCommand>());
-	else if (cmd->is<DeviceUnpairCommand>())
-		doUnpairCommand(cmd.cast<DeviceUnpairCommand>());
 	else
 		DeviceManager::handleGeneric(cmd, result);
 }
@@ -173,22 +172,6 @@ void JablotronDeviceManager::handleAccept(const DeviceAcceptCommand::Ptr cmd)
 		throw NotFoundException("accept: " + cmd->deviceID().toString());
 
 	DeviceManager::handleAccept(cmd);
-}
-
-void JablotronDeviceManager::doUnpairCommand(
-	const DeviceUnpairCommand::Ptr cmd)
-{
-	Mutex::ScopedLock guard(m_lock);
-	auto it = m_devices.find(cmd->deviceID());
-
-	if (it != m_devices.end()) {
-		it->second = nullptr;
-	}
-	else {
-		logger().warning(
-			"attempt to unpair unknown device: "
-			+ cmd->deviceID().toString());
-	}
 }
 
 AsyncWork<>::Ptr JablotronDeviceManager::startDiscovery(const Timespan &timeout)
@@ -210,6 +193,30 @@ AsyncWork<>::Ptr JablotronDeviceManager::startDiscovery(const Timespan &timeout)
 	};
 
 	return new DelayedAsyncWork<>(finish, finish, timeout);
+}
+
+AsyncWork<set<DeviceID>>::Ptr JablotronDeviceManager::startUnpair(
+		const DeviceID &id,
+		const Timespan &)
+{
+	set<DeviceID> result;
+
+	auto work = BlockingAsyncWork<set<DeviceID>>::instance();
+
+	Mutex::ScopedLock guard(m_lock);
+	auto it = m_devices.find(id);
+
+	if (it != m_devices.end()) {
+		it->second = nullptr;
+		work->setResult({id});
+	}
+	else {
+		logger().warning(
+			"attempt to unpair unknown device: "
+			+ id.toString());
+	}
+
+	return work;
 }
 
 void JablotronDeviceManager::doNewDevice(const DeviceID &deviceID,

src/jablotron/JablotronDeviceManager.h
@@ -56,6 +56,9 @@ protected:
 	void handleGeneric(const Command::Ptr cmd, Result::Ptr result) override;
 	void handleAccept(const DeviceAcceptCommand::Ptr cmd) override;
 	AsyncWork<>::Ptr startDiscovery(const Poco::Timespan &timeout) override;
+	AsyncWork<std::set<DeviceID>>::Ptr startUnpair(
+			const DeviceID &id,
+			const Poco::Timespan &timeout) override;
 
 private:
 	enum MessageType {
@@ -121,7 +124,6 @@ private:
 	void doSetValue(DeviceSetValueCommand::Ptr cmd);
 	void doNewDevice(const DeviceID &deviceID,
 		std::map<DeviceID, JablotronDevice::Ptr>::iterator &it);
-	void doUnpairCommand(const DeviceUnpairCommand::Ptr cmd);
 
 	bool getResponse();
 	bool isResponse(MessageType type);

commit eba867ae90f64058b004c51946ca15cc9050d23e

JablotronDeviceManager: call markUnpaired() on startUnpair()
The unpair was originally done only by removing the appropriate device
from the m_devices container. During migration, this has changed because
the pairing is now managed by DeviceCache only. Thus, we need to call
markUnpaired() during the unpair process.

Fixes: dfdb7d9f41ee ("JablotronDeviceManager: migrate to generic handleUnpair()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -207,6 +207,7 @@ AsyncWork<set<DeviceID>>::Ptr JablotronDeviceManager::startUnpair(
 
 	if (it != m_devices.end()) {
 		it->second = nullptr;
+		deviceCache()->markUnpaired(id);
 		work->setResult({id});
 	}
 	else {