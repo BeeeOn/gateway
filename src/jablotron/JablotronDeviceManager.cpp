BeeeOn/gateway

commit 4b49348726e4542f59068c9f9bebbe413f6a0d4b

JablotronDeviceManager: manage pairing state by DeviceCache
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -138,7 +138,7 @@ void JablotronDeviceManager::jablotronProcess()
 		Mutex::ScopedLock guard(m_lock);
 
 		auto it = m_devices.find(id);
-		if (it != m_devices.end() && !it->second.isNull() && it->second->paired()) {
+		if (it != m_devices.end() && !it->second.isNull() && deviceCache()->paired(id)) {
 			shipMessage(message, it->second);
 		}
 		else if (it != m_devices.end() && m_isListen) {
@@ -176,7 +176,7 @@ void JablotronDeviceManager::doDeviceAcceptCommand(
 	if (it == m_devices.end())
 		throw NotFoundException("accept: " + cmd->deviceID().toString());
 
-	it->second->setPaired(true);
+	deviceCache()->markPaired(cmd->deviceID());
 }
 
 void JablotronDeviceManager::doUnpairCommand(
@@ -214,7 +214,7 @@ void JablotronDeviceManager::doListenCommand(
 			if (device.second.isNull())
 				continue;
 
-			if (!device.second->paired())
+			if (!deviceCache()->paired(device.second->deviceID()))
 				device.second = nullptr;
 		}
 	}));
@@ -236,7 +236,7 @@ void JablotronDeviceManager::doNewDevice(const DeviceID &deviceID,
 			return;
 		}
 
-		it->second->setPaired(false);
+		deviceCache()->markUnpaired(deviceID);
 	}
 
 	NewDeviceCommand::Ptr cmd =
@@ -268,7 +268,7 @@ void JablotronDeviceManager::loadDeviceList()
 				continue;
 			}
 
-			it->second->setPaired(true);
+			deviceCache()->markPaired(id);
 		}
 	}
 
@@ -492,7 +492,7 @@ bool JablotronDeviceManager::modifyValue(
 			+ " is not Jablotron AC-88");
 	}
 
-	if (!it->second->paired()) {
+	if (!deviceCache()->paired(deviceID)) {
 		InvalidArgumentException(
 			"device " + it->first.toString()
 			+ " is not paired");
@@ -516,7 +516,7 @@ void JablotronDeviceManager::obtainLastValue()
 		if (device.second.cast<JablotronDeviceAC88>().isNull())
 			continue;
 
-		if (!device.second->paired())
+		if (!deviceCache()->paired(device.second->deviceID()))
 			continue;
 
 		try {

src/jablotron/JablotronDevice.cpp
@@ -69,16 +69,6 @@ int JablotronDevice::parseValue(const string &msg) const
 	return NumberParser::parse(token[1]);
 }
 
-void JablotronDevice::setPaired(bool paired)
-{
-	m_paired = paired;
-}
-
-bool JablotronDevice::paired() const
-{
-	return m_paired;
-}
-
 DeviceID JablotronDevice::buildID(uint32_t serialNumber)
 {
 	return DeviceID(DevicePrefix::PREFIX_JABLOTRON, serialNumber);

src/jablotron/JablotronDevice.h
@@ -18,9 +18,6 @@ public:
 
 	virtual ~JablotronDevice();
 
-	void setPaired(bool paired);
-	bool paired() const;
-
 	DeviceID deviceID() const;
 
 	static DeviceID buildID(uint32_t serialNumber);
@@ -76,7 +73,6 @@ protected:
 
 private:
 	DeviceID m_deviceID;
-	bool m_paired;
 	std::string m_name;
 };
 

commit 8edaf39d321fe28933afa4aec497bd977b629f48

JablotronDeviceManager: avoid markUnpaired() in doNewDevice()
Originally, the doNewDevice() was explicitly setting a newly recognized
device to unpaired. This is however a bad practice when migrated to
the DeviceCache. When a new device is discovered, we make no claims
about its pairing state and go with the current DeviceCache setup.

Fixes: 4b49348726e4 ("JablotronDeviceManager: manage pairing state by DeviceCache")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronDeviceManager.cpp
@@ -230,8 +230,6 @@ void JablotronDeviceManager::doNewDevice(const DeviceID &deviceID,
 			logger().log(ex, __FILE__, __LINE__);
 			return;
 		}
-
-		deviceCache()->markUnpaired(deviceID);
 	}
 
 	NewDeviceCommand::Ptr cmd =