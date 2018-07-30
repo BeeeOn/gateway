BeeeOn/gateway

commit dbb9fb1dc20601cf65f28eff43b2c97a7f2d0955

ColorBrightness: new class
The class stores color represent by red, green and blue component.
It allows to count the brightness according to the biggest RGB component.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/util/ColorBrightness.cpp
@@ -1,0 +1,122 @@
+#include <cmath>
+#include <string>
+
+#include <Poco/Exception.h>
+
+#include "util/ColorBrightness.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+ColorBrightness::ColorBrightness(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue,
+		const uint8_t maxColorElement):
+	m_maxColorElement(maxColorElement)
+{
+	setColor(red, green, blue);
+}
+
+uint8_t ColorBrightness::brightness() const
+{
+	return m_brightness;
+}
+
+void ColorBrightness::setBrightness(const uint8_t brightness)
+{
+	if (brightness > 100)
+		throw IllegalStateException("brightness could not be greater than 100");
+
+	m_brightness = brightness;
+}
+
+uint8_t ColorBrightness::red() const
+{
+	return applyBrightness(m_red);
+}
+
+uint8_t ColorBrightness::green() const
+{
+	return applyBrightness(m_green);
+}
+
+uint8_t ColorBrightness::blue() const
+{
+	return applyBrightness(m_blue);
+}
+
+void ColorBrightness::setColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue)
+{
+	assertValidColor(red, green, blue);
+
+	normalizeColor(red, green, blue);
+}
+
+void ColorBrightness::assertValidColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue) const
+{
+	if (red > m_maxColorElement) {
+		throw IllegalStateException(
+			"red component(" + to_string(int(red)) + ") "
+			"could not be bigger then " + to_string(int(m_maxColorElement)));
+	}
+
+	if (green > m_maxColorElement) {
+		throw IllegalStateException(
+			"green component(" + to_string(int(green)) + ") "
+			"could not be bigger then " + to_string(int(m_maxColorElement)));
+	}
+
+	if (blue > m_maxColorElement) {
+		throw IllegalStateException(
+			"blue component(" + to_string(int(green)) + ") "
+			"could not be bigger then " + to_string(int(m_maxColorElement)));
+	}
+}
+
+void ColorBrightness::deriveBrightness(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue)
+{
+	if (red >= green && red >= blue)
+		m_brightness = brightnessFromColor(red);
+	else if (green >= red && green >= blue)
+		m_brightness = brightnessFromColor(green);
+	else
+		m_brightness = brightnessFromColor(blue);
+}
+
+void ColorBrightness::normalizeColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue)
+{
+	deriveBrightness(red, green, blue);
+
+	m_red = omitBrightness(red);
+	m_green = omitBrightness(green);
+	m_blue = omitBrightness(blue);
+}
+
+uint8_t ColorBrightness::brightnessFromColor(const uint8_t component) const
+{
+	return round((double(component) / m_maxColorElement) * 100);
+}
+
+uint8_t ColorBrightness::omitBrightness(const uint8_t component) const
+{
+	return round(component / (m_brightness / 100.0));
+}
+
+uint8_t ColorBrightness::applyBrightness(const uint8_t component) const
+{
+	return round(component * (m_brightness / 100.0));
+}

test/util/ColorBrightnessTest.cpp
@@ -1,0 +1,133 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Exception.h>
+
+#include "util/ColorBrightness.h"
+
+using namespace Poco;
+
+namespace BeeeOn {
+
+class ColorBrightnessTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(ColorBrightnessTest);
+	CPPUNIT_TEST(testCreation);
+	CPPUNIT_TEST(testModifyBrightness);
+	CPPUNIT_TEST(testModifyColor);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testCreation();
+	void testModifyBrightness();
+	void testModifyColor();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ColorBrightnessTest);
+
+/**
+ * @brief Test of creating ColorBrightness and counting of brightness.
+ */
+void ColorBrightnessTest::testCreation()
+{
+	ColorBrightness colorBrightness1(0xff, 0xff, 0xff, 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.red()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.green()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.blue()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.brightness()), 100);
+
+	ColorBrightness colorBrightness2(0x7f, 0x00, 0x00, 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.red()), 0x7f);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.green()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.blue()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.brightness()), 50);
+
+	ColorBrightness colorBrightness3(0x09, 0x00, 0x09, 0x60);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.red()), 0x09);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.green()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.blue()), 0x09);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.brightness()), 9);
+
+	ColorBrightness colorBrightness4(0x00, 0x56, 0x00, 0x60);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.red()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.green()), 0x56);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.blue()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.brightness()), 90);
+
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"red component(255) could not be bigger then 96",
+		ColorBrightness(0xff, 0x00, 0x00, 0x60),
+		IllegalStateException);
+}
+
+/**
+ * @brief Test of modifing RGB components by brightness.
+ */
+void ColorBrightnessTest::testModifyBrightness()
+{
+	ColorBrightness colorBrightness(0xff, 0xff, 0xff, 0xff);
+
+	colorBrightness.setBrightness(50);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0x80);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0x80);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0x80);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 50);
+
+	colorBrightness.setBrightness(25);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0x40);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0x40);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0x40);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 25);
+
+	colorBrightness.setBrightness(100);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 100);
+
+	colorBrightness.setBrightness(75);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0xbf);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0xbf);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0xbf);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 75);
+
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"brightness could not be greater than 100",
+		colorBrightness.setBrightness(150),
+		IllegalStateException);
+}
+
+/**
+ * @brief Test of modifing RGB components and counting of brightness.
+ */
+void ColorBrightnessTest::testModifyColor()
+{
+	ColorBrightness colorBrightness(0x60, 0x60, 0x60, 0x60);
+
+	colorBrightness.setColor(0x30, 0x30, 0x30);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0x30);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0x30);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0x30);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 50);
+
+	colorBrightness.setColor(0x59, 0x00, 0x12);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0x59);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0x00);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0x12);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 93);
+
+	colorBrightness.setColor(0x12, 0x35, 0x48);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.red()), 0x12);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.green()), 0x35);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.blue()), 0x48);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness.brightness()), 75);
+
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"green component(255) could not be bigger then 96",
+		colorBrightness.setColor(0x00, 0xff, 0x00),
+		IllegalStateException);
+
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"blue component(97) could not be bigger then 96",
+		colorBrightness.setColor(0x60, 0x60, 0x61),
+		IllegalStateException);
+}
+
+}

src/CMakeLists.txt
@@ -126,6 +126,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/GWSOutputQueue.cpp
 	${PROJECT_SOURCE_DIR}/server/GWServerConnector.cpp
 	${PROJECT_SOURCE_DIR}/server/ServerAnswer.cpp
+	${PROJECT_SOURCE_DIR}/util/ColorBrightness.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/DataReader.cpp
 	${PROJECT_SOURCE_DIR}/util/DataWriter.cpp

src/util/ColorBrightness.h
@@ -1,0 +1,100 @@
+#pragma once
+
+#include <cstdint>
+
+namespace BeeeOn {
+
+/**
+ * @brief The class stores color represent by red, green and blue component.
+ * It allows to count the brightness according to the biggest RGB component.
+ */
+class ColorBrightness {
+public:
+	/**
+	 * @param maxColorElement maximum allowed value of RGB component
+	 */
+	ColorBrightness(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue,
+		const uint8_t maxColorElement = 0xff);
+
+	/**
+	 * @brief Returns brightness in percents.
+	 */
+	uint8_t brightness() const;
+
+	/**
+	 * @param brightness in percents
+	 * @throw IllegalStateException in case of the brightness
+	 * is bigger then 100
+	 */
+	void setBrightness(const uint8_t brightness);
+
+	uint8_t red() const;
+	uint8_t green() const;
+	uint8_t blue() const;
+
+	/**
+	 * @throw IllegalStateException in case of some component
+	 * is bigger than max color element
+	 */
+	void setColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue);
+
+private:
+	/**
+	 * @throw IllegalStateException in case of some component
+	 * is bigger than max color element
+	 */
+	void assertValidColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue) const;
+
+	/**
+	 * @brief Counts the brightness according to the biggest
+	 * RGB component.
+	 */
+	void deriveBrightness(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue);
+
+	/**
+	 * @brief Normlizes the color which means that it adjusts
+	 * each RGB component so that the brightness is 100%.
+	 */
+	void normalizeColor(
+		const uint8_t red,
+		const uint8_t green,
+		const uint8_t blue);
+
+	/**
+	 * @brief Counts the brightness by the given RGB component.
+	 * @return brightness
+	 */
+	uint8_t brightnessFromColor(const uint8_t component) const;
+
+	/**
+	 * @brief Omits the brightness from the given RGB component.
+	 */
+	uint8_t omitBrightness(const uint8_t component) const;
+
+	/**
+	 * @brief Applies the brightness to the given RGB component.
+	 */
+	uint8_t applyBrightness(const uint8_t component) const;
+
+private:
+	uint8_t m_red;
+	uint8_t m_green;
+	uint8_t m_blue;
+	uint8_t m_brightness;
+
+	uint8_t m_maxColorElement;
+};
+
+}

test/CMakeLists.txt
@@ -53,6 +53,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsTest.cpp
 	${PROJECT_SOURCE_DIR}/exporters/JournalQueuingStrategyTest.cpp
 	${PROJECT_SOURCE_DIR}/exporters/RecoverableJournalQueuingStrategyTest.cpp
+	${PROJECT_SOURCE_DIR}/util/ColorBrightnessTest.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatterTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DataWriterTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DataReaderTest.cpp

commit 89110818eb833a780c72736ac03f3d2e1001fd87

ColorBrightness: fix omitBrightness() to deal with rounding error
Rounding error caused when counting brightness in brightnessFromColor()
causes an incorrect result of omitBrightness(). The resulting
RGB value may be greater than the defined maximum value.

Fixes: dbb9fb1 ("ColorBrightness: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/ColorBrightness.cpp
@@ -113,7 +113,8 @@ uint8_t ColorBrightness::brightnessFromColor(const uint8_t component) const
 
 uint8_t ColorBrightness::omitBrightness(const uint8_t component) const
 {
-	return round(component / (m_brightness / 100.0));
+	uint32_t result = round(component / (m_brightness / 100.0));
+	return result > m_maxColorElement ? m_maxColorElement : result;
 }
 
 uint8_t ColorBrightness::applyBrightness(const uint8_t component) const

test/util/ColorBrightnessTest.cpp
@@ -13,11 +13,13 @@ class ColorBrightnessTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testCreation);
 	CPPUNIT_TEST(testModifyBrightness);
 	CPPUNIT_TEST(testModifyColor);
+	CPPUNIT_TEST(testRounding);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void testCreation();
 	void testModifyBrightness();
 	void testModifyColor();
+	void testRounding();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ColorBrightnessTest);
@@ -128,6 +130,32 @@ void ColorBrightnessTest::testModifyColor()
 		"blue component(97) could not be bigger then 96",
 		colorBrightness.setColor(0x60, 0x60, 0x61),
 		IllegalStateException);
+}
+
+/**
+ * @brief Test of modifing small RGB components by brightness.
+ */
+void ColorBrightnessTest::testRounding()
+{
+	ColorBrightness colorBrightness1(0x00, 0x00, 0x05, 0x60);
+	colorBrightness1.setBrightness(100);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.blue()), 0x60);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness1.brightness()), 100);
+
+	ColorBrightness colorBrightness2(0x00, 0x01, 0x00, 0x60);
+	colorBrightness2.setBrightness(100);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.green()), 0x60);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness2.brightness()), 100);
+
+	ColorBrightness colorBrightness3(0x00, 0x08, 0x00, 0xff);
+	colorBrightness3.setBrightness(100);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.green()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness3.brightness()), 100);
+
+	ColorBrightness colorBrightness4(0x03, 0x00, 0x00, 0xff);
+	colorBrightness4.setBrightness(100);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.red()), 0xff);
+	CPPUNIT_ASSERT_EQUAL(int(colorBrightness4.brightness()), 100);
 }
 
 }