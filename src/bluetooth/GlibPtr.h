BeeeOn/gateway

commit fa6decfb45d23f7e2c27288395ec891ac54c1fe3

GlibPtr: new class
The class is used to store the references to objects from the GLib library.
It is also responsible for automatically freeing of memory.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/bluetooth/GlibPtr.h
@@ -1,0 +1,266 @@
+#pragma once
+
+#include <glib.h>
+#include <gio/gio.h>
+
+#include <utility>
+
+#include <Poco/Exception.h>
+
+#include "bluetooth/GlibPtr.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The class is used to store the references to objects from the GLib library.
+ * It is also responsible for automatically freeing of memory.
+ *
+ * In specialization for GList are used g_object_ref and g_object_unref for its items.
+ */
+template <typename T>
+class GlibPtr {
+public:
+	GlibPtr();
+	GlibPtr(T* ptr);
+	GlibPtr(const GlibPtr& ptr);
+	GlibPtr(GlibPtr&& ptr);
+	~GlibPtr();
+
+	GlibPtr& operator =(T* ptr);
+	GlibPtr& operator =(const GlibPtr& ptr);
+	GlibPtr& operator =(GlibPtr&& ptr);
+	T* operator ->();
+	const T* operator ->() const;
+	T& operator *();
+	const T& operator *() const;
+	T* raw();
+	const T* raw() const;
+
+	/**
+	 * @brief Returns reference of the m_ptr attribute.
+	 * @throws IllegalStateException in case m_ptr is not null
+	 */
+	T** operator &();
+
+	/**
+	 * @brief Returns reference of the m_ptr attribute.
+	 */
+	const T** operator &() const;
+	bool operator !() const;
+	bool isNull() const;
+
+protected:
+	void release();
+	void assign(T* ptr);
+
+private:
+	T* m_ptr;
+};
+
+template <typename T>
+GlibPtr<T>::GlibPtr():
+	m_ptr(nullptr)
+{
+}
+
+template <typename T>
+GlibPtr<T>::GlibPtr(T* ptr):
+	m_ptr(ptr)
+{
+}
+
+template <typename T>
+GlibPtr<T>::GlibPtr(const GlibPtr& ptr):
+	m_ptr(nullptr)
+{
+	assign(ptr.m_ptr);
+}
+
+
+template <typename T>
+GlibPtr<T>::GlibPtr(GlibPtr&& ptr):
+	m_ptr(nullptr)
+{
+	std::swap(m_ptr, ptr.m_ptr);
+}
+
+template <typename T>
+GlibPtr<T>::~GlibPtr()
+{
+	release();
+}
+
+template <typename T>
+GlibPtr<T>& GlibPtr<T>::operator =(T* ptr)
+{
+	return (*this = GlibPtr<T>{ptr});
+}
+
+template <typename T>
+GlibPtr<T>& GlibPtr<T>::operator =(const GlibPtr& ptr)
+{
+	if (ptr.m_ptr != m_ptr)
+		assign(ptr.m_ptr);
+
+	return *this;
+}
+
+template <typename T>
+GlibPtr<T>& GlibPtr<T>::operator =(GlibPtr&& ptr)
+{
+	std::swap(m_ptr, ptr.m_ptr);
+	return *this;
+}
+
+template <typename T>
+T* GlibPtr<T>::operator ->()
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr operator ->");
+
+	return m_ptr;
+}
+
+template <typename T>
+const T* GlibPtr<T>::operator ->() const
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr const operator ->");
+
+	return m_ptr;
+}
+
+template <typename T>
+T& GlibPtr<T>::operator *()
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr operator *");
+
+	return *m_ptr;
+}
+
+template <typename T>
+const T& GlibPtr<T>::operator *() const
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr const operator *");
+
+	return *m_ptr;
+}
+
+template <typename T>
+T* GlibPtr<T>::raw()
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr method raw");
+
+	return m_ptr;
+}
+
+template <typename T>
+const T* GlibPtr<T>::raw() const
+{
+	if (isNull())
+		throw Poco::NullPointerException(
+			"GlibPtr method const raw");
+
+	return m_ptr;
+}
+
+template <typename T>
+T** GlibPtr<T>::operator &()
+{
+	if (isNull())
+		return &m_ptr;
+
+	throw Poco::IllegalStateException(
+		"cannot dereference non-const non-null GlibPtr");
+}
+
+template <typename T>
+const T** GlibPtr<T>::operator &() const
+{
+	return &m_ptr;
+}
+
+template <typename T>
+bool GlibPtr<T>::operator !() const
+{
+	return isNull();
+}
+
+template <typename T>
+bool GlibPtr<T>::isNull() const
+{
+	return m_ptr == nullptr;
+}
+
+template <typename GO>
+inline void GlibPtr<GO>::release()
+{
+	g_clear_object(&m_ptr);
+}
+
+template <typename GO>
+inline void GlibPtr<GO>::assign(GO* ptr)
+{
+	release();
+	if (ptr != nullptr)
+		m_ptr = reinterpret_cast<GO*>(::g_object_ref(ptr));
+}
+
+template <>
+inline void GlibPtr<GMainLoop>::release()
+{
+	if (!isNull()) {
+		::g_main_loop_unref(m_ptr);
+		m_ptr = nullptr;
+	}
+}
+
+template <>
+inline void GlibPtr<GMainLoop>::assign(GMainLoop* ptr)
+{
+	release();
+	if (ptr != nullptr)
+		m_ptr = ::g_main_loop_ref(ptr);
+}
+
+template <>
+inline void GlibPtr<GError>::release()
+{
+	::g_clear_error(&m_ptr);
+}
+
+template <>
+inline void GlibPtr<GError>::assign(GError* ptr)
+{
+	release();
+	if (ptr != nullptr)
+		m_ptr = ::g_error_copy(ptr);
+}
+
+template <>
+inline void GlibPtr<GList>::release()
+{
+	if (!isNull()) {
+		::g_list_free_full(m_ptr, g_object_unref);
+		m_ptr = nullptr;
+	}
+}
+
+template <>
+inline void GlibPtr<GList>::assign(GList* ptr)
+{
+	release();
+	if (ptr != nullptr)
+		m_ptr = ::g_list_copy_deep(ptr, GCopyFunc(g_object_ref), nullptr);
+}
+
+
+}

test/CMakeLists.txt
@@ -55,6 +55,15 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataParserTest.cpp
 )
 
+if(ENABLE_BLUETOOTH)
+	if(HAS_DBUS_BLUEZ)
+		file(GLOB BLUETOOTH_SOURCES
+			${PROJECT_SOURCE_DIR}/bluetooth/GlibPtrTest.cpp
+		)
+		list(APPEND TEST_SOURCES ${BLUETOOTH_SOURCES})
+	endif()
+endif()
+
 if(ENABLE_VPT)
 	file(GLOB VPT_TEST_SOURCES
 		${PROJECT_SOURCE_DIR}/vpt/VPTDeviceTest.cpp

test/bluetooth/GlibPtrTest.cpp
@@ -1,0 +1,156 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "bluetooth/GlibPtr.h"
+
+using namespace Poco;
+using namespace std;
+
+namespace BeeeOn {
+
+class GlibPtrTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(GlibPtrTest);
+	CPPUNIT_TEST(testGObject);
+	CPPUNIT_TEST(testGMainLoop);
+	CPPUNIT_TEST(testGError);
+	CPPUNIT_TEST(testGList);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testGObject();
+	void testGMainLoop();
+	void testGError();
+	void testGList();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(GlibPtrTest);
+
+void GlibPtrTest::testGObject()
+{
+	GlibPtr<GObject> object1;
+	CPPUNIT_ASSERT_EQUAL(object1.isNull(), true);
+	CPPUNIT_ASSERT_THROW(*object1, NullPointerException);
+	CPPUNIT_ASSERT_THROW(object1.raw(), NullPointerException);
+
+	object1 = reinterpret_cast<GObject*>(::g_object_new(G_TYPE_OBJECT, 0));
+	CPPUNIT_ASSERT_EQUAL(object1.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*object1);
+	CPPUNIT_ASSERT_NO_THROW(object1.raw());
+
+	GlibPtr<GObject> object2 = object1;
+	CPPUNIT_ASSERT_EQUAL(object2.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*object2);
+	CPPUNIT_ASSERT_NO_THROW(object2.raw());
+
+	GlibPtr<GObject> object3 = GlibPtr<GObject>(
+		reinterpret_cast<GObject*>(::g_object_new(G_TYPE_OBJECT, 0)));
+	CPPUNIT_ASSERT_EQUAL(object3.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*object3);
+	CPPUNIT_ASSERT_NO_THROW(object3.raw());
+}
+
+void GlibPtrTest::testGMainLoop()
+{
+	GlibPtr<GMainLoop> loop1;
+	CPPUNIT_ASSERT_EQUAL(loop1.isNull(), true);
+	CPPUNIT_ASSERT_THROW(*loop1, NullPointerException);
+	CPPUNIT_ASSERT_THROW(loop1.raw(), NullPointerException);
+
+	loop1 = ::g_main_loop_new(nullptr, false);
+	CPPUNIT_ASSERT_EQUAL(loop1.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*loop1);
+	CPPUNIT_ASSERT_NO_THROW(loop1.raw());
+
+	GlibPtr<GMainLoop> loop2 = loop1;
+	CPPUNIT_ASSERT_EQUAL(loop2.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*loop2);
+	CPPUNIT_ASSERT_NO_THROW(loop2.raw());
+
+	GlibPtr<GMainLoop> loop3 = GlibPtr<GMainLoop>(::g_main_loop_new(nullptr, false));
+	CPPUNIT_ASSERT_EQUAL(loop3.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*loop3);
+	CPPUNIT_ASSERT_NO_THROW(loop3.raw());
+}
+
+void GlibPtrTest::testGError()
+{
+	GlibPtr<GError> error1;
+	CPPUNIT_ASSERT_EQUAL(error1.isNull(), true);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"GlibPtr operator *",
+		*error1,
+		NullPointerException);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"GlibPtr method get",
+		error1.raw(),
+		NullPointerException);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"GlibPtr operator ->",
+		error1->message,
+		NullPointerException);
+	CPPUNIT_ASSERT_NO_THROW(&error1);
+
+	error1 = ::g_error_new(1, 1, "error");
+	CPPUNIT_ASSERT_EQUAL(error1.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*error1);
+	CPPUNIT_ASSERT_NO_THROW(error1.raw());
+	CPPUNIT_ASSERT_NO_THROW(error1->message);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"cannot dereference non-const non-null GlibPtr",
+		&error1,
+		IllegalStateException);
+
+	GlibPtr<GError> error2 = error1;
+	CPPUNIT_ASSERT_EQUAL(error2.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*error2);
+	CPPUNIT_ASSERT_NO_THROW(error2.raw());
+	CPPUNIT_ASSERT_NO_THROW(error2->message);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"cannot dereference non-const non-null GlibPtr",
+		&error2,
+		IllegalStateException);
+
+	GlibPtr<GError> error3 = GlibPtr<GError>(::g_error_new(1, 1, "error"));
+	CPPUNIT_ASSERT_EQUAL(error3.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*error3);
+	CPPUNIT_ASSERT_NO_THROW(error3.raw());
+	CPPUNIT_ASSERT_NO_THROW(error3->message);
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"cannot dereference non-const non-null GlibPtr",
+		&error3,
+		IllegalStateException);
+}
+
+void GlibPtrTest::testGList()
+{
+	GlibPtr<GList> list1;
+	CPPUNIT_ASSERT_EQUAL(list1.isNull(), true);
+	CPPUNIT_ASSERT_THROW(*list1, NullPointerException);
+	CPPUNIT_ASSERT_THROW(list1.raw(), NullPointerException);
+
+	GList* rawList1 = nullptr;
+	rawList1 = ::g_list_append(rawList1, ::g_object_new(G_TYPE_OBJECT, 0));
+	rawList1 = ::g_list_append(rawList1, ::g_object_new(G_TYPE_OBJECT, 0));
+
+	list1 = rawList1;
+	CPPUNIT_ASSERT_EQUAL(list1.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*list1);
+	CPPUNIT_ASSERT_NO_THROW(list1.raw());
+	CPPUNIT_ASSERT_EQUAL(static_cast<int>(::g_list_length(list1.raw())), 2);
+
+	GlibPtr<GList> list2 = list1;
+	CPPUNIT_ASSERT_EQUAL(list2.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*list2);
+	CPPUNIT_ASSERT_NO_THROW(list2.raw());
+	CPPUNIT_ASSERT_EQUAL(static_cast<int>(::g_list_length(list2.raw())), 2);
+
+	GList* rawList2 = nullptr;
+	rawList2 = ::g_list_append(rawList2, ::g_object_new(G_TYPE_OBJECT, 0));
+	rawList2 = ::g_list_append(rawList2, ::g_object_new(G_TYPE_OBJECT, 0));
+
+	GlibPtr<GList> list3 = GlibPtr<GList>(rawList2);
+	CPPUNIT_ASSERT_EQUAL(list3.isNull(), false);
+	CPPUNIT_ASSERT_NO_THROW(*list3);
+	CPPUNIT_ASSERT_NO_THROW(list3.raw());
+	CPPUNIT_ASSERT_EQUAL(static_cast<int>(::g_list_length(list3.raw())), 2);
+}
+
+}

commit b093a386fa0d0f754fc026225d0c7730e1defdb1

GlibPtr: improve using ::g_object_ref() in ::g_list_copy_deep()
Fixes: fa6decfb45d2 ("GlibPtr: new class")

Suggested-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/GlibPtr.h
@@ -11,6 +11,11 @@
 
 namespace BeeeOn {
 
+static inline gpointer g_object_ref_copy(gconstpointer src, gpointer)
+{
+	return ::g_object_ref(const_cast<gpointer>(src));
+}
+
 /**
  * @brief The class is used to store the references to objects from the GLib library.
  * It is also responsible for automatically freeing of memory.
@@ -259,7 +264,7 @@ inline void GlibPtr<GList>::assign(GList* ptr)
 {
 	release();
 	if (ptr != nullptr)
-		m_ptr = ::g_list_copy_deep(ptr, GCopyFunc(g_object_ref), nullptr);
+		m_ptr = ::g_list_copy_deep(ptr, g_object_ref_copy, nullptr);
 }
 
 template <>