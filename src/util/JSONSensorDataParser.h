BeeeOn/gateway

commit d216c15c827478fef94a95059ecddb2a7c06c8dc

JSONSensorDataParser: new class
Works complementary to JSONSensorDataFormatter. Provides a method to
parse SensorData stored in string as JSONObject.

The test covers parsing SensorData stored in string form to SensorData object
in normal situation and extreme situations such as not-a-number or empty values.

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/JSONSensorDataParser.h
@@ -1,0 +1,26 @@
+#pragma once
+
+#include "model/SensorData.h"
+#include "util/SensorDataParser.h"
+
+namespace BeeeOn {
+
+/**
+ * @class JSONSensorDataParser
+ * @brief Provides a method to parse SensorData from a string containing JSONObject to a SensorData object
+ *
+ * Example string:
+ *
+ *		{"device_id":"0x499602d2","timestamp":150000000000,"data":[{"module_id":5,"value":4.2},{"module_id":4,"value":0.5}]}
+ *
+ * would be parsed to a SensorData object with relevant values. It is assumed that the "timestamp" contains
+ * epochMicroseconds.
+ * When a "value" is null, it is parsed to NaN.
+ */
+class JSONSensorDataParser : SensorDataParser {
+public:
+	SensorData parse(const std::string &data) const override;
+};
+
+}
+

src/CMakeLists.txt
@@ -73,6 +73,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/server/GWServerConnector.cpp
 	${PROJECT_SOURCE_DIR}/util/CSVSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatter.cpp
+	${PROJECT_SOURCE_DIR}/util/JSONSensorDataParser.cpp
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveListener.cpp

src/util/JSONSensorDataParser.cpp
@@ -1,0 +1,31 @@
+#include "util/JsonUtil.h"
+#include "util/JSONSensorDataParser.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace std;
+
+SensorData JSONSensorDataParser::parse(const string &data) const
+{
+	SensorData sensorData;
+	Object::Ptr object = JsonUtil::parse(data);
+
+	DeviceID id = DeviceID::parse(object->getValue<string>("device_id"));
+	sensorData.setDeviceID(id);
+
+	sensorData.setTimestamp(Timestamp(object->getValue<Timestamp::TimeVal>("timestamp")));
+
+	Array::Ptr array = object->getArray("data");
+
+	for (unsigned int i = 0; i < array->size(); ++i) {
+		Object::Ptr tmpObject = array->getObject(i);
+
+		ModuleID tmpID = ModuleID::parse(tmpObject->getValue<string>("module_id"));
+		double tmpValue = tmpObject->optValue<double>("value", NAN);
+
+		sensorData.insertValue(SensorValue(tmpID, tmpValue));
+	}
+
+	return sensorData;
+}

test/CMakeLists.txt
@@ -35,6 +35,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsStorageTest.cpp
 	${PROJECT_SOURCE_DIR}/credentials/CredentialsTest.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatterTest.cpp
+	${PROJECT_SOURCE_DIR}/util/JSONSensorDataParserTest.cpp
 )
 
 if(ENABLE_VPT)

test/util/JSONSensorDataParserTest.cpp
@@ -1,0 +1,80 @@
+#include <cmath>
+
+#include <Poco/Timespan.h>
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BetterAssert.h>
+
+#include "model/SensorData.h"
+#include "util/JSONSensorDataParser.h"
+
+using namespace Poco;
+using namespace std;
+
+namespace BeeeOn {
+
+class JSONSensorDataParserTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(JSONSensorDataParserTest);
+	CPPUNIT_TEST(testParse);
+	CPPUNIT_TEST(testParseNaN);
+	CPPUNIT_TEST(testParseNoValues);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testParse();
+	void testParseNaN();
+	void testParseNoValues();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(JSONSensorDataParserTest);
+
+void JSONSensorDataParserTest::testParse()
+{
+	const string stringForm =
+			R"({"device_id":"0x499602d2","timestamp":95000000000,"data":[{"module_id":5,"value":4.2},{"module_id":4,"value":0.5}]})";
+
+	JSONSensorDataParser parser;
+	SensorData data = parser.parse(stringForm);
+
+	CPPUNIT_ASSERT_EQUAL("0x499602d2", data.deviceID().toString());
+	CPPUNIT_ASSERT_EQUAL(95000000000, data.timestamp().value().epochMicroseconds());
+
+	CPPUNIT_ASSERT_EQUAL(5, data[0].moduleID());
+	CPPUNIT_ASSERT_EQUAL(4.2, data[0].value());
+
+	CPPUNIT_ASSERT_EQUAL(4, data[1].moduleID());
+	CPPUNIT_ASSERT_EQUAL(0.5, data[1].value());
+}
+
+void JSONSensorDataParserTest::testParseNaN()
+{
+	const string stringForm =
+			R"({"device_id":"0x499602d2","timestamp":95000000000,"data":[{"module_id":5,"value":15439.15},{"module_id":4,"value":null}]})";
+
+	JSONSensorDataParser parser;
+	SensorData data = parser.parse(stringForm);
+
+	CPPUNIT_ASSERT_EQUAL("0x499602d2", data.deviceID().toString());
+	CPPUNIT_ASSERT_EQUAL(95000000000, data.timestamp().value().epochMicroseconds());
+
+	CPPUNIT_ASSERT_EQUAL(5, data[0].moduleID());
+	CPPUNIT_ASSERT_EQUAL(15439.15, data[0].value());
+
+	CPPUNIT_ASSERT_EQUAL(4, data[1].moduleID());
+	CPPUNIT_ASSERT(std::isnan(data[1].value()));
+}
+
+void JSONSensorDataParserTest::testParseNoValues()
+{
+	SensorData data;
+	data.setDeviceID(0x499602d4);
+
+	const string stringForm = R"({"device_id":"0x499602d4","timestamp":)"
+							+ to_string(data.timestamp().value().epochMicroseconds())
+							+ R"(,"data":[]})";
+
+	JSONSensorDataParser parser;
+
+	CPPUNIT_ASSERT(data == parser.parse(stringForm));
+}
+
+}

commit 889ced5bb83a087164e2efda2d596decd5b5c2e9

JSONSensorDataParser: inherit SensorDataParser publicly
Non-public inheritance does not provide any useful polymorphism.

Fixes: d216c15c8274 ("JSONSensorDataParser: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/JSONSensorDataParser.h
@@ -17,7 +17,7 @@ namespace BeeeOn {
  * epochMicroseconds.
  * When a "value" is null, it is parsed to NaN.
  */
-class JSONSensorDataParser : SensorDataParser {
+class JSONSensorDataParser : public SensorDataParser {
 public:
 	SensorData parse(const std::string &data) const override;
 };