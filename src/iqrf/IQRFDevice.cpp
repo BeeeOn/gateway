64b64221297afa4057f3267e27231b892735d923
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-08-08 16:02:27 +0200

    IQRFDevice: new class
    Provides representation of a IQRF device in IQRF network. IQRF device
    contains its properties (HWPID, NodeID, vendor name, product name, ...).
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
    Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

---
+++ src/iqrf/IQRFDevice.cpp
@@ -0,0 +1,319 @@
+#include <Poco/Clock.h>
+#include <Poco/Logger.h>
+
+#include "iqrf/DPAProtocol.h"
+#include "iqrf/DPAResponse.h"
+#include "iqrf/IQRFDevice.h"
+#include "iqrf/IQRFUtil.h"
+#include "iqrf/request/DPAOSPeripheralInfoRequest.h"
+#include "iqrf/response/DPAOSPeripheralInfoResponse.h"
+#include "util/ClassInfo.h"
+#include "IQRFDevice.h"
+
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+IQRFDevice::IQRFDevice(
+		IQRFMqttConnector::Ptr connector,
+		const Timespan &receiveTimeout,
+		DPAMessage::NetworkAddress address,
+		DPAProtocol::Ptr protocol,
+		const RefreshTime &refreshTime,
+		const RefreshTime &refreshTimePeripheralInfo):
+	m_connector(connector),
+	m_receiveTimeout(receiveTimeout),
+	m_address(address),
+	m_protocol(protocol),
+	m_refreshTime(refreshTime),
+	m_refreshTimePeripheralInfo(refreshTimePeripheralInfo),
+	m_remainingValueTime(0),
+	m_remainingPeripheralInfoTime(0),
+	m_remaining(1 * Timespan::SECONDS)
+{
+}
+
+DPAMessage::NetworkAddress IQRFDevice::networkAddress() const
+{
+	return m_address;
+}
+
+uint32_t IQRFDevice::mid() const
+{
+	return m_mid;
+}
+
+list<ModuleType> IQRFDevice::modules() const
+{
+	return m_modules;
+}
+
+DPAProtocol::Ptr IQRFDevice::protocol() const
+{
+	return m_protocol;
+}
+
+uint16_t IQRFDevice::HWPID() const
+{
+	return m_HWPID;
+}
+
+void IQRFDevice::setHWPID(uint16_t HWPID)
+{
+	m_HWPID = HWPID;
+}
+
+DeviceID IQRFDevice::id() const
+{
+	uint64_t id = 0;
+	id = uint64_t(m_mid) << 16;
+	id |= m_HWPID;
+
+	return DeviceID(DevicePrefix::PREFIX_IQRF, id);
+}
+
+string IQRFDevice::vendorName() const
+{
+	return m_vendorName;
+}
+
+string IQRFDevice::productName() const
+{
+	return m_productName;
+}
+
+ModuleID IQRFDevice::batteryModuleID() const
+{
+	return m_modules.size() - 2;
+}
+
+ModuleID IQRFDevice::rssiModuleID() const
+{
+	return m_modules.size() - 1;
+}
+
+string IQRFDevice::toString() const
+{
+	string repr;
+
+	repr += "deviceID: ";
+	repr += id().toString();
+	repr += "; ";
+
+	repr += "node: ";
+	repr += to_string(m_address);
+	repr += "; ";
+
+	repr += "protocol: ";
+	repr += (m_protocol) ?
+		ClassInfo::forPointer(m_protocol.get()).name() : "none";
+	repr += "; ";
+
+	repr += "vendor: ";
+	repr += m_vendorName;
+	repr += "; ";
+
+	repr += "product: ";
+	repr += m_productName;
+	repr += "; ";
+
+	repr += "mid: ";
+	repr += NumberFormatter::formatHex(m_mid, true);
+
+	if (!m_modules.empty()) {
+		repr += "; ";
+		repr += "modules: ";
+
+		for (const auto &module : m_modules) {
+			repr += module.type().toString();
+			repr += ", ";
+		}
+
+		repr.pop_back();
+		repr.pop_back();
+	}
+
+	return repr;
+}
+
+void IQRFDevice::probe(
+	const Timespan &methodTimeout)
+{
+	const Clock started;
+
+	m_HWPID = detectNodeHWPID(methodTimeout - started.elapsed());
+	m_mid = detectMID(methodTimeout - started.elapsed());
+	m_modules = detectModules(methodTimeout - started.elapsed());
+
+	const DPAProtocol::ProductInfo productInfo =
+		detectProductInfo(methodTimeout - started.elapsed());
+	m_vendorName = productInfo.vendorName;
+	m_productName = productInfo.productName;
+}
+
+uint16_t IQRFDevice::detectNodeHWPID(
+		const Timespan &methodTimeout)
+{
+	if (logger().trace())
+		logger().trace("detect of HWPID", __FILE__, __LINE__);
+
+	if (m_receiveTimeout > methodTimeout) {
+		throw TimeoutException(
+			"received timeout is less than maximum timeout of method");
+	}
+
+	const IQRFJsonResponse::Ptr response =
+		IQRFUtil::makeRequest(
+			m_connector,
+			m_protocol->pingRequest(m_address),
+			m_receiveTimeout
+		);
+
+	return DPAResponse::fromRaw(response->response())->HWPID();
+}
+
+uint32_t IQRFDevice::detectMID(const Timespan &methodTimeout)
+{
+	if (logger().trace())
+		logger().trace("detect of MID", __FILE__, __LINE__);
+
+	if (m_receiveTimeout > methodTimeout) {
+		throw TimeoutException(
+			"received timeout is less than maximum timeout of method");
+	}
+
+	const IQRFJsonResponse::Ptr response =
+		IQRFUtil::makeRequest(
+			m_connector,
+			new DPAOSPeripheralInfoRequest(m_address),
+			m_receiveTimeout
+		);
+
+	const DPAResponse::Ptr dpa = DPAResponse::fromRaw(response->response());
+	return dpa.cast<DPAOSPeripheralInfoResponse>()->mid();
+}
+
+list<ModuleType> IQRFDevice::detectModules(const Timespan &methodTimeout)
+{
+	if (logger().trace()) {
+		logger().trace(
+			"detect of modules for node "
+			+ NumberFormatter::formatHex(m_address, true),
+			__FILE__, __LINE__);
+	}
+
+	if (m_receiveTimeout > methodTimeout) {
+		throw TimeoutException(
+			"received timeout is less than maximum timeout of method");
+	}
+
+	const IQRFJsonResponse::Ptr response =
+		IQRFUtil::makeRequest(
+			m_connector,
+			m_protocol->dpaModulesRequest(m_address),
+			m_receiveTimeout
+		);
+
+	return m_protocol->extractModules(
+		DPAResponse::fromRaw(response->response())
+			->peripheralData()
+	);
+}
+
+DPAProtocol::ProductInfo IQRFDevice::detectProductInfo(
+		const Timespan &methodTimeout)
+{
+	if (logger().trace()) {
+		logger().trace(
+			"detect of product info for node "
+			+ NumberFormatter::formatHex(m_address, true),
+			__FILE__, __LINE__);
+	}
+
+	if (m_receiveTimeout > methodTimeout) {
+		throw TimeoutException(
+			"received timeout is less than maximum timeout of method");
+	}
+
+	const IQRFJsonResponse::Ptr response =
+		IQRFUtil::makeRequest(
+			m_connector,
+			m_protocol->dpaProductInfoRequest(m_address),
+			m_receiveTimeout
+		);
+
+	const DPAResponse::Ptr dpa = DPAResponse::fromRaw(response->response());
+	return m_protocol->extractProductInfo(dpa->peripheralData(), m_HWPID);
+}
+
+SensorData IQRFDevice::obtainValues() const
+{
+	const IQRFJsonResponse::Ptr valueResponse =
+		IQRFUtil::makeRequest(
+			m_connector,
+			m_protocol->dpaValueRequest(m_address, m_modules),
+			m_receiveTimeout
+		);
+	DPAResponse::Ptr dpaValue =
+		DPAResponse::fromRaw(valueResponse->response());
+
+	SensorData sensorData =
+		m_protocol->parseValue(
+			m_modules,
+			dpaValue->peripheralData()
+		);
+	sensorData.setDeviceID(id());
+
+	return sensorData;
+}
+
+SensorData IQRFDevice::obtainPeripheralInfo() const
+{
+	ModuleID batteryID = batteryModuleID();
+	ModuleID rssiID = rssiModuleID();
+
+	const IQRFJsonResponse::Ptr peripheralResponse =
+		IQRFUtil::makeRequest(
+			m_connector,
+			new DPAOSPeripheralInfoRequest(m_address),
+			m_receiveTimeout
+		);
+	DPAOSPeripheralInfoResponse::Ptr dpaPeripheral =
+		DPAResponse::fromRaw(peripheralResponse->response()).cast<DPAOSPeripheralInfoResponse>();
+
+	SensorData sensorData;
+	sensorData.setDeviceID(id());
+	sensorData.insertValue({batteryID, dpaPeripheral->percentageSupplyVoltage()});
+	sensorData.insertValue({rssiID, dpaPeripheral->rssiPercentage()});
+
+	return sensorData;
+}
+
+RefreshTime IQRFDevice::refresh() const
+{
+	return RefreshTime::fromSeconds(m_remaining.totalSeconds());
+}
+
+void IQRFDevice::poll(Distributor::Ptr distributor)
+{
+	if (m_remainingValueTime.totalSeconds() <= 0) {
+		m_remainingValueTime = m_refreshTime;
+		distributor->exportData(obtainValues());
+	}
+
+	if (m_remainingPeripheralInfoTime.totalSeconds() <= 0) {
+		m_remainingPeripheralInfoTime = m_refreshTimePeripheralInfo;
+		distributor->exportData(obtainPeripheralInfo());
+	}
+
+	Timespan remainingTime;
+	if (m_remainingValueTime < m_remainingPeripheralInfoTime)
+		remainingTime = m_remainingValueTime;
+	else
+		remainingTime = m_remainingPeripheralInfoTime;
+
+	m_remainingValueTime -= remainingTime;
+	m_remainingPeripheralInfoTime -= remainingTime;
+	m_remaining = remainingTime;
+}
---
+++ src/iqrf/IQRFDevice.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "core/PollableDevice.h"
+#include "iqrf/DPAMessage.h"
+#include "iqrf/DPAProtocol.h"
+#include "iqrf/IQRFMqttConnector.h"
+#include "model/DeviceID.h"
+#include "model/ModuleType.h"
+#include "model/RefreshTime.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief IQRFDevice represents information about a particular device from
+ * the IQRF network. Each IQRF device is identified by network address.
+ * Network address is a unique identifier in IQRF network. Each IQRF device
+ * has IQRF transceiver. The MID (Module ID) is globally unique. Each IQRF
+ * device can communicate using own or general protocol.
+ *
+ * The class allows to get basic device information such as Mid, supported
+ * modules, HWPID, and peripheral info.
+ */
+class IQRFDevice : public PollableDevice, Loggable {
+public:
+	typedef Poco::SharedPtr<IQRFDevice> Ptr;
+
+	IQRFDevice(
+		IQRFMqttConnector::Ptr connector,
+		const Poco::Timespan &receiveTimeout,
+		DPAMessage::NetworkAddress address,
+		DPAProtocol::Ptr protocol,
+		const RefreshTime &refreshTime,
+		const RefreshTime &refreshTimePeripheralInfo);
+
+	/**
+	 * @returns identification of node in the IQRF network.
+	 */
+	DPAMessage::NetworkAddress networkAddress() const;
+
+	/**
+	 * @returns mid unique identification of IQRF transceiver.
+	 */
+	uint32_t mid() const;
+
+	/**
+	 * @brief Protocol that communicates with the coordinator.
+	 */
+	DPAProtocol::Ptr protocol() const;
+
+	/**
+	 * @brief Supported modules on the device.
+	 *
+	 * @returns empty list if device can not obtain modules from device.
+	 */
+	std::list<ModuleType> modules() const;
+
+	/**
+	 * @brief Identification of node in the IQRF repository. Device
+	 * name and manufacturer can be obtained on the basis of the number.
+	 */
+	uint16_t HWPID() const;
+	void setHWPID(uint16_t HWPID);
+
+	/**
+	 * @return Creates deviceID from:
+	 *
+	 *  - Device Prefix (1B)
+	 *  - Zero byte (1B)
+	 *  - IQRF MID (4B)
+	 *  - HWPID (2B)
+	 */
+	DeviceID id() const override;
+
+	std::string vendorName() const;
+	std::string productName() const;
+
+	RefreshTime refresh() const override;
+	void poll(Distributor::Ptr distributor) override;
+
+
+	/**
+	* @brief Converts all device parameters into one string for
+	* easy viewing.
+	*/
+	std::string toString() const;
+
+	/**
+	 * @brief Probes information about IQRF device in a network.
+	 */
+	void probe(const Poco::Timespan &methodTimeout);
+
+	/**
+	 * @return SensorData from values measured by the sensor.
+	 */
+	SensorData obtainValues() const;
+
+	/**
+	 * @return SensorData from peripheral info (battery and RSSI)
+	 * of the sensor.
+	 */
+	SensorData obtainPeripheralInfo() const;
+
+private:
+	/**
+	 * @return Identification that uniquely specifies the functionality of the
+	 * device, the user peripherals it implements, its behavior etc.
+	 */
+	uint16_t detectNodeHWPID(const Poco::Timespan &methodTimeout);
+
+	/**
+	 * @brief Detects global unique identification of IQRF device.
+	 *
+	 * @return Global unique identification of IQRF device.
+	 */
+	uint32_t detectMID(const Poco::Timespan &methodTimeout);
+
+	/**
+	 * @brief Detects supported modules used by IQRF device.
+	 *
+	 * @return Supported modules used by IQRF device.
+	 */
+	std::list<ModuleType> detectModules(
+		const Poco::Timespan &methodTimeout);
+
+	/**
+	 * @brief Detects product name and vendor of IQRF device.
+	 *
+	 * @return Product name and vendor of IQRF device.
+	 */
+	DPAProtocol::ProductInfo detectProductInfo(
+		const Poco::Timespan &methodTimeout);
+
+	ModuleID batteryModuleID() const;
+	ModuleID rssiModuleID() const;
+
+private:
+	IQRFMqttConnector::Ptr m_connector;
+	Poco::Timespan m_receiveTimeout;
+	DPAMessage::NetworkAddress m_address;
+	DPAProtocol::Ptr m_protocol;
+	RefreshTime m_refreshTime;
+	RefreshTime m_refreshTimePeripheralInfo;
+
+	mutable Poco::Timespan m_remainingValueTime;
+	mutable Poco::Timespan m_remainingPeripheralInfoTime;
+	mutable Poco::Timespan m_remaining;
+
+	uint32_t m_mid;
+	std::list<ModuleType> m_modules;
+	uint16_t m_HWPID;
+	std::string m_vendorName;
+	std::string m_productName;
+};
+
+}
