BeeeOn/gateway

commit 406cb6655a1f50bbcc3e7d4fe829018b4f65b42f

LoggingCollector: new class
Class LoggingCollector is a collector for testing purposes. It logs events coming
from the registered sources. It should demonstrate all methods inherited from the
AbstractCollector.

The compilation of the LoggingCollector works even when disabling wither the Z-Wave
or Bluetooth support due to macros HAVE_ZWAVE and HAVE_HCI.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/core/LoggingCollector.cpp
@@ -1,0 +1,127 @@
+#include <string>
+
+#include <Poco/Logger.h>
+
+#include "bluetooth/HciInfo.h"
+#include "core/LoggingCollector.h"
+#include "di/Injectable.h"
+#include "model/SensorData.h"
+#include "util/Occasionally.h"
+#include "zwave/ZWaveDriverEvent.h"
+#include "zwave/ZWaveNodeEvent.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, LoggingCollector)
+BEEEON_OBJECT_CASTABLE(DistributorListener)
+BEEEON_OBJECT_CASTABLE(ZWaveListener)
+BEEEON_OBJECT_CASTABLE(BluetoothListener)
+BEEEON_OBJECT_CASTABLE(CommandDispatcherListener)
+BEEEON_OBJECT_END(BeeeOn, LoggingCollector)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+// frequency of reporting of sensor data
+#define SENSOR_DATA_FREQ 7
+
+LoggingCollector::LoggingCollector():
+	m_seenData(0)
+{
+}
+
+LoggingCollector::~LoggingCollector()
+{
+}
+
+void LoggingCollector::onExport(const SensorData &data)
+{
+	static Occasionally occasionally(SENSOR_DATA_FREQ);
+
+	++m_seenData;
+
+	occasionally.execute([&]() {
+		logger().information("seen " + to_string(m_seenData) + " data");
+	});
+}
+
+#ifdef HAVE_ZWAVE
+void LoggingCollector::onDriverStats(const ZWaveDriverEvent &e)
+{
+	logger().information("Z-Wave Driver: "
+			+ to_string(e.readCount())
+			+ "/"
+			+ to_string(e.writeCount())
+			+ "/"
+			+ to_string(e.CANCount())
+			+ "/"
+			+ to_string(e.NAKCount())
+			+ "/"
+			+ to_string(e.ACKCount())
+			+ "/"
+			+ to_string(e.dropped())
+			+ "/"
+			+ to_string(e.badChecksum()));
+}
+
+void LoggingCollector::onNodeStats(const ZWaveNodeEvent &e)
+{
+	logger().information("Z-Wave Node: "
+			+ to_string(e.nodeID())
+			+ "/"
+			+ to_string(e.sentCount())
+			+ "/"
+			+ to_string(e.sentFailed())
+			+ "/"
+			+ to_string(e.retries())
+			+ "/"
+			+ to_string(e.receivedCount())
+			+ "/"
+			+ to_string(e.receiveDuplications())
+			+ "/"
+			+ to_string(e.receiveUnsolicited())
+			+ "/"
+			+ to_string(e.quality()));
+}
+#else
+void LoggingCollector::onDriverStats(const ZWaveDriverEvent &)
+{
+}
+
+void LoggingCollector::onNodeStats(const ZWaveNodeEvent &)
+{
+}
+#endif
+
+#ifdef HAVE_HCI
+void LoggingCollector::onHciStats(const HciInfo &info)
+{
+	logger().information("HCI: "
+			+ info.name()
+			+ " "
+			+ to_string(info.rxBytes())
+			+ "/"
+			+ to_string(info.rxErrors())
+			+ "/"
+			+ to_string(info.rxEvents())
+			+ " "
+			+ to_string(info.txBytes())
+			+ "/"
+			+ to_string(info.txErrors())
+			+ "/"
+			+ to_string(info.txCmds())
+			+ " "
+			+ to_string(info.aclPackets())
+			+ "/"
+			+ to_string(info.scoPackets()));
+}
+#else
+void LoggingCollector::onHciStats(const HciInfo &)
+{
+}
+#endif
+
+
+void LoggingCollector::onDispatch(const Command::Ptr cmd)
+{
+	logger().information("dispatch " + cmd->toString());
+}

conf/config.d/factory.xml
@@ -167,5 +167,7 @@
 
 		<instance name="defaultZWaveDeviceInfoRegistry" class="BeeeOn::DefaultZWaveDeviceInfoRegistry">
 		</instance>
+
+		<instance name="loggingCollector" class="BeeeOn::LoggingCollector" />
 	</factory>
 </system>

src/CMakeLists.txt
@@ -54,6 +54,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/DongleDeviceManager.cpp
 	${PROJECT_SOURCE_DIR}/core/Exporter.cpp
 	${PROJECT_SOURCE_DIR}/core/GatewayInfo.cpp
+	${PROJECT_SOURCE_DIR}/core/LoggingCollector.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoAnswerImpl.cpp
 	${PROJECT_SOURCE_DIR}/core/PocoCommandDispatcher.cpp
 	${PROJECT_SOURCE_DIR}/core/Result.cpp

src/core/LoggingCollector.h
@@ -1,0 +1,28 @@
+#ifndef BEEEON_LOGGING_COLLECTOR_H
+#define BEEEON_LOGGING_COLLECTOR_H
+
+#include <Poco/AtomicCounter.h>
+
+#include "core/AbstractCollector.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class LoggingCollector : public AbstractCollector, Loggable {
+public:
+	LoggingCollector();
+	~LoggingCollector();
+
+	void onExport(const SensorData &data) override;
+	void onDriverStats(const ZWaveDriverEvent &event) override;
+	void onNodeStats(const ZWaveNodeEvent &event) override;
+	void onHciStats(const HciInfo &info) override;
+	void onDispatch(const Command::Ptr cmd) override;
+
+private:
+	Poco::AtomicCounter m_seenData;
+};
+
+}
+
+#endif

commit eeab9fe34c82d07f5ee89eea126e2646fe4b4bc1

LoggingCollector: avoid including ZWave* headers if unavailable
Fixes: 406cb6655a1f ("LoggingCollector: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/LoggingCollector.cpp
@@ -10,9 +10,12 @@
 #include "philips/PhilipsHueBulbInfo.h"
 #include "philips/PhilipsHueBridgeInfo.h"
 #include "util/Occasionally.h"
+
+#ifdef HAVE_ZWAVE
 #include "zwave/ZWaveDriverEvent.h"
 #include "zwave/ZWaveNodeEvent.h"
 #include "zwave/ZWaveNotificationEvent.h"
+#endif
 
 BEEEON_OBJECT_BEGIN(BeeeOn, LoggingCollector)
 BEEEON_OBJECT_CASTABLE(DistributorListener)