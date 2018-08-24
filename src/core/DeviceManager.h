BeeeOn/gateway

commit 51749e188719f0096c733d569ad8551b28d4d2f3

DeviceManager: implement handleSetValue()
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/core/DeviceManager.h
@@ -9,6 +9,7 @@
 #include <Poco/SharedPtr.h>
 
 #include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceSetValueCommand.h"
 #include "commands/DeviceUnpairCommand.h"
 #include "commands/GatewayListenCommand.h"
 #include "core/AnswerQueue.h"
@@ -150,6 +151,33 @@ protected:
 	std::set<DeviceID> handleUnpair(const DeviceUnpairCommand::Ptr cmd);
 
 	/**
+	 * @brief Starts set-value operation in a technology specific way.
+	 * The method is always called inside a critical section and so its
+	 * implementation does not have to be thread-save nor reentrant
+	 * (unless it cooperates with other threads itself).
+	 *
+	 * The set-value process might a be a non-blocking operation. The value
+	 * set by the set-value is expected as a result of the returned AsyncWork
+	 * instance.
+	 */
+	virtual AsyncWork<double>::Ptr startSetValue(
+			const DeviceID &id,
+			const ModuleID &module,
+			const double value,
+			const Poco::Timespan &timespan);
+
+	/**
+	 * @brief Implements handling of the set-value command in a generic way.
+	 * The method ensures that only 1 thread can execute set-value process
+	 * at a time. If the set-value operation succeeds, it ships the set value.
+	 *
+	 * It uses the method startSetValue() to initialize and start the set-value
+	 * process. The method startSetValue() is always called exactly once at a
+	 * time and until it finishes, no the set-value is started.
+	 */
+	void handleSetValue(const DeviceSetValueCommand::Ptr cmd);
+
+	/**
 	* Ship data received from a physical device into a collection point.
 	*/
 	void ship(const SensorData &sensorData);
@@ -230,6 +258,7 @@ private:
 	DeviceCache::Ptr m_deviceCache;
 	Poco::FastMutex m_listenLock;
 	Poco::FastMutex m_unpairLock;
+	Poco::FastMutex m_setValueLock;
 	Poco::SharedPtr<Distributor> m_distributor;
 	std::set<std::type_index> m_acceptable;
 	CancellableSet m_cancellable;

src/core/DeviceManager.cpp
@@ -9,6 +9,7 @@
 #include "core/DeviceManager.h"
 #include "core/MemoryDeviceCache.h"
 #include "core/PrefixCommand.h"
+#include "model/SensorData.h"
 #include "util/ClassInfo.h"
 
 using namespace BeeeOn;
@@ -99,6 +100,9 @@ void DeviceManager::handleGeneric(const Command::Ptr cmd, Result::Ptr result)
 		const auto &result = handleUnpair(cmd.cast<DeviceUnpairCommand>());
 		unpair->setUnpaired(result);
 	}
+	else if (cmd->is<DeviceSetValueCommand>()) {
+		handleSetValue(cmd.cast<DeviceSetValueCommand>());
+	}
 	else
 		throw NotImplementedException(cmd->toString());
 }
@@ -185,6 +189,55 @@ set<DeviceID> DeviceManager::handleUnpair(const DeviceUnpairCommand::Ptr cmd)
 	}
 
 	return result;
+}
+
+AsyncWork<double>::Ptr DeviceManager::startSetValue(
+		const DeviceID &,
+		const ModuleID &,
+		const double,
+		const Timespan &)
+{
+	throw NotImplementedException("generic set-value is not supported");
+}
+
+void DeviceManager::handleSetValue(const DeviceSetValueCommand::Ptr cmd)
+{
+	const Clock started;
+	const Timespan &duration = cmd->timeout();
+
+	ScopedLock<FastMutex> guard(m_setValueLock, duration.totalMilliseconds());
+
+	const Timespan &timeout = checkDelayedOperation("set-value", started, duration);
+
+	logger().information("starting set-value", __FILE__, __LINE__);
+
+	auto operation = startSetValue(
+		cmd->deviceID(),
+		cmd->moduleID(),
+		cmd->value(),
+		timeout);
+	manageUntilFinished("set-value", operation, timeout);
+
+	if (operation->result().isNull())
+		throw IllegalStateException("result of set-value was not provided");
+
+	const auto value = operation->result().value();
+
+	try {
+		if (logger().debug()) {
+			logger().debug(
+				"shipping value " + to_string(value)
+				+ " that has just been set",
+				__FILE__, __LINE__);
+		}
+
+		ship({
+			cmd->deviceID(),
+			Timestamp{},
+			{{cmd->moduleID(), value}}
+		});
+	}
+	BEEEON_CATCH_CHAIN(logger())
 }
 
 void DeviceManager::ship(const SensorData &sensorData)

commit 4efb6f0446f33ab3b7984411b6bea447d070d0df

DeviceManager: fix typo timespan -> timeout
Fixes: 51749e188719 ("DeviceManager: implement handleSetValue()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/DeviceManager.h
@@ -177,7 +177,7 @@ protected:
 			const DeviceID &id,
 			const ModuleID &module,
 			const double value,
-			const Poco::Timespan &timespan);
+			const Poco::Timespan &timeout);
 
 	/**
 	 * @brief Implements handling of the set-value command in a generic way.