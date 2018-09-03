BeeeOn/gateway

commit 3d794a75fc3ea159dce8bc70960baf721fb9cbe2

AbstractZWaveNetwork: new abstract class
Certain parts of the ZWaveNetwork interface can be implemented generically.
The AbstractZWaveNetwork simplifies implementation of ZWaveNetwork interface
by providing partial functionality.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/AbstractZWaveNetwork.cpp
@@ -1,0 +1,107 @@
+#include <Poco/Clock.h>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "zwave/AbstractZWaveNetwork.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+AbstractZWaveNetwork::AbstractZWaveNetwork()
+{
+}
+
+ZWaveNetwork::PollEvent AbstractZWaveNetwork::pollEvent(
+		const Timespan &timeout)
+{
+	const Clock started;
+
+	PollEvent event;
+	bool mightBlock = true;
+
+	do {
+		ScopedLockWithUnlock<FastMutex> guard(m_lock);
+
+		if (logger().trace()) {
+			logger().trace(
+				"polling attempt, queue depth: " + to_string(m_eventsQueue.size())
+				+ (mightBlock? " (might block)" : " (might not block)"),
+				__FILE__, __LINE__);
+		}
+
+
+		if (!m_eventsQueue.empty()) {
+			event = m_eventsQueue.front();
+			m_eventsQueue.pop_front();
+			break;
+		}
+
+		guard.unlock();
+
+		if (logger().trace()) {
+			logger().trace(
+				"polling queue is empty",
+				__FILE__, __LINE__);
+		}
+
+		if (mightBlock && timeout >= 1 * Timespan::MILLISECONDS) {
+			Timespan remaining = timeout - started.elapsed();
+
+			if (remaining < 0)
+				break;
+			if (remaining < 1 * Timespan::MILLISECONDS)
+				remaining = 1 * Timespan::MILLISECONDS;
+
+			if (logger().trace()) {
+				logger().trace(
+					"sleeping while polling for "
+					+ DateTimeFormatter::format(remaining),
+					__FILE__, __LINE__);
+			}
+
+			if (m_event.tryWait(remaining.totalMilliseconds()))
+				mightBlock = false;
+		}
+		else if (mightBlock && timeout < 0) {
+			if (logger().trace()) {
+				logger().trace(
+					"sleeping while polling...",
+					__FILE__, __LINE__);
+			}
+
+			m_event.wait();
+			mightBlock = false;
+		}
+		else {
+			mightBlock = false;
+		}
+	} while(1);
+
+	return event;
+}
+
+void AbstractZWaveNetwork::notifyEvent(const PollEvent &event)
+{
+	FastMutex::ScopedLock guard(m_lock);
+
+	m_eventsQueue.emplace_back(event);
+	m_event.set();
+}
+
+void AbstractZWaveNetwork::interrupt()
+{
+	FastMutex::ScopedLock guard(m_lock);
+
+	if (logger().trace()) {
+		logger().trace(
+			"interrupting pollers, queue depth: "
+			+ to_string(m_eventsQueue.size()),
+			__FILE__, __LINE__);
+	}
+
+	PollEvent none;
+	m_eventsQueue.emplace_back(none);
+	m_event.set();
+}

src/CMakeLists.txt
@@ -294,6 +294,7 @@ endif()
 
 if(OPENZWAVE_LIBRARY AND ENABLE_ZWAVE)
 	file(GLOB ZWAVE_SOURCES
+		${PROJECT_SOURCE_DIR}/zwave/AbstractZWaveNetwork.cpp
 		${PROJECT_SOURCE_DIR}/zwave/OZWNetwork.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveDriverEvent.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveNetwork.cpp

src/zwave/AbstractZWaveNetwork.h
@@ -1,0 +1,51 @@
+#pragma once
+
+#include <deque>
+
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+
+#include "util/Loggable.h"
+#include "zwave/ZWaveNetwork.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Abstract implementation of the ZWaveNetwork class. It provides
+ * a pre-implemented polling mechanism. It is assumed that exactly one
+ * thread calls the method pollEvent() periodically to read the events
+ * (using multiple threads might be an issue because we use Poco::Event).
+ */
+class AbstractZWaveNetwork :
+	public ZWaveNetwork,
+	protected virtual Loggable {
+public:
+	AbstractZWaveNetwork();
+
+	/**
+	 * Implements the pollEvent() operation generically. It just
+	 * waits on the m_event and reads events from the m_eventsQueue.
+	 */
+	PollEvent pollEvent(
+		const Poco::Timespan &timeout) override;
+
+	/**
+	 * Interrupt the pollEvent() operation to return regardless of
+	 * the state of the m_eventsQueue.
+	 */
+	void interrupt() override;
+
+protected:
+	/**
+	 * This method enqueues the given event in the m_eventsQueue and
+	 * wake ups the pollEvent() operation.
+	 */
+	void notifyEvent(const PollEvent &event);
+
+private:
+	std::deque<PollEvent> m_eventsQueue;
+	Poco::Event m_event;
+	mutable Poco::FastMutex m_lock;
+};
+
+}

test/CMakeLists.txt
@@ -63,6 +63,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/util/JournalTest.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataFormatterTest.cpp
 	${PROJECT_SOURCE_DIR}/util/JSONSensorDataParserTest.cpp
+	${PROJECT_SOURCE_DIR}/zwave/AbstractZWaveNetworkTest.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveNodeTest.cpp
 )
 

test/zwave/AbstractZWaveNetworkTest.cpp
@@ -1,0 +1,61 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Clock.h>
+
+#include "cppunit/BetterAssert.h"
+#include "zwave/AbstractZWaveNetwork.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+using PollEvent = ZWaveNetwork::PollEvent;
+
+namespace BeeeOn {
+
+class AbstractZWaveNetworkTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(AbstractZWaveNetworkTest);
+	CPPUNIT_TEST(testPollTimeout);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void testPollTimeout();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(AbstractZWaveNetworkTest);
+
+class TestableAbstractZWaveNetwork : public AbstractZWaveNetwork {
+public:
+	void startInclusion() override
+	{
+		notifyEvent(PollEvent::createInclusionStart());
+	}
+
+	void cancelInclusion() override
+	{
+		notifyEvent(PollEvent::createInclusionDone());
+	}
+
+	void startRemoveNode() override
+	{
+		notifyEvent(PollEvent::createRemoveNodeStart());
+	}
+
+	void cancelRemoveNode() override
+	{
+		notifyEvent(PollEvent::createRemoveNodeDone());
+	}
+};
+
+void AbstractZWaveNetworkTest::testPollTimeout()
+{
+	TestableAbstractZWaveNetwork network;
+
+	const Clock started;
+
+	PollEvent e = network.pollEvent(10 * Timespan::MILLISECONDS);
+	CPPUNIT_ASSERT_EQUAL(PollEvent::EVENT_NONE, e.type());
+	CPPUNIT_ASSERT(started.elapsed() >= 10 * Timespan::MILLISECONDS);
+}
+
+}

commit f20eac4b9357030c9ac06ebf258ea0e1e96190d0

AbstractZWaveNetwork: fix wrong polling behaviour
The pollEvent() method was polling too fast under the following
circumstances:

* push 1 event into the queue
* call pollEvent()
* pollEvent() returns with then 1 event popped off the queue
* call pollEvent() again, queue is empty
* m_event.{tryWait,wait}() does not wait and returns immediatelly
* mightBlock == false, looping fast

This bug does not cause malfunction but causes too hgih CPU load
with no real effect.

Fixes: 3d794a75fc3e ("AbstractZWaveNetwork: new abstract class")

Reported-by: Martin Hosala <xhosal00@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/AbstractZWaveNetwork.cpp
@@ -75,10 +75,11 @@ ZWaveNetwork::PollEvent AbstractZWaveNetwork::pollEvent(
 			mightBlock = false;
 		}
 		else {
-			mightBlock = false;
+			break;
 		}
 	} while(1);
 
+	m_event.reset();
 	return event;
 }
 