BeeeOn/gateway

commit 451c6de278bd905c5ab6a2d2978a48cd5a783fc1

ZWaveNode::CommandClass: new class
Represent Z-Wave command class of a certain node value.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/ZWaveNode.cpp
@@ -6,6 +6,70 @@ using namespace std;
 using namespace Poco;
 using namespace BeeeOn;
 
+ZWaveNode::CommandClass::CommandClass(
+		uint8_t id,
+		uint8_t index,
+		uint8_t instance,
+		const string &name):
+	m_id(id),
+	m_index(index),
+	m_instance(instance),
+	m_name(name)
+{
+}
+
+uint8_t ZWaveNode::CommandClass::id() const
+{
+	return m_id;
+}
+
+uint8_t ZWaveNode::CommandClass::index() const
+{
+	return m_index;
+}
+
+uint8_t ZWaveNode::CommandClass::instance() const
+{
+	return m_instance;
+}
+
+string ZWaveNode::CommandClass::name() const
+{
+	return m_name;
+}
+
+string ZWaveNode::CommandClass::toString() const
+{
+	string repr;
+
+	repr = to_string(m_id) + ":" + to_string(m_index);
+
+	if (m_instance)
+		repr += "[" + to_string(m_instance) + "]";
+
+	if (m_name.empty())
+		repr += " (" + m_name + ")";
+
+	return repr;
+}
+
+bool ZWaveNode::CommandClass::operator <(const CommandClass &cc) const
+{
+	if (m_id < cc.m_id)
+		return true;
+
+	if (m_id > cc.m_id)
+		return false;
+
+	if (m_index < cc.m_index)
+		return true;
+
+	if (m_index > cc.m_index)
+		return false;
+
+	return m_instance < cc.m_instance;
+}
+
 ZWaveNode::Identity::Identity(const uint32_t home, const uint8_t node):
 	home(home),
 	node(node)
@@ -148,6 +212,16 @@ void ZWaveNode::setQueried(bool queried)
 bool ZWaveNode::queried() const
 {
 	return m_queried;
+}
+
+void ZWaveNode::add(const CommandClass &cc)
+{
+	m_commandClasses.emplace(cc);
+}
+
+const set<ZWaveNode::CommandClass> &ZWaveNode::commandClasses() const
+{
+	return m_commandClasses;
 }
 
 string ZWaveNode::toString() const

src/zwave/ZWaveNode.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <set>
 #include <string>
 
 namespace BeeeOn {
@@ -35,6 +36,60 @@ public:
 		bool operator <(const Identity &other) const;
 
 		std::string toString() const;
+	};
+
+	/**
+	 * @brief Command class representation of a Z-Wave value.
+	 * We support only a subset of command classes that are
+	 * relevant for using with the BeeeOn system.
+	 */
+	class CommandClass {
+	public:
+		enum {
+			BASIC         = 32,
+			SWITCH_BINARY = 37,
+			SENSOR_BINARY = 48,
+			SENSOR_MULTILEVEL = 49,
+			BATTERY       = 128,
+			WAKE_UP       = 132,
+			ALARM         = 113,
+		};
+
+		CommandClass(
+			uint8_t id,
+			uint8_t index,
+			uint8_t instance,
+			const std::string &name = "");
+
+		/**
+		 * @returns command class's ID (SWITCH_BINARY, BATTERY, ...)
+		 */
+		uint8_t id() const;
+
+		/**
+		 * @returns index of the specific value represented by the command class
+		 */
+		uint8_t index() const;
+
+		/**
+		 * @returns identifier for situations when certain command class is duplicated
+		 */
+		uint8_t instance() const;
+
+		/**
+		 * @returns command class's name, it can be empty if not initialized
+		 */
+		std::string name() const;
+
+		std::string toString() const;
+
+		bool operator <(const CommandClass &cc) const;
+
+	private:
+		uint8_t m_id;
+		uint8_t m_index;
+		uint8_t m_instance;
+		std::string m_name;
 	};
 
 	/**
@@ -142,6 +197,9 @@ public:
 	 */
 	bool queried() const;
 
+	void add(const CommandClass &cc);
+	const std::set<CommandClass> &commandClasses() const;
+
 	std::string toString() const;
 	std::string toInfoString() const;
 
@@ -157,6 +215,7 @@ private:
 	uint16_t m_vendorId;
 	std::string m_vendor;
 	uint16_t m_productType;
+	std::set<CommandClass> m_commandClasses;
 };
 
 }

test/zwave/ZWaveNodeTest.cpp
@@ -9,9 +9,11 @@ namespace BeeeOn {
 class ZWaveNodeTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(ZWaveNodeTest);
 	CPPUNIT_TEST(testLessThan);
+	CPPUNIT_TEST(testCommandClassLessThan);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void testLessThan();
+	void testCommandClassLessThan();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ZWaveNodeTest);
@@ -33,6 +35,28 @@ void ZWaveNodeTest::testLessThan()
 	CPPUNIT_ASSERT(!(c < c));
 	CPPUNIT_ASSERT(!(d < d));
 	CPPUNIT_ASSERT(!(a < d));
+}
+
+void ZWaveNodeTest::testCommandClassLessThan()
+{
+	ZWaveNode::CommandClass a(10, 0, 0);
+	ZWaveNode::CommandClass b(10, 1, 0);
+	ZWaveNode::CommandClass c(10, 1, 1);
+	ZWaveNode::CommandClass d(10, 2, 0);
+	ZWaveNode::CommandClass e( 5, 3, 0);
+
+	CPPUNIT_ASSERT(a < b);
+	CPPUNIT_ASSERT(b < c);
+	CPPUNIT_ASSERT(c < d);
+	CPPUNIT_ASSERT(e < a);
+	CPPUNIT_ASSERT(e < b);
+	CPPUNIT_ASSERT(e < c);
+	CPPUNIT_ASSERT(e < d);
+	CPPUNIT_ASSERT(!(a < a));
+	CPPUNIT_ASSERT(!(b < b));
+	CPPUNIT_ASSERT(!(c < c));
+	CPPUNIT_ASSERT(!(d < d));
+	CPPUNIT_ASSERT(!(e < e));
 }
 
 }

commit 7f2a4c475d2506bcf9019d0603c15487b8f30b04

ZWaveNode::CommandClass: fix toString() does not print name
If the command class name is known, print it.

Fixes: 451c6de278bd ("ZWaveNode::CommandClass: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/ZWaveNode.cpp
@@ -52,7 +52,7 @@ string ZWaveNode::CommandClass::toString() const
 	if (m_instance)
 		repr += "[" + to_string(m_instance) + "]";
 
-	if (m_name.empty())
+	if (!m_name.empty())
 		repr += " (" + m_name + ")";
 
 	return repr;