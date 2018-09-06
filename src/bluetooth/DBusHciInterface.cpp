BeeeOn/gateway

commit 9281bd9e91565939485a3f0ee70907768c6804b7

DBusHciInterfaceManager: maintain single DBusHciInterface by name
DBusHciInterfaceManager ensures that there is at most one DBusHciInterface
instance for each bluetooth adapter. This provides better work with locks in
DBusHciInterface and the possibility of future updates with processing of
advertising data.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -18,11 +18,8 @@ using namespace std;
 static int CHANGE_POWER_ATTEMPTS = 5;
 static Timespan CHANGE_POWER_DELAY = 200 * Timespan::MILLISECONDS;
 
-DBusHciInterface::DBusHciInterface(
-		const string& name,
-		SharedPtr<FastMutex> statusMutex):
-	m_name(name),
-	m_statusMutex(statusMutex)
+DBusHciInterface::DBusHciInterface(const string& name):
+	m_name(name)
 {
 }
 
@@ -40,7 +37,7 @@ void DBusHciInterface::up() const
 	if (logger().debug())
 		logger().debug("bringing up " + m_name, __FILE__, __LINE__);
 
-	ScopedLock<FastMutex> guard(*m_statusMutex);
+	ScopedLock<FastMutex> guard(m_statusMutex);
 
 	const string path = createAdapterPath();
 	GlibPtr<OrgBluezAdapter1> adapter = retrieveBluezAdapter(path);
@@ -57,7 +54,7 @@ void DBusHciInterface::down() const
 	if (logger().debug())
 		logger().debug("switching down " + m_name, __FILE__, __LINE__);
 
-	ScopedLock<FastMutex> guard(*m_statusMutex);
+	ScopedLock<FastMutex> guard(m_statusMutex);
 
 	const string path = createAdapterPath();
 	GlibPtr<OrgBluezAdapter1> adapter = retrieveBluezAdapter(path);
@@ -149,7 +146,7 @@ void DBusHciInterface::waitUntilPoweredChange(const string& path, const bool pow
 		if (::org_bluez_adapter1_get_powered(adapter.raw()) == powered)
 			return;
 
-		m_condition.tryWait(*m_statusMutex, CHANGE_POWER_DELAY.totalMilliseconds());
+		m_condition.tryWait(m_statusMutex, CHANGE_POWER_DELAY.totalMilliseconds());
 	}
 
 	throw TimeoutException("failed to change power of interface" + m_name);
@@ -313,12 +310,17 @@ GlibPtr<OrgBluezDevice1> DBusHciInterface::retrieveBluezDevice(const string& pat
 }
 
 
-DBusHciInterfaceManager::DBusHciInterfaceManager():
-	m_statusMutex(new FastMutex)
+DBusHciInterfaceManager::DBusHciInterfaceManager()
 {
 }
 
 HciInterface::Ptr DBusHciInterfaceManager::lookup(const string &name)
 {
-	return new DBusHciInterface(name, m_statusMutex);
+	auto it = m_interfaces.find(name);
+	if (it != m_interfaces.end())
+		return it->second;
+
+	DBusHciInterface::Ptr newHci = new DBusHciInterface(name);
+	m_interfaces.emplace(name, newHci);
+	return newHci;
 }

src/bluetooth/DBusHciInterface.h
@@ -40,15 +40,13 @@ namespace BeeeOn {
  */
 class DBusHciInterface : public HciInterface, Loggable {
 public:
+	typedef Poco::SharedPtr<DBusHciInterface> Ptr;
 	typedef std::function<bool(const std::string& path)> PathFilter;
 
 	/**
 	 * @param name name of hci
-	 * @param statusMutex lock ensures exclusive access to state of hci
 	 */
-	DBusHciInterface(
-		const std::string& name,
-		Poco::SharedPtr<Poco::FastMutex> statusMutex);
+	DBusHciInterface(const std::string& name);
 
 	/**
 	 * @brief Sets hci interface down.
@@ -161,7 +159,7 @@ private:
 private:
 	std::string m_name;
 	mutable Poco::Condition m_condition;
-	mutable Poco::SharedPtr<Poco::FastMutex> m_statusMutex;
+	mutable Poco::FastMutex m_statusMutex;
 };
 
 class DBusHciInterfaceManager : public HciInterfaceManager {
@@ -171,7 +169,7 @@ public:
 	HciInterface::Ptr lookup(const std::string &name) override;
 
 private:
-	Poco::SharedPtr<Poco::FastMutex> m_statusMutex;
+	std::map<std::string, DBusHciInterface::Ptr> m_interfaces;
 };
 
 }

commit 8e92e3d955397aa5cfc85418593eb92acb2181d7

DBusHciInterfaceManager: fix race condition
Fix the race condition to map of instances of DBusHci.
From now there can not be more DBusHci with the same name.

Fixes: 9281bd9e9 ("DBusHciInterfaceManager: maintain single DBusHciInterface by name")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -514,6 +514,8 @@ DBusHciInterfaceManager::DBusHciInterfaceManager()
 
 HciInterface::Ptr DBusHciInterfaceManager::lookup(const string &name)
 {
+	ScopedLock<FastMutex> guard(m_mutex);
+
 	auto it = m_interfaces.find(name);
 	if (it != m_interfaces.end())
 		return it->second;

src/bluetooth/DBusHciInterface.h
@@ -285,6 +285,7 @@ public:
 	HciInterface::Ptr lookup(const std::string &name) override;
 
 private:
+	Poco::FastMutex m_mutex;
 	std::map<std::string, DBusHciInterface::Ptr> m_interfaces;
 };
 