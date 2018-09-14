BeeeOn/gateway

commit ccce884771af30722c08ec3fa34b40285aa2c402

DeviceStatusFetcher: new class
DeviceStatusFetcher is a standalone thread that requests for paired
devices via the ServerDeviceListCommand. Each received result
is then propagated to the appropriate device manager.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/DeviceStatusFetcher.cpp
@@ -1,0 +1,354 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "commands/ServerDeviceListCommand.h"
+#include "commands/ServerDeviceListResult.h"
+#include "core/DeviceStatusFetcher.h"
+#include "core/CommandDispatcher.h"
+#include "di/Injectable.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, DeviceStatusFetcher)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_PROPERTY("idleDuration", &DeviceStatusFetcher::setIdleDuration)
+BEEEON_OBJECT_PROPERTY("waitTimeout", &DeviceStatusFetcher::setWaitTimeout)
+BEEEON_OBJECT_PROPERTY("repeatTimeout", &DeviceStatusFetcher::setRepeatTimeout)
+BEEEON_OBJECT_PROPERTY("commandDispatcher", &DeviceStatusFetcher::setCommandDispatcher)
+BEEEON_OBJECT_PROPERTY("handlers", &DeviceStatusFetcher::registerHandler)
+BEEEON_OBJECT_HOOK("cleanup", &DeviceStatusFetcher::clearHandlers)
+BEEEON_OBJECT_END(BeeeOn, DeviceStatusFetcher)
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+DeviceStatusFetcher::PrefixAnswer::PrefixAnswer(
+		AnswerQueue &queue,
+		const DevicePrefix &prefix):
+	Answer(queue),
+	m_prefix(prefix)
+{
+}
+
+DevicePrefix DeviceStatusFetcher::PrefixAnswer::prefix() const
+{
+	return m_prefix;
+}
+
+DeviceStatusFetcher::PrefixStatus::PrefixStatus():
+	m_lastRequested(0),
+	m_successful(false)
+{
+}
+
+void DeviceStatusFetcher::PrefixStatus::startRequest()
+{
+	poco_assert(!m_successful);
+	m_lastRequested.update();
+}
+
+void DeviceStatusFetcher::PrefixStatus::deliverResponse(
+		bool successful)
+{
+	poco_assert(!m_successful);
+	m_successful = successful;
+}
+
+bool DeviceStatusFetcher::PrefixStatus::needsRequest() const
+{
+	return !m_successful;
+}
+
+bool DeviceStatusFetcher::PrefixStatus::shouldRepeat(
+		const Timespan &repeatTimeout) const
+{
+	if (m_successful)
+		return false;
+
+	if (!m_lastRequested.isElapsed(repeatTimeout.totalMicroseconds()))
+		return false;
+
+	return true;
+}
+
+DeviceStatusFetcher::DeviceStatusFetcher():
+	m_idleDuration(30 * Timespan::MINUTES),
+	m_waitTimeout(1 * Timespan::SECONDS),
+	m_repeatTimeout(5 * Timespan::MINUTES)
+{
+}
+
+void DeviceStatusFetcher::setIdleDuration(const Timespan &duration)
+{
+	if (duration < 1 * Timespan::SECONDS)
+		throw InvalidArgumentException("idleDuration must be at least 1 s");
+
+	m_idleDuration = duration;
+}
+
+void DeviceStatusFetcher::setWaitTimeout(const Timespan &timeout)
+{
+	if (timeout < 1 * Timespan::MILLISECONDS)
+		throw InvalidArgumentException("waitTimeout must be at least 1 ms");
+
+	m_waitTimeout = timeout;
+}
+
+void DeviceStatusFetcher::setRepeatTimeout(const Timespan &timeout)
+{
+	if (timeout < 1 * Timespan::MILLISECONDS)
+		throw InvalidArgumentException("repeatTimeout must be at least 1 ms");
+
+	m_repeatTimeout = timeout;
+}
+
+void DeviceStatusFetcher::registerHandler(DeviceStatusHandler::Ptr handler)
+{
+	auto result = m_handlers.emplace(handler->prefix(), set<DeviceStatusHandler::Ptr>{handler});
+	if (!result.second) {
+		set<DeviceStatusHandler::Ptr> &handlers = result.first->second;
+		handlers.emplace(handler);
+	}
+}
+
+void DeviceStatusFetcher::clearHandlers()
+{
+	m_handlers.clear();
+}
+
+DeviceStatusFetcher::FetchStatus DeviceStatusFetcher::fetchUndone()
+{
+	if (m_status.empty()) {
+		for (const auto &pair : m_handlers)
+			m_status.emplace(pair.first, PrefixStatus());
+	}
+
+	bool wouldRepeat = false;
+	bool started = false;
+
+	for (auto &pair : m_status) {
+		const auto &prefix = pair.first;
+		auto &status = pair.second;
+
+		if (!status.needsRequest()) {
+			if (status.shouldRepeat(m_repeatTimeout))
+				wouldRepeat = true;
+
+			continue;
+		}
+
+		if (logger().debug()) {
+			logger().debug("fetching paired devices for " + prefix.toString(),
+					__FILE__, __LINE__);
+		}
+
+		ServerDeviceListCommand::Ptr cmd = new ServerDeviceListCommand(prefix);
+		dispatch(cmd, new PrefixAnswer(answerQueue(), prefix));
+
+		status.startRequest();
+		started = true;
+	}
+
+	if (started)
+		return ACTIVE;
+	if (wouldRepeat)
+		return WOULD_REPEAT;
+
+	return NOTHING;
+}
+
+DeviceStatusFetcher::PrefixAnswer::Ptr DeviceStatusFetcher::handleDirtyAnswer(
+		Answer::Ptr answer)
+{
+	if (answer->isPending()) {
+		if (logger().debug()) {
+			logger().debug("answer is pending",
+				__FILE__, __LINE__);
+		}
+
+		return NULL;
+	}
+
+	answerQueue().remove(answer);
+
+	if (answer->handlersCount() == 0) {
+		logger().warning("answer has no handlers",
+				__FILE__, __LINE__);
+		return NULL;
+	}
+
+	PrefixAnswer::Ptr prefixAnswer = answer.cast<PrefixAnswer>();
+	if (prefixAnswer.isNull()) {
+		logger().warning("received answer is not PrefixAnswer",
+				__FILE__, __LINE__);
+		return NULL;
+	}
+
+	return prefixAnswer;
+}
+
+set<DeviceStatusHandler::Ptr> DeviceStatusFetcher::matchHandlers(
+		const PrefixAnswer::Ptr answer) const
+{
+	auto it = m_handlers.find(answer->prefix());
+	if (it == m_handlers.end())
+		return {};
+
+	return it->second;
+}
+
+void DeviceStatusFetcher::run()
+{
+	logger().information("starting device fetcher...");
+
+	StopControl::Run run(m_stopControl);
+
+	while (run) {
+		switch (fetchUndone()) {
+		case NOTHING:
+			if (answerQueue().size() == 0) {
+				if (logger().debug()) {
+					logger().debug(
+						"nothing to do, sleeping...",
+						__FILE__, __LINE__);
+				}
+
+				run.waitStoppable(m_idleDuration);
+				continue;
+			}
+			break;
+
+		case WOULD_REPEAT:
+			if (answerQueue().size() == 0) {
+				if (logger().debug()) {
+					logger().debug(
+						"would repeat some, sleeping now...",
+						__FILE__, __LINE__);
+				}
+
+				run.waitStoppable(m_repeatTimeout);
+				continue;
+			}
+
+			break;
+
+		default:
+			break;
+		}
+
+		list<Answer::Ptr> dirty;
+		answerQueue().wait(m_waitTimeout, dirty);
+
+		if (dirty.empty())
+			continue;
+
+		if (logger().debug()) {
+			logger().debug(
+				"processing " + to_string(dirty.size()) + " answers",
+				__FILE__, __LINE__);
+		}
+
+		for (Answer::Ptr answer : dirty) {
+			Answer::ScopedLock guard(*answer);
+
+			auto prefixAnswer = handleDirtyAnswer(answer);
+			const auto handlers = matchHandlers(prefixAnswer);
+
+			if (handlers.empty()) {
+				logger().warning("no handlers for prefix "
+						+ prefixAnswer->prefix().toString(),
+						__FILE__, __LINE__);
+				continue;
+			}
+
+			processAnswer(prefixAnswer, handlers);
+		}
+	}
+}
+
+void DeviceStatusFetcher::processAnswer(
+		PrefixAnswer::Ptr answer,
+		set<DeviceStatusHandler::Ptr> handlers)
+{
+	set<DeviceID> paired;
+	bool failed = false;
+	bool success = false;
+
+	size_t i = 0;
+
+	for (const auto result : *answer) {
+		i += 1;
+
+		if (result->status() != Result::Status::SUCCESS) {
+			logger().warning(
+				"result " + to_string(i) + "/" + to_string(answer->resultsCount())
+				+ " has failed",
+				__FILE__, __LINE__);
+
+			failed = true;
+			continue;
+		}
+		else {
+			success = true;
+		}
+
+		ServerDeviceListResult::Ptr data = result.cast<ServerDeviceListResult>();
+		if (data.isNull()) {
+			logger().warning("result is not ServerDeviceListResult",
+					__FILE__, __LINE__);
+			continue;
+		}
+
+		collectPaired(paired, data->deviceList(), answer->prefix());
+	}
+
+	auto status = m_status.find(answer->prefix());
+	poco_assert(status != m_status.end()); // it MUST be there
+
+	status->second.deliverResponse(!failed);
+
+	if (success && failed) {
+		if (logger().debug()) {
+			logger().debug(
+				"answer was partially successful "
+				"and thus the request would be repeated",
+				__FILE__, __LINE__);
+		}
+	}
+
+	if (!success)
+		return;
+
+	for (auto handler : handlers)
+		handler->handleRemoteStatus(answer->prefix(), paired, {});
+}
+
+void DeviceStatusFetcher::collectPaired(
+		std::set<DeviceID> &paired,
+		const std::vector<DeviceID> &received,
+		const DevicePrefix &prefix) const
+{
+	for (const auto &id : received) {
+		if (id.prefix() != prefix) {
+			logger().warning(
+				"ID " + id.toString()
+				+ " has not prefix "
+				+ prefix.toString(),
+				__FILE__, __LINE__);
+
+			continue;
+		}
+
+		if (logger().trace()) {
+			logger().trace("received ID " + id.toString(),
+					__FILE__, __LINE__);
+		}
+
+		paired.emplace(id);
+	}
+}
+
+void DeviceStatusFetcher::stop()
+{
+	m_stopControl.requestStop();
+	answerQueue().dispose();
+}

src/core/DeviceStatusFetcher.h
@@ -1,0 +1,188 @@
+#pragma once
+
+#include <map>
+#include <set>
+
+#include <Poco/Clock.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include "core/CommandSender.h"
+#include "core/DeviceStatusHandler.h"
+#include "loop/StoppableRunnable.h"
+#include "loop/StopControl.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief DeviceStatusFetcher is responsible for fetching pairing state of
+ * devices for the registered status handlers. The fetching is performed
+ * asynchronously and independently resulting in calling to the method
+ * DeviceStatusHandler::handleRemoteStatus() on the appropriate handlers.
+ */
+class DeviceStatusFetcher :
+	public CommandSender,
+	public StoppableRunnable,
+	Loggable {
+public:
+	typedef Poco::SharedPtr<DeviceStatusFetcher> Ptr;
+
+	DeviceStatusFetcher();
+
+	/**
+	 * @brief Set duration how long to sleep while there is nothing to do.
+	 */
+	void setIdleDuration(const Poco::Timespan &duration);
+
+	/**
+	 * @brief Set timeout for AnswerQueue::wait() call.
+	 */
+	void setWaitTimeout(const Poco::Timespan &timeout);
+
+	/**
+	 * @brief Set timeout to wait until a request is re-issued after its
+	 * unsuccessful finish (Answer was not fully successful).
+	 */
+	void setRepeatTimeout(const Poco::Timespan &timeout);
+
+	/**
+	 * @brief Register the given device status handler. The DeviceStatusFetcher
+	 * would request a remote pairing registry (server) for the paired devices
+	 * for that handler.
+	 */
+	void registerHandler(DeviceStatusHandler::Ptr handler);
+
+	/**
+	 * @brief Unregister all registered device status handlers.
+	 */
+	void clearHandlers();
+
+	void run() override;
+	void stop() override;
+
+protected:
+	/**
+	 * @brief To simplify Answer management, include the prefix
+	 * of the connected ServerDeviceListCommand inside the answer.
+	 */
+	class PrefixAnswer : public Answer {
+	public:
+		typedef Poco::AutoPtr<PrefixAnswer> Ptr;
+
+		PrefixAnswer(
+			AnswerQueue &queue,
+			const DevicePrefix &prefix);
+
+		DevicePrefix prefix() const;
+
+	private:
+		const DevicePrefix m_prefix;
+	};
+
+	class PrefixStatus {
+	public:
+		PrefixStatus();
+
+		/**
+		 * @brief Update the status of an associated prefix that
+		 * the request for the associated prefix has been initiated.
+		 */
+		void startRequest();
+
+		/**
+		 * @brief Update the status of an associated prefix that an
+		 * appropriate response has been delivered. The parameter
+		 * denotes whether the response has been fully successful or
+		 * not. This influences whether the request would be repeated.
+		 */
+		void deliverResponse(bool successful);
+
+		/**
+		 * @returns true if the status for the associated prefix needs
+		 * to be requested (not requested yet, or not fully successful).
+		 */
+		bool needsRequest() const;
+
+		/**
+		 * @brief When the response was not fully successful, the request
+		 * should be repeated. However, we do not want to DoS the remote
+		 * server. Thus, the repeated requested would be performed after
+		 * the given repeat timeout. The method returns true only, if the
+		 * last response was not fully successful and the timeout exceeded
+		 * sice the last request.
+		 *
+		 * @returns true whether the request of the status should be repeated
+		 */
+		bool shouldRepeat(const Poco::Timespan &repeatTimeout) const;
+
+	private:
+		Poco::Clock m_lastRequested;
+		bool m_successful;
+	};
+
+	/**
+	 * @brief Denote certain situations of the status fetching.
+	 */
+	enum FetchStatus {
+		NOTHING,
+		WOULD_REPEAT,
+		ACTIVE
+	};
+
+protected:
+	/**
+	 * @brief Determine status handlers for which no fully successful
+	 * request was made and dispatch ServerDeviceListCommand for them.
+	 *
+	 * @returns whether there something to do
+	 */
+	FetchStatus fetchUndone();
+
+	/**
+	 * @brief Check status of the given answer and if it is not pending
+	 * and castable to PrefixAnswer, it performs cast and returns it.
+	 *
+	 * @returns instance of PrefixAnswer if it should be processed or null
+	 */
+	PrefixAnswer::Ptr handleDirtyAnswer(Answer::Ptr answer);
+
+	/**
+	 * @return status handler matching the given answer (by prefix)
+	 */
+	std::set<DeviceStatusHandler::Ptr> matchHandlers(
+			const PrefixAnswer::Ptr answer) const;
+
+	/**
+	 * @brief Process results of the given answer and the matching status handler.
+	 * When any of the results is unsuccessful, all the answer is considered
+	 * unsuccessful and it would be marked for repeated fetch.
+	 *
+	 * If there is at least one successful result, the method
+	 * DeviceStatusHandler::handleRemoteStatus() is called once on
+	 * the given handler as a result of the answer processing.
+	 */
+	void processAnswer(
+		PrefixAnswer::Ptr answer,
+		std::set<DeviceStatusHandler::Ptr> handler);
+
+	/**
+	 * @brief Process paired devices as given in a single Answer result.
+	 * Only devices matching the given prefix would be used. Other devices
+	 * are considered as a bug somewhere.
+	 */
+	void collectPaired(
+		std::set<DeviceID> &paired,
+		const std::vector<DeviceID> &received,
+		const DevicePrefix &prefix) const;
+
+private:
+	StopControl m_stopControl;
+	Poco::Timespan m_idleDuration;
+	Poco::Timespan m_waitTimeout;
+	Poco::Timespan m_repeatTimeout;
+	std::map<DevicePrefix, std::set<DeviceStatusHandler::Ptr>> m_handlers;
+	std::map<DevicePrefix, PrefixStatus> m_status;
+};
+
+}

conf/config.d/factory.xml
@@ -8,6 +8,7 @@
 			<add name="runnables" ref="mqttGWExporterClient" />
 			<add name="runnables" ref="distributor" />
 			<add name="loops" ref="managersRunner" />
+			<add name="runnables" ref="deviceStatusFetcher" />
 		</instance>
 
 		<instance name="gatewayInfo" class="BeeeOn::GatewayInfo" init="early">
@@ -52,6 +53,13 @@
 			<add name="listeners" ref="bluetoothAvailability" if-yes="${bluetooth.enable}" />
 			<add name="listeners" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 			<add name="listeners" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
+		</instance>
+
+		<instance name="deviceStatusFetcher" class="BeeeOn::DeviceStatusFetcher">
+			<set name="idleDuration" time="30 m" />
+			<set name="waitTimeout" time="1 s" />
+			<set name="repeatTimeout" time="5 m" />
+			<set name="commandDispatcher" ref="commandDispatcher" />
 		</instance>
 
 		<instance name="pipeHotplugMonitor" class="BeeeOn::PipeHotplugMonitor">

conf/config.d/logging-loggers.ini
@@ -92,3 +92,6 @@ loggers.GWServerConnector.level = debug
 
 loggers.FitpDeviceManager.name = BeeeOn::FitpDeviceManager
 loggers.FitpDeviceManager.level = debug
+
+loggers.DeviceStatusFetcher.name = BeeeOn::DeviceStatusFetcher
+loggers.DeviceStatusFetcher.level = trace

src/CMakeLists.txt
@@ -97,6 +97,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/core/CommandSender.cpp
 	${PROJECT_SOURCE_DIR}/core/DeviceCache.cpp
 	${PROJECT_SOURCE_DIR}/core/DeviceManager.cpp
+	${PROJECT_SOURCE_DIR}/core/DeviceStatusFetcher.cpp
 	${PROJECT_SOURCE_DIR}/core/DeviceStatusHandler.cpp
 	${PROJECT_SOURCE_DIR}/core/Distributor.cpp
 	${PROJECT_SOURCE_DIR}/core/DistributorListener.cpp

test/CMakeLists.txt
@@ -45,6 +45,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/bluetooth/HciInterfaceTest.cpp
 	${PROJECT_SOURCE_DIR}/core/AnswerQueueTest.cpp
 	${PROJECT_SOURCE_DIR}/core/CommandDispatcherTest.cpp
+	${PROJECT_SOURCE_DIR}/core/DeviceStatusFetcherTest.cpp
 	${PROJECT_SOURCE_DIR}/core/DongleDeviceManagerTest.cpp
 	${PROJECT_SOURCE_DIR}/core/FilesystemDeviceCacheTest.cpp
 	${PROJECT_SOURCE_DIR}/core/MemoryDeviceCacheTest.cpp

test/core/DeviceStatusFetcherTest.cpp
@@ -1,0 +1,251 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Event.h>
+#include <Poco/Thread.h>
+
+#include "commands/ServerDeviceListCommand.h"
+#include "commands/ServerDeviceListResult.h"
+#include "core/CommandDispatcher.h"
+#include "core/DeviceStatusFetcher.h"
+#include "cppunit/BetterAssert.h"
+
+using namespace Poco;
+using namespace std;
+
+namespace BeeeOn {
+
+class DeviceStatusFetcherTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(DeviceStatusFetcherTest);
+	CPPUNIT_TEST(testSingleHandler);
+	CPPUNIT_TEST(testMultipleHandlers);
+	CPPUNIT_TEST(testNoDevicesForHandlers);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+
+	void testSingleHandler();
+	void testMultipleHandlers();
+	void testNoDevicesForHandlers();
+
+private:
+	DeviceStatusFetcher::Ptr m_fetcher;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DeviceStatusFetcherTest);
+
+class TestingDeviceStatusHandler : public DeviceStatusHandler {
+public:
+	typedef SharedPtr<TestingDeviceStatusHandler> Ptr;
+
+	TestingDeviceStatusHandler(const DevicePrefix &prefix):
+		m_prefix(prefix)
+	{
+	}
+
+	DevicePrefix prefix() const override
+	{
+		return m_prefix;
+	}
+
+	void handleRemoteStatus(
+		const DevicePrefix &prefix,
+		const set<DeviceID> &paired,
+		const DeviceStatusHandler::DeviceValues &values)
+	{
+		handledPrefix = prefix;
+		handledPaired = paired;
+		handledValues = values;
+
+		handled.set();
+	}
+
+	Event handled;
+	DevicePrefix handledPrefix = DevicePrefix::PREFIX_INVALID;
+	set<DeviceID> handledPaired;
+	DeviceStatusHandler::DeviceValues handledValues;
+
+private:
+	DevicePrefix m_prefix;
+};
+
+class TestingCommandDispatcherForFetcher : public CommandDispatcher {
+public:
+	typedef SharedPtr<TestingCommandDispatcherForFetcher> Ptr;
+
+protected:
+	void dispatchImpl(Command::Ptr cmd, Answer::Ptr answer) override
+	{
+		ServerDeviceListCommand::Ptr request = cmd.cast<ServerDeviceListCommand>();
+
+		answer->setHandlersCount(1);
+		const auto devices = this->devices;
+
+		m_thread.startFunc([request, answer, devices]() mutable
+		{
+			ServerDeviceListResult::Ptr result =
+				new ServerDeviceListResult(answer);
+			vector<DeviceID> list;
+
+			for (const auto &id : devices) {
+				if (id.prefix() != request->devicePrefix())
+					continue;
+
+				list.emplace_back(id);
+			}
+
+			result->setDeviceList(list);
+			result->setStatus(Result::Status::SUCCESS);
+			answer->addResult(result);
+		});
+
+		m_thread.join();
+	}
+
+public:
+	set<DeviceID> devices;
+	Thread m_thread;
+};
+
+void DeviceStatusFetcherTest::setUp()
+{
+	m_fetcher = new DeviceStatusFetcher;
+	m_fetcher->setIdleDuration(2 * Timespan::SECONDS);
+	m_fetcher->setWaitTimeout(20 * Timespan::MILLISECONDS);
+	m_fetcher->setRepeatTimeout(200 * Timespan::MILLISECONDS);
+}
+
+/**
+ * @brief Test that the DeviceStatusFetcher handles a single registered
+ * DeviceStatusHandler properly.
+ */
+void DeviceStatusFetcherTest::testSingleHandler()
+{
+	Thread thread;
+	TestingDeviceStatusHandler::Ptr handler =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	TestingCommandDispatcherForFetcher::Ptr dispatcher =
+		new TestingCommandDispatcherForFetcher;
+
+	m_fetcher->registerHandler(handler);
+	m_fetcher->setCommandDispatcher(dispatcher);
+
+	dispatcher->devices = {
+		0xa300000000000001,
+		0xa300000000000002,
+	};
+
+	thread.start(*m_fetcher);
+
+	CPPUNIT_ASSERT(handler->handled.tryWait(10000));
+
+	CPPUNIT_ASSERT(handler->handledPrefix == DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	CPPUNIT_ASSERT_EQUAL(2, handler->handledPaired.size());
+
+	m_fetcher->stop();
+	CPPUNIT_ASSERT_NO_THROW(thread.join(10000));
+}
+
+/**
+ * @brief Test that the DeviceStatusFetcher handles a single registered
+ * DeviceStatusHandler properly.
+ */
+void DeviceStatusFetcherTest::testMultipleHandlers()
+{
+	Thread thread;
+	TestingDeviceStatusHandler::Ptr handler0 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	TestingDeviceStatusHandler::Ptr handler1 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_PRESSURE_SENSOR);
+	TestingDeviceStatusHandler::Ptr handler2 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_FITPROTOCOL);
+	TestingDeviceStatusHandler::Ptr handler3 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_BLUETOOTH);
+
+	TestingCommandDispatcherForFetcher::Ptr dispatcher =
+		new TestingCommandDispatcherForFetcher;
+
+	m_fetcher->registerHandler(handler0);
+	m_fetcher->registerHandler(handler1);
+	m_fetcher->registerHandler(handler2);
+	m_fetcher->registerHandler(handler3);
+	m_fetcher->setCommandDispatcher(dispatcher);
+
+	dispatcher->devices = {
+		0xa100000000000001, // fitp
+		0xa100000000000002, // fitp
+		0xa200000000000003, // pressure
+		0xa300000000000004, // vdev
+		0xa600000000000005, // bluetooth
+		0xa600000000000006, // bluetooth
+		0xa600000000000007, // bluetooth
+	};
+
+	thread.start(*m_fetcher);
+
+	CPPUNIT_ASSERT(handler0->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler0->handledPrefix == DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	CPPUNIT_ASSERT_EQUAL(1, handler0->handledPaired.size());
+
+	CPPUNIT_ASSERT(handler1->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler1->handledPrefix == DevicePrefix::PREFIX_PRESSURE_SENSOR);
+	CPPUNIT_ASSERT_EQUAL(1, handler1->handledPaired.size());
+
+	CPPUNIT_ASSERT(handler2->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler2->handledPrefix == DevicePrefix::PREFIX_FITPROTOCOL);
+	CPPUNIT_ASSERT_EQUAL(2, handler2->handledPaired.size());
+
+	CPPUNIT_ASSERT(handler3->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler3->handledPrefix == DevicePrefix::PREFIX_BLUETOOTH);
+	CPPUNIT_ASSERT_EQUAL(3, handler3->handledPaired.size());
+
+	m_fetcher->stop();
+	CPPUNIT_ASSERT_NO_THROW(thread.join(10000));
+}
+
+/**
+ * @brief Test that empty set of devices is received when no devices of the
+ * expected prefix are received.
+ */
+void DeviceStatusFetcherTest::testNoDevicesForHandlers()
+{
+	Thread thread;
+	TestingDeviceStatusHandler::Ptr handler0 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	TestingDeviceStatusHandler::Ptr handler1 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_FITPROTOCOL);
+	TestingDeviceStatusHandler::Ptr handler2 =
+		new TestingDeviceStatusHandler(DevicePrefix::PREFIX_BLUETOOTH);
+	TestingCommandDispatcherForFetcher::Ptr dispatcher =
+		new TestingCommandDispatcherForFetcher;
+
+	m_fetcher->registerHandler(handler0);
+	m_fetcher->registerHandler(handler1);
+	m_fetcher->registerHandler(handler2);
+	m_fetcher->setCommandDispatcher(dispatcher);
+
+	dispatcher->devices = {
+		0xa600000000000001,
+		0xa600000000000002,
+		0xa600000000000003,
+		0xa600000000000004,
+	};
+
+	thread.start(*m_fetcher);
+
+	CPPUNIT_ASSERT(handler0->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler0->handledPrefix == DevicePrefix::PREFIX_VIRTUAL_DEVICE);
+	CPPUNIT_ASSERT_EQUAL(0, handler0->handledPaired.size());
+
+	CPPUNIT_ASSERT(handler1->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler1->handledPrefix == DevicePrefix::PREFIX_FITPROTOCOL);
+	CPPUNIT_ASSERT_EQUAL(0, handler1->handledPaired.size());
+
+	CPPUNIT_ASSERT(handler2->handled.tryWait(5000));
+	CPPUNIT_ASSERT(handler2->handledPrefix == DevicePrefix::PREFIX_BLUETOOTH);
+	CPPUNIT_ASSERT_EQUAL(4, handler2->handledPaired.size());
+
+	m_fetcher->stop();
+	CPPUNIT_ASSERT_NO_THROW(thread.join(10000));
+}
+
+}

commit 04dc52af15dc059fd9bd0013ad2fc71933cda19d

DeviceStatusFetcher: fix PrefixStatus::needsRequest()
The implementation of needsRequest() was insufficient. It was not
possible to determine whether we need to make a request just from
the m_successful state. Consider:

 If m_successful == false, is it possible to say that a request
 has been made?

No. Thus, we have introduced a variable m_started that stores
this information. If a request has started, we can still perform
a repeat but that is another story.

Without this fix, we fail with 2 or more handlers on:

 Assertion violation: !m_successful in file
 	"src/core/DeviceStatusFetcher.cpp", line 52

Fixes: ccce884771af ("DeviceStatusFetcher: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/core/DeviceStatusFetcher.cpp
@@ -36,6 +36,7 @@ DevicePrefix DeviceStatusFetcher::PrefixAnswer::prefix() const
 
 DeviceStatusFetcher::PrefixStatus::PrefixStatus():
 	m_lastRequested(0),
+	m_started(false),
 	m_successful(false)
 {
 }
@@ -44,6 +45,7 @@ void DeviceStatusFetcher::PrefixStatus::startRequest()
 {
 	poco_assert(!m_successful);
 	m_lastRequested.update();
+	m_started = true;
 }
 
 void DeviceStatusFetcher::PrefixStatus::deliverResponse(
@@ -55,7 +57,7 @@ void DeviceStatusFetcher::PrefixStatus::deliverResponse(
 
 bool DeviceStatusFetcher::PrefixStatus::needsRequest() const
 {
-	return !m_successful;
+	return !m_started;
 }
 
 bool DeviceStatusFetcher::PrefixStatus::shouldRepeat(
@@ -230,6 +232,11 @@ void DeviceStatusFetcher::run()
 			}
 
 			break;
+
+		case ACTIVE:
+			if (logger().debug()) {
+				logger().debug("some request is still active", __FILE__, __LINE__);
+			}
 
 		default:
 			break;

src/core/DeviceStatusFetcher.h
@@ -118,6 +118,7 @@ protected:
 
 	private:
 		Poco::Clock m_lastRequested;
+		bool m_started;
 		bool m_successful;
 	};
 