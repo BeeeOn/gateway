BeeeOn/gateway

commit 45ace9e30651b610f56514b2a911c591a4e522c7

BLESmartDeviceManager: new class
The class implements the work with BLE smart devices. This means
seeking for BLE smart devices on the network, modifying state of
the devices and obtaining data from the devices.

There are two ways to obtain data. The first way is when the
manager requests the data from the device at certain time interval.
The second way is when the device sends data and the manager immediately
processes it. This allows to send data immediately after the data are
recieved to the server, which is convenient for the motion sensor and
door/window sensor.

The BLESmartDeviceManager does not use the DeviceStatusFetcher but
it can be modified that way easily.

Duration of the discovery process depends on the parameter scanTimeout.
Its value defines the time period of 1 discovery iteration. Thus, it
takes N * scanTimeout to perform discovery.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/BLESmartDeviceManager.cpp
@@ -1,0 +1,445 @@
+#include <Poco/ScopedLock.h>
+
+#include "bluetooth/BLESmartDeviceManager.h"
+#include "commands/NewDeviceCommand.h"
+#include "core/CommandDispatcher.h"
+#include "di/Injectable.h"
+#include "hotplug/HotplugEvent.h"
+#include "model/DevicePrefix.h"
+#include "util/BlockingAsyncWork.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, BLESmartDeviceManager)
+BEEEON_OBJECT_CASTABLE(CommandHandler)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_CASTABLE(HotplugListener)
+BEEEON_OBJECT_PROPERTY("deviceCache", &BLESmartDeviceManager::setDeviceCache)
+BEEEON_OBJECT_PROPERTY("distributor", &BLESmartDeviceManager::setDistributor)
+BEEEON_OBJECT_PROPERTY("commandDispatcher", &BLESmartDeviceManager::setCommandDispatcher)
+BEEEON_OBJECT_PROPERTY("hciManager", &BLESmartDeviceManager::setHciManager)
+BEEEON_OBJECT_PROPERTY("scanTimeout", &BLESmartDeviceManager::setScanTimeout)
+BEEEON_OBJECT_PROPERTY("deviceTimeout", &BLESmartDeviceManager::setDeviceTimeout)
+BEEEON_OBJECT_PROPERTY("refresh", &BLESmartDeviceManager::setRefresh)
+BEEEON_OBJECT_PROPERTY("attemptsCount", &BLESmartDeviceManager::setAttemptsCount)
+BEEEON_OBJECT_PROPERTY("retryTimeout", &BLESmartDeviceManager::setRetryTimeout)
+BEEEON_OBJECT_END(BeeeOn, BLESmartDeviceManager)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+const UUID BLESmartDeviceManager::CHAR_MODEL_NUMBER = UUID("00002a24-0000-1000-8000-00805f9b34fb");
+
+BLESmartDeviceManager::BLESmartDeviceManager():
+	DongleDeviceManager(DevicePrefix::PREFIX_BLE_SMART, {
+		typeid(GatewayListenCommand),
+		typeid(DeviceAcceptCommand),
+		typeid(DeviceUnpairCommand),
+		typeid(DeviceSetValueCommand),
+	}),
+	m_scanTimeout(10 * Timespan::SECONDS),
+	m_deviceTimeout(5 * Timespan::SECONDS),
+	m_refresh(30 * Timespan::SECONDS)
+{
+	m_watchCallback = new HciInterface::WatchCallback(
+		[&](const MACAddress& address, vector<unsigned char>& data) {
+			processAsyncData(address, data);
+		});
+
+	m_matchedNames = {
+		"unknown",
+	};
+}
+
+void BLESmartDeviceManager::setScanTimeout(const Timespan &timeout)
+{
+	if (timeout.totalSeconds() <= 0)
+		throw InvalidArgumentException("scan timeout time must be at least a second");
+
+	m_scanTimeout = timeout;
+}
+
+void BLESmartDeviceManager::setDeviceTimeout(const Timespan &timeout)
+{
+	if (timeout.totalSeconds() <= 0)
+		throw InvalidArgumentException("device timeout time must be at least a second");
+
+	m_deviceTimeout = timeout;
+}
+
+void BLESmartDeviceManager::setRefresh(const Timespan &refresh)
+{
+	if (refresh.totalSeconds() <= 0)
+		throw InvalidArgumentException("refresh time must at least a second");
+
+	m_refresh = refresh;
+}
+
+void BLESmartDeviceManager::setHciManager(HciInterfaceManager::Ptr manager)
+{
+	m_hciManager = manager;
+}
+
+void BLESmartDeviceManager::dongleAvailable()
+{
+	logger().information("starting BLE Smart device manager", __FILE__, __LINE__);
+
+	m_hci = m_hciManager->lookup(dongleName());
+
+	set<DeviceID> paired;
+	try {
+		paired = deviceList(-1);
+	}
+	catch (Exception& e) {
+		logger().log(e, __FILE__, __LINE__);
+	}
+
+	handleRemoteStatus(prefix(), paired);
+
+	while (!m_stopControl.shouldStop()) {
+		Timestamp now;
+
+		refreshPairedDevices();
+
+		Timespan sleepTime = m_refresh - now.elapsed();
+		m_stopControl.waitStoppable(sleepTime);
+	}
+
+	logger().information("stopping BLE Smart device manager", __FILE__, __LINE__);
+}
+
+void BLESmartDeviceManager::notifyDongleRemoved()
+{
+	m_stopControl.requestWakeup();
+}
+
+string BLESmartDeviceManager::dongleMatch(const HotplugEvent &e)
+{
+	if (e.subsystem() == "bluetooth" && e.type() == "host") {
+		if (e.name().empty()) {
+			logger().warning("missing bluetooth interface name, skipping",
+				__FILE__, __LINE__);
+		}
+		return e.name();
+	}
+
+	return "";
+}
+
+void BLESmartDeviceManager::dongleFailed(const FailDetector &dongleStatus)
+{
+	eraseAllDevices();
+	DongleDeviceManager::dongleFailed(dongleStatus);
+}
+
+void BLESmartDeviceManager::stop()
+{
+	DongleDeviceManager::stop();
+	answerQueue().dispose();
+}
+
+bool BLESmartDeviceManager::dongleMissing()
+{
+	eraseAllDevices();
+	return true;
+}
+
+void BLESmartDeviceManager::refreshPairedDevices()
+{
+	vector<BLESmartDevice::Ptr> devices;
+	set<DeviceID> notFoundDevices;
+	m_hci->up();
+
+	ScopedLockWithUnlock<FastMutex> lock(m_devicesMutex);
+	for (auto &id : deviceCache()->paired(prefix())) {
+		auto it = m_devices.find(id);
+		if (it == m_devices.end())
+			notFoundDevices.insert(id);
+		else
+			devices.push_back(it->second);
+	}
+	lock.unlock();
+
+	if (notFoundDevices.size() > 0)
+		seekPairedDevices(notFoundDevices, devices);
+
+	for (auto &device : devices) {
+		try {
+			ship(device->requestState(m_hci));
+		}
+		catch (const NotImplementedException& e) {
+			// Some devices do not support getting the actual values.
+			continue;
+		}
+		catch (const Exception& e) {
+			logger().log(e, __FILE__, __LINE__);
+			logger().warning("failed to obtain data from device " + device->deviceID().toString(),
+				__FILE__, __LINE__);
+		}
+	}
+}
+
+void BLESmartDeviceManager::eraseAllDevices()
+{
+	ScopedLock<FastMutex> lock(m_devicesMutex);
+
+	m_devices.clear();
+}
+
+void BLESmartDeviceManager::handleRemoteStatus(
+		const DevicePrefix&,
+		const std::set<DeviceID> &paired)
+{
+	for (const auto &id : paired)
+		deviceCache()->markPaired(id);
+}
+
+AsyncWork<>::Ptr BLESmartDeviceManager::startDiscovery(const Timespan &timeout)
+{
+	BLESmartSeeker::Ptr seeker = new BLESmartSeeker(*this, timeout);
+	seeker->start();
+	return seeker;
+}
+
+AsyncWork<set<DeviceID>>::Ptr BLESmartDeviceManager::startUnpair(
+		const DeviceID &id,
+		const Timespan &timeout)
+{
+	auto work = BlockingAsyncWork<set<DeviceID>>::instance();
+
+	ScopedLock<FastMutex> lock(m_devicesMutex, timeout.totalMilliseconds());
+
+	if (!deviceCache()->paired(id)) {
+		logger().warning("unpairing device that is not paired: " + id.toString(),
+			__FILE__, __LINE__);
+	}
+	else {
+		deviceCache()->markUnpaired(id);
+
+		auto it = m_devices.find(id);
+		if (it != m_devices.end())
+			m_devices.erase(id);
+
+		work->setResult({id});
+	}
+
+	return work;
+}
+
+void BLESmartDeviceManager::handleAccept(const DeviceAcceptCommand::Ptr cmd)
+{
+	ScopedLock<FastMutex> lock(m_devicesMutex);
+
+	auto it = m_devices.find(cmd->deviceID());
+	if (it == m_devices.end())
+		throw NotFoundException("accept: " + cmd->deviceID().toString());
+
+	it->second->pair(m_hci, m_watchCallback);
+
+	DeviceManager::handleAccept(cmd);
+}
+
+AsyncWork<double>::Ptr BLESmartDeviceManager::startSetValue(
+		const DeviceID &id,
+		const ModuleID &module,
+		const double value,
+		const Timespan &timeout)
+{
+	ScopedLock<FastMutex> lock(m_devicesMutex, timeout.totalMilliseconds());
+
+	auto it = m_devices.find(id);
+	if (it == m_devices.end())
+		throw NotFoundException("set-value: " + id.toString());
+
+	m_hci->up();
+	it->second->requestModifyState(module, value, m_hci);
+
+	if (logger().debug()) {
+		logger().debug("success to change state of device " + id.toString(),
+			__FILE__, __LINE__);
+	}
+
+	auto work = BlockingAsyncWork<double>::instance();
+	work->setResult(value);
+	return work;
+}
+
+void BLESmartDeviceManager::processAsyncData(
+		const MACAddress& address,
+		std::vector<unsigned char> &data)
+{
+	logger().information("recieved async message from device " + address.toString(':'),
+		__FILE__, __LINE__);
+
+	ScopedLock<FastMutex> lock(m_devicesMutex);
+
+	DeviceID id(DevicePrefix::PREFIX_BLE_SMART, address);
+	auto it = m_devices.find(id);
+	if (it != m_devices.end()) {
+		try {
+			ship(it->second->parseAdvertisingData(data));
+		}
+		catch (const Exception& e) {
+			logger().log(e, __FILE__, __LINE__);
+		}
+	}
+	else {
+		logger().warning("recieved async message from unknown device " + address.toString(':'),
+			__FILE__, __LINE__);
+	}
+}
+
+void BLESmartDeviceManager::seekPairedDevices(
+		const set<DeviceID> pairedDevices,
+		vector<BLESmartDevice::Ptr>& devices)
+{
+	logger().information("discovering of paired BLE devices...", __FILE__, __LINE__);
+
+	map<MACAddress, string> foundDevices = m_hci->lescan(m_scanTimeout);
+
+	for (const auto &device : foundDevices) {
+		if (m_stopControl.shouldStop())
+			break;
+
+		DeviceID id(DevicePrefix::PREFIX_BLE_SMART, device.first);
+		if (pairedDevices.find(id) == pairedDevices.end())
+			continue;
+
+		BLESmartDevice::Ptr newDevice;
+		try {
+			newDevice = createDevice(device.first);
+			newDevice->pair(m_hci, m_watchCallback);
+		}
+		catch (const Exception& e) {
+			logger().log(e, __FILE__, __LINE__);
+			continue;
+		}
+
+		ScopedLock<FastMutex> lock(m_devicesMutex);
+		m_devices.emplace(newDevice->deviceID(), newDevice);
+		devices.push_back(newDevice);
+
+		logger().information("found " + newDevice->productName() + " " + newDevice->deviceID().toString(),
+			__FILE__, __LINE__);
+	}
+}
+
+void BLESmartDeviceManager::seekDevices(vector<BLESmartDevice::Ptr>& foundDevices, const StopControl& stop)
+{
+	map<MACAddress, string> devices;
+
+	logger().information("discovering BLE devices...", __FILE__, __LINE__);
+
+	m_hci->up();
+	devices = m_hci->lescan(m_scanTimeout);
+
+	logger().information("found " + to_string(devices.size()) + " BLE device(s)",
+		__FILE__, __LINE__);
+
+	for (const auto &device : devices) {
+		if (stop.shouldStop())
+			break;
+
+		if (!fastPrefilter(device.second))
+			continue;
+
+		ScopedLockWithUnlock<FastMutex> lock(m_devicesMutex);
+		auto it = m_devices.find(DeviceID(DevicePrefix::PREFIX_BLE_SMART, device.first));
+		if (it != m_devices.end()) {
+			foundDevices.push_back(it->second);
+			logger().information("found " + device.second + " " + it->first.toString(),
+				__FILE__, __LINE__);
+			continue;
+		}
+		lock.unlock();
+
+		BLESmartDevice::Ptr newDevice;
+		try {
+			newDevice = createDevice(device.first);
+		}
+		catch (const NotFoundException&) {
+			// unsupported device
+			continue;
+		}
+		catch (const Exception& e) {
+			logger().log(e, __FILE__, __LINE__);
+			continue;
+		}
+		foundDevices.push_back(newDevice);
+
+		logger().information("found " + newDevice->productName() + " " + newDevice->deviceID().toString(),
+			__FILE__, __LINE__);
+	}
+}
+
+bool BLESmartDeviceManager::fastPrefilter(const string& name) const
+{
+	return m_matchedNames.find(name) != m_matchedNames.end();
+}
+
+BLESmartDevice::Ptr BLESmartDeviceManager::createDevice(const MACAddress& address) const
+{
+	BLESmartDevice::Ptr newDevice;
+	HciConnection::Ptr conn;
+	vector<unsigned char> modelIDRaw;
+
+	try {
+		conn = m_hci->connect(address, m_deviceTimeout);
+		modelIDRaw = conn->read(CHAR_MODEL_NUMBER);
+	}
+	catch (const Exception& e) {
+		throw IOException(
+			"failed to read model number of device " + address.toString(), e);
+	}
+
+	string modelID = {modelIDRaw.begin(), modelIDRaw.end()};
+	throw NotFoundException("device " + modelID + "not supported");
+}
+
+void BLESmartDeviceManager::processNewDevice(BLESmartDevice::Ptr newDevice)
+{
+	ScopedLock<FastMutex> lock(m_devicesMutex);
+
+	auto it = m_devices.emplace(newDevice->deviceID(), newDevice);
+	if (!it.second)
+		return;
+
+	logger().debug("found device " + newDevice->deviceID().toString(),
+		__FILE__, __LINE__);
+
+	auto types = newDevice->moduleTypes();
+	dispatch(
+		new NewDeviceCommand(
+			newDevice->deviceID(),
+			newDevice->vendor(),
+			newDevice->productName(),
+			types,
+			m_refresh));
+}
+
+BLESmartDeviceManager::BLESmartSeeker::BLESmartSeeker(
+		BLESmartDeviceManager& parent,
+		const Timespan& duration):
+	AbstractSeeker(duration),
+	m_parent(parent)
+{
+}
+
+void BLESmartDeviceManager::BLESmartSeeker::seekLoop(StopControl &control)
+{
+	StopControl::Run run(control);
+
+	while (remaining() > 0) {
+		vector<BLESmartDevice::Ptr> newDevices;
+
+		m_parent.seekDevices(newDevices, control);
+		for (auto device : newDevices) {
+			if (!run)
+				break;
+
+			m_parent.processNewDevice(device);
+		}
+
+		if (!run)
+			break;
+	}
+}

src/bluetooth/BLESmartDeviceManager.h
@@ -1,0 +1,165 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include <Poco/Mutex.h>
+#include <Poco/Thread.h>
+#include <Poco/Timespan.h>
+#include <Poco/UUID.h>
+
+#include "bluetooth/BLESmartDevice.h"
+#include "bluetooth/HciInterface.h"
+#include "commands/DeviceAcceptCommand.h"
+#include "commands/DeviceSetValueCommand.h"
+#include "core/AbstractSeeker.h"
+#include "core/DongleDeviceManager.h"
+#include "loop/StopControl.h"
+#include "model/DeviceID.h"
+#include "net/MACAddress.h"
+#include "util/AsyncWork.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The class implements the work with Bluetooth Low Energy devices.
+ * Allows us to process and execute the commands from server and gather
+ * data from the devices.
+ */
+class BLESmartDeviceManager : public DongleDeviceManager {
+public:
+	static const Poco::UUID CHAR_MODEL_NUMBER;
+
+	/**
+	 * @brief Provides searching BLE devices on network
+	 * in own thread.
+	 */
+	class BLESmartSeeker : public AbstractSeeker {
+	public:
+		typedef Poco::SharedPtr<BLESmartSeeker> Ptr;
+
+		BLESmartSeeker(
+			BLESmartDeviceManager& parent,
+			const Poco::Timespan& duration);
+
+	protected:
+		void seekLoop(StopControl &control) override;
+
+	private:
+		BLESmartDeviceManager& m_parent;
+	};
+
+	BLESmartDeviceManager();
+
+	void dongleAvailable() override;
+	std::string dongleMatch(const HotplugEvent &e) override;
+
+	/**
+	 * @brief Erases all instances of the device.
+	 */
+	void dongleFailed(const FailDetector &dongleStatus) override;
+
+	/**
+	 * @brief Erases all instances of the device.
+	 */
+	bool dongleMissing() override;
+	void stop() override;
+
+	void setScanTimeout(const Poco::Timespan &timeout);
+	void setDeviceTimeout(const Poco::Timespan &timeout);
+	void setRefresh(const Poco::Timespan &refresh);
+	void setHciManager(HciInterfaceManager::Ptr manager);
+
+	void handleRemoteStatus(
+		const DevicePrefix &prefix,
+		const std::set<DeviceID> &paired);
+
+protected:
+	/**
+	 * @brief Wakes up the main thread.
+	 */
+	void notifyDongleRemoved() override;
+	void handleAccept(const DeviceAcceptCommand::Ptr cmd) override;
+	AsyncWork<>::Ptr startDiscovery(const Poco::Timespan &timeout) override;
+	AsyncWork<std::set<DeviceID>>::Ptr startUnpair(
+		const DeviceID &id,
+		const Poco::Timespan &timeout) override;
+	AsyncWork<double>::Ptr startSetValue(
+		const DeviceID &id,
+		const ModuleID &module,
+		const double value,
+		const Poco::Timespan &timeout) override;
+
+	/**
+	 * @brief In the first step, it tries to find not found paired
+	 * devices and then requests the actual state of all found paired
+	 * devices.
+	 */
+	void refreshPairedDevices();
+
+	/**
+	 * @brief Clears m_devices.
+	 */
+	void eraseAllDevices();
+
+	/**
+	 * @brief Processes the asynchronous data of the paired device.
+	 * If the message is correct it is already shipped to server.
+	 */
+	void processAsyncData(
+		const MACAddress& address,
+		std::vector<unsigned char> &data);
+
+	/**
+	 * @brief Tries to find not found paired devices. Each found device
+	 * is added to parametr devices and attribute m_devices.
+	 */
+	void seekPairedDevices(
+		const std::set<DeviceID> pairedDevices,
+		std::vector<BLESmartDevice::Ptr>& devices);
+
+	/**
+	 * @brief Seeks for new devices on Bluetooth LE network.
+	 *
+	 * The process of identification device consists of two steps.
+	 * In the first step, it is determined wheter the device name
+	 * is in set of names of potentially supported device. If so,
+	 * then the model id of device is obtained according to which
+	 * the device is identified.
+	 */
+	void seekDevices(
+		std::vector<BLESmartDevice::Ptr>& foundDevices,
+		const StopControl& stop);
+
+	/**
+	 * @brief It is faster then asking for device identification.
+	 * In this way, we speed up lookup for supported devices only.
+	 */
+	bool fastPrefilter(const std::string& name) const;
+
+	/**
+	 * @brief Creates BLE device based on its Model ID.
+	 */
+	BLESmartDevice::Ptr createDevice(const MACAddress& address) const;
+
+	void processNewDevice(BLESmartDevice::Ptr newDevice);
+
+private:
+	Poco::FastMutex m_devicesMutex;
+	std::map<DeviceID, BLESmartDevice::Ptr> m_devices;
+	Poco::SharedPtr<HciInterface::WatchCallback> m_watchCallback;
+	/**
+	 * @brief The set of device names that can be one of the supported
+	 * devices. If a device with this name is found in the seeking for
+	 * new devices, it is further investigated.
+	 */
+	std::set<std::string> m_matchedNames;
+
+	Poco::Timespan m_scanTimeout;
+	Poco::Timespan m_deviceTimeout;
+	Poco::Timespan m_refresh;
+	HciInterfaceManager::Ptr m_hciManager;
+	HciInterface::Ptr m_hci;
+};
+
+}

src/CMakeLists.txt
@@ -212,6 +212,7 @@ endif()
 if(ENABLE_BLUETOOTH AND ENABLE_BLE_SMART)
 	file(GLOB BLE_SMART_SOURCES
 		${PROJECT_SOURCE_DIR}/bluetooth/BLESmartDevice.cpp
+		${PROJECT_SOURCE_DIR}/bluetooth/BLESmartDeviceManager.cpp
 	)
 	list(APPEND SOURCES ${BLE_SMART_SOURCES})
 else()

commit 0da5b20c5bacd9acbf3957ffce83aa95a0ab33d9

BLESmartDeviceManager: fix searching of new devices
From the device search process, the fast prefilter was removed
because it was found out that the device names are not 100% specified.

Fixes: 45ace9e30 ("BLESmartDeviceManager: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/BLESmartDeviceManager.cpp
@@ -51,16 +51,6 @@ BLESmartDeviceManager::BLESmartDeviceManager():
 		[&](const MACAddress& address, vector<unsigned char>& data) {
 			processAsyncData(address, data);
 		});
-
-	m_matchedNames = {
-		"unknown",
-		BeeWiSmartClim::NAME,
-		BeeWiSmartMotion::NAME,
-		BeeWiSmartDoor::NAME,
-		BeeWiSmartWatt::NAME,
-		BeeWiSmartLite::NAME,
-		TabuLumenSmartLite::NAME,
-	};
 }
 
 void BLESmartDeviceManager::setScanTimeout(const Timespan &timeout)
@@ -344,9 +334,6 @@ void BLESmartDeviceManager::seekDevices(vector<BLESmartDevice::Ptr>& foundDevice
 		if (stop.shouldStop())
 			break;
 
-		if (!fastPrefilter(device.second))
-			continue;
-
 		ScopedLockWithUnlock<FastMutex> lock(m_devicesMutex);
 		auto it = m_devices.find(DeviceID(DevicePrefix::PREFIX_BLE_SMART, device.first));
 		if (it != m_devices.end()) {
@@ -374,11 +361,6 @@ void BLESmartDeviceManager::seekDevices(vector<BLESmartDevice::Ptr>& foundDevice
 		logger().information("found " + newDevice->productName() + " " + newDevice->deviceID().toString(),
 			__FILE__, __LINE__);
 	}
-}
-
-bool BLESmartDeviceManager::fastPrefilter(const string& name) const
-{
-	return m_matchedNames.find(name) != m_matchedNames.end();
 }
 
 BLESmartDevice::Ptr BLESmartDeviceManager::createDevice(const MACAddress& address) const

src/bluetooth/BLESmartDeviceManager.h
@@ -137,12 +137,6 @@ protected:
 		const StopControl& stop);
 
 	/**
-	 * @brief It is faster then asking for device identification.
-	 * In this way, we speed up lookup for supported devices only.
-	 */
-	bool fastPrefilter(const std::string& name) const;
-
-	/**
 	 * @brief Creates BLE device based on its Model ID.
 	 */
 	BLESmartDevice::Ptr createDevice(const MACAddress& address) const;
@@ -153,12 +147,6 @@ private:
 	Poco::FastMutex m_devicesMutex;
 	std::map<DeviceID, BLESmartDevice::Ptr> m_devices;
 	Poco::SharedPtr<HciInterface::WatchCallback> m_watchCallback;
-	/**
-	 * @brief The set of device names that can be one of the supported
-	 * devices. If a device with this name is found in the seeking for
-	 * new devices, it is further investigated.
-	 */
-	std::set<std::string> m_matchedNames;
 
 	Poco::Timespan m_scanTimeout;
 	Poco::Timespan m_deviceTimeout;

src/bluetooth/TabuLumenSmartLite.cpp
@@ -27,7 +27,6 @@ const vector<uint8_t> TabuLumenSmartLite::ADD_KEY = {
 const vector<uint8_t> TabuLumenSmartLite::XOR_KEY = {
 	0x00, 0x2b, 0x3c, 0x4d, 0x5e, 0x6f, 0xf7, 0xe8, 0xd9, 0xca,
 	0xbb, 0xac, 0x9d, 0x8e, 0x7f, 0x5e, 0x6f, 0xf7, 0xe8, 0xd9};
-const string TabuLumenSmartLite::NAME = "Lighting_AVC";
 const string TabuLumenSmartLite::LIGHT_NAME = "TL 100S Smart Light";
 const string TabuLumenSmartLite::VENDOR_NAME = "Tabu Lumen";
 

src/bluetooth/TabuLumenSmartLite.h
@@ -16,8 +16,6 @@ class TabuLumenSmartLite : public BLESmartDevice {
 public:
 	typedef Poco::SharedPtr<TabuLumenSmartLite> Ptr;
 
-	static const std::string NAME;
-
 private:
 	/**
 	 * UUID of characteristics to modify device status and to authorize.