BeeeOn/gateway

commit daebd8f33e5cd58586b864104faafdeeed82de94

BeeWiSmartWatt: new class
The class represents BeeWi smart switch. It allows
to gather and to control all its modules. It's modules
are on/off, light on/off, power, voltage, current and frequence.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/BeeWiSmartWatt.cpp
@@ -1,0 +1,140 @@
+#include <Poco/Exception.h>
+#include <Poco/Timestamp.h>
+
+#include "bluetooth/BeeWiSmartWatt.h"
+
+#define ON_OFF_MODULE_ID 0
+#define LIGHT_ON_OFF_MODULE_ID 1
+#define POWER_MODULE_ID 2
+#define VOLTAGE_MODULE_ID 3
+#define CURRENT_MODULE_ID 4
+#define FREQUENCY_MODULE_ID 5
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+static list<ModuleType> DEVICE_MODULE_TYPES = {
+	{ModuleType::Type::TYPE_ON_OFF, {ModuleType::Attribute::ATTR_CONTROLLABLE}},
+	{ModuleType::Type::TYPE_ON_OFF, {ModuleType::Attribute::ATTR_CONTROLLABLE}},
+	{ModuleType::Type::TYPE_POWER},
+	{ModuleType::Type::TYPE_VOLTAGE},
+	{ModuleType::Type::TYPE_CURRENT},
+	{ModuleType::Type::TYPE_FREQUENCY},
+};
+
+const UUID BeeWiSmartWatt::ACTUAL_VALUES = UUID("a8b3ff07-4834-4051-89d0-3de95cddd318");
+const UUID BeeWiSmartWatt::ON_OFF = UUID("a8b3ff04-4834-4051-89d0-3de95cddd318");
+const UUID BeeWiSmartWatt::LIGHT_ON_OFF = UUID("a8b3ff06-4834-4051-89d0-3de95cddd318");
+const string BeeWiSmartWatt::NAME = "BeeWi Smart Wattt";
+
+BeeWiSmartWatt::BeeWiSmartWatt(const MACAddress& address, const Timespan& timeout):
+	BeeWiDevice(address, timeout, NAME, DEVICE_MODULE_TYPES)
+{
+}
+
+BeeWiSmartWatt::BeeWiSmartWatt(
+		const MACAddress& address,
+		const Timespan& timeout,
+		HciConnection::Ptr conn):
+	BeeWiDevice(address, timeout, NAME, DEVICE_MODULE_TYPES)
+{
+	initLocalTime(conn);
+}
+
+BeeWiSmartWatt::~BeeWiSmartWatt()
+{
+}
+
+void BeeWiSmartWatt::requestModifyState(
+	const ModuleID& moduleID,
+	const double value,
+	HciInterface::Ptr hci)
+{
+	SynchronizedObject::ScopedLock guard(*this);
+
+	vector<unsigned char> data;
+	UUID characteristics;
+
+	switch (moduleID.value()) {
+	case ON_OFF_MODULE_ID: {
+		if (value != 0 && value != 1)
+			throw IllegalStateException("value is not allowed");
+
+		characteristics = ON_OFF;
+		data.push_back(value);
+		break;
+	}
+	case LIGHT_ON_OFF_MODULE_ID: {
+		if (value != 0 && value != 1)
+			throw IllegalStateException("value is not allowed");
+
+		characteristics = LIGHT_ON_OFF;
+		data.push_back(value);
+		break;
+	}
+	default:
+		throw IllegalStateException("invalid module ID: " + to_string(moduleID.value()));
+	}
+
+	HciConnection::Ptr conn = hci->connect(m_address, m_timeout);
+	conn->write(characteristics, data);
+}
+
+SensorData BeeWiSmartWatt::requestState(const HciInterface::Ptr hci)
+{
+	SynchronizedObject::ScopedLock guard(*this);
+
+	HciConnection::Ptr conn = hci->connect(m_address, m_timeout);
+	vector<unsigned char> data = conn->read(ACTUAL_VALUES);
+
+	return parseValues(data);
+}
+
+SensorData BeeWiSmartWatt::parseAdvertisingData(
+		const vector<unsigned char>& data) const
+{
+	if (data.size() != 13)
+		throw ProtocolException("expected 13 B, received " + to_string(data.size()) + " B");
+
+	double onOff = data[2];
+	double power = (data[6] + (data[7] << 8)) / 10.0;
+
+	return {
+		m_deviceId,
+		Timestamp{},
+		{
+			{ON_OFF_MODULE_ID, onOff},
+			{POWER_MODULE_ID, power},
+		}
+	};
+}
+
+SensorData BeeWiSmartWatt::parseValues(vector<unsigned char>& values) const
+{
+	if (values.size() != 7)
+		throw ProtocolException("expected 7 B, received " + to_string(values.size()) + " B");
+
+	double onOff = values[0];
+	double power = (values[1] + (values[2] << 8)) / 10.0;
+	double voltage = values[3];
+	double current = (values[4] + (values[5] << 8)) / 1000.0;
+	double frequency = values[6];
+
+	return {
+		m_deviceId,
+		Timestamp{},
+		{
+			{ON_OFF_MODULE_ID, onOff},
+			{POWER_MODULE_ID, power},
+			{VOLTAGE_MODULE_ID, voltage},
+			{CURRENT_MODULE_ID, current},
+			{FREQUENCY_MODULE_ID, frequency},
+		}
+	};
+}
+
+bool BeeWiSmartWatt::match(const string& modelID)
+{
+	return modelID.find("BeeWi BP1WC") != string::npos;
+}

src/CMakeLists.txt
@@ -215,6 +215,7 @@ if(ENABLE_BLUETOOTH AND ENABLE_BLE_SMART)
 		${PROJECT_SOURCE_DIR}/bluetooth/BeeWiSmartClim.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/BeeWiSmartDoor.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/BeeWiSmartMotion.cpp
+		${PROJECT_SOURCE_DIR}/bluetooth/BeeWiSmartWatt.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/BLESmartDevice.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/BLESmartDeviceManager.cpp
 	)

src/bluetooth/BLESmartDeviceManager.cpp
@@ -4,6 +4,7 @@
 #include "bluetooth/BeeWiSmartClim.h"
 #include "bluetooth/BeeWiSmartDoor.h"
 #include "bluetooth/BeeWiSmartMotion.h"
+#include "bluetooth/BeeWiSmartWatt.h"
 #include "commands/NewDeviceCommand.h"
 #include "core/CommandDispatcher.h"
 #include "di/Injectable.h"
@@ -53,6 +54,7 @@ BLESmartDeviceManager::BLESmartDeviceManager():
 		BeeWiSmartClim::NAME,
 		BeeWiSmartMotion::NAME,
 		BeeWiSmartDoor::NAME,
+		BeeWiSmartWatt::NAME,
 	};
 }
 
@@ -404,6 +406,8 @@ BLESmartDevice::Ptr BLESmartDeviceManager::createDevice(const MACAddress& addres
 		newDevice = new BeeWiSmartMotion(address, m_deviceTimeout, conn);
 	else if (BeeWiSmartDoor::match(modelID))
 		newDevice = new BeeWiSmartDoor(address, m_deviceTimeout, conn);
+	else if (BeeWiSmartWatt::match(modelID))
+		newDevice = new BeeWiSmartWatt(address, m_deviceTimeout, conn);
 	else
 		throw NotFoundException("device " + modelID + "not supported");
 

src/bluetooth/BeeWiSmartWatt.h
@@ -1,0 +1,73 @@
+#pragma once
+
+#include <Poco/SharedPtr.h>
+#include <Poco/UUID.h>
+
+#include "bluetooth/BeeWiDevice.h"
+#include "bluetooth/HciConnection.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The class represents BeeWi smart switch. It allows
+ * to gather and to control all its modules.
+ */
+class BeeWiSmartWatt : public BeeWiDevice {
+public:
+	typedef Poco::SharedPtr<BeeWiSmartWatt> Ptr;
+
+	static const std::string NAME;
+
+private:
+	/**
+	 * UUID of characteristics containing actual values of all sensor modules.
+	 */
+	static const Poco::UUID ACTUAL_VALUES;
+	/**
+	 * UUID of characteristics to switch on/off the switch.
+	 */
+	static const Poco::UUID ON_OFF;
+	/**
+	 * UUID of characteristics to switch on/off the light on the switch.
+	 */
+	static const Poco::UUID LIGHT_ON_OFF;
+
+protected:
+	/**
+	 * The intention of this constructor is only for testing.
+	 */
+	BeeWiSmartWatt(const MACAddress& address, const Poco::Timespan& timeout);
+
+public:
+	BeeWiSmartWatt(
+		const MACAddress& address,
+		const Poco::Timespan& timeout,
+		HciConnection::Ptr conn);
+	~BeeWiSmartWatt();
+
+	void requestModifyState(
+		const ModuleID& moduleID,
+		const double value,
+		const HciInterface::Ptr hci) override;
+	SensorData requestState(const HciInterface::Ptr hci) override;
+
+	/**
+	 * <pre>
+	 * | ID (1 B) | 1 B | on/off (1 B) | 3 B | power (2 B) | 5 B |
+	 * </pre>
+	 */
+	SensorData parseAdvertisingData(
+		const std::vector<unsigned char>& data) const override;
+
+	static bool match(const std::string& modelID);
+
+protected:
+	/**
+	* <pre>
+	 * | on/off (1 B) | power (2 B) | voltage (1 B) | current (2 B) | frequency (1 B) |
+	 * </pre>
+	 */
+	SensorData parseValues(std::vector<unsigned char>& values) const;
+};
+
+}

test/bluetooth/BLESmartDeviceTest.cpp
@@ -4,6 +4,7 @@
 #include "bluetooth/BeeWiSmartClim.h"
 #include "bluetooth/BeeWiSmartDoor.h"
 #include "bluetooth/BeeWiSmartMotion.h"
+#include "bluetooth/BeeWiSmartWatt.h"
 #include "model/SensorData.h"
 #include "net/MACAddress.h"
 
@@ -23,6 +24,9 @@ class BLESmartDeviceTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testBeeWiSmartDoorParseValidData);
 	CPPUNIT_TEST(testBeeWiSmartDoorParseTooLongMessage);
 	CPPUNIT_TEST(testBeeWiSmartDoorParseTooShortMessage);
+	CPPUNIT_TEST(testBeeWiSmartWattParseValidData);
+	CPPUNIT_TEST(testBeeWiSmartWattParseTooLongMessage);
+	CPPUNIT_TEST(testBeeWiSmartWattParseTooShortMessage);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void testBeeWiSmartClimParseValidData();
@@ -34,6 +38,9 @@ public:
 	void testBeeWiSmartDoorParseValidData();
 	void testBeeWiSmartDoorParseTooLongMessage();
 	void testBeeWiSmartDoorParseTooShortMessage();
+	void testBeeWiSmartWattParseValidData();
+	void testBeeWiSmartWattParseTooLongMessage();
+	void testBeeWiSmartWattParseTooShortMessage();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(BLESmartDeviceTest);
@@ -52,6 +59,15 @@ public:
 		BeeWiSmartDoor(address, timeout)
 	{
 	}
+};
+
+class TestableBeeWiSmartWatt: public BeeWiSmartWatt {
+public:
+	TestableBeeWiSmartWatt(const MACAddress& address, const Timespan& timeout):
+		BeeWiSmartWatt(address, timeout)
+	{
+	}
+	using BeeWiSmartWatt::parseValues;
 };
 
 /**
@@ -207,6 +223,86 @@ void BLESmartDeviceTest::testBeeWiSmartDoorParseTooShortMessage()
 	CPPUNIT_ASSERT_THROW_MESSAGE(
 		"expected 5 B, received 2 B",
 		sensor.parseAdvertisingData(values),
+		ProtocolException);
+}
+
+/**
+ * @brief Test of parsing valid values from BeeWi Smart Watt.
+ */
+void BLESmartDeviceTest::testBeeWiSmartWattParseValidData()
+{
+	TestableBeeWiSmartWatt sensor(MACAddress::parse("FF:FF:FF:FF:FF:FF"), 0);
+
+	vector<unsigned char> values1 =
+		{0x00, 0x10, 0x00, 0xf1, 0x02, 0x00, 0x32};
+	SensorData data1 = sensor.parseValues(values1);
+	CPPUNIT_ASSERT_EQUAL(int(data1[0].value()), 0);
+	CPPUNIT_ASSERT_EQUAL(data1[1].value(), 1.6);
+	CPPUNIT_ASSERT_EQUAL(int(data1[2].value()), 241);
+	CPPUNIT_ASSERT_EQUAL(data1[3].value(), 0.002);
+	CPPUNIT_ASSERT_EQUAL(int(data1[4].value()), 50);
+
+	vector<unsigned char> values2 =
+		{0x01, 0x10, 0x01, 0xf0, 0x02, 0x01, 0x31};
+	SensorData data2 = sensor.parseValues(values2);
+	CPPUNIT_ASSERT_EQUAL(int(data2[0].value()), 1);
+	CPPUNIT_ASSERT_EQUAL(data2[1].value(), 27.2);
+	CPPUNIT_ASSERT_EQUAL(int(data2[2].value()), 240);
+	CPPUNIT_ASSERT_EQUAL(data2[3].value(), 0.258);
+	CPPUNIT_ASSERT_EQUAL(int(data2[4].value()), 49);
+
+	vector<unsigned char> values5 =
+		{0x0a, 0x03, 0x00, 0x0a, 0x00, 0x0d, 0x10, 0x00, 0x0e, 0x00, 0x00, 0x00, 0x00};
+	SensorData data5 = sensor.parseAdvertisingData(values5);
+	CPPUNIT_ASSERT_EQUAL(int(data5[0].value()), 0);
+	CPPUNIT_ASSERT_EQUAL(data5[1].value(), 1.6);
+
+	vector<unsigned char> values6 =
+		{0x0a, 0x03, 0x01, 0x0a, 0x00, 0x0d, 0x10, 0x01, 0x0e, 0x00, 0x00, 0x00, 0x00};
+	SensorData data6 = sensor.parseAdvertisingData(values6);
+	CPPUNIT_ASSERT_EQUAL(int(data6[0].value()), 1);
+	CPPUNIT_ASSERT_EQUAL(data6[1].value(), 27.2);
+}
+
+/**
+ * @brief Test of parsing too long message from BeeWi Smart Watt.
+ */
+void BLESmartDeviceTest::testBeeWiSmartWattParseTooLongMessage()
+{
+	TestableBeeWiSmartWatt sensor(MACAddress::parse("FF:FF:FF:FF:FF:FF"), 0);
+
+	vector<unsigned char> values1 =
+		{0x01, 0x10, 0x01, 0xf0, 0x02, 0x01, 0x31, 0x00};
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"expected 7 B, received 8 B",
+		sensor.parseValues(values1),
+		ProtocolException);
+
+	vector<unsigned char> values2 =
+		{0x0a, 0x03, 0x00, 0x0a, 0x00, 0x0d, 0x10, 0x00, 0x0e, 0x00, 0x00, 0x00 , 0x00, 0x00};
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"expected 13 B, received 14 B",
+		sensor.parseAdvertisingData(values2),
+		ProtocolException);
+}
+
+/**
+ * @brief Test of parsing too short message from BeeWi Smart Watt.
+ */
+void BLESmartDeviceTest::testBeeWiSmartWattParseTooShortMessage()
+{
+	TestableBeeWiSmartWatt sensor(MACAddress::parse("FF:FF:FF:FF:FF:FF"), 0);
+
+	vector<unsigned char> values1 = {0x00, 0x00};
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"expected 7 B, received 2 B",
+		sensor.parseValues(values1),
+		ProtocolException);
+
+	vector<unsigned char> values2 = {0x00, 0x00};
+	CPPUNIT_ASSERT_THROW_MESSAGE(
+		"expected 13 B, received 2 B",
+		sensor.parseAdvertisingData(values2),
 		ProtocolException);
 }
 

commit 7aafaf1fbdd79b02e7db62561f0d726c3654f1e0

BeeWiSmartWatt: fix product name
Fixes: daebd8f33 ("BeeWiSmartWatt: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/BeeWiSmartWatt.cpp
@@ -26,7 +26,7 @@ static list<ModuleType> DEVICE_MODULE_TYPES = {
 const UUID BeeWiSmartWatt::ACTUAL_VALUES = UUID("a8b3ff07-4834-4051-89d0-3de95cddd318");
 const UUID BeeWiSmartWatt::ON_OFF = UUID("a8b3ff04-4834-4051-89d0-3de95cddd318");
 const UUID BeeWiSmartWatt::LIGHT_ON_OFF = UUID("a8b3ff06-4834-4051-89d0-3de95cddd318");
-const string BeeWiSmartWatt::NAME = "BeeWi Smart Wattt";
+const string BeeWiSmartWatt::NAME = "BeeWi Smart Watt";
 
 BeeWiSmartWatt::BeeWiSmartWatt(const MACAddress& address, const Timespan& timeout):
 	BeeWiDevice(address, timeout, NAME, DEVICE_MODULE_TYPES)