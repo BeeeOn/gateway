BeeeOn/gateway

commit 7225dc57e2d0c7557d1e24898641240c747b75f7

XmlTypeMappingParser: new abstract template class
Abstract specialization of TypeMappingParser to simplify construction
of types mapping parsers represented via XML.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/XmlTypeMappingParserHelper.cpp
@@ -1,0 +1,88 @@
+#include <iostream>
+
+#include <Poco/Exception.h>
+#include <Poco/String.h>
+
+#include <Poco/DOM/NamedNodeMap.h>
+#include <Poco/DOM/NodeFilter.h>
+
+#include "util/SecureXmlParser.h"
+#include "util/XmlTypeMappingParserHelper.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+XmlTypeMappingParserHelper::XmlTypeMappingParserHelper(
+		const string &techNode,
+		Logger &logger):
+	m_techNode(techNode)
+{
+	setupLogger(&logger);
+}
+
+void XmlTypeMappingParserHelper::parseDOM(istream &in)
+{
+	SecureXmlParser parser;
+
+	m_document = parser.parse(in);
+	m_iterator = new NodeIterator(
+			m_document->documentElement(),
+			NodeFilter::SHOW_ELEMENT);
+}
+
+pair<AutoPtr<Node>, ModuleType> XmlTypeMappingParserHelper::next()
+{
+	Node *node = m_iterator->nextNode();
+
+	while (node != nullptr) {
+		if (node->localName() != "beeeon")
+			continue;
+
+		const Node *mapNode = node->parentNode();
+		if (mapNode == nullptr) {
+			logger().warning(
+				"element beeeon has no parent",
+				__FILE__, __LINE__);
+		}
+		else if (mapNode->localName() != "map") {
+			logger().warning(
+				"skipping element beeeon with parent '" + mapNode->localName() + "'",
+				__FILE__, __LINE__);
+			continue;
+		}
+
+		const Node *commentNode = mapNode->attributes()->getNamedItem("comment");
+		if (commentNode == nullptr) {
+			logger().warning(
+				"missing comment attribute for element map",
+				__FILE__, __LINE__);
+		}
+		else if (logger().debug()) {
+			logger().debug(
+				"parsing '" + commentNode->getNodeValue() + "'",
+				__FILE__, __LINE__);
+		}
+
+		const Node *techNode = node->previousSibling();
+		if (techNode->localName() != m_techNode) {
+			if (logger().trace()) {
+				logger().trace(
+					"skipping element beeeon with previous sibling '" + techNode->localName() + "'",
+					__FILE__, __LINE__);
+			}
+
+			continue;
+		}
+
+		const Node *typeNode = node->attributes()->getNamedItem("type");
+		if (typeNode == nullptr)
+			throw SyntaxException("missing attribute type on element beeeon");
+
+		const string type = trim(typeNode->getNodeValue());
+		return make_pair(techNode->cloneNode(false), ModuleType::parse(type));
+	}
+
+	return make_pair(nullptr, ModuleType{});
+}

src/CMakeLists.txt
@@ -155,6 +155,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/NullSensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataFormatter.cpp
 	${PROJECT_SOURCE_DIR}/util/SensorDataParser.cpp
+	${PROJECT_SOURCE_DIR}/util/XmlTypeMappingParserHelper.cpp
 	${PROJECT_SOURCE_DIR}/zwave/ZWaveListener.cpp
 )
 

src/util/XmlTypeMappingParser.h
@@ -1,0 +1,83 @@
+#pragma once
+
+#include <Poco/DOM/Node.h>
+
+#include "util/TypeMappingParser.h"
+#include "util/XmlTypeMappingParserHelper.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief XmlTypeMappingParser is an abstract specialization of the
+ * TypeMappingParser. It is used to parse an external input stream
+ * with type mapping definitions represented by a XML document.
+ *
+ * @see XmlTypeMappingParserHelper
+ */
+template <typename TechType>
+class XmlTypeMappingParser : public TypeMappingParser<TechType>, protected Loggable {
+public:
+	XmlTypeMappingParser(const std::string &techNode, Poco::Logger &logger);
+
+	/**
+	 * @brief Parse the input stream as XML file via the XmlTypeMappingParserHelper.
+	 *
+	 * @see XmlTypeMappingParserHelper
+	 */
+	typename TypeMappingParser<TechType>::TypeMappingSequence parse(std::istream &in) override;
+
+protected:
+	/**
+	 * @brief Parse the XML node describing a technology-specific data type.
+	 * @throws Poco::SyntaxException
+	 */
+	virtual TechType parseTechType(const Poco::XML::Node &node) = 0;
+
+	/**
+	 * @returns string representation of the given technology-specific type.
+	 */
+	virtual std::string techTypeRepr(const TechType &type) = 0;
+
+private:
+	XmlTypeMappingParserHelper m_helper;
+};
+
+template <typename TechType>
+XmlTypeMappingParser<TechType>::XmlTypeMappingParser(
+		const std::string &techNode,
+		Poco::Logger &logger):
+	m_helper(techNode, logger)
+{
+}
+
+template <typename TechType>
+typename TypeMappingParser<TechType>::TypeMappingSequence XmlTypeMappingParser<TechType>::parse(std::istream &in)
+{
+	typename TypeMappingParser<TechType>::TypeMappingSequence sequence;
+	std::pair<Poco::AutoPtr<Poco::XML::Node>, ModuleType> mapping;
+
+	if (logger().trace()) {
+		logger().trace(
+			"loading DOM representation of type mappings",
+			__FILE__, __LINE__);
+	}
+
+	m_helper.parseDOM(in);
+
+	while (!(mapping = m_helper.next()).first.isNull()) {
+		const TechType techType = parseTechType(*mapping.first);
+
+		if (logger().debug()) {
+			logger().debug(
+				"parsed mapping " + techTypeRepr(techType)
+				+ " to " + mapping.second.type().toString(),
+				__FILE__, __LINE__);
+		}
+
+		sequence.emplace_back(techType, mapping.second);
+	}
+
+	return sequence;
+}
+
+}

src/util/XmlTypeMappingParserHelper.h
@@ -1,0 +1,80 @@
+#pragma once
+
+#include <iosfwd>
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Logger.h>
+#include <Poco/SharedPtr.h>
+
+#include <Poco/DOM/Document.h>
+#include <Poco/DOM/Node.h>
+#include <Poco/DOM/NodeIterator.h>
+
+#include "model/ModuleType.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Helper defining method for parsing an input stream as
+ * an XML input. The purpose is to extract information about type
+ * mapping. The class is stateful and represents a single loop over
+ * the parsed XML document.
+ *
+ * The helper expects the following XML structure:
+ * <pre>
+ *   <some-root>
+ *     ...
+ *     <some-mapping-group>
+ *       <map comment="Type xxx">
+ *         <TECH-SPEC TECH-ATTR="xxx" />
+ *         <beeeon type="temperature,outer" />
+ *       <map>
+ *       <map comment="Type yyy">
+ *         <TECH-SPEC TECH-ATTR="yyy" />
+ *         <beeeon type="humidity" />
+ *       </map>
+ *       ...
+ *     </some-mapping-group>
+ *     ...
+ *   </some-root>
+ * </pre>
+ *
+ * The parser does not care about the depth of element <code><map></code>.
+ * The <code><TECH-SPEC /></code> element is unknown to the helper
+ * but it MUST be the previous sibling of the <code><beeeon /></code> element.
+ * <code>TECH-ATTR</code> is technology-specific attribute that the parser
+ * does not take care of either. The element <code><beeeon /></code> specifies
+ * the BeeeOn type to map to.
+ */
+class XmlTypeMappingParserHelper : Loggable {
+public:
+	/**
+	 * @param techNode - name of the technology-specific XML element
+	 * @param logger - logger to log into
+	 */
+	XmlTypeMappingParserHelper(
+		const std::string &techNode,
+		Poco::Logger &logger);
+
+	/**
+	 * @brief Parse the given input stream and create its DOM representation
+	 * internally.
+	 */
+	void parseDOM(std::istream &in);
+
+	/**
+	 * @returns the next available pair <code>(TECH-SPEC, ModuleType)</code>.
+	 * The <code>TECH-SPEC</code> must be parsed by upper layer.
+	 * @throws Poco::SyntaxException
+	 */
+	std::pair<Poco::AutoPtr<Poco::XML::Node>, ModuleType> next();
+
+private:
+	std::string m_techNode;
+	Poco::AutoPtr<Poco::XML::Document> m_document;
+	Poco::SharedPtr<Poco::XML::NodeIterator> m_iterator;
+};
+
+}
+

commit d8315350610d3c22c7ec33b140ff62b40838cf70

XmlTypeMappingParserHelper: fix missing "continue"
Credits to cppcheck:

Either the condition 'mapNode==nullptr' is redundant or there
is possible null pointer dereference: mapNode

Fixes: 7225dc57e2d0 ("XmlTypeMappingParser: new abstract template class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/XmlTypeMappingParserHelper.cpp
@@ -45,6 +45,7 @@ pair<AutoPtr<Node>, ModuleType> XmlTypeMappingParserHelper::next()
 			logger().warning(
 				"element beeeon has no parent",
 				__FILE__, __LINE__);
+			continue;
 		}
 		else if (mapNode->localName() != "map") {
 			logger().warning(