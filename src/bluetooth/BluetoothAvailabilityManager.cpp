BeeeOn/gateway

commit 95d327ab222941e12cb40c7b804e977b8c2c3a33

BluetoothAvailabilityManager: manage pairing state by DeviceCache
BluetoothAvailabilityManager uses quite a different approach among the implemented
device managers. We currently do not change the principle just make it closer to
the future approach that is based on the DeviceManager pairing management.

Pairing is controlled by methods addDevice(), removeDevice() and hasDevice(). Those
methods are used *only* while accepting, unpairing or discovering. The availability
detection *does not use those methods at all* and just iterates over the list of
added devices.

Thus, the hasDevice() method can be dropped (and we do so) as it can be easily
replaced by method DeviceCache::paired(). It has two possible (but inprobable)
side-effects:

1. If a device is marked as paired (by DeviceCache::markPaired()) and the
   m_deviceList is not updated, such device will not be reported during discovery
   and thus it might be impossible to pair it easily.

2. If a device is marked as unpaired (by DeviceCache::markUnpaired()) and the
   m_deviceList is not updated, such device would continue to ship data without
   any notice.

Both situations are not a big deal while we have no external access to the pairing
state enabled for the BluetoothAvailabilityManager. Otherwise, somebody has to
change that behaviour...

The BluetoothAvailabilityManager also forgets all paired devices on a failure
(removing dongle). This behaviour must be reworked in the future.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/bluetooth/BluetoothAvailabilityManager.cpp
@@ -330,6 +330,8 @@ void BluetoothAvailabilityManager::fetchDeviceList()
 {
 	set<DeviceID> idList;
 	idList = deviceList();
+
+	deviceCache()->markPaired(prefix(), {});
 	m_deviceList.clear();
 
 	FastMutex::ScopedLock lock(m_lock);
@@ -363,7 +365,7 @@ void BluetoothAvailabilityManager::reportFoundDevices(
 		else
 			return;
 
-		if (!hasDevice(id))
+		if (!deviceCache()->paired(id))
 			sendNewDevice(id, scannedDevice.second);
 	}
 }
@@ -394,11 +396,7 @@ void BluetoothAvailabilityManager::listen()
 void BluetoothAvailabilityManager::addDevice(const DeviceID &id)
 {
 	m_deviceList.emplace(id, BluetoothDevice(id));
-}
-
-bool BluetoothAvailabilityManager::hasDevice(const DeviceID &id)
-{
-	return m_deviceList.find(id) != m_deviceList.end();
+	deviceCache()->markPaired(id);
 }
 
 void BluetoothAvailabilityManager::removeDevice(const DeviceID &id)
@@ -407,6 +405,8 @@ void BluetoothAvailabilityManager::removeDevice(const DeviceID &id)
 
 	if (it != m_deviceList.end())
 		m_deviceList.erase(it);
+
+	deviceCache()->markUnpaired(id);
 }
 
 void BluetoothAvailabilityManager::shipStatusOf(const BluetoothDevice &device)

src/bluetooth/BluetoothAvailabilityManager.h
@@ -109,8 +109,6 @@ private:
 
 	void addDevice(const DeviceID &id);
 
-	bool hasDevice(const DeviceID &id);
-
 	void removeDevice(const DeviceID &id);
 
 	void shipStatusOf(const BluetoothDevice &device);

commit 914c77c88d013b8507b37943f5751360bebdcff6

BluetoothAvailabilityManager: fix unlocked access to m_deviceList
Fixes: 95d327ab2229 ("BluetoothAvailabilityManager: manage pairing state by DeviceCache")
Fixes: 3f49a0830430 ("BluetoothAvailabilityManager: new class")

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/bluetooth/BluetoothAvailabilityManager.cpp
@@ -307,10 +307,10 @@ void BluetoothAvailabilityManager::fetchDeviceList()
 	set<DeviceID> idList;
 	idList = deviceList();
 
+	FastMutex::ScopedLock lock(m_lock);
+
 	deviceCache()->markPaired(prefix(), {});
 	m_deviceList.clear();
-
-	FastMutex::ScopedLock lock(m_lock);
 
 	for (const auto &id : idList)
 		addDevice(id);