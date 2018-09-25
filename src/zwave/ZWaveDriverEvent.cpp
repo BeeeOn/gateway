BeeeOn/gateway

commit 92ba583493867a64ceb643048d24bbf0a845f47e

ZWaveDriverEvent: new class
The class enables to create statistics from ZWave network for USB driver
using OpenZWave library.

Statistics includes counters for ACK, NAK, CAN received bytes,
broadcasts sent/read etc.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/zwave/ZWaveDriverEvent.cpp
@@ -1,0 +1,133 @@
+#include "zwave/ZWaveDriverEvent.h"
+
+using namespace BeeeOn;
+
+ZWaveDriverEvent::ZWaveDriverEvent(const OpenZWave::Driver::DriverData &data):
+	m_SOFCnt(data.m_SOFCnt),
+	m_ACKWaiting(data.m_ACKWaiting),
+	m_readAborts(data.m_readAborts),
+	m_badChecksum(data.m_badChecksum),
+	m_readCnt(data.m_readCnt),
+	m_writeCnt(data.m_writeCnt),
+	m_CANCnt(data.m_CANCnt),
+	m_NAKCnt(data.m_NAKCnt),
+	m_ACKCnt(data.m_ACKCnt),
+	m_OOFCnt(data.m_OOFCnt),
+	m_dropped(data.m_dropped),
+	m_retries(data.m_retries),
+	m_callbacks(data.m_callbacks),
+	m_badroutes(data.m_badroutes),
+	m_noACK(data.m_noack),
+	m_netbusy(data.m_netbusy),
+	m_notidle(data.m_notidle),
+	m_nondelivery(data.m_nondelivery),
+	m_routedbusy(data.m_routedbusy),
+	m_broadcastReadCnt(data.m_broadcastReadCnt),
+	m_broadcastWriteCnt(data.m_broadcastWriteCnt)
+{
+}
+
+uint32_t ZWaveDriverEvent::SOAFCount() const
+{
+	return m_SOFCnt;
+}
+
+uint32_t ZWaveDriverEvent::ACKWaiting() const
+{
+	return m_ACKWaiting;
+}
+
+uint32_t ZWaveDriverEvent::readAborts() const
+{
+	return m_readAborts;
+}
+
+uint32_t ZWaveDriverEvent::badChecksum() const
+{
+	return m_badChecksum;
+}
+
+uint32_t ZWaveDriverEvent::readCount() const
+{
+	return m_readCnt;
+}
+
+uint32_t ZWaveDriverEvent::writeCount() const
+{
+	return m_writeCnt;
+}
+
+uint32_t ZWaveDriverEvent::CANCount() const
+{
+	return m_CANCnt;
+}
+
+uint32_t ZWaveDriverEvent::NAKCount() const
+{
+	return m_NAKCnt;
+}
+
+uint32_t ZWaveDriverEvent::ACKCount() const
+{
+	return m_ACKCnt;
+}
+
+uint32_t ZWaveDriverEvent::OOFCount() const
+{
+	return m_OOFCnt;
+}
+
+uint32_t ZWaveDriverEvent::dropped() const
+{
+	return m_dropped;
+}
+
+uint32_t ZWaveDriverEvent::retries() const
+{
+	return m_retries;
+}
+
+uint32_t ZWaveDriverEvent::callbacks() const
+{
+	return m_callbacks;
+}
+
+uint32_t ZWaveDriverEvent::badroutes() const
+{
+	return m_badroutes;
+}
+
+uint32_t ZWaveDriverEvent::noACK() const
+{
+	return m_noACK;
+}
+
+uint32_t ZWaveDriverEvent::netBusy() const
+{
+	return m_netbusy;
+}
+
+uint32_t ZWaveDriverEvent::notIdle() const
+{
+	return m_notidle;
+}
+
+uint32_t ZWaveDriverEvent::nonDelivery() const
+{
+	return m_nondelivery;
+}
+
+uint32_t ZWaveDriverEvent::routedBusy() const
+{
+	return m_routedbusy;
+}
+
+uint32_t ZWaveDriverEvent::broadcastReadCount() const
+{
+	return m_broadcastReadCnt;
+}
+
+uint32_t ZWaveDriverEvent::broadcastWriteCount() const
+{
+	return m_broadcastWriteCnt;
+}

src/zwave/ZWaveDriverEvent.h
@@ -1,0 +1,68 @@
+#pragma once
+
+#include <cstdint>
+
+#include <Driver.h>
+
+namespace BeeeOn {
+
+/**
+ * Statistics from ZWave network from USB driver.
+ *
+ * @see https://github.com/OpenZWave/open-zwave/blob/master/cpp/src/Driver.h (struct DriverData)
+ * @see http://www.openzwave.com/dev/classOpenZWave_1_1Manager.html (GetDriverStatistics())
+ */
+class ZWaveDriverEvent {
+public:
+	/**
+	 * Creates statistics data from node.
+	 */
+	ZWaveDriverEvent(const OpenZWave::Driver::DriverData &data);
+
+	uint32_t SOAFCount() const;
+	uint32_t ACKWaiting() const;
+	uint32_t readAborts() const;
+	uint32_t badChecksum() const;
+	uint32_t readCount() const;
+	uint32_t writeCount() const;
+	uint32_t CANCount() const;
+	uint32_t NAKCount() const;
+	uint32_t ACKCount() const;
+	uint32_t OOFCount() const;
+	uint32_t dropped() const;
+	uint32_t retries() const;
+	uint32_t callbacks() const;
+	uint32_t badroutes() const;
+	uint32_t noACK() const;
+	uint32_t netBusy() const;
+	uint32_t notIdle() const;
+	uint32_t nonDelivery() const;
+	uint32_t routedBusy() const;
+	uint32_t broadcastReadCount() const;
+	uint32_t broadcastWriteCount() const;
+
+private:
+	uint32_t m_SOFCnt;            // Number of SOF bytes received
+	uint32_t m_ACKWaiting;        // Number of unsolicited messages while waiting for an ACK
+	uint32_t m_readAborts;        // Number of times read were aborted due to timeouts
+	uint32_t m_badChecksum;       // Number of bad checksums
+	uint32_t m_readCnt;           // Number of messages successfully read
+	uint32_t m_writeCnt;          // Number of messages successfully sent
+	uint32_t m_CANCnt;            // Number of CAN bytes received
+	uint32_t m_NAKCnt;            // Number of NAK bytes received
+	uint32_t m_ACKCnt;            // Number of ACK bytes received
+	uint32_t m_OOFCnt;            // Number of bytes out of framing
+	uint32_t m_dropped;           // Number of messages dropped & not delivered
+	uint32_t m_retries;           // Number of retransmitted messages
+	uint32_t m_callbacks;         // Number of unexpected callbacks
+	uint32_t m_badroutes;         // Number of failed messages due to bad route response
+	uint32_t m_noACK;             // Number of no ACK returned errors
+	uint32_t m_netbusy;           // Number of network busy/failure messages
+	uint32_t m_notidle;           // Number of not idle messages
+	uint32_t m_nondelivery;       // Number of messages not delivered to network
+	uint32_t m_routedbusy;        // Number of messages received with routed busy status
+	uint32_t m_broadcastReadCnt;  // Number of broadcasts read
+	uint32_t m_broadcastWriteCnt; // Number of broadcasts sent
+};
+
+}

src/CMakeLists.txt
@@ -139,6 +139,7 @@ if(ENABLE_ZWAVE)
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveDeviceInfo.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveDeviceManager.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveDriver.cpp
+		${PROJECT_SOURCE_DIR}/zwave/ZWaveDriverEvent.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveNodeEvent.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWaveNodeInfo.cpp
 		${PROJECT_SOURCE_DIR}/zwave/ZWavePocoLoggerAdapter.cpp

commit e183b3fe055d2481aa5e7d1916eca0aaeab1ffe1

ZWaveDriverEvent: deprecate SOAFCount() on behalf of SOFCount()
Fixes: 92ba58349386 ("ZWaveDriverEvent: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/zwave/ZWaveDriverEvent.cpp
@@ -19,7 +19,7 @@ uint32_t ZWaveDriverEvent::lookup(const string &key) const
 	return it->second;
 }
 
-uint32_t ZWaveDriverEvent::SOAFCount() const
+uint32_t ZWaveDriverEvent::SOFCount() const
 {
 	return lookup("SOFCnt");
 }

src/zwave/ZWaveDriverEvent.h
@@ -18,7 +18,13 @@ public:
 	 */
 	ZWaveDriverEvent(const std::map<std::string, uint32_t> &stats);
 
-	uint32_t SOAFCount() const;
+	[[deprecated("use SOFCount instead")]]
+	uint32_t SOAFCount() const
+	{
+		return SOFCount();
+	}
+
+	uint32_t SOFCount() const;
 	uint32_t ACKWaiting() const;
 	uint32_t readAborts() const;
 	uint32_t badChecksum() const;