BeeeOn/gateway

commit fe5c28e33d8482181af587877098523f55ad18cc

JablotronGadget: new class
JablotronGadget and JablotronGadget::Info represent the Jablotron
devices and their properties.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronGadget.cpp
@@ -1,0 +1,258 @@
+#include <vector>
+
+#include <Poco/Exception.h>
+#include <Poco/NumberFormatter.h>
+
+#include "jablotron/JablotronGadget.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+static const Timespan REFRESH_TIME_9MIN = 9 * Timespan::MINUTES;
+static const Timespan REFRESH_TIME_NONE = -1;
+
+static const uint32_t RC86K_FIRST = 0x800000;
+static const uint32_t RC86K_LAST  = 0x87ffff;
+static const uint32_t RC86K_DIFF  = 0x100000;
+
+static const uint32_t RC86K_SECONDARY_FIRST = RC86K_FIRST + RC86K_DIFF;
+static const uint32_t RC86K_SECONDARY_LAST  = RC86K_LAST + RC86K_DIFF;
+
+const vector<JablotronGadget::Info> JablotronGadget::GADGETS = {
+	{0xcf0000, 0xcfffff,   AC88, REFRESH_TIME_NONE, {
+		{ModuleType::Type::TYPE_ON_OFF},
+	}},
+	{0x580000, 0x59ffff,  JA80L, REFRESH_TIME_NONE, {
+		{ModuleType::Type::TYPE_ON_OFF},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+	}},
+	{0x180000, 0x1bffff,  JA81M, REFRESH_TIME_9MIN, {
+		{ModuleType::Type::TYPE_OPEN_CLOSE},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{0x7f0000, 0x7fffff, JA82SH, REFRESH_TIME_9MIN, {
+		{ModuleType::Type::TYPE_SHAKE},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{0x1c0000, 0x1dffff,  JA83M, REFRESH_TIME_9MIN, {
+		{ModuleType::Type::TYPE_OPEN_CLOSE},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{0x640000, 0x65ffff,  JA83P, REFRESH_TIME_9MIN, {
+		{ModuleType::Type::TYPE_MOTION},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{0x760000, 0x76ffff, JA85ST, REFRESH_TIME_9MIN, {
+		{ModuleType::Type::TYPE_FIRE},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{RC86K_FIRST, RC86K_LAST, RC86K,  REFRESH_TIME_NONE, {
+		{ModuleType::Type::TYPE_OPEN_CLOSE},
+		{ModuleType::Type::TYPE_OPEN_CLOSE},
+		{ModuleType::Type::TYPE_SECURITY_ALERT},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+	{0x240000, 0x25ffff, TP82N,  REFRESH_TIME_NONE, {
+		{ModuleType::Type::TYPE_TEMPERATURE, {
+			ModuleType::Attribute::ATTR_INNER,
+			ModuleType::Attribute::ATTR_MANUAL_ONLY,
+			ModuleType::Attribute::ATTR_CONTROLLABLE,
+		}},
+		{ModuleType::Type::TYPE_TEMPERATURE, {
+			ModuleType::Attribute::ATTR_INNER
+		}},
+		{ModuleType::Type::TYPE_BATTERY},
+	}},
+};
+
+JablotronGadget::Info::operator bool() const
+{
+	return firstAddress < lastAddress;
+}
+
+bool JablotronGadget::Info::operator !() const
+{
+	return !(firstAddress < lastAddress);
+}
+
+string JablotronGadget::Info::name() const
+{
+	switch (type) {
+	case AC88:
+		return "AC-88 (sensor)"; // the " (sensor)" is to handle incompatibility
+	case JA80L:
+		return "JA-80L";
+	case JA81M:
+		return "JA-81M";
+	case JA82SH:
+		return "JA-82SH";
+	case JA83M:
+		return "JA-83M";
+	case JA83P:
+		return "JA-83P";
+	case JA85ST:
+		return "JA-85ST";
+	case RC86K:
+		return "RC-86K (dual)";
+	case TP82N:
+		return "TP-82N";
+	default:
+		throw InvalidArgumentException(
+			"unrecognized Jablotron device type: "
+			+ to_string(type));
+	}
+}
+
+JablotronGadget::Info JablotronGadget::Info::resolve(
+		const uint32_t address)
+{
+	const auto primary = primaryAddress(address);
+
+	for (const auto &gadget : GADGETS) {
+		if (primary < gadget.firstAddress || gadget.lastAddress < primary)
+			continue;
+
+		return gadget;
+	}
+
+	static const Info invalid = {0, 0, NONE, -1, {}};
+	return invalid;
+}
+
+vector<SensorValue> JablotronGadget::Info::parse(const JablotronReport &report) const
+{
+	vector<SensorValue> values;
+
+	switch (type) {
+	case JablotronGadget::AC88:
+		values.push_back({0, static_cast<double>(report.get("RELAY"))});
+		break;
+
+	case JablotronGadget::JA80L:
+		if (report.has("BUTTON"))
+			values.push_back({0, 1.0});
+		if (report.has("TAMPER"))
+			values.push_back({1, 1.0});
+
+		values.push_back({2, static_cast<double>(report.get("BLACKOUT"))});
+		break;
+
+	case JablotronGadget::JA81M:
+		if (report.has("SENSOR"))
+			values.push_back({0, static_cast<double>(report.get("ACT"))});
+		if (report.has("TAMPER"))
+			values.push_back({1, static_cast<double>(report.get("ACT"))});
+
+		values.push_back({2, static_cast<double>(report.battery())});
+		break;
+
+	case JablotronGadget::JA83M:
+		if (report.has("SENSOR"))
+			values.push_back({0, static_cast<double>(!report.get("ACT"))});
+		if (report.has("TAMPER"))
+			values.push_back({1, static_cast<double>(report.get("ACT"))});
+
+		values.push_back({2, static_cast<double>(report.battery())});
+		break;
+
+	case JablotronGadget::JA82SH:
+	case JablotronGadget::JA83P:
+	case JablotronGadget::JA85ST:
+		if (report.has("SENSOR"))
+			values.push_back({0, 1.0});
+		if (report.has("TAMPER"))
+			values.push_back({1, static_cast<double>(report.get("ACT"))});
+
+		values.push_back({2, static_cast<double>(report.battery())});
+		break;
+
+	case JablotronGadget::RC86K:
+		if (!report.has("PANIC")) {
+			const bool primary = report.address == primaryAddress(report.address);
+			const ModuleID module = primary? 0 : 1;
+			values.push_back({module, static_cast<double>(report.get("ARM"))});
+		}
+		else {
+			values.push_back({2, 1.0});
+		}
+
+		values.push_back({3, static_cast<double>(report.battery())});
+		break;
+
+	case JablotronGadget::TP82N:
+		if (report.has("INT", true))
+			values.push_back({0, report.temperature("INT")});
+		if (report.has("SET", true))
+			values.push_back({1, report.temperature("SET")});
+
+		values.push_back({2, static_cast<double>(report.battery())});
+		break;
+
+	case JablotronGadget::NONE:
+		poco_assert(false);
+	}
+
+	return values;
+}
+
+uint32_t JablotronGadget::Info::primaryAddress(const uint32_t address)
+{
+	if (RC86K_SECONDARY_FIRST <= address && address <= RC86K_SECONDARY_LAST)
+		return address - RC86K_DIFF;
+
+	return address;
+}
+
+uint32_t JablotronGadget::Info::secondaryAddress(const uint32_t address)
+{
+	if (RC86K_FIRST <= address && address <= RC86K_LAST)
+		return address + RC86K_DIFF;
+
+	return address;
+}
+
+JablotronGadget::JablotronGadget(
+		const unsigned int slot,
+		const uint32_t address,
+		const Info &info):
+	m_slot(slot),
+	m_address(address),
+	m_info(info)
+{
+}
+
+unsigned int JablotronGadget::slot() const
+{
+	return m_slot;
+}
+
+uint32_t JablotronGadget::address() const
+{
+	return m_address;
+}
+
+const JablotronGadget::Info &JablotronGadget::info() const
+{
+	return m_info;
+}
+
+bool JablotronGadget::isSecondary() const
+{
+	return Info::primaryAddress(m_address) != m_address;
+}
+
+string JablotronGadget::toString() const
+{
+	const string name = m_info ? m_info.name() : "<unknown>";
+
+	return "SLOT:" + NumberFormatter::format0(m_slot, 2)
+		+ " [" + NumberFormatter::format0(m_address, 8) + "] "
+		+ name;
+}

src/CMakeLists.txt
@@ -256,6 +256,7 @@ if(ENABLE_JABLOTRON)
 		${PROJECT_SOURCE_DIR}/jablotron/JablotronDeviceOpenClose.cpp
 		${PROJECT_SOURCE_DIR}/jablotron/JablotronDeviceRC86K.cpp
 		${PROJECT_SOURCE_DIR}/jablotron/JablotronDeviceTP82N.cpp
+		${PROJECT_SOURCE_DIR}/jablotron/JablotronGadget.cpp
 		${PROJECT_SOURCE_DIR}/jablotron/JablotronReport.cpp
 	)
 	list(APPEND SOURCES ${JABLOTRON_SOURCES})

src/jablotron/JablotronGadget.h
@@ -1,0 +1,99 @@
+#pragma once
+
+#include <list>
+#include <string>
+#include <vector>
+
+#include <Poco/Timespan.h>
+
+#include "model/ModuleType.h"
+#include "model/SensorValue.h"
+#include "jablotron/JablotronReport.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Representation of a real Jablotron Gadget registered
+ * inside the associated Turris Dongle.
+ */
+class JablotronGadget {
+public:
+	enum Type {
+		NONE = -1,
+		AC88,
+		JA80L,
+		JA81M,
+		JA82SH,
+		JA83M,
+		JA83P,
+		JA85ST,
+		RC86K,
+		TP82N,
+	};
+
+	/**
+	 * @brief Information about a Jablotron Gadget device type.
+	 * Gadget types are distinguished by their address. A certain
+	 * address range denotes a gadget device type.
+	 */
+	struct Info {
+		const uint32_t firstAddress;
+		const uint32_t lastAddress;
+		const Type type;
+		const Poco::Timespan refreshTime;
+		const std::list<ModuleType> modules;
+
+		operator bool() const;
+		bool operator !() const;
+		std::string name() const;
+
+		/**
+		 * @brief Parses the data payload of the given report and converts
+		 * it into BeeeOn-specific data format.
+		 */
+		std::vector<SensorValue> parse(const JablotronReport &report) const;
+
+		/**
+		 * @returns gadget info instance based on the given address
+		 */
+		static Info resolve(const uint32_t address);
+
+		/**
+		 * @returns primary address of the gadget which is usually the
+		 * address itself unless it is an address of RC-86K gadget
+		 */
+		static uint32_t primaryAddress(const uint32_t address);
+
+		/**
+		 * @returns secondary address of the gadget.
+		 * @throws NotFoundException when the address is not of type RC-86K
+		 */
+		static uint32_t secondaryAddress(const uint32_t address);
+	};
+
+	JablotronGadget(
+		const unsigned int slot,
+		const uint32_t address,
+		const Info &info);
+
+	unsigned int slot() const;
+	uint32_t address() const;
+	const Info &info() const;
+
+	/**
+	 * @returns true if the gadget represents a secondary part which
+	 * applies especially to RC-86K.
+	 */
+	bool isSecondary() const;
+
+	std::string toString() const;
+
+private:
+	const unsigned int m_slot;
+	const uint32_t m_address;
+	const Info m_info;
+
+	static const std::vector<Info> GADGETS;
+};
+
+}

test/CMakeLists.txt
@@ -84,6 +84,7 @@ endif()
 
 if(ENABLE_JABLOTRON)
 	file(GLOB JABLOTRON_TEST_SOURCES
+		${PROJECT_SOURCE_DIR}/jablotron/JablotronGadgetTest.cpp
 		${PROJECT_SOURCE_DIR}/jablotron/JablotronReportTest.cpp
 	)
 	list(APPEND TEST_SOURCES ${JABLOTRON_TEST_SOURCES})

test/jablotron/JablotronGadgetTest.cpp
@@ -1,0 +1,137 @@
+#include <string>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Exception.h>
+
+#include "cppunit/BetterAssert.h"
+#include "jablotron/JablotronGadget.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class JablotronGadgetTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(JablotronGadgetTest);
+	CPPUNIT_TEST(testResolve);
+	CPPUNIT_TEST(testToString);
+	CPPUNIT_TEST(testPrimary);
+	CPPUNIT_TEST(testSecondary);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testResolve();
+	void testToString();
+	void testPrimary();
+	void testSecondary();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(JablotronGadgetTest);
+
+void JablotronGadgetTest::testResolve()
+{
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(13565952));
+	CPPUNIT_ASSERT_EQUAL("AC-88 (sensor)", JablotronGadget::Info::resolve(13565952).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(13631487));
+	CPPUNIT_ASSERT_EQUAL("AC-88 (sensor)", JablotronGadget::Info::resolve(13631487).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(5767168));
+	CPPUNIT_ASSERT_EQUAL("JA-80L", JablotronGadget::Info::resolve(5767168).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(5898239));
+	CPPUNIT_ASSERT_EQUAL("JA-80L", JablotronGadget::Info::resolve(5898239).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(1572864));
+	CPPUNIT_ASSERT_EQUAL("JA-81M", JablotronGadget::Info::resolve(1572864).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(1835007));
+	CPPUNIT_ASSERT_EQUAL("JA-81M", JablotronGadget::Info::resolve(1835007).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(8323072));
+	CPPUNIT_ASSERT_EQUAL("JA-82SH", JablotronGadget::Info::resolve(8323072).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(8388607));
+	CPPUNIT_ASSERT_EQUAL("JA-82SH", JablotronGadget::Info::resolve(8388607).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(1835008));
+	CPPUNIT_ASSERT_EQUAL("JA-83M", JablotronGadget::Info::resolve(1835008).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(1966079));
+	CPPUNIT_ASSERT_EQUAL("JA-83M", JablotronGadget::Info::resolve(1966079).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(6553600));
+	CPPUNIT_ASSERT_EQUAL("JA-83P", JablotronGadget::Info::resolve(6553600).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(6684671));
+	CPPUNIT_ASSERT_EQUAL("JA-83P", JablotronGadget::Info::resolve(6684671).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(7733248));
+	CPPUNIT_ASSERT_EQUAL("JA-85ST", JablotronGadget::Info::resolve(7733248).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(7798783));
+	CPPUNIT_ASSERT_EQUAL("JA-85ST", JablotronGadget::Info::resolve(7798783).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(8388608));
+	CPPUNIT_ASSERT_EQUAL("RC-86K (dual)", JablotronGadget::Info::resolve(8388608).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(8912895));
+	CPPUNIT_ASSERT_EQUAL("RC-86K (dual)", JablotronGadget::Info::resolve(8912895).name());
+
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(2359296));
+	CPPUNIT_ASSERT_EQUAL("TP-82N", JablotronGadget::Info::resolve(2359296).name());
+	CPPUNIT_ASSERT(JablotronGadget::Info::resolve(2490367));
+	CPPUNIT_ASSERT_EQUAL("TP-82N", JablotronGadget::Info::resolve(2490367).name());
+}
+
+void JablotronGadgetTest::testToString()
+{
+	JablotronGadget ac88   = {0, 13565952, JablotronGadget::Info::resolve(13565952)};
+	JablotronGadget ja80l  = {1,  5767168, JablotronGadget::Info::resolve(5767168)};
+	JablotronGadget ja81m  = {2,  1572864, JablotronGadget::Info::resolve(1572864)};
+	JablotronGadget ja82sh = {3,  8323072, JablotronGadget::Info::resolve(8323072)};
+	JablotronGadget ja83m  = {4,  1835008, JablotronGadget::Info::resolve(1835008)};
+	JablotronGadget ja83p  = {5,  6553600, JablotronGadget::Info::resolve(6553600)};
+	JablotronGadget ja85st = {6,  7733248, JablotronGadget::Info::resolve(7733248)};
+	JablotronGadget rc86k  = {7,  8388608, JablotronGadget::Info::resolve(8388608)};
+	JablotronGadget tp82n  = {8,  2359296, JablotronGadget::Info::resolve(2359296)};
+
+	CPPUNIT_ASSERT_EQUAL("SLOT:00 [13565952] AC-88 (sensor)", ac88.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:01 [05767168] JA-80L",  ja80l.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:02 [01572864] JA-81M",  ja81m.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:03 [08323072] JA-82SH", ja82sh.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:04 [01835008] JA-83M",  ja83m.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:05 [06553600] JA-83P",  ja83p.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:06 [07733248] JA-85ST", ja85st.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:07 [08388608] RC-86K (dual)",  rc86k.toString());
+	CPPUNIT_ASSERT_EQUAL("SLOT:08 [02359296] TP-82N",  tp82n.toString());
+}
+
+void JablotronGadgetTest::testPrimary()
+{
+	CPPUNIT_ASSERT_EQUAL(       0, JablotronGadget::Info::primaryAddress(0));
+	CPPUNIT_ASSERT_EQUAL(13565952, JablotronGadget::Info::primaryAddress(13565952));
+	CPPUNIT_ASSERT_EQUAL( 5767168, JablotronGadget::Info::primaryAddress(5767168));
+	CPPUNIT_ASSERT_EQUAL( 1572864, JablotronGadget::Info::primaryAddress(1572864));
+	CPPUNIT_ASSERT_EQUAL( 8323072, JablotronGadget::Info::primaryAddress(8323072));
+	CPPUNIT_ASSERT_EQUAL( 1835008, JablotronGadget::Info::primaryAddress(1835008));
+	CPPUNIT_ASSERT_EQUAL( 6553600, JablotronGadget::Info::primaryAddress(6553600));
+	CPPUNIT_ASSERT_EQUAL( 7733248, JablotronGadget::Info::primaryAddress(7733248));
+	CPPUNIT_ASSERT_EQUAL( 2359296, JablotronGadget::Info::primaryAddress(2359296));
+
+	CPPUNIT_ASSERT_EQUAL( 8388608, JablotronGadget::Info::primaryAddress(8388608));
+	CPPUNIT_ASSERT_EQUAL( 8912895, JablotronGadget::Info::primaryAddress(8912895));
+	CPPUNIT_ASSERT_EQUAL( 8388608, JablotronGadget::Info::primaryAddress(9437184));
+	CPPUNIT_ASSERT_EQUAL( 8912895, JablotronGadget::Info::primaryAddress(9961471));
+}
+
+void JablotronGadgetTest::testSecondary()
+{
+	CPPUNIT_ASSERT_EQUAL(       0, JablotronGadget::Info::secondaryAddress(0));
+	CPPUNIT_ASSERT_EQUAL(13565952, JablotronGadget::Info::secondaryAddress(13565952));
+	CPPUNIT_ASSERT_EQUAL( 5767168, JablotronGadget::Info::secondaryAddress(5767168));
+	CPPUNIT_ASSERT_EQUAL( 1572864, JablotronGadget::Info::secondaryAddress(1572864));
+	CPPUNIT_ASSERT_EQUAL( 8323072, JablotronGadget::Info::secondaryAddress(8323072));
+	CPPUNIT_ASSERT_EQUAL( 1835008, JablotronGadget::Info::secondaryAddress(1835008));
+	CPPUNIT_ASSERT_EQUAL( 6553600, JablotronGadget::Info::secondaryAddress(6553600));
+	CPPUNIT_ASSERT_EQUAL( 7733248, JablotronGadget::Info::secondaryAddress(7733248));
+	CPPUNIT_ASSERT_EQUAL( 2359296, JablotronGadget::Info::secondaryAddress(2359296));
+
+	CPPUNIT_ASSERT_EQUAL( 9437184, JablotronGadget::Info::secondaryAddress(9437184));
+	CPPUNIT_ASSERT_EQUAL( 9961471, JablotronGadget::Info::secondaryAddress(9961471));
+	CPPUNIT_ASSERT_EQUAL( 9437184, JablotronGadget::Info::secondaryAddress(8388608));
+	CPPUNIT_ASSERT_EQUAL( 9961471, JablotronGadget::Info::secondaryAddress(8912895));
+}
+
+}

commit 9535495ca000af8d05f421ceb6f3d385cad38328

JablotronGadget: fix handling of JA-83M
JA-83M reports 1 when disconnected and thus it reports open/close
values compatible with BeeeOn.

Fixes: fe5c28e33d84 ("JablotronGadget: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/jablotron/JablotronGadget.cpp
@@ -155,7 +155,7 @@ vector<SensorValue> JablotronGadget::Info::parse(const JablotronReport &report) 
 
 	case JablotronGadget::JA83M:
 		if (report.has("SENSOR"))
-			values.push_back({0, static_cast<double>(!report.get("ACT"))});
+			values.push_back({0, static_cast<double>(report.get("ACT"))});
 		if (report.has("TAMPER"))
 			values.push_back({1, static_cast<double>(report.get("ACT"))});
 