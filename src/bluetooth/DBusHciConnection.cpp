BeeeOn/gateway

commit 732811c482be34203f6626acee43a62c538fb3a5

DBusHciConnection: new class
The class DBusHciConnection implements the HciConnection so it allows
to send read/write requests to the Bluetooth Low Energy devices.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciConnection.cpp
@@ -1,0 +1,203 @@
+#include <Poco/Event.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "bluetooth/DBusHciConnection.h"
+#include "bluetooth/DBusHciInterface.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+
+static const string GATT_CHARACTERISTIC = "org.bluez.GattCharacteristic1";
+
+/**
+ * Convert the given GError into the appropriate exception and throw it.
+ */
+static void throwErrorIfAny(const GlibPtr<GError> error)
+{
+	if (!error.isNull())
+		throw IOException(error->message);
+}
+
+DBusHciConnection::DBusHciConnection(
+		const string& hciName,
+		GlibPtr<OrgBluezDevice1> device,
+		const Timespan& timeout):
+	m_hciName(hciName),
+	m_device(device),
+	m_timeout(timeout)
+{
+	m_address = MACAddress::parse(::org_bluez_device1_get_address(m_device.raw()), ':');
+	resolveServices();
+}
+
+DBusHciConnection::~DBusHciConnection()
+{
+	disconnect();
+}
+
+vector<unsigned char> DBusHciConnection::read(const UUID& uuid)
+{
+	if (logger().debug()) {
+		logger().debug("sending read request to device " + m_address.toString(':'),
+			__FILE__, __LINE__);
+	}
+
+	GlibPtr<OrgBluezGattCharacteristic1> characteristic = findGATTCharacteristic(uuid);
+	if (characteristic.isNull())
+		throw NotFoundException("no such GATT characteristic " + uuid.toString());
+
+	::g_dbus_proxy_set_default_timeout(G_DBUS_PROXY(characteristic.raw()), m_timeout.totalMilliseconds());
+
+	GlibPtr<GVariant> requiredValue;
+	GVariantBuilder args;
+	::g_variant_builder_init(&args, G_VARIANT_TYPE("a{sv}"));
+	GlibPtr<GError> error;
+	::org_bluez_gatt_characteristic1_call_read_value_sync(
+		characteristic.raw(), ::g_variant_builder_end(&args), &requiredValue, nullptr, &error);
+
+	throwErrorIfAny(error);
+
+	gsize size = 0;
+	const unsigned char* data = reinterpret_cast<const unsigned char*>(
+		::g_variant_get_fixed_array(requiredValue.raw(), &size, sizeof(unsigned char)));
+	vector<unsigned char> result(data, data + size);
+
+	return result;
+}
+
+void DBusHciConnection::write(
+		const UUID& uuid,
+		const vector<unsigned char>& value)
+{
+	if (logger().debug()) {
+		logger().debug("sending write request to device " + m_address.toString(':'),
+			__FILE__, __LINE__);
+	}
+
+	GlibPtr<OrgBluezGattCharacteristic1> characteristic = findGATTCharacteristic(uuid);
+	if (characteristic.isNull())
+		throw NotFoundException("no such GATT characteristic " + uuid.toString());
+
+	::g_dbus_proxy_set_default_timeout(G_DBUS_PROXY(characteristic.raw()), m_timeout.totalMilliseconds());
+
+	GVariant* data = ::g_variant_new_from_data(
+		G_VARIANT_TYPE("ay"), &value[0], value.size(), true, nullptr, nullptr);
+	GVariantBuilder args;
+	::g_variant_builder_init(&args, G_VARIANT_TYPE("a{sv}"));
+	GlibPtr<GError> error;
+	::org_bluez_gatt_characteristic1_call_write_value_sync(
+		characteristic.raw(), data, ::g_variant_builder_end(&args), nullptr, &error);
+
+	throwErrorIfAny(error);
+}
+
+void DBusHciConnection::resolveServices()
+{
+	if (logger().debug()) {
+		logger().debug("resolving the services of device " + m_address.toString(':'),
+			__FILE__, __LINE__);
+	}
+
+	// services are already loaded
+	if (::org_bluez_device1_get_services_resolved(m_device.raw()))
+		return;
+
+	Event resolved;
+
+	uint32_t handle = ::g_signal_connect(
+		m_device.raw(),
+		"g-properties-changed",
+		G_CALLBACK(onDeviceServicesResolved),
+		&resolved);
+
+	resolved.tryWait(m_timeout.totalMilliseconds());
+
+	::g_signal_handler_disconnect(m_device.raw(), handle);
+
+	if (!::org_bluez_device1_get_services_resolved(m_device.raw()))
+		throw TimeoutException("resolving of services failed");
+}
+
+gboolean DBusHciConnection::onDeviceServicesResolved(
+		OrgBluezDevice1*,
+		GVariant* properties,
+		const gchar* const*,
+		gpointer userData)
+{
+	if (::g_variant_n_children(properties) == 0)
+		return true;
+
+	Event &resolved = *(reinterpret_cast<Event*>(userData));
+	GVariantIter* iter;
+	const char* key;
+	GVariant* value;
+
+	::g_variant_get(properties, "a{sv}", &iter);
+	while (::g_variant_iter_loop(iter, "{&sv}", &key, &value)) {
+		if (string(key) == "ServicesResolved") {
+			resolved.set();
+
+			break;
+		}
+	}
+
+	return true;
+}
+
+void DBusHciConnection::disconnect()
+{
+	if (logger().debug()) {
+		logger().debug("disconnecting device " + m_address.toString(':'),
+			__FILE__, __LINE__);
+	}
+
+	::org_bluez_device1_call_disconnect_sync(m_device.raw(), nullptr, nullptr);
+}
+
+GlibPtr<OrgBluezGattCharacteristic1> DBusHciConnection::findGATTCharacteristic(
+		const UUID& uuid)
+{
+	GlibPtr<OrgBluezGattCharacteristic1> characteristic;
+
+	GlibPtr<GDBusObjectManager> objectManager = DBusHciInterface::createBluezObjectManager();
+	function<bool(const string& path)> pathFilter =
+		[&](const string& path)-> bool {
+			if (path.find(m_hciName + "/dev_" + m_address.toString('_')) == string::npos)
+				return true;
+			else
+				return false;
+		};
+
+	for (auto path : DBusHciInterface::retrievePathsOfBluezObjects(objectManager, pathFilter, GATT_CHARACTERISTIC)) {
+		try {
+			characteristic = retrieveBluezGATTCharacteristic(path);
+		}
+		BEEEON_CATCH_CHAIN_ACTION(logger(),
+			continue);
+
+		const string charUUID = ::org_bluez_gatt_characteristic1_get_uuid(characteristic.raw());
+		if (charUUID == uuid.toString())
+			return characteristic;
+	}
+
+	return nullptr;
+}
+
+GlibPtr<OrgBluezGattCharacteristic1> DBusHciConnection::retrieveBluezGATTCharacteristic(
+	const string& path)
+{
+	GlibPtr<GError> error;
+
+	GlibPtr<OrgBluezGattCharacteristic1> characteristic = ::org_bluez_gatt_characteristic1_proxy_new_for_bus_sync(
+		G_BUS_TYPE_SYSTEM,
+		G_DBUS_PROXY_FLAGS_NONE,
+		"org.bluez",
+		path.c_str(),
+		nullptr,
+		&error);
+
+	throwErrorIfAny(error);
+	return characteristic;
+}

conf/config.d/logging-loggers.ini
@@ -20,6 +20,9 @@ loggers.BluezHciInterface.level = debug
 loggers.DBusHciInterface.name = BeeeOn::DBusHciInterface
 loggers.DBusHciInterface.level = debug
 
+loggers.DBusHciConnection.name = BeeeOn::DBusHciConnection
+loggers.DBusHciConnection.level = debug
+
 loggers.DependencyInjector.name = BeeeOn::DependencyInjector
 loggers.DependencyInjector.level = information
 

src/CMakeLists.txt
@@ -171,6 +171,7 @@ endif()
 if(ENABLE_BLUETOOTH)
 	file(GLOB BLUETOOTH_SOURCES
 		${PROJECT_SOURCE_DIR}/bluetooth/BluezHciInterface.cpp
+		${PROJECT_SOURCE_DIR}/bluetooth/DBusHciConnection.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/DBusHciInterface.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/HciConnection.cpp
 		${PROJECT_SOURCE_DIR}/bluetooth/HciInfo.cpp

src/bluetooth/DBusHciConnection.h
@@ -1,0 +1,89 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+#include <Poco/UUID.h>
+
+#include "bluetooth/GlibPtr.h"
+#include "bluetooth/HciConnection.h"
+#include "bluetooth/org-bluez-device1.h"
+#include "bluetooth/org-bluez-gattcharacteristic1.h"
+#include "net/MACAddress.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The class represents connection with Bluetooth Low energy device.
+ * It allows sending read/write requests.
+ */
+class DBusHciConnection:
+	public HciConnection,
+	Loggable {
+public:
+	typedef Poco::SharedPtr<DBusHciConnection> Ptr;
+
+	DBusHciConnection(
+		const std::string& hciName,
+		GlibPtr<OrgBluezDevice1> device,
+		const Poco::Timespan& timeout);
+	~DBusHciConnection();
+
+	/**
+	 * @brief Reads value from the GATT characteristic defined
+	 * by UUID.
+	 */
+	std::vector<unsigned char> read(const Poco::UUID& uuid) override;
+
+	/**
+	 * @brief Writes value to the GATT characteristic defined
+	 * by UUID.
+	 */
+	void write(
+		const Poco::UUID& uuid,
+		const std::vector<unsigned char>& value) override;
+
+protected:
+	static gboolean onDeviceServicesResolved(
+		OrgBluezDevice1* device,
+		GVariant* properties,
+		const gchar* const* invalidatedProperties,
+		gpointer userData);
+
+private:
+	/**
+	 * @brief Disconnects device.
+	 */
+	void disconnect();
+
+	/**
+	 * @brief Resolves the services of device.
+	 * @throws TimeoutException in case of a failure
+	 */
+	void resolveServices();
+
+	/**
+	 * @brief Tries to find GATT characteristic defined by UUID for
+	 * the given device. If the characteristic is not found it returns nullptr.
+	 */
+	GlibPtr<OrgBluezGattCharacteristic1> findGATTCharacteristic(
+		const Poco::UUID& uuid);
+
+	/**
+	 * @brief Returns DBus object of GATT characteristic defined by it's path.
+	 * @throws IOException in case of a failure
+	 */
+	static GlibPtr<OrgBluezGattCharacteristic1> retrieveBluezGATTCharacteristic(
+		const std::string& path);
+
+private:
+	std::string m_hciName;
+	GlibPtr<OrgBluezDevice1> m_device;
+	MACAddress m_address;
+	Poco::Timespan m_timeout;
+};
+
+}

src/bluetooth/DBusHciInterface.h
@@ -43,6 +43,8 @@ namespace BeeeOn {
  */
 class DBusHciInterface : public HciInterface, Loggable {
 public:
+	friend class DBusHciConnection;
+
 	typedef Poco::SharedPtr<DBusHciInterface> Ptr;
 	typedef std::function<bool(const std::string& path)> PathFilter;
 

commit 8e99c46e3de9ae4b88e5c3343dfe35b559efe29e

DBusHciConnection: fix constructor
The resolving of services in constructor is put in the try catch block.
When the resolving of services fails (Exception is thrown) so in the catch
block is called method disconnect(). It is needed because it caused that
the connection does not close.

Fixes: 732811c48 ("DBusHciConnection: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciConnection.cpp
@@ -29,7 +29,14 @@ DBusHciConnection::DBusHciConnection(
 	m_timeout(timeout)
 {
 	m_address = MACAddress::parse(::org_bluez_device1_get_address(m_device.raw()), ':');
-	resolveServices();
+
+	try {
+		resolveServices();
+	}
+	catch (Exception &e) {
+		disconnect();
+		e.rethrow();
+	}
 }
 
 DBusHciConnection::~DBusHciConnection()