BeeeOn/gateway

commit 1a5ffbf2098435bf8df09a936aa9b40705725cd4

DBusHciInterface: extract methods startDiscovery(), stopDiscovery()
Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -103,10 +103,7 @@ map<MACAddress, string> DBusHciInterface::lescan(const Timespan& timeout) const
 		G_CALLBACK(onDBusObjectAdded),
 		&allDevices);
 
-	initDiscoveryFilter(adapter, "le");
-
-	GlibPtr<GError> error;
-	::org_bluez_adapter1_call_start_discovery_sync(adapter.raw(), nullptr, &error);
+	startDiscovery(adapter, "le");
 
 	GlibPtr<GMainLoop> loop = ::g_main_loop_new(nullptr, false);
 	::g_timeout_add_seconds(timeout.seconds(), onStopLoop, loop.raw());
@@ -128,7 +125,7 @@ map<MACAddress, string> DBusHciInterface::lescan(const Timespan& timeout) const
 	logger().information("BLE scan has finished, found " +
 		to_string(foundDevices.size()) + " device(s)", __FILE__, __LINE__);
 
-	::org_bluez_adapter1_call_stop_discovery_sync(adapter.raw(), nullptr, nullptr);
+	stopDiscovery(adapter);
 
 	return foundDevices;
 }
@@ -150,6 +147,23 @@ void DBusHciInterface::waitUntilPoweredChange(const string& path, const bool pow
 	}
 
 	throw TimeoutException("failed to change power of interface" + m_name);
+}
+
+void DBusHciInterface::startDiscovery(
+		GlibPtr<OrgBluezAdapter1> adapter,
+		const std::string& trasport) const
+{
+	ScopedLock<FastMutex> guard(m_discoveringMutex);
+
+	initDiscoveryFilter(adapter, trasport);
+	::org_bluez_adapter1_call_start_discovery_sync(adapter.raw(), nullptr, nullptr);
+}
+
+void DBusHciInterface::stopDiscovery(GlibPtr<OrgBluezAdapter1> adapter) const
+{
+	ScopedLock<FastMutex> guard(m_discoveringMutex);
+
+	::org_bluez_adapter1_call_stop_discovery_sync(adapter.raw(), nullptr, nullptr);
 }
 
 void DBusHciInterface::initDiscoveryFilter(

src/bluetooth/DBusHciInterface.h
@@ -102,6 +102,20 @@ private:
 	void waitUntilPoweredChange(const std::string& path, const bool powered) const;
 
 	/**
+	 * @brief Switch on the discovery on the adapter.
+	 * @param trasnport type of device to be searched for (auto, bredr, le)
+	 * @throws IOException in case of failure
+	 */
+	void startDiscovery(
+		GlibPtr<OrgBluezAdapter1> adapter,
+		const std::string& trasport) const;
+
+	/**
+	 * @brief Switch off the discovery on the adapter
+	 */
+	void stopDiscovery(GlibPtr<OrgBluezAdapter1> adapter) const;
+
+	/**
 	 * @brief Sets discovery to search given type of bluetooth devices.
 	 * Possible types of devices are auto, bredr (classic) and le.
 	 * @throws IOException in case of a failure
@@ -160,6 +174,7 @@ private:
 	std::string m_name;
 	mutable Poco::Condition m_condition;
 	mutable Poco::FastMutex m_statusMutex;
+	mutable Poco::FastMutex m_discoveringMutex;
 };
 
 class DBusHciInterfaceManager : public HciInterfaceManager {

commit 78b7e021ed50b4f86774c1ceba4560f1df2351f9

DBusHciInterface: verify start discovery
The startDiscovery() method verifies whether the discovery
has actually started. Starting of discovery is verified based
on GError returned by the method to start discovery.

Fixes: 1a5ffbf20 ("DBusHciInterface: extract methods startDiscovery(), stopDiscovery()")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -247,8 +247,10 @@ void DBusHciInterface::startDiscovery(
 	if (::org_bluez_adapter1_get_discovering(adapter.raw()))
 		return;
 
+	GlibPtr<GError> error;
 	initDiscoveryFilter(adapter, trasport);
-	::org_bluez_adapter1_call_start_discovery_sync(adapter.raw(), nullptr, nullptr);
+	::org_bluez_adapter1_call_start_discovery_sync(adapter.raw(), nullptr, &error);
+	throwErrorIfAny(error);
 }
 
 void DBusHciInterface::stopDiscovery(GlibPtr<OrgBluezAdapter1> adapter) const