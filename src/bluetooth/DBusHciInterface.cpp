BeeeOn/gateway

commit c76cbb5f3e059d7c3b935cca9a38c3ddadeaa658

DBusHciInterface: new methods watch(), unwatch()
The DBusHciInterface is extended by new methods watch() and unwatch().
The watch() method is used to register a device to process advertising data.
And the unwatch() method to unregister the device.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -171,6 +171,49 @@ HciConnection::Ptr DBusHciInterface::connect(
 	return new DBusHciConnection(m_name, device, timeout);
 }
 
+void DBusHciInterface::watch(
+		const MACAddress& address,
+		SharedPtr<WatchCallback> callBack)
+{
+	ScopedLock<FastMutex> lock(m_watchMutex);
+
+	auto it = m_watchedDevices.find(address);
+	if (it != m_watchedDevices.end())
+		return;
+
+	if (logger().debug())
+		logger().debug("watch the device " + address.toString(':'), __FILE__, __LINE__);
+
+	GlibPtr<OrgBluezDevice1> device = retrieveBluezDevice(createDevicePath(m_name, address));
+
+	uint64_t handle = ::g_signal_connect(
+		device.raw(),
+		"g-properties-changed",
+		G_CALLBACK(onDeviceManufacturerDataRecieved),
+		callBack.get());
+
+	if (handle == 0)
+		throw IOException("failed to watch device " + address.toString());
+
+	WatchedDevice watchedDevice(device, handle, callBack);
+	m_watchedDevices.emplace(address, watchedDevice);
+}
+
+void DBusHciInterface::unwatch(const MACAddress& address)
+{
+	ScopedLock<FastMutex> lock(m_watchMutex);
+
+	auto it = m_watchedDevices.find(address);
+	if (it == m_watchedDevices.end())
+		return;
+
+	if (logger().debug())
+		logger().debug("unwatch the device " + address.toString(':'), __FILE__, __LINE__);
+
+	::g_signal_handler_disconnect(it->second.device().raw(), it->second.signalHandle());
+	m_watchedDevices.erase(address);
+}
+
 void DBusHciInterface::waitUntilPoweredChange(const string& path, const bool powered) const
 {
 	for (int i = 0; i < CHANGE_POWER_ATTEMPTS; ++i) {
@@ -312,6 +355,53 @@ void DBusHciInterface::onDBusObjectAdded(
 	foundDevices.emplace(mac, name);
 }
 
+gboolean DBusHciInterface::onDeviceManufacturerDataRecieved(
+		OrgBluezDevice1* device,
+		GVariant* properties,
+		const gchar* const*,
+		gpointer userData)
+{
+	if (::g_variant_n_children(properties) == 0)
+		return true;
+
+	GVariantIter* iter;
+	const char* property;
+	GVariant* value;
+
+	::g_variant_get(properties, "a{sv}", &iter);
+	while (::g_variant_iter_loop(iter, "{&sv}", &property, &value)) {
+		if (string(property) == "ManufacturerData") {
+			processManufacturerData(device, value, userData);
+
+			break;
+		}
+	}
+
+	return true;
+}
+
+void DBusHciInterface::processManufacturerData(
+		OrgBluezDevice1* device,
+		GVariant* value,
+		gpointer userData)
+{
+	GVariantIter* iter;
+	GVariant* data;
+	size_t size;
+
+	::g_variant_get(value, "a{qv}", &iter);
+	while (::g_variant_iter_loop(iter, "{qv}", nullptr, &data)) {
+		const unsigned char* tmpData = reinterpret_cast<const unsigned char*>(
+			::g_variant_get_fixed_array(data, &size, sizeof(unsigned char)));
+
+		vector<unsigned char> result(tmpData, tmpData + size);
+		const auto mac = MACAddress::parse(::org_bluez_device1_get_address(device), ':');
+
+		WatchCallback &func = *(reinterpret_cast<WatchCallback*>(userData));
+		func(mac, result);
+	}
+}
+
 const string DBusHciInterface::createAdapterPath(const string& name)
 {
 	return "/org/bluez/" + name;
@@ -368,6 +458,16 @@ GlibPtr<OrgBluezDevice1> DBusHciInterface::retrieveBluezDevice(const string& pat
 
 	throwErrorIfAny(error);
 	return device;
+}
+
+DBusHciInterface::WatchedDevice::WatchedDevice(
+		const GlibPtr<OrgBluezDevice1> device,
+		const uint64_t signalHandle,
+		const Poco::SharedPtr<WatchCallback> callBack):
+	m_device(device),
+	m_signalHandle(signalHandle),
+	m_callBack(callBack)
+{
 }
 
 

src/bluetooth/DBusHciInterface.h
@@ -47,6 +47,40 @@ public:
 
 	typedef Poco::SharedPtr<DBusHciInterface> Ptr;
 	typedef std::function<bool(const std::string& path)> PathFilter;
+	typedef std::function<void(const MACAddress&, std::vector<unsigned char>&)> WatchCallback;
+
+	/**
+	 * @brief The class is used to store necessary data about device, from
+	 * which the advertising data is processed, such as instance of device,
+	 * handle of signal and pointer to callback.
+	 */
+	class WatchedDevice {
+	public:
+		WatchedDevice(
+			const GlibPtr<OrgBluezDevice1> device,
+			const uint64_t signalHandle,
+			const Poco::SharedPtr<WatchCallback> callBack);
+
+		GlibPtr<OrgBluezDevice1> device() const
+		{
+			return m_device;
+		}
+
+		uint64_t signalHandle() const
+		{
+			return m_signalHandle;
+		}
+
+		Poco::SharedPtr<WatchCallback> callBack() const
+		{
+			return m_callBack;
+		}
+
+	private:
+		GlibPtr<OrgBluezDevice1> m_device;
+		uint64_t m_signalHandle;
+		Poco::SharedPtr<WatchCallback> m_callBack;
+	};
 
 	/**
 	 * @param name name of hci
@@ -95,6 +129,11 @@ public:
 		const MACAddress& address,
 		const Poco::Timespan& timeout) const override;
 
+	void watch(
+		const MACAddress& address,
+		Poco::SharedPtr<WatchCallback> callBack);
+	void unwatch(const MACAddress& address);
+
 protected:
 	/**
 	 * @brief Callback handling the timeout event which stops
@@ -109,6 +148,24 @@ protected:
 	static void onDBusObjectAdded(
 		GDBusObjectManager* objectManager,
 		GDBusObject* object,
+		gpointer userData);
+
+	/**
+	 * @brief Callback handling the event of receiving advertising data.
+	 */
+	static gboolean onDeviceManufacturerDataRecieved(
+		OrgBluezDevice1* device,
+		GVariant* properties,
+		const gchar* const* invalidatedProperties,
+		gpointer userData);
+
+	/**
+	 * @brief Process the single advertising data and call callback
+	 * stored in userData.
+	 */
+	static void processManufacturerData(
+		OrgBluezDevice1* device,
+		GVariant* value,
 		gpointer userData);
 
 private:
@@ -204,11 +261,13 @@ private:
 	GlibPtr<GMainLoop> m_loop;
 	Poco::RunnableAdapter<DBusHciInterface> m_loopThread;
 	Poco::Thread m_thread;
+	std::map<MACAddress, WatchedDevice> m_watchedDevices;
 	mutable GlibPtr<OrgBluezAdapter1> m_adapter;
 
 	mutable Poco::Condition m_condition;
 	mutable Poco::FastMutex m_statusMutex;
 	mutable Poco::FastMutex m_discoveringMutex;
+	Poco::FastMutex m_watchMutex;
 };
 
 class DBusHciInterfaceManager : public HciInterfaceManager {

commit 46c59aa23eb65ce5debd8de6faffc6fed4132752

DBusHciInterface: fix unwatch()
To prevent race conditon, the unwatch() method is modified to
first erase (unwatch) the device and than disconnect the signal.
The purpose of this order is to be opposite to the watch() method.

Fixes: c76cbb5f3 ("DBusHciInterface: new methods watch(), unwatch()")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/bluetooth/DBusHciInterface.cpp
@@ -222,8 +222,10 @@ void DBusHciInterface::unwatch(const MACAddress& address)
 	if (logger().debug())
 		logger().debug("unwatch the device " + address.toString(':'), __FILE__, __LINE__);
 
-	::g_signal_handler_disconnect(it->second.device().raw(), it->second.signalHandle());
+	GlibPtr<OrgBluezDevice1> device = it->second.device();
+	const auto handle = it->second.signalHandle();
 	m_watchedDevices.erase(address);
+	::g_signal_handler_disconnect(device.raw(), handle);
 }
 
 void DBusHciInterface::waitUntilPoweredChange(GlibPtr<OrgBluezAdapter1> adapter, const bool powered) const