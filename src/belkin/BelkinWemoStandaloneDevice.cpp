BeeeOn/gateway

commit 1c047bded4bce6feddbb8c77041cf7437624170d

BelkinWemoStandaloneDevice: initialize device ID via initializer list
We avoid to use the empty constructor of BelkinWemoDevice and instead
directly initialize the device ID. All members are now initialized
via the initializer list.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/belkin/BelkinWemoStandaloneDevice.cpp
@@ -21,10 +21,10 @@ using namespace Poco::XML;
 using namespace std;
 
 BelkinWemoStandaloneDevice::BelkinWemoStandaloneDevice(const URI& uri, const Timespan &httpTimeout):
+	BelkinWemoDevice(buildDeviceID()),
 	m_uri(uri),
 	m_httpTimeout(httpTimeout)
 {
-	m_deviceId = buildDeviceID();
 }
 
 MACAddress BelkinWemoStandaloneDevice::requestMacAddr(

commit b569b8189ed719b6db15d52f0c75522e0b2659a0

BelkinWemoStandaloneDevice: fix construction
Method buildDeviceID() required that all attributes of BelkinWemoStandaloneDevice
are initialized but they were not. The buildDeviceID() method is marked static so
that it can be called in the initialization list in constructor.

Fixes: 1c047bded ("BelkinWemoStandaloneDevice: initialize device ID via initializer list")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/belkin/BelkinWemoStandaloneDevice.cpp
@@ -21,7 +21,7 @@ using namespace Poco::XML;
 using namespace std;
 
 BelkinWemoStandaloneDevice::BelkinWemoStandaloneDevice(const URI& uri, const Timespan &httpTimeout):
-	BelkinWemoDevice(buildDeviceID()),
+	BelkinWemoDevice(buildDeviceID(uri, httpTimeout)),
 	m_uri(uri),
 	m_httpTimeout(httpTimeout)
 {
@@ -176,7 +176,9 @@ void BelkinWemoStandaloneDevice::setAddress(const SocketAddress& address)
 	m_uri.setPort(address.port());
 }
 
-DeviceID BelkinWemoStandaloneDevice::buildDeviceID() const
+DeviceID BelkinWemoStandaloneDevice::buildDeviceID(
+		const URI& uri,
+		const Timespan& httpTimeout)
 {
-	return {DevicePrefix::PREFIX_BELKIN_WEMO, requestMacAddr(m_uri, m_httpTimeout)};
+	return {DevicePrefix::PREFIX_BELKIN_WEMO, requestMacAddr(uri, httpTimeout)};
 }

src/belkin/BelkinWemoStandaloneDevice.h
@@ -58,7 +58,9 @@ private:
 	 * @brief Called internally when constructing the instance.
 	 * Creates DeviceID based on Mac address of device.
 	 */
-	DeviceID buildDeviceID() const;
+	static DeviceID buildDeviceID(
+		const Poco::URI& uri,
+		const Poco::Timespan& httpTimeout);
 
 protected:
 	Poco::URI m_uri;