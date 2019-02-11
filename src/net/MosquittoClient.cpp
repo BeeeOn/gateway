BeeeOn/gateway

commit 2daa8443dd7a30b9f3ab5227fec74d390820bd14

MosquittoClient: new class
Client for the communication uses MQTT protocol. MqttClient
runs in a thread that enables to keep connection with server
and reconnect in case of a failure.

Client enables to configure client ID, host, port,
reconnect timeout and connect timeout.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/net/MosquittoClient.cpp
@@ -1,0 +1,259 @@
+#include <Poco/Clock.h>
+#include <Poco/Error.h>
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/Thread.h>
+#include <Poco/Net/NetException.h>
+
+#include "di/Injectable.h"
+#include "net/MosquittoClient.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, MosquittoClient)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_PROPERTY("port", &MosquittoClient::setPort)
+BEEEON_OBJECT_PROPERTY("host", &MosquittoClient::setHost)
+BEEEON_OBJECT_PROPERTY("clientID", &MosquittoClient::setClientID)
+BEEEON_OBJECT_PROPERTY("reconnectTimeout", &MosquittoClient::setReconnectTimeout)
+BEEEON_OBJECT_PROPERTY("subTopics", &MosquittoClient::setSubTopics)
+BEEEON_OBJECT_END(BeeeOn, MosquittoClient)
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace std;
+using namespace mosqpp;
+
+static const string DEFAULT_HOST = "localhost";
+static const int DEFAULT_PORT = 1883;
+static const Timespan RECONNECT_WAIT_TIMEOUT = 5 * Timespan::SECONDS;
+static const int MAXIMUM_MESSAGE_SIZE = 1024;
+
+MosquittoClient::MosquittoClient():
+	m_host(DEFAULT_HOST),
+	m_reconnectTimeout(RECONNECT_WAIT_TIMEOUT),
+	m_port(DEFAULT_PORT),
+	m_stop(false)
+{
+	// Mandatory initialization for mosquitto library
+	mosqpp::lib_init();
+}
+
+MosquittoClient::~MosquittoClient()
+{
+	// never throws
+	disconnect();
+	mosqpp::lib_cleanup();
+}
+
+bool MosquittoClient::initConnection()
+{
+	const string clientID = buildClientID();
+	reinitialise(clientID.c_str(), true);
+
+	try {
+		connect();
+		subscribeToAll();
+		return true;
+	}
+	catch (const Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+		return false;
+	}
+}
+
+string MosquittoClient::buildClientID() const
+{
+	if (m_clientID.empty())
+		throw IllegalStateException("client ID is not set");
+
+	return m_clientID;
+}
+
+void MosquittoClient::publish(const MqttMessage &msg)
+{
+	int res = mosquittopp::publish(
+		NULL,
+		msg.topic().c_str(),
+		msg.message().length(),
+		msg.message().c_str(),
+		msg.qos());
+
+	if (res != MOSQ_ERR_SUCCESS)
+		throwMosquittoError(res);
+}
+
+void MosquittoClient::connect()
+{
+	// non blocking connection to broker request
+	int ret = connect_async(m_host.c_str(), m_port);
+
+	if (ret != MOSQ_ERR_SUCCESS)
+		throwMosquittoError(ret);
+}
+
+MqttMessage MosquittoClient::nextMessage()
+{
+	FastMutex::ScopedLock guard(m_queueMutex);
+	MqttMessage msg;
+
+	if (m_msgQueue.empty())
+		return msg;
+
+	msg = m_msgQueue.front();
+	m_msgQueue.pop();
+
+	return msg;
+}
+
+MqttMessage MosquittoClient::receive(const Timespan &timeout)
+{
+	const Poco::Clock now;
+
+	while (!m_stop) {
+		if (now.isElapsed(timeout.totalMicroseconds()) && timeout > 0)
+			throw TimeoutException("receive timeout expired");
+
+		MqttMessage msg = nextMessage();
+		if (!msg.isEmpty())
+			return msg;
+
+		if (timeout < 0) {
+			m_receiveEvent.wait();
+		}
+		else {
+			Timespan waitTime = timeout.totalMicroseconds() - now.elapsed();
+
+			if (waitTime <= 0)
+				throw TimeoutException("receive timeout expired");
+
+			if (waitTime.totalMilliseconds() < 1)
+				waitTime = 1 * Timespan::MILLISECONDS;
+
+			m_receiveEvent.wait(waitTime.totalMilliseconds());
+		}
+	}
+
+	return {};
+}
+
+void MosquittoClient::on_message(const struct mosquitto_message *message)
+{
+	if (message->payloadlen > MAXIMUM_MESSAGE_SIZE) {
+		throw RangeException(
+			"maximum message size ("
+			+ to_string(MAXIMUM_MESSAGE_SIZE)
+			+ ") was exceeded");
+	}
+
+	FastMutex::ScopedLock guard(m_queueMutex);
+	m_msgQueue.push({
+		message->topic,
+		string(reinterpret_cast<const char *>(message->payload), message->payloadlen)
+	});
+
+	m_receiveEvent.set();
+}
+
+void MosquittoClient::run()
+{
+	while (!m_stop) {
+		if (initConnection())
+			break;
+
+		m_reconnectEvent.tryWait(m_reconnectTimeout.totalMilliseconds());
+	}
+
+	while (!m_stop) {
+		int rc = loop();
+		if (rc != MOSQ_ERR_SUCCESS) {
+			m_reconnectEvent.tryWait(m_reconnectTimeout.totalMilliseconds());
+
+			logger().trace("trying to reconnect", __FILE__, __LINE__);
+			reconnect();
+		}
+	}
+}
+
+void MosquittoClient::stop()
+{
+	m_stop = true;
+	m_receiveEvent.set();
+	m_reconnectEvent.set();
+}
+
+void MosquittoClient::setSubTopics(const list<string> &subTopics)
+{
+	for (const auto &topic : subTopics) {
+		const auto it = m_subTopics.emplace(topic);
+		if (it.second) {
+			logger().warning(
+				"duplicated subscription topic "
+				+ topic,
+				__FILE__, __LINE__);
+		}
+	}
+}
+
+void MosquittoClient::subscribeToAll()
+{
+	for (const auto &topic : m_subTopics) {
+		int ret = mosquittopp::subscribe(NULL, topic.c_str());
+		if (ret != MOSQ_ERR_SUCCESS)
+			throwMosquittoError(ret);
+	}
+}
+
+void MosquittoClient::setPort(int port)
+{
+	if (port < 0 || port > 65535)
+		throw InvalidArgumentException("port is out of range");
+
+	m_port = port;
+}
+
+void MosquittoClient::setHost(const string &host)
+{
+	m_host = host;
+}
+
+void MosquittoClient::setReconnectTimeout(const Timespan &timeout)
+{
+	if (timeout.totalSeconds() <= 0) {
+		throw InvalidArgumentException(
+			"reconnect timeout time must be at least a second");
+	}
+
+	m_reconnectTimeout = timeout;
+}
+
+void MosquittoClient::setClientID(const string &id)
+{
+	m_clientID = id;
+}
+
+string MosquittoClient::clientID() const
+{
+	return m_clientID;
+}
+
+void MosquittoClient::throwMosquittoError(int returnCode) const
+{
+	switch (returnCode) {
+	case MOSQ_ERR_INVAL:
+		throw InvalidArgumentException(Error::getMessage(returnCode));
+	case MOSQ_ERR_NOMEM:
+		throw OutOfMemoryException(Error::getMessage(returnCode));
+	case MOSQ_ERR_NO_CONN:
+		throw IOException(Error::getMessage(returnCode));
+	case MOSQ_ERR_PROTOCOL:
+		throw ProtocolException(Error::getMessage(returnCode));
+	case MOSQ_ERR_PAYLOAD_SIZE:
+		throw ProtocolException(Error::getMessage(returnCode));
+	case MOSQ_ERR_ERRNO:
+		if (errno == ECONNREFUSED)
+			throw Net::ConnectionRefusedException(Error::getMessage(returnCode));
+		else
+			throw SystemException("system call returned an error: " + Error::getMessage(returnCode));
+	default:
+		throw IllegalStateException(Error::getMessage(returnCode));
+	}
+}

CMakeLists.txt
@@ -36,6 +36,7 @@ option(ENABLE_QUEUING_DISTRIBUTOR "Enable support of Queuing Distributor" ON)
 option(ENABLE_TESTING_CENTER "Enable support of Testing Center" ON)
 option(ENABLE_PRESSURE_SENSOR "Enable support of internal air pressure sensor" ON)
 option(ENABLE_FITP "Enable support of FITP" ON)
+option(ENABLE_MOSQUITTO_CLIENT "Enable support of Mosquitto Client" ON)
 
 add_subdirectory(src)
 add_subdirectory(base)

src/CMakeLists.txt
@@ -31,6 +31,10 @@ if(ENABLE_FITP)
 	find_library (FITP_LIB fitp)
 endif()
 
+if (ENABLE_MOSQUITTO_CLIENT)
+	find_library (MOSQUITTO_CPP mosquittopp)
+endif()
+
 file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/bluetooth/BluetoothListener.cpp
 	${PROJECT_SOURCE_DIR}/commands/DeviceAcceptCommand.cpp
@@ -261,6 +265,15 @@ if(ENABLE_FITP)
 	list(APPEND SOURCES ${FITP_SOURCES})
 else()
 	message(STATUS "Fitp support is disabled")
+endif()
+
+if (ENABLE_MOSQUITTO_CLIENT)
+	file(GLOB MOSQUITTO_CLIENT_SOURCES
+		${PROJECT_SOURCE_DIR}/net/MosquittoClient.cpp
+	)
+	list(APPEND SOURCES ${MOSQUITTO_CLIENT_SOURCES})
+else()
+	message(STATUS "Mosquitto Client support is disabled")
 endif()
 
 configure_file(core/Version.cpp.in ${PROJECT_BINARY_DIR}/core/Version.cpp)

src/net/MosquittoClient.h
@@ -1,0 +1,135 @@
+#ifndef BEEEON_MOSQUITTO_CLIENT_H
+#define BEEEON_MOSQUITTO_CLIENT_H
+
+#include <list>
+#include <queue>
+#include <set>
+#include <string>
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include <mosquittopp.h>
+
+#include "loop/StoppableRunnable.h"
+#include "net/MqttMessage.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * Mosquitto client runs in a thread. Before its run
+ * it is necessary to set following parameters:
+ *
+ *  - host: default localhost
+ *  - port: default 1883
+ *  - reconnect wait timeout: 5 s
+ *  - client id: default - empty
+ */
+class MosquittoClient:
+	Loggable,
+	protected mosqpp::mosquittopp,
+	public StoppableRunnable {
+public:
+	typedef Poco::SharedPtr<MosquittoClient> Ptr;
+
+	MosquittoClient();
+	~MosquittoClient();
+
+	/**
+	 * Keeps communication between the client and broker working.
+	 *
+	 * @see: https://mosquitto.org/api/files/mosquitto-h.html#mosquitto_loop
+	 */
+	void run() override;
+	void stop() override;
+
+	void setHost(const std::string &host);
+	void setPort(int port);
+
+	/**
+	 * Subscribe to a topic.
+	 */
+	void setSubTopics(const std::list<std::string> &subTopics);
+
+	/**
+	 * Timeout between reconnecting to the server when server
+	 * connection is lost.
+	 */
+	void setReconnectTimeout(const Poco::Timespan &timeout);
+
+	void setClientID(const std::string &id);
+	std::string clientID() const;
+
+	/**
+	 * Publish a message on a given topic.
+	 */
+	void publish(const MqttMessage &msq);
+
+	/**
+	 * Waiting for a new message according to given timeout.
+	 * Returns message, if some message is in queue, otherwise
+	 * waiting for a new message or timeout exception.
+	 *
+	 * This method should not be called by multiple threads,
+	 * received message could be given to only one thread.
+	 *
+	 * Timeout:
+	 *  - 0 - non-blocking
+	 *  - negative - blocking
+	 *  - positive blocking with timeout
+	 */
+	MqttMessage receive(const Poco::Timespan &timeout);
+
+protected:
+	virtual std::string buildClientID() const;
+
+	/**
+	 * Non-blocking connection to broker request.
+	 * After set client name must be call MosquittoClient::connect();
+	 */
+	void connect();
+
+private:
+	/**
+	 * Setting clientID and connection to the server. After successful connection,
+	 * it is necessary to set m_isConnected variable to true.
+	 */
+	bool initConnection();
+
+	void subscribeToAll();
+
+	void throwMosquittoError(int returnCode) const;
+
+	/**
+	 * Message variable contains data and name of topic. This variable and associated
+	 * memory will be freed by the library after the callback completes.
+	 *
+	 * @see: https://mosquitto.org/api/files/mosquitto-h.html#mosquitto_message_callback_set
+	 */
+	void on_message(const struct mosquitto_message *message) override;
+
+	/**
+	 * Returns message from queue.
+	 */
+	MqttMessage nextMessage();
+
+private:
+	std::string m_clientID;
+	std::string m_host;
+	Poco::Timespan m_reconnectTimeout;
+	int m_port;
+	std::set<std::string> m_subTopics;
+	Poco::AtomicCounter m_stop;
+	Poco::Event m_receiveEvent;
+	Poco::Event m_reconnectEvent;
+	Poco::FastMutex m_queueMutex;
+	std::queue<MqttMessage> m_msgQueue;
+};
+
+}
+
+#endif

commit cbba0e312a8d941edc492cb1a8e656e2556897e3

MosquittoClient: fix logging of duplication of subscribed topic
Fixes: 2daa8443 ("MosquittoClient: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/net/MosquittoClient.cpp
@@ -185,7 +185,7 @@ void MosquittoClient::setSubTopics(const list<string> &subTopics)
 {
 	for (const auto &topic : subTopics) {
 		const auto it = m_subTopics.emplace(topic);
-		if (it.second) {
+		if (!it.second) {
 			logger().warning(
 				"duplicated subscription topic "
 				+ topic,