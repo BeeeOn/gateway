BeeeOn/gateway

commit ed49dfec32b347cd4a4060c15a236dd004332e3c

config: install IQRFDeviceManager
Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

conf/config.d/factory-managers.xml
@@ -13,6 +13,9 @@
 			<add name="runnables" ref="fitpDeviceManager" if-yes="${fitp.enable}" />
 			<add name="runnables" ref="zwaveDeviceManager" if-yes="${zwave.enable}" />
 			<add name="loops" ref="hciInfoReporter" if-yes="${bluetooth.reporting.enable}" />
+			<add name="runnables" ref="iqrfMqttClient" if-yes="${iqrf.enable}" />
+			<add name="runnables" ref="iqrfDeviceManager" if-yes="${iqrf.enable}" />
+			<add name="runnables" ref="iqrfMqttConnector" if-yes="${iqrf.enable}" />
 		</instance>
 
 		<instance name="pressureSensorManager" class="BeeeOn::PressureSensorManager">
@@ -207,6 +210,31 @@
 			<set name="registry" ref="zwaveMapperRegistry" />
 			<set name="commandDispatcher" ref="commandDispatcher" />
 			<set name="distributor" ref="distributor" />
+		</instance>
+
+		<instance name="iqrfMqttClient" class="BeeeOn::GatewayMosquittoClient">
+			<set name="host" text="${iqrf.mqtt.host}" />
+			<set name="port" number="${iqrf.mqtt.port}" />
+			<set name="clientID" text="${iqrf.mqtt.clientID}" />
+			<set name="subTopics" list="${iqrf.subscribeTopics}" />
+			<set name="gatewayInfo" ref="gatewayInfo" />
+		</instance>
+
+		<instance name="iqrfDeviceManager" class="BeeeOn::IQRFDeviceManager">
+			<set name="distributor" ref="distributor" />
+			<set name="commandDispatcher" ref="commandDispatcher" />
+			<set name="mqttConnector" ref="iqrfMqttConnector" />
+			<set name="receiveTimeout" time="${iqrf.receiveTimeout}" />
+			<set name="refreshTime" time="${iqrf.refreshTime}" />
+			<set name="refreshTimePeripheralInfo" time="${iqrf.refreshTimePeripheralInfo}" />
+			<set name="devicesRetryTimeout" time="${iqrf.devicesRetryTimeout}" />
+			<set name="coordinatorReset" text="${iqrf.coordinatorReset}" />
+			<set name="devicePoller" ref="devicePoller" />
+		</instance>
+
+		<instance name="iqrfMqttConnector" class="BeeeOn::IQRFMqttConnector">
+			<set name="mqttClient" ref="iqrfMqttClient" />
+			<set name="publishTopic" text="${iqrf.publishTopic}" />
 		</instance>
 	</factory>
 </system>

conf/config.d/factory-hotplug.xml
@@ -1,12 +1,13 @@
 <system>
 	<factory>
 		<instance name="udevMonitor" class="BeeeOn::UDevMonitor">
-			<set name="matches" list="bluetooth,tty" />
+			<set name="matches" list="bluetooth,tty,spi" />
 			<add name="listeners" ref="bluetoothAvailability" if-yes="${bluetooth.availability.enable}" />
 			<add name="listeners" ref="bleSmartDeviceManager" if-yes="${blesmart.enable}" />
 			<add name="listeners" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 			<add name="listeners" ref="zwaveNetwork" if-yes="${zwave.enable}"/>
 			<add name="listeners" ref="hciInfoReporter" if-yes="${bluetooth.reporting.enable}"/>
+			<add name="listeners" ref="iqrfDeviceManager" if-yes="${iqrf.enable}"/>
 		</instance>
 
 		<instance name="pipeHotplugMonitor" class="BeeeOn::PipeHotplugMonitor">
@@ -16,6 +17,7 @@
 			<add name="listeners" ref="jablotronDeviceManager" if-yes="${jablotron.enable}"/>
 			<add name="listeners" ref="zwaveNetwork" if-yes="${zwave.enable}"/>
 			<add name="listeners" ref="hciInfoReporter" if-yes="${bluetooth.reporting.enable}"/>
+			<add name="listeners" ref="iqrfDeviceManager" if-yes="${iqrf.enable}"/>
 		</instance>
 
 		<alias name="hotplugMonitor" ref="${hotplug.impl}Monitor" />

conf/config.d/factory.xml
@@ -51,6 +51,7 @@
 			<add name="handlers" ref="virtualDeviceManager" if-yes="${vdev.enable}"/>
 			<add name="handlers" ref="vptDeviceManager" if-yes="${vpt.enable}"/>
 			<add name="handlers" ref="pressureSensorManager" if-yes="${psdev.enable}"/>
+			<add name="handlers" ref="iqrfDeviceManager" if-yes="${iqrf.enable}"/>
 			<add name="listeners" ref="loggingCollector" if-yes="${testing.collector.enable}" />
 			<add name="handlers" ref="fitpDeviceManager" if-yes="${fitp.enable}"/>
 			<add name="handlers" ref="zwaveDeviceManager" if-yes="${zwave.enable}"/>
@@ -71,6 +72,7 @@
 			<add name="handlers" ref="bluetoothAvailability" if-yes="${bluetooth.availability.enable}" />
 			<add name="handlers" ref="jablotronDeviceManager" if-yes="${jablotron.enable}" />
 			<add name="handlers" ref="bleSmartDeviceManager" if-yes="${blesmart.enable}" />
+			<add name="handlers" ref="iqrfDeviceManager" if-yes="${iqrf.enable}" />
 		</instance>
 
 		<instance name="pollExecutor" class="BeeeOn::SequentialAsyncExecutor">

conf/gateway-startup.ini
@@ -194,3 +194,18 @@ channels.async_remote.channel = remote
 channels.split.class = SplitterChannel
 channels.split.channels = console
 #channels.split.channels = console, async_remote
+
+[iqrf]
+enable = yes
+subscribeTopics = Iqrf/DpaResponse
+publishTopic = Iqrf/DpaRequest
+receiveTimeout = 1 s
+refreshTime = 60 s
+refreshTimePeripheralInfo = 300 s
+devicesRetryTimeout = 300 s
+coordinatorReset = no
+
+mqtt.host = localhost
+mqtt.port = 1883
+mqtt.qos = 0
+mqtt.clientID = IQRFClient

conf/gateway-testing.ini
@@ -186,3 +186,18 @@ channels.ozwave.purgeCount = 2
 
 channels.split.class = SplitterChannel
 channels.split.channels = console
+
+[iqrf]
+enable = yes
+subscribeTopics = Iqrf/DpaResponse
+publishTopic = Iqrf/DpaRequest
+receiveTimeout = 1 s
+refreshTime = 60 s
+refreshTimePeripheralInfo = 300 s
+devicesRetryTimeout = 300 s
+coordinatorReset = no
+
+mqtt.host = localhost
+mqtt.port = 1883
+mqtt.qos = 0
+mqtt.clientID = IQRFClient

commit beb9257189a06b98d31292e693c05f51c00d10b1

IQRFDeviceManager: fix missing reference to DeviceCache
Fixes: ed49dfec3 ("config: install IQRFDeviceManager")
Fixes: b313ef32b ("IQRFDeviceManager: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

conf/config.d/factory-managers.xml
@@ -229,6 +229,7 @@
 			<set name="refreshTimePeripheralInfo" time="${iqrf.refreshTimePeripheralInfo}" />
 			<set name="devicesRetryTimeout" time="${iqrf.devicesRetryTimeout}" />
 			<set name="coordinatorReset" text="${iqrf.coordinatorReset}" />
+			<set name="deviceCache" ref="deviceCache" />
 			<set name="devicePoller" ref="devicePoller" />
 			<set name="protocols" ref="iqHomeDPAProtocol" />
 		</instance>

src/iqrf/IQRFDeviceManager.cpp
@@ -40,6 +40,7 @@ BEEEON_OBJECT_PROPERTY("protocols", &IQRFDeviceManager::registerDPAProtocol)
 BEEEON_OBJECT_PROPERTY("mqttConnector", &IQRFDeviceManager::setMqttConnector)
 BEEEON_OBJECT_PROPERTY("devicesRetryTimeout", &IQRFDeviceManager::setIQRFDevicesRetryTimeout)
 BEEEON_OBJECT_PROPERTY("coordinatorReset", &IQRFDeviceManager::setCoordinatorReset)
+BEEEON_OBJECT_PROPERTY("deviceCache", &IQRFDeviceManager::setDeviceCache)
 BEEEON_OBJECT_PROPERTY("devicePoller", &IQRFDeviceManager::setDevicePoller)
 BEEEON_OBJECT_END(BeeeOn, IQRFDeviceManager)
 