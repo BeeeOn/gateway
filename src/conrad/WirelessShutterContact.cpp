ffcf50a3c3434f72a8685094c746357c666c617d
Pepa Hajek <hajek@rehivetech.com>
2019-10-03 08:30:57 +0200

    WirelessShutterContact: new class
    The class represents WirelessShutterContact sensor. It allows to gather
    data from Conrad WirelessShutterContact. Its modules are open/close and rssi.
    
    Signed-off-by: Pepa Hajek <hajek@rehivetech.com>
    Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

---
+++ src/conrad/WirelessShutterContact.cpp
@@ -0,0 +1,59 @@
+#include <Poco/JSON/Object.h>
+
+#include "conrad/WirelessShutterContact.h"
+#include "util/JsonUtil.h"
+
+#define OPEN_CLOSE_MODULE_ID 0
+#define RSSI_MODULE_ID 1
+
+using namespace BeeeOn;
+using namespace Poco::JSON;
+using namespace std;
+
+static list<ModuleType> DEVICE_MODULE_TYPES = {
+	{ModuleType::Type::TYPE_OPEN_CLOSE},
+	{ModuleType::Type::TYPE_RSSI},
+};
+
+const string WirelessShutterContact::PRODUCT_NAME = "HM-Sec-SC-2";
+
+WirelessShutterContact::WirelessShutterContact(
+		const DeviceID& id,
+		const RefreshTime &refresh):
+	ConradDevice(id, refresh, PRODUCT_NAME, DEVICE_MODULE_TYPES)
+{
+}
+
+WirelessShutterContact::~WirelessShutterContact()
+{
+}
+
+/**
+ * Message example:
+ * {
+ *     "channels": {
+ *         "Main": "open"
+ *     },
+ *     "dev": "30B0BE",
+ *     "event": "message",
+ *     "raw": "A0C3FA64130B0BEF11034013FC8",
+ *     "rssi": -41.0,
+ *     "type": "threeStateSensor"
+ * }
+ */
+SensorData WirelessShutterContact::parseMessage(const Object::Ptr message)
+{
+	SensorData data;
+	data.setDeviceID(m_deviceId);
+
+	Object::Ptr object = message->getObject("channels");
+
+	if (!object->getValue<std::string>("Main").compare("open"))
+		data.insertValue(SensorValue(OPEN_CLOSE_MODULE_ID, 1));
+	else
+		data.insertValue(SensorValue(OPEN_CLOSE_MODULE_ID, 0));
+
+	data.insertValue(SensorValue(RSSI_MODULE_ID, message->getValue<double>("rssi")));
+
+	return data;
+}
