BeeeOn/gateway

commit 8b145d2318eb0c10ee7d499fdb81c50f0ee0cb44

LoggingCollector: implement IQRFListener's methods
The class implements IQRFListener's methods to log statistics about
DPA packets.

Signed-off-by: Petr Malanik <xmalan02@stud.fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/core/LoggingCollector.cpp
@@ -6,6 +6,7 @@
 #include "bluetooth/HciInfo.h"
 #include "core/LoggingCollector.h"
 #include "di/Injectable.h"
+#include "iqrf/IQRFEvent.h"
 #include "model/SensorData.h"
 #include "philips/PhilipsHueBulbInfo.h"
 #include "philips/PhilipsHueBridgeInfo.h"
@@ -20,12 +21,17 @@
 #include "zwave/OZWNotificationEvent.h"
 #endif
 
+#ifdef HAVE_IQRF
+#include "iqrf/IQRFEvent.h"
+#endif
+
 BEEEON_OBJECT_BEGIN(BeeeOn, LoggingCollector)
 BEEEON_OBJECT_CASTABLE(DistributorListener)
 BEEEON_OBJECT_CASTABLE(ZWaveListener)
 BEEEON_OBJECT_CASTABLE(HciListener)
 BEEEON_OBJECT_CASTABLE(PhilipsHueListener)
 BEEEON_OBJECT_CASTABLE(CommandDispatcherListener)
+BEEEON_OBJECT_CASTABLE(IQRFListener)
 BEEEON_OBJECT_END(BeeeOn, LoggingCollector)
 
 using namespace std;
@@ -216,6 +222,21 @@ void LoggingCollector::onBulbStats(const PhilipsHueBulbInfo &)
 }
 
 void LoggingCollector::onBridgeStats(const PhilipsHueBridgeInfo &)
+{
+}
+#endif
+
+
+#ifdef HAVE_IQRF
+void LoggingCollector::onReceiveDPA(const IQRFEvent &info)
+{
+	logger().information("IQRF event: from address: "
+			+ to_string(info.networkAddress())
+			+ " payload size: "
+			+ to_string(info.payload().size()));
+}
+#else
+void LoggingCollector::onReceiveDPA(const IQRFEvent &info)
 {
 }
 #endif

src/core/LoggingCollector.h
@@ -20,6 +20,7 @@ public:
 	void onBulbStats(const PhilipsHueBulbInfo &info) override;
 	void onBridgeStats(const PhilipsHueBridgeInfo &info) override;
 	void onDispatch(const Command::Ptr cmd) override;
+	void onReceiveDPA(const IQRFEvent &info) override;
 
 private:
 	Poco::AtomicCounter m_seenData;

commit b87ae37868dd72d21b706dfbfe2e249fd432d615

LoggingCollector: remove unnecessary dependencies
The unnecessary include of IQRFEvent was removed. It caused
compilation failure when the IQRF support was disabled.

The name of unused parameter was removed too.

Fixes: 8b145d231 ("LoggingCollector: implement IQRFListener's methods")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/core/LoggingCollector.cpp
@@ -6,7 +6,6 @@
 #include "bluetooth/HciInfo.h"
 #include "core/LoggingCollector.h"
 #include "di/Injectable.h"
-#include "iqrf/IQRFEvent.h"
 #include "model/SensorData.h"
 #include "philips/PhilipsHueBulbInfo.h"
 #include "philips/PhilipsHueBridgeInfo.h"
@@ -236,7 +235,7 @@ void LoggingCollector::onReceiveDPA(const IQRFEvent &info)
 			+ to_string(info.payload().size()));
 }
 #else
-void LoggingCollector::onReceiveDPA(const IQRFEvent &info)
+void LoggingCollector::onReceiveDPA(const IQRFEvent &)
 {
 }
 #endif