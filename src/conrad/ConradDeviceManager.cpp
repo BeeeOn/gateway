BeeeOn/gateway

commit 2ee93fdc5c99af84c9209b0ad2b35163fa66be99

ConradDeviceManager: communicate via FHEMClient
The ConradDeviceManager do not use anymore conrad-interface for controling
Conrad devices. From now it uses FHEMClient to interact with FHEM server and
thus control the Conrad devices. This make the communication easier and lighter
because now the BeeeOn Gateway communicate directly with th FHEM server and not
over middle conrad-interface.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/conrad/ConradDeviceManager.cpp
@@ -1,4 +1,4 @@
-#include <zmq.h>
+#include <Poco/RegularExpression.h>
 
 #include "commands/DeviceUnpairCommand.h"
 #include "commands/DeviceSetValueCommand.h"
@@ -17,7 +17,6 @@
 #include "model/DevicePrefix.h"
 
 #define CONRAD_VENDOR "Conrad"
-#define RSP_BUFFER_SIZE 129070
 
 BEEEON_OBJECT_BEGIN(BeeeOn, ConradDeviceManager)
 BEEEON_OBJECT_CASTABLE(StoppableRunnable)
@@ -26,8 +25,7 @@ BEEEON_OBJECT_CASTABLE(DeviceStatusHandler)
 BEEEON_OBJECT_PROPERTY("deviceCache", &ConradDeviceManager::setDeviceCache)
 BEEEON_OBJECT_PROPERTY("distributor", &ConradDeviceManager::setDistributor)
 BEEEON_OBJECT_PROPERTY("commandDispatcher", &ConradDeviceManager::setCommandDispatcher)
-BEEEON_OBJECT_PROPERTY("cmdZmqIface", &ConradDeviceManager::setCmdZmqIface)
-BEEEON_OBJECT_PROPERTY("eventZmqIface", &ConradDeviceManager::setEventZmqIface)
+BEEEON_OBJECT_PROPERTY("fhemClient", &ConradDeviceManager::setFHEMClient)
 BEEEON_OBJECT_PROPERTY("eventsExecutor", &ConradDeviceManager::setEventsExecutor)
 BEEEON_OBJECT_PROPERTY("listeners", &ConradDeviceManager::registerListener)
 BEEEON_OBJECT_END(BeeeOn, ConradDeviceManager)
@@ -47,14 +45,9 @@ ConradDeviceManager::ConradDeviceManager() :
 {
 }
 
-void ConradDeviceManager::setCmdZmqIface(const string &cmdZmqIface)
+void ConradDeviceManager::setFHEMClient(FHEMClient::Ptr fhemClient)
 {
-	m_cmdZmqIface = URI(cmdZmqIface);
-}
-
-void ConradDeviceManager::setEventZmqIface(const string &eventZmqIface)
-{
-	m_eventZmqIface = URI(eventZmqIface);
+	m_fhemClient = fhemClient;
 }
 
 void ConradDeviceManager::setEventsExecutor(AsyncExecutor::Ptr executor)
@@ -71,21 +64,12 @@ void ConradDeviceManager::run()
 {
 	logger().information("starting Conrad device manager", __FILE__, __LINE__);
 
-	char buffer[RSP_BUFFER_SIZE];
-	void *context = zmq_ctx_new();
-	void *subscriber = zmq_socket(context, ZMQ_SUB);
-	zmq_connect(subscriber, m_eventZmqIface.toString().c_str());
-	zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
-
 	StopControl::Run run(m_stopControl);
 	while (run) {
-		int size = zmq_recv(subscriber, buffer, RSP_BUFFER_SIZE, 0);
-		if (size < 0)
-			continue;
-		buffer[size] = '\0';
+		Object::Ptr event = m_fhemClient->receive(-1);
 
 		try {
-			processMessage(buffer);
+			processEvent(event);
 		}
 		catch (const Exception& e) {
 			logger().log(e, __FILE__, __LINE__);
@@ -93,38 +77,41 @@ void ConradDeviceManager::run()
 	}
 
 	logger().information("stopping Conrad device manager", __FILE__, __LINE__);
-
-	zmq_close(subscriber);
-	zmq_ctx_destroy(context);
 }
 
-void ConradDeviceManager::processMessage(const string &message)
+void ConradDeviceManager::processEvent(const Object::Ptr event)
 {
-	Object::Ptr object = JsonUtil::parse(message);
-	if (!object->has("dev"))
-		throw IllegalStateException("message does not contain 'dev' element");
+	if (!event->has("dev"))
+		throw IllegalStateException("event does not contain 'dev' element");
 
-	uint32_t id = NumberParser::parseHex(object->getValue<string>("dev"));
+	string devId = event->getValue<string>("dev");
+	RegularExpression reDeviceId("HM_([a-fA-F0-9]+)");
+	RegularExpression::MatchVec matches;
+	if (reDeviceId.match(event->getValue<string>("dev"), 0, matches) == 0)
+		throw IllegalStateException("event contains 'dev' element with wrong format");
+
+	devId = devId.substr(matches[1].offset, matches[1].length);
+	uint32_t id = NumberParser::parseHex(devId);
 	DeviceID deviceID = DeviceID(DevicePrefix::PREFIX_CONRAD, id);
 
 	if (logger().debug()) {
-		logger().debug("Event " + object->getValue<string>("event") +
+		logger().debug("Event " + event->getValue<string>("event") +
 			" from " + deviceID.toString(), __FILE__, __LINE__);
 	}
 
-	fireMessage(deviceID, object);
+	fireMessage(deviceID, event);
 
 	ScopedLock<FastMutex> lock(m_devicesMutex);
 
-	if (!object->getValue<string>("event").compare("new_device")) {
-		createNewDeviceUnlocked(deviceID, object->getValue<string>("type"));
+	if (!event->getValue<string>("event").compare("new_device")) {
+		createNewDeviceUnlocked(deviceID, event->getValue<string>("type"));
 	}
-	else if (!object->getValue<string>("event").compare("message")) {
+	else if (!event->getValue<string>("event").compare("message")) {
 		auto it = m_devices.find(deviceID);
 		if (it == m_devices.end())
-			createNewDeviceUnlocked(deviceID, object->getValue<string>("type"));
+			createNewDeviceUnlocked(deviceID, event->getValue<string>("type"));
 
-		SensorData data = it->second->parseMessage(object);
+		SensorData data = it->second->parseMessage(event);
 
 		if (!deviceCache()->paired(deviceID))
 			return;
@@ -138,7 +125,7 @@ void ConradDeviceManager::processMessage(const string &message)
 		}
 	}
 	else {
-		throw IllegalStateException("unknown message");
+		throw IllegalStateException("unknown event");
 	}
 }
 
@@ -180,14 +167,9 @@ void ConradDeviceManager::stop()
 AsyncWork<>::Ptr ConradDeviceManager::startDiscovery(const Timespan &timeout)
 {
 	auto work = BlockingAsyncWork<>::instance();
-	Object::Ptr obj = new Object();
-	ostringstream cmdStream;
 
-	obj->set("cmd", "pair");
-	obj->set("tout", to_string(timeout.totalSeconds()));
-
-	obj->stringify(cmdStream);
-	sendCmdRequest(cmdStream.str());
+	string request = "set CUL_0 hmPairForSec " + to_string(timeout.totalSeconds());
+	m_fhemClient->sendRequest(request);
 
 	return work;
 }
@@ -208,8 +190,6 @@ AsyncWork<set<DeviceID>>::Ptr ConradDeviceManager::startUnpair(
 		const Timespan &timeout)
 {
 	auto work = BlockingAsyncWork<set<DeviceID>>::instance();
-	Object::Ptr obj = new Object();
-	ostringstream cmdStream;
 
 	ScopedLock<FastMutex> lock(m_devicesMutex, timeout.totalMilliseconds());
 
@@ -225,11 +205,8 @@ AsyncWork<set<DeviceID>>::Ptr ConradDeviceManager::startUnpair(
 		// must be formated as upper case to be acceptable by unpair command
 		for (auto & c: conradID) c = toupper(c);
 
-		obj->set("cmd", "unpair");
-		obj->set("device", conradID);
-
-		obj->stringify(cmdStream);
-		sendCmdRequest(cmdStream.str());
+		string request = "delete HM_" + conradID;
+		m_fhemClient->sendRequest(request);
 
 		auto it = m_devices.find(id);
 		if (it != m_devices.end())
@@ -239,24 +216,6 @@ AsyncWork<set<DeviceID>>::Ptr ConradDeviceManager::startUnpair(
 	}
 
 	return work;
-}
-
-Object::Ptr ConradDeviceManager::sendCmdRequest(const string &request)
-{
-	void *context = zmq_ctx_new();
-	void *requester = zmq_socket (context, ZMQ_REQ);
-	zmq_connect(requester, m_cmdZmqIface.toString().c_str());
-
-	fireMessage(DeviceID(),JsonUtil::parse(request));
-
-	char buffer[RSP_BUFFER_SIZE];
-	zmq_send(requester, request.c_str(), request.length(), 0);
-	zmq_recv(requester, buffer, RSP_BUFFER_SIZE, 0);
-
-	zmq_close(requester);
-	zmq_ctx_destroy(context);
-
-	return JsonUtil::parse(string(buffer));
 }
 
 void ConradDeviceManager::fireMessage(

conf/config.d/factory-managers.xml
@@ -18,6 +18,7 @@
 			<add name="runnables" ref="iqrfMqttConnector" if-yes="${iqrf.enable}" />
 			<add name="runnables" ref="vektivaDeviceManager" if-yes="${vektiva.enable}" />
 			<add name="runnables" ref="conradDeviceManager" if-yes="${conrad.enable}" />
+			<add name="runnables" ref="conradFHEMClient" if-yes="${conrad.enable}" />
 			<add name="runnables" ref="vektivaMqttClient" if-yes="${vektiva.enable}" />
 			<add name="runnables" ref="vektivaStatusMqttClient" if-yes="${vektiva.enable}" />
 			<add name="runnables" ref="sonoffDeviceManager" if-yes="${sonoff.enable}" />
@@ -272,11 +273,17 @@
 			<set name="deviceCache" ref="deviceCache" />
 			<set name="distributor" ref="distributor" />
 			<set name="commandDispatcher" ref="commandDispatcher" />
-			<set name="cmdZmqIface" text="${conrad.cmdZmqIface}" />
-			<set name="eventZmqIface" text="${conrad.eventZmqIface}" />
+			<set name="fhemClient" ref="conradFHEMClient"/>
 			<set name="eventsExecutor" ref="asyncExecutor"/>
 			<add name="listeners" ref="loggingCollector" if-yes="${testing.collector.enable}" />
 			<add name="listeners" ref="nemeaCollector" if-yes="${nemea.collector.enable}" />
+		</instance>
+
+		<instance name="conradFHEMClient" class="BeeeOn::FHEMClient">
+			<set name="address" text="${conrad.fhem.address}" />
+			<set name="refreshTime" time="${conrad.fhem.refreshTime}" />
+			<set name="receiveTimeout" time="${conrad.fhem.receiveTimeout}" />
+			<set name="reconnectTime" time="${conrad.fhem.reconnectTime}" />
 		</instance>
 
 		<instance name="iqrfMqttConnector" class="BeeeOn::IQRFMqttConnector">

conf/config.d/logging-loggers.ini
@@ -17,6 +17,9 @@ loggers.VektivaDeviceManager.level = debug
 loggers.ConradDeviceManager.name = BeeeOn::ConradDeviceManager
 loggers.ConradDeviceManager.level = debug
 
+loggers.FHEMClient.name = BeeeOn::FHEMClient
+loggers.FHEMClient.level = debug
+
 loggers.BLESmartDeviceManager.name = BeeeOn::BLESmartDeviceManager
 loggers.BLESmartDeviceManager.level = debug
 

conf/gateway-startup.ini
@@ -28,8 +28,10 @@ mqtt.statusClientID = VektivaStatusClient
 
 [conrad]
 enable = yes
-cmdZmqIface = tcp://localhost:7778
-eventZmqIface = tcp://localhost:7779
+fhem.address = 127.0.0.1:7072
+fhem.refreshTime = 5 s
+fhem.receiveTimeout = 2 s
+fhem.reconnectTime = 5 s
 
 [bluetooth]
 hci.impl = dbus

conf/gateway-testing.ini
@@ -28,8 +28,10 @@ mqtt.statusClientID = VektivaStatusClient
 
 [conrad]
 enable = yes
-cmdZmqIface = tcp://localhost:7778
-eventZmqIface = tcp://localhost:7779
+fhem.address = 127.0.0.1:7072
+fhem.refreshTime = 5 s
+fhem.receiveTimeout = 2 s
+fhem.reconnectTime = 5 s
 
 [bluetooth]
 hci.impl = dbus

src/CMakeLists.txt
@@ -41,7 +41,6 @@ find_package(OpenZWave)
 find_library (LIBTRAP trap)
 find_library (UNIREC unirec)
 find_library (PCAP pcap)
-find_library (ZMQ zmq)
 
 if(GLIB_FOUND AND GIO_UNIX_FOUND AND GDBUS_CODEGEN AND BLUEZ_FOUND)
 	include_directories(${GIO_UNIX_INCLUDE_DIRS})
@@ -218,7 +217,7 @@ else()
     message(STATUS "Vektiva support is disabled")
 endif()
 
-if(ZMQ AND ENABLE_CONRAD)
+if(ENABLE_CONRAD)
 	file(GLOB CONRAD_SOURCES
 		${PROJECT_SOURCE_DIR}/conrad/ConradDeviceManager.cpp
 		${PROJECT_SOURCE_DIR}/conrad/ConradDevice.cpp
@@ -231,7 +230,6 @@ if(ZMQ AND ENABLE_CONRAD)
 	)
 	add_library(BeeeOnConrad ${CONRAD_SOURCES})
 	list(APPEND MODULE_LIBS BeeeOnConrad)
-	list(APPEND LIBS ${ZMQ})
 	add_definitions(-DHAVE_CONRAD=1)
 else()
 	message(STATUS "Conrad support is disabled")

src/conrad/ConradDeviceManager.h
@@ -4,11 +4,11 @@
 
 #include <Poco/Timespan.h>
 #include <Poco/JSON/Object.h>
-#include <Poco/URI.h>
 
 #include "core/DeviceManager.h"
 #include "conrad/ConradDevice.h"
 #include "conrad/ConradListener.h"
+#include "conrad/FHEMClient.h"
 #include "model/DeviceID.h"
 #include "util/AsyncExecutor.h"
 #include "util/BlockingAsyncWork.h"
@@ -20,8 +20,9 @@ namespace BeeeOn {
 /**
  * @brief The class implements the work with Conrad devices. Allows us
  * to process and execute the commands from server and gather
- * data from the devices. This class requires ZMQ interface that is
- * available at https://github.com/pepa-cz/conrad-interface.
+ * data from the devices. It communicates with the Conrad devices
+ * using FHEM server. To communicate with FHEM server the class using
+ * FHEMClient which communicates with the FHEM server over telnet.
  */
 class ConradDeviceManager : public DeviceManager {
 public:
@@ -30,16 +31,7 @@ public:
 	void run() override;
 	void stop() override;
 
-	/**
-	 * @brief Sets command ZMQ interface.
-	 */
-	void setCmdZmqIface(const std::string &cmdZmqIface);
-
-	/**
-	 * @brief Sets event ZMQ interface.
-	 */
-	void setEventZmqIface(const std::string &eventZmqIface);
-
+	void setFHEMClient(FHEMClient::Ptr fhemClient);
 	void setEventsExecutor(AsyncExecutor::Ptr executor);
 
 	void registerListener(ConradListener::Ptr listener);
@@ -52,15 +44,10 @@ protected:
 	void handleAccept(const DeviceAcceptCommand::Ptr cmd) override;
 
 	/**
-	 * @brief Send request via ZMQ conrad interface and returns response.
-	 */
-	Poco::JSON::Object::Ptr sendCmdRequest(const std::string &request);
-
-	/**
-	 * @brief Processes the incoming ZMQ message, which means creating
+	 * @brief Processes the incoming event, which means creating
 	 * a new device or sending the gathered data to the server.
 	 */
-	void processMessage(const std::string &message);
+	void processEvent(const Poco::JSON::Object::Ptr event);
 
 	/**
 	 * @brief Creates instance of Conrad device appends it into m_devices.
@@ -73,12 +60,10 @@ protected:
 	void fireMessage(DeviceID const &deviceID, const Poco::JSON::Object::Ptr message);
 
 private:
-	Poco::URI m_cmdZmqIface;
-	Poco::URI m_eventZmqIface;
-
 	Poco::FastMutex m_devicesMutex;
 	std::map<DeviceID, ConradDevice::Ptr> m_devices;
 
+	FHEMClient::Ptr m_fhemClient;
 	EventSource<ConradListener> m_eventSource;
 };
 

test/CMakeLists.txt
@@ -38,7 +38,6 @@ find_package(OpenZWave)
 find_library (LIBTRAP trap)
 find_library (UNIREC unirec)
 find_library (PCAP pcap)
-find_library (ZMQ zmq)
 
 if(GLIB_FOUND AND GIO_UNIX_FOUND)
 	include_directories(${GIO_UNIX_INCLUDE_DIRS})
@@ -177,9 +176,8 @@ if (ENABLE_IQRF AND MOSQUITTO_CPP)
 	list(APPEND TEST_MODULE_LIBS BeeeOnIQRF BeeeOnIQRFTest BeeeOnMosquitto)
 endif()
 
-if (ZMQ AND ENABLE_CONRAD)
+if (ENABLE_CONRAD)
 	list(APPEND TEST_MODULE_LIBS BeeeOnConrad)
-	list(APPEND LIBS ${ZMQ})
 endif()
 
 if(LIBTRAP AND UNIREC AND PCAP AND ENABLE_NEMEA)

commit 2b8803acf04c085f26701f9ad835202770703d1a

ConradDeviceManager: fix stopping of FHEMClient
The ConradDeviceManager stops FHEMClient in stop method.

Fixes: 2ee93fdc5 ("ConradDeviceManager: communicate via FHEMClient")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/conrad/ConradDeviceManager.cpp
@@ -184,6 +184,7 @@ void ConradDeviceManager::stop()
 {
 	DeviceManager::stop();
 	answerQueue().dispose();
+	m_fhemClient->stop();
 }
 
 AsyncWork<>::Ptr ConradDeviceManager::startDiscovery(const Timespan &timeout)